a22245136000d680ddceb04e9f502aeb4c15a669
David Blasby <dblasby@gmail.com>
2004-04-28 19:17:25 +0000

    inital version of regression tests
    git-svn-id: http://svn.osgeo.org/postgis/trunk@524 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ lwgeom/regress/lwgeom_regress3.sql
@@ -0,0 +1,19 @@
+--- build a larger database
+\set ECHO none
+\i regress_lots_of_points.sql
+\set ECHO all
+
+--- test some of the searching capabilities
+
+-- GiST index
+
+CREATE INDEX quick_gist on test using gist (lwgeom gist_lwgeom_ops);
+vacuum analyse test;
+
+ select * from test where lwgeom && lwgeom(envelope('BOX3D(125 125,135 135)'::box3d)) order by num;
+
+set enable_seqscan = off;
+
+ select * from test where lwgeom && lwgeom(envelope('BOX3D(125 125,135 135)'::box3d)) order by num;
+
+
