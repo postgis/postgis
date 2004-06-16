151a8155fd4174a291697ab255f87277c5e01ff5
Sandro Santilli <strk@keybit.net>
2004-06-16 18:47:18 +0000

    initial import
    git-svn-id: http://svn.osgeo.org/postgis/trunk@627 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ geos_version.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+	echo "Usage `basename $0` <geosdir>" >&2
+	exit 1
+fi
+
+geos_dir=$1
+version=`${geos_dir}/bin/geos-config --version`
+if [ "$version" = "@GEOS_VERSION@" ]; then
+	geos_version=100
+else
+	major=`echo $version | sed 's/\..*//'`
+	minor=`echo $version | sed 's/.*\.\([^.]*\)\.*/\1/'`
+	geos_version=`printf %d%2.2d $major $minor`
+fi
+cat <<EOF
+#define GEOS_VERSION=$geos_version
+EOF
