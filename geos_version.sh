a317d59a986b9744540b9031f9e539ea59cf75fb
Sandro Santilli <strk@keybit.net>
2004-06-18 17:11:10 +0000

    Fixed a bug in minor version extractor. Wrapped GEOS_VERSION in an #ifndef block. More runtime checks.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@634 b70326c6-7e19-0410-871a-916f4a2858ee

--- geos_version.sh
+++ geos_version.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-	echo "Usage `basename $0` <geosdir>" >&2
+	echo "Usage: `basename $0` <geosdir>" >&2
 	exit 1
 fi
 
 geos_dir=$1
 version=`${geos_dir}/bin/geos-config --version`
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
 if [ "$version" = "@GEOS_VERSION@" ]; then
 	geos_version=100
+	version="1.0.0"
 else
 	major=`echo $version | sed 's/\..*//'`
-	minor=`echo $version | sed 's/.*\.\([^.]*\)\.*/\1/'`
+	minor=`echo $version | sed 's/[^\.]*\.\([^.]*\)\.*/\1/'`
 	geos_version=`printf %d%2.2d $major $minor`
 fi
 cat <<EOF
 #define POSTGIS_GEOS_VERSION $geos_version
-#define GEOS_VERSION $version
+#ifndef GEOS_VERSION
+#define GEOS_VERSION "$version"
+#endif
 EOF
