8bf9b37d4aca0fb970283a3f917e3c15fb2f499b
Sandro Santilli <strk@keybit.net>
2004-08-20 09:31:32 +0000

    bug fixed in lwgeom_mem_size
    git-svn-id: http://svn.osgeo.org/postgis/trunk@706 b70326c6-7e19-0410-871a-916f4a2858ee

--- lwgeom/lwgeom_functions_basic.c
+++ lwgeom/lwgeom_functions_basic.c
@@ -193,7 +193,9 @@ Datum combine_box2d(PG_FUNCTION_ARGS)
 PG_FUNCTION_INFO_V1(lwgeom_mem_size);
 Datum lwgeom_mem_size(PG_FUNCTION_ARGS)
 {
-	//char *geom = (char *)  PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
-	//struct varlena *vl = (struct varlena *)(PG_GETARG_POINTER(0));
-	PG_RETURN_INT32(VARSIZE(PG_GETARG_POINTER(0)));
+	char *geom = (char *)  PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
+	int size = *((int *)geom);
+	PG_FREE_IF_COPY(geom,0);
+	int32 vlsize = VARSIZE(PG_GETARG_POINTER(0));
+	PG_RETURN_INT32(size);
 }
