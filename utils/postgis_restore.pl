a1e7b5daec7da144f3b1f99a2fd38809eda3d5df
Sandro Santilli <strk@keybit.net>
2004-12-15 09:41:44 +0000

    Die if target db already exists.
    git-svn-id: http://svn.osgeo.org/postgis/branches/pgis_0_9_0@1147 b70326c6-7e19-0410-871a-916f4a2858ee

--- utils/postgis_restore.pl
+++ utils/postgis_restore.pl
@@ -562,6 +562,7 @@ close(OUTPUT);
 #
 print "Creating db ($dbname)\n";
 `createdb $dbname`;
+die "Can't restore in an existing database\n" if ($?);
 print "Adding plpgsql\n";
 `createlang plpgsql $dbname`;
 
