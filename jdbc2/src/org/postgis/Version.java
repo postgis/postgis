a59181934e6a86f1bf630f5220f0a0d16eb18ec8
Markus Schaber <markus@schabi.de>
2005-03-04 15:22:01 +0000

    - Separates the version config variables into the toplevel Version.config
    - Adds new version config variables for jdbc
    
    - jdbc "make jar" additionally creates files named like
      postgis_1_0_0RC4.jar using the info from Version.config
    
    - org/postgis/Version.java now uses a Makefile generated ressource to
      initialize its values, so it is always "in sync" with Version.config
    
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@1484 b70326c6-7e19-0410-871a-916f4a2858ee

--- jdbc2/src/org/postgis/Version.java
+++ jdbc2/src/org/postgis/Version.java
@@ -24,20 +24,65 @@
 
 package org.postgis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 /** Corresponds to the appropriate PostGIS that carried this source */
 public class Version {
-
+    /** We read our version information from this ressource... */
+    private static final String RESSOURCENAME = "org/postgis/version.properties";
+    
     /** The major version */
-    public static final int MAJOR = 1;
+    public static final int MAJOR;
 
     /** The minor version */
-    public static final int MINOR = 0;
+    public static final int MINOR;
 
     /**
      * The micro version, usually a number including possibly textual suffixes
      * like RC3.
      */
-    public static final String MICRO = "0RC4";
+    public static final String MICRO;
+
+    static {
+        int major = -1;
+        int minor = -1;
+        String micro = "INVALID";
+        try {
+            ClassLoader loader = Version.class.getClassLoader();
+            URL ver = loader.getResource(RESSOURCENAME);
+            if (ver == null) {
+                throw new ExceptionInInitializerError(
+                        "Error initializing PostGIS JDBC version! Cause: Ressource "+RESSOURCENAME+" not found!");
+            }
+            BufferedReader rd = new BufferedReader(new InputStreamReader(ver.openStream()));
+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith("JDBC_MAJOR_VERSION")) {
+                    major = Integer.parseInt(line.substring(19));
+                } else if (line.startsWith("JDBC_MINOR_VERSION")) {
+                    minor = Integer.parseInt(line.substring(19));
+                } else if (line.startsWith("JDBC_MICRO_VERSION")) {
+                    micro = line.substring(19);
+                } else {
+                    // ignore line
+                }
+            }
+            if (major == -1 || minor == -1 || micro.equals("INVALID")) {
+                throw new ExceptionInInitializerError("Error initializing PostGIS JDBC version! Cause: Ressource "+RESSOURCENAME+" incomplete!");
+            }
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError("Error initializing PostGIS JDBC version! Cause: " + e.getMessage());
+        } finally {
+            MAJOR = major;
+            MINOR = minor;
+            MICRO = micro;
+        }
+    }
 
     /** Full version for human reading - code should use the constants above */
     public static final String FULL = "PostGIS JDBC V" + MAJOR + "." + MINOR + "." + MICRO;
