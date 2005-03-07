a003017908650e8ace78c811d9842144982436f4
Sandro Santilli <strk@keybit.net>
2005-03-07 21:11:07 +0000

    Removed stub functions (moved to lwgeom_nojts.c) and added stub for JTSnoop.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@1504 b70326c6-7e19-0410-871a-916f4a2858ee

--- lwgeom/lwgeom_geos.c
+++ lwgeom/lwgeom_geos.c
@@ -38,13 +38,11 @@ Datum GEOS_polygonize_garray(PG_FUNCTION_ARGS);
 
 
 
-#if USE_GEOS
-
 /*
  * Define this to have have many notices printed
  * during postgis->geos and geos->postgis conversions
  */
-//#undef PGIS_DEBUG_CONVERTER 
+//#define PGIS_DEBUG_CONVERTER 1
 #ifdef PGIS_DEBUG_CONVERTER
 #define PGIS_DEBUG_POSTGIS2GEOS 1
 #define PGIS_DEBUG_GEOS2POSTGIS 1
@@ -2505,167 +2503,9 @@ Datum GEOS_polygonize_garray(PG_FUNCTION_ARGS)
 
 }
 
-#else // ! USE_GEOS
-
-PG_FUNCTION_INFO_V1(postgis_geos_version);
-Datum postgis_geos_version(PG_FUNCTION_ARGS)
+PG_FUNCTION_INFO_V1(JTSnoop);
+Datum JTSnoop(PG_FUNCTION_ARGS)
 {
-	//elog(ERROR,"GEOSversion:: operation not implemented - compile PostGIS with GEOS support");
+	elog(NOTICE, "JTS support is disabled, use GEOSnoop instead");
 	PG_RETURN_NULL();
 }
-
-PG_FUNCTION_INFO_V1(intersection);
-Datum intersection(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"intersection:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(convexhull);
-Datum convexhull(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"convexhull:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(difference);
-Datum difference(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"difference:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(boundary);
-Datum boundary(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"boundary:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(symdifference);
-Datum symdifference(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"symdifference:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(geomunion);
-Datum geomunion(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"geomunion:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-PG_FUNCTION_INFO_V1(buffer);
-Datum buffer(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"buffer:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-
-
-PG_FUNCTION_INFO_V1(relate_full);
-Datum relate_full(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"relate_full:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(relate_pattern);
-Datum relate_pattern(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"relate_pattern:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(disjoint);
-Datum disjoint(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"disjoint:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(intersects);
-Datum intersects(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"intersects:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(touches);
-Datum touches(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"touches:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(crosses);
-Datum crosses(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"crosses:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(within);
-Datum within(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"within:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(contains);
-Datum contains(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"contains:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(overlaps);
-Datum overlaps(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"overlaps:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(isvalid);
-Datum isvalid(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"isvalid:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-PG_FUNCTION_INFO_V1(issimple);
-Datum issimple(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"issimple:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(geomequals);
-Datum geomequals(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"geomequals:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-PG_FUNCTION_INFO_V1(isring);
-Datum isring(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"isring:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-PG_FUNCTION_INFO_V1(pointonsurface);
-Datum pointonsurface(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"pointonsurface:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-PG_FUNCTION_INFO_V1(unite_garray);
-Datum unite_garray(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"unite_garray:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-PG_FUNCTION_INFO_V1(GEOS_polygonize_garray);
-Datum GEOS_polygonize_garray(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"GEOS_polygonize_garray:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-PG_FUNCTION_INFO_V1(GEOSnoop);
-Datum GEOSnoop(PG_FUNCTION_ARGS)
-{
-	elog(ERROR,"GEOSnoop:: operation not implemented - compile PostGIS with GEOS support");
-	PG_RETURN_NULL(); // never get here
-}
-
-#endif // ! USE_GEOS
