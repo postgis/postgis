826d50293a5fc313111f1e167629c3c3a581a042
Sandro Santilli <strk@keybit.net>
2005-11-28 11:00:09 +0000

    Added test for ExteriorRing and Segentize
    git-svn-id: http://svn.osgeo.org/postgis/branches/pgis_1_0@2078 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/regress.sql
+++ regress/regress.sql
@@ -242,3 +242,5 @@ select '144', asewkt(force_3dm('POINT(1 2 3)'));
 select '145', asewkt(force_3dz('POINTM(1 2 3)'));
 select '146', asewkt(force_4d('POINTM(1 2 3)'));
 select '147', asewkt(force_4d('POINT(1 2 3)'));
+
+select '148', astext(segmentize('LINESTRING(0 0, 10 0)', 5));
