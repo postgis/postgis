27ddf609cf00d52033283f074176bbe770aa3fc6
Sandro Santilli <strk@keybit.net>
2006-07-28 13:07:52 +0000

    Added tests for makeline, makebox2d and makebox3d
    git-svn-id: http://svn.osgeo.org/postgis/trunk@2462 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/ctors.sql
+++ regress/ctors.sql
@@ -2,3 +2,13 @@
 SELECT SRID(collect('SRID=32749;POINT(0 0)', 'SRID=32749;POINT(1 1)'));
 
 SELECT collect('SRID=32749;POINT(0 0)', 'SRID=32740;POINT(1 1)');
+
+select asewkt(makeline('SRID=3;POINT(0 0)', 'SRID=3;POINT(1 1)'));
+select makeline('POINT(0 0)', 'SRID=3;POINT(1 1)');
+
+-- postgis-users/2006-July/012788.html
+select makebox2d('SRID=3;POINT(0 0)', 'SRID=3;POINT(1 1)');
+select makebox2d('POINT(0 0)', 'SRID=3;POINT(1 1)');
+
+select makebox3d('SRID=3;POINT(0 0)', 'SRID=3;POINT(1 1)');
+select makebox3d('POINT(0 0)', 'SRID=3;POINT(1 1)');
