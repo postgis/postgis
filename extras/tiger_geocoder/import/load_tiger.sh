19a12b1f54b536f339dc38fee91e332b39b5b8ea
Stephen Frost <sfrost@snowman.net>
2007-07-03 21:36:28 +0000

    - Change to generic database name
    git-svn-id: http://svn.osgeo.org/postgis/trunk@2645 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/import/load_tiger.sh
+++ extras/tiger_geocoder/import/load_tiger.sh
@@ -2,7 +2,7 @@
 
 FIRST=""
 for file in /data/tiger/www2.census.gov/geo/tiger/tiger2006se/*/*.ZIP; do
-  ./tigerimport.sh append networx $file $FIRST
+  ./tigerimport.sh append mydb $file $FIRST
   if [ -z "$FIRST" ]; then
     FIRST="-a"
   fi
