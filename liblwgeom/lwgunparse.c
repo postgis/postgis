442d938075ae897a0d39e1e5e1edcc54a4fe6516
Mark Cave-Ayland <mark.cave-ayland@siriusit.co.uk>
2008-09-23 19:53:12 +0000

    Revert commit 2990: the real bug was that the LWDEBUG statement was incorrect rather than the code - the additional read_int() was causing an off-by-one error when outputting WKB causing several regression tests to fail.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@2996 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwgunparse.c
+++ liblwgeom/lwgunparse.c
@@ -743,8 +743,7 @@ output_wkb_polygon_ring_collection(uchar* geom,outwkbfunc func)
 uchar *
 output_wkb_polygon_collection(uchar* geom)
 {
-	int cnt = read_int(&geom);
-	LWDEBUGF(2, "output_wkb_polygon_collection: %d iterations loop", cnt);
+	LWDEBUG(2, "output_wkb_polygon_collection");
 
 	return output_wkb_polygon_ring_collection(geom,output_wkb_point); 
 }
