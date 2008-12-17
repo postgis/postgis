postgis/postgis

commit b78c5c500d5f007e81659a5f9cd5651aa7d0415d

Fix for one of Regina's torture failures as posted to the -devel list here: http://postgis.refractions.net/pipermail/postgis-devel/2008-December/004362.html. A CurvePolygon has a "standard header" and so must be passed through a function that understands the optional existence of SRIDs/BBOXes etc. in the header.
git-svn-id: http://svn.osgeo.org/postgis/trunk@3431 b70326c6-7e19-0410-871a-916f4a2858ee

liblwgeom/lwgunparse.c
@@ -529,7 +529,7 @@ output_wkt(uchar* geom, int supress)
                                 if(writeM) write_str("CURVEPOLYGONM");
                                 else write_str("CURVEPOLYGON");
                         }
-                        geom = output_collection(geom, output_compound,0);
+                        geom = output_collection(geom, output_wkt,0);
                         break;
 		case MULTIPOINTTYPE:
 			if ( supress < 2 )

commit 1d40f4e51d36f9a772615c41f6adb27107821459

#HISTORY: Really fix what was broken in r3431/3432. See the notes there for the full detail.
git-svn-id: http://svn.osgeo.org/postgis/trunk@3436 b70326c6-7e19-0410-871a-916f4a2858ee

liblwgeom/lwgunparse.c
@@ -415,40 +415,41 @@ uchar *output_multipoint(uchar* geom,int suppress){
 	return output_wkt(geom,suppress);
 }
 
-/* special case for compound to suppress linestring but not circularstring */
+/* Special case for compound curves: suppress the LINESTRING prefix from a curve if it appears as
+   a component of a COMPOUNDCURVE, but not CIRCULARSTRING */
 uchar *output_compound(uchar* geom, int suppress) {
         unsigned type;
 
         LWDEBUG(2, "output_compound called.");
 
-        type=*geom++;
+        type=*geom;
         switch(TYPE_GETTYPE(type)) 
         {
                 case LINETYPE:
-                        geom = output_collection(geom,output_point,0);
+                        geom = output_wkt(geom,2);
                         break;
                 case CURVETYPE:
-                        write_str("CIRCULARSTRING");
-                        geom = output_curve_collection(geom,output_point,1);
+                        geom = output_wkt(geom,1);
                         break;
         }
 	return geom;
 }
 
+/* Special case for multisurfaces: suppress the POLYGON prefix from a surface if it appears as
+   a component of a MULTISURFACE, but not CURVEPOLYGON */
 uchar *output_multisurface(uchar* geom, int suppress) {
         unsigned type;
 
         LWDEBUG(2, "output_multisurface called.");
 
-        type=*geom++;
+        type=*geom;
         switch(TYPE_GETTYPE(type))
         {
                 case POLYGONTYPE:
-                        geom = output_collection(geom, output_polygon_collection,0);
+                        geom = output_wkt(geom,2);
                         break;
                 case CURVEPOLYTYPE:
-                        write_str("CURVEPOLYGON");
-                        geom = output_collection(geom, output_compound,1);
+                        geom = output_wkt(geom,1);
                         break;
         }
         return geom;