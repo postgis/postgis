d170351f42f4516c8b1445f4657df9beb183a5b1
Paul Ramsey <pramsey@cleverelephant.ca>
2009-03-19 17:50:36 +0000

    Make autogen.sh a little more tolerant of missing / re-named autotools.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@3901 b70326c6-7e19-0410-871a-916f4a2858ee

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,43 @@
 #!/bin/sh
-aclocal -I macros
-autoconf
+#
+# $Id$
+#
+# Autoconf Boot-strapping Script
+#
+
+giveup()
+{
+        echo
+        echo "  Something went wrong, giving up!"
+        echo
+        exit 1
+}
+
+OSTYPE=`uname -s`
+
+#
+# Look for aclocal under various names. Solaris uses 
+# versioned names.
+#
+for aclocal in aclocal aclocal-1.10 aclocal-1.9; do
+    ACLOCAL=`which $aclocal 2>/dev/null`
+    if test -x "${ACLOCAL}"; then
+        break;
+    fi
+done
+if [ ! $ACLOCAL ]; then
+    echo "Missing aclocal!"
+    exit
+fi
+
+#
+# Run the actual autotools routines.
+# 
+echo "Running aclocal -I macros"
+$ACLOCAL -I macros || giveup
+echo "Running autoconf"
+autoconf || giveup
+
+echo "======================================"
+echo "Now you are ready to run './configure'"
+echo "======================================"
