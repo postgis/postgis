107250eaf05b4a91d94b1524aaa14205434aadf2
Paul Ramsey <pramsey@cleverelephant.ca>
2010-03-16 18:23:17 +0000

    More PgSQL 9.0 compatibility changes.
    git-svn-id: http://svn.osgeo.org/postgis/branches/1.4@5442 b70326c6-7e19-0410-871a-916f4a2858ee

--- postgis/lwgeom_estimate.c
+++ postgis/lwgeom_estimate.c
@@ -239,6 +239,9 @@ Datum LWGEOM_gist_joinsel(PG_FUNCTION_ARGS)
 
 	if ( ! get_attstatsslot(stats1_tuple, 0, 0,
 	                        STATISTIC_KIND_GEOMETRY, InvalidOid, NULL, NULL,
+#if POSTGIS_PGSQL_VERSION >= 85
+	                        NULL,
+#endif
 	                        (float4 **)gs1ptr, &geomstats1_nvalues) )
 	{
 		POSTGIS_DEBUG(3, " STATISTIC_KIND_GEOMETRY stats not found - returning default geometry join selectivity");
@@ -263,6 +266,9 @@ Datum LWGEOM_gist_joinsel(PG_FUNCTION_ARGS)
 
 	if ( ! get_attstatsslot(stats2_tuple, 0, 0,
 	                        STATISTIC_KIND_GEOMETRY, InvalidOid, NULL, NULL,
+#if POSTGIS_PGSQL_VERSION >= 85
+	                        NULL,
+#endif
 	                        (float4 **)gs2ptr, &geomstats2_nvalues) )
 	{
 		POSTGIS_DEBUG(3, " STATISTIC_KIND_GEOMETRY stats not found - returning default geometry join selectivity");
@@ -715,6 +721,9 @@ Datum LWGEOM_gist_sel(PG_FUNCTION_ARGS)
 
 	if ( ! get_attstatsslot(stats_tuple, 0, 0,
 	                        STATISTIC_KIND_GEOMETRY, InvalidOid, NULL, NULL,
+#if POSTGIS_PGSQL_VERSION >= 85
+	                        NULL,
+#endif
 	                        (float4 **)gsptr, &geomstats_nvalues) )
 	{
 		POSTGIS_DEBUG(3, " STATISTIC_KIND_GEOMETRY stats not found - returning default geometry selectivity");
