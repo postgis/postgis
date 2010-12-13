79320ce2bbc47fda69b9d23e988885fa3b7bb65f
Paul Ramsey <pramsey@cleverelephant.ca>
2010-12-13 23:50:52 +0000

    Second thoughts on stringbuffer changes
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6367 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/stringbuffer.h
+++ liblwgeom/stringbuffer.h
@@ -59,8 +59,6 @@ extern void stringbuffer_clear(stringbuffer_t *sb);
 void stringbuffer_set(stringbuffer_t *sb, const char *s);
 void stringbuffer_copy(stringbuffer_t *sb, stringbuffer_t *src);
 extern void stringbuffer_append(stringbuffer_t *sb, const char *s);
-extern void stringbuffer_append_char(stringbuffer_t *s, const char *a);
-extern void stringbuffer_append_with_size(stringbuffer_t *sb, const char *s, size_t s_size);
 extern int stringbuffer_aprintf(stringbuffer_t *sb, const char *fmt, ...);
 extern const char *stringbuffer_getstring(stringbuffer_t *sb);
 extern char *stringbuffer_getstringcopy(stringbuffer_t *sb);
