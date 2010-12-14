ff64d5a9c85473aa8986c2a1e2e636f895557194
Paul Ramsey <pramsey@cleverelephant.ca>
2010-12-14 20:30:24 +0000

    Return non-zero when tests fail
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6382 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/cunit/cu_tester.c
+++ liblwgeom/cunit/cu_tester.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
 	CU_pSuite suite_to_run;
 	char *test_name;
 	CU_pTest test_to_run;
-	CU_ErrorCode errCode;
+	CU_ErrorCode errCode = 0;
 	CU_pTestRegistry registry;
 	int num_run;
 	int num_failed;
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 	CU_basic_set_mode(CU_BRM_VERBOSE);
 	if (argc <= 1)
 	{
-		CU_basic_run_tests();
+		errCode = CU_basic_run_tests();
 	}
 	else
 	{
@@ -181,8 +181,9 @@ int main(int argc, char *argv[])
 		CU_basic_show_failures(CU_get_failure_list());
 		printf("\n\n"); /* basic_show_failures leaves off line breaks. */
 	}
+	num_failed = CU_get_number_of_failures();
 	CU_cleanup_registry();
-	return CU_get_error();
+	return num_failed;
 }
 /**
  * CUnit error handler
