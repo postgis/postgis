b7766a573d37076cafe23040e9ef0c25a3d4819b
Sandro Santilli <strk@keybit.net>
2010-12-17 16:16:41 +0000

    Use newer quoting paradigm ($$); use ST_Crosses when willing to check crossing, not just intersection...; add proper credits
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6428 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/sql/populate.sql
+++ topology/sql/populate.sql
@@ -8,11 +8,18 @@
 -- This is free software; you can redistribute and/or modify it under
 -- the terms of the GNU General Public Licence. See the COPYING file.
 --
--- Author: Sandro Santilli <strk@refractions.net>
---  
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 --
 -- Functions used to populate a topology
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Developed by Sandro Santilli <strk@keybit.net>
+-- for Faunalia (http://www.faunalia.it) with funding from
+-- Regione Toscana - Sistema Informativo per la Gestione del Territorio
+-- e dell' Ambiente [RT-SIGTA].
+-- For the project: "Sviluppo strumenti software per il trattamento di dati
+-- geografici basati su QuantumGIS e Postgis (CIG 0494241492)"
 --
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -24,14 +31,16 @@
 -- Add a node primitive to a topology and get it's identifier.
 -- Returns an existing node at the same location, if any.
 --
--- When adding a _new_ node, it checks for existance of any
--- edge crossing the given point, and bails out if any was found.
+-- When adding a _new_ node it checks for the existance of any
+-- edge crossing the given point, raising an exception if found.
+--
+-- The newly added nodes have no containing face.
 --
 -- 
-CREATEFUNCTION topology.AddNode(varchar, geometry)
+CREATE OR REPLACE FUNCTION topology.AddNode(varchar, geometry)
 	RETURNS int
 AS
-'
+$$
 DECLARE
 	atopology ALIAS FOR $1;
 	apoint ALIAS FOR $2;
@@ -42,15 +51,15 @@ BEGIN
 	-- Atopology and apoint are required
 	-- 
 	IF atopology IS NULL OR apoint IS NULL THEN
-		RAISE EXCEPTION ''Invalid null argument'';
+		RAISE EXCEPTION 'Invalid null argument';
 	END IF;
 
 	--
 	-- Apoint must be a point
 	--
-	IF substring(geometrytype(apoint), 1, 5) != ''POINT''
+	IF substring(geometrytype(apoint), 1, 5) != 'POINT'
 	THEN
-		RAISE EXCEPTION ''Node geometry must be a point'';
+		RAISE EXCEPTION 'Node geometry must be a point';
 	END IF;
 
 	--
@@ -58,34 +67,33 @@ BEGIN
 	-- 
 	-- We use index AND x/y equality
 	--
-	FOR rec IN EXECUTE ''SELECT node_id FROM ''
-		|| quote_ident(atopology) || ''.node '' ||
-		''WHERE geom && '' || quote_literal(apoint::text) || ''::geometry''
-		||'' AND x(geom) = x(''||quote_literal(apoint::text)||''::geometry)''
-		||'' AND y(geom) = y(''||quote_literal(apoint::text)||''::geometry)''
+	FOR rec IN EXECUTE 'SELECT node_id FROM '
+		|| quote_ident(atopology) || '.node ' ||
+		'WHERE geom && ' || quote_literal(apoint::text) || '::geometry'
+		||' AND x(geom) = x('||quote_literal(apoint::text)||'::geometry)'
+		||' AND y(geom) = y('||quote_literal(apoint::text)||'::geometry)'
 	LOOP
 		RETURN  rec.node_id;
 	END LOOP;
 
 	--
-	-- Check if any edge crosses (intersects) this node
-	-- I used _intersects_ here to include boundaries (endpoints)
+	-- Check if any edge crosses this node
+	-- (endpoints are fine)
 	--
-	FOR rec IN EXECUTE ''SELECT edge_id FROM ''
-		|| quote_ident(atopology) || ''.edge '' 
-		|| ''WHERE geom && '' || quote_literal(apoint::text) 
-		|| '' AND intersects(geom, '' || quote_literal(apoint::text)
-		|| '')''
+	FOR rec IN EXECUTE 'SELECT edge_id FROM '
+		|| quote_ident(atopology) || '.edge ' 
+		|| 'WHERE ST_Crosses(' || quote_literal(apoint::text) 
+		|| ', geom)'
 	LOOP
 		RAISE EXCEPTION
-		''An edge crosses the given node.'';
+		'An edge crosses the given node.';
 	END LOOP;
 
 	--
 	-- Get new node id from sequence
 	--
-	FOR rec IN EXECUTE ''SELECT nextval('''''' ||
-		atopology || ''.node_node_id_seq'''')''
+	FOR rec IN EXECUTE 'SELECT nextval(''' ||
+		atopology || '.node_node_id_seq'')'
 	LOOP
 		nodeid = rec.nextval;
 	END LOOP;
@@ -93,14 +101,15 @@ BEGIN
 	--
 	-- Insert the new row
 	--
-	EXECUTE ''INSERT INTO '' || quote_ident(atopology)
-		|| ''.node(node_id, geom) 
-		VALUES(''||nodeid||'',''||quote_literal(apoint::text)||
-		'')'';
+	EXECUTE 'INSERT INTO ' || quote_ident(atopology)
+		|| '.node(node_id, geom) 
+		VALUES('||nodeid||','||quote_literal(apoint::text)||
+		')';
 
 	RETURN nodeid;
 	
 END
-'
+$$
 LANGUAGE 'plpgsql';
 --} AddNode
+
