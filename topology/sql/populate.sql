4dcc6fc5df67f863ee61c0362acf2298b365cc14
Sandro Santilli <strk@keybit.net>
2010-12-17 17:16:00 +0000

    New topology.AddEdge function [RT-SIGTA]
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6431 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/sql/populate.sql
+++ topology/sql/populate.sql
@@ -113,3 +113,139 @@ $$
 LANGUAGE 'plpgsql';
 --} AddNode
 
+--{
+--
+-- AddEdge(atopology, line)
+--
+-- Add an edge primitive to a topology and get it's identifier.
+-- Edge endpoints will also be added as nodes. 
+--
+-- An exception is raised if the given line crosses an existing
+-- node or interects with an existing edge on anything but endnodes.
+--
+-- The newly added edge has "universe" face on both sides
+-- and links to itself.
+-- Calling code is expected to do further linking.
+--
+-- 
+CREATE OR REPLACE FUNCTION topology.AddEdge(varchar, geometry)
+	RETURNS int
+AS
+$$
+DECLARE
+	atopology ALIAS FOR $1;
+	aline ALIAS FOR $2;
+	edgeid int;
+	rec RECORD;
+BEGIN
+	--
+	-- Atopology and apoint are required
+	-- 
+	IF atopology IS NULL OR aline IS NULL THEN
+		RAISE EXCEPTION 'Invalid null argument';
+	END IF;
+
+	--
+	-- Aline must be a linestring
+	--
+	IF substring(geometrytype(aline), 1, 4) != 'LINE'
+	THEN
+		RAISE EXCEPTION 'Edge geometry must be a linestring';
+	END IF;
+
+	--
+	-- Check if the edge crosses an existing node
+	--
+	FOR rec IN EXECUTE 'SELECT node_id FROM '
+		|| quote_ident(atopology) || '.node '
+		|| 'WHERE ST_Crosses('
+		|| quote_literal(aline::text) || '::geometry, geom'
+		|| ')'
+	LOOP
+		RAISE EXCEPTION 'Edge crosses node %', rec.node_id;
+	END LOOP;
+
+	--
+	-- Check if the edge intersects an existing edge
+	-- on anything but endpoints
+	--
+	-- Following DE-9 Intersection Matrix represent
+	-- the only relation we accept. 
+	--
+	--    FF1
+	--    F*0
+	--    102
+	--
+	FOR rec IN EXECUTE 'SELECT edge_id FROM '
+		|| quote_ident(atopology) || '.edge '
+		|| 'WHERE '
+		|| quote_literal(aline::text) || '::geometry && geom'
+		|| ' AND NOT ST_Relate('
+		|| quote_literal(aline::text)
+		|| '::geometry, geom, ''FF1F*0102'''
+		|| ')'
+
+	LOOP
+		-- TODO: reuse an EQUAL edge ?
+		RAISE EXCEPTION 'Edge intersects (not on endpoints) with existing edge % ', rec.edge_id;
+	END LOOP;
+
+	--
+	-- Get new edge id from sequence
+	--
+	FOR rec IN EXECUTE 'SELECT nextval(''' ||
+		atopology || '.edge_data_edge_id_seq'')'
+	LOOP
+		edgeid = rec.nextval;
+	END LOOP;
+
+	--
+	-- Insert the new row
+	--
+	EXECUTE 'INSERT INTO '
+		|| quote_ident(atopology)
+		|| '.edge(edge_id, start_node, end_node, '
+		|| 'next_left_edge, next_right_edge, '
+		|| 'left_face, right_face, '
+		|| 'geom) '
+		|| ' VALUES('
+
+		-- edge_id
+		|| edgeid ||','
+
+		-- start_node
+		|| 'topology.addNode('
+		|| quote_literal(atopology)
+		|| ', ST_StartPoint('
+		|| quote_literal(aline::text)
+		|| ')) ,'
+
+		-- end_node
+		|| 'topology.addNode('
+		|| quote_literal(atopology)
+		|| ', ST_EndPoint('
+		|| quote_literal(aline::text)
+		|| ')) ,'
+
+		-- next_left_edge
+		|| edgeid ||','
+
+		-- next_right_edge
+		|| edgeid ||','
+
+		-- left_face
+		|| '0,'
+
+		-- right_face
+		|| '0,'
+
+		-- geom
+		||quote_literal(aline::text)
+		|| ')';
+
+	RETURN edgeid;
+	
+END
+$$
+LANGUAGE 'plpgsql';
+--} AddEdge
