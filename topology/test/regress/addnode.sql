13559b749e224a28160b77f147e10a8b50733b96
Sandro Santilli <strk@keybit.net>
2010-12-17 15:51:04 +0000

    Add an inspection in the final node table
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6427 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/regress/addnode.sql
+++ topology/test/regress/addnode.sql
@@ -12,4 +12,9 @@ SELECT 'p1b', topology.addNode('nodes', 'POINT(0 0)');
 SELECT 'p2',  topology.addNode('nodes', 'POINT(1 0)');
 SELECT 'p2b', topology.addNode('nodes', 'POINT(1 0)');
 
+-- Check we only have two points, both with unknown containing face
+
+SELECT node_id, containing_face, st_astext(geom) from nodes.node
+ORDER by node_id;
+
 SELECT topology.DropTopology('nodes');
