d7ec3dcd839060efae88dd634c4dd01a476e8a7f
Regina Obe <lr@pcorp.us>
2010-12-29 15:17:08 +0000

    remove deprecated tests
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6534 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/sql-mm-multicurve.sql
+++ regress/sql-mm-multicurve.sql
@@ -1,76 +1,3 @@
-SELECT 'ndims01', ndims(geomfromewkt('MULTICURVE((
-                5 5 1 3,
-                3 5 2 2,
-                3 3 3 1,
-                0 3 1 1)
-                ,CIRCULARSTRING(
-                0 0 0 0, 
-                0.26794919243112270647255365849413 1 3 -2, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097 1 2))'));
-SELECT 'geometrytype01', geometrytype(geomfromewkt('MULTICURVE((
-                5 5 1 3,
-                3 5 2 2,
-                3 3 3 1,
-                0 3 1 1)
-                ,CIRCULARSTRING(
-                0 0 0 0, 
-                0.26794919243112270647255365849413 1 3 -2, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097 1 2))'));
-SELECT 'ndims02', ndims(geomfromewkt('MULTICURVE((
-                5 5 1,
-                3 5 2,
-                3 3 3,
-                0 3 1)
-                ,CIRCULARSTRING(
-                0 0 0, 
-                0.26794919243112270647255365849413 1 3, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097 1))'));
-SELECT 'geometrytype02', geometrytype(geomfromewkt('MULTICURVE((
-                5 5 1,
-                3 5 2,
-                3 3 3,
-                0 3 1)
-                ,CIRCULARSTRING(
-                0 0 0, 
-                0.26794919243112270647255365849413 1 3, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097 1))'));
-SELECT 'ndims03', ndims(geomfromewkt('MULTICURVEM((
-                5 5 3,
-                3 5 2,
-                3 3 1,
-                0 3 1)
-                ,CIRCULARSTRING(
-                0 0 0, 
-                0.26794919243112270647255365849413 1 -2, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097 2))'));
-SELECT 'geometrytype03', geometrytype(geomfromewkt('MULTICURVEM((
-                5 5 3,
-                3 5 2,
-                3 3 1,
-                0 3 1)
-                ,CIRCULARSTRING(
-                0 0 0, 
-                0.26794919243112270647255365849413 1 -2, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097 2))'));
-SELECT 'ndims04', ndims(geomfromewkt('MULTICURVE((
-                5 5,
-                3 5,
-                3 3,
-                0 3)
-                ,CIRCULARSTRING(
-                0 0, 
-                0.26794919243112270647255365849413 1, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097))'));
-SELECT 'geometrytype04', geometrytype(geomfromewkt('MULTICURVE((
-                5 5,
-                3 5,
-                3 3,
-                0 3)
-                ,CIRCULARSTRING(
-                0 0, 
-                0.26794919243112270647255365849413 1, 
-                0.5857864376269049511983112757903 1.4142135623730950488016887242097))'));
-
 -- Repeat the tests with the new function names.
 SELECT 'ndims01', ST_ndims(ST_geomfromewkt('MULTICURVE((
                 5 5 1 3,
@@ -157,7 +84,7 @@ INSERT INTO public.multicurve (
       ) VALUES (
         1, 'multicurve');
 UPDATE public.multicurve
-        SET the_geom_4d = geomfromewkt('MULTICURVE((
+        SET the_geom_4d = ST_Geomfromewkt('MULTICURVE((
                 5 5 1 3,
                 3 5 2 2,
                 3 3 3 1,
@@ -167,7 +94,7 @@ UPDATE public.multicurve
                 0.26794919243112270647255365849413 1 3 -2, 
                 0.5857864376269049511983112757903 1.4142135623730950488016887242097 1 2))');
 UPDATE public.multicurve
-        SET the_geom_3dz = geomfromewkt('MULTICURVE((
+        SET the_geom_3dz = ST_Geomfromewkt('MULTICURVE((
                 5 5 1,
                 3 5 2,
                 3 3 3,
@@ -177,7 +104,7 @@ UPDATE public.multicurve
                 0.26794919243112270647255365849413 1 3, 
                 0.5857864376269049511983112757903 1.4142135623730950488016887242097 1))');
 UPDATE public.multicurve        
-        SET the_geom_3dm = geomfromewkt('MULTICURVEM((
+        SET the_geom_3dm = ST_Geomfromewkt('MULTICURVEM((
                 5 5 3,
                 3 5 2,
                 3 3 1,
@@ -187,7 +114,7 @@ UPDATE public.multicurve
                 0.26794919243112270647255365849413 1 -2, 
                 0.5857864376269049511983112757903 1.4142135623730950488016887242097 2))');
 UPDATE public.multicurve
-        SET the_geom_2d = geomfromewkt('MULTICURVE((
+        SET the_geom_2d = ST_Geomfromewkt('MULTICURVE((
                 5 5,
                 3 5,
                 3 3,
@@ -196,16 +123,6 @@ UPDATE public.multicurve
                 0 0, 
                 0.26794919243112270647255365849413 1, 
                 0.5857864376269049511983112757903 1.4142135623730950488016887242097))');
-
-SELECT 'astext01', astext(the_geom_2d) FROM public.multicurve;
-SELECT 'astext02', astext(the_geom_3dm) FROM public.multicurve;
-SELECT 'astext03', astext(the_geom_3dz) FROM public.multicurve;
-SELECT 'astext04', astext(the_geom_4d) FROM public.multicurve;
-
-SELECT 'asewkt01', asewkt(the_geom_2d) FROM public.multicurve;
-SELECT 'asewkt02', asewkt(the_geom_3dm) FROM public.multicurve;
-SELECT 'asewkt03', asewkt(the_geom_3dz) FROM public.multicurve;
-SELECT 'asewkt04', asewkt(the_geom_4d) FROM public.multicurve;
 
 -- These tests will fail on different architectures
 -- We need a way to handle multiple byte orderings
@@ -219,20 +136,20 @@ SELECT 'asewkt04', asewkt(the_geom_4d) FROM public.multicurve;
 --SELECT 'asewkb03', encode(asewkb(the_geom_3dz), 'hex') FROM public.multicurve;
 --SELECT 'asewkb04', encode(asewkb(the_geom_4d), 'hex') FROM public.multicurve;
 
-SELECT 'ST_CurveToLine-201', asewkt(snapToGrid(ST_CurveToLine(the_geom_2d, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine-202', asewkt(snapToGrid(ST_CurveToLine(the_geom_3dm, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine-203', asewkt(snapToGrid(ST_CurveToLine(the_geom_3dz, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine-204', asewkt(snapToGrid(ST_CurveToLine(the_geom_4d, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-201', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_2d, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-202', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_3dm, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-203', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_3dz, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-204', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_4d, 2), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
 
-SELECT 'ST_CurveToLine-401', asewkt(snapToGrid(ST_CurveToLine(the_geom_2d, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine-402', asewkt(snapToGrid(ST_CurveToLine(the_geom_3dm, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine-403', asewkt(snapToGrid(ST_CurveToLine(the_geom_3dz, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine-404', asewkt(snapToGrid(ST_CurveToLine(the_geom_4d, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-401', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_2d, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-402', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_3dm, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-403', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_3dz, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine-404', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_4d, 4), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
 
-SELECT 'ST_CurveToLine01', asewkt(snapToGrid(ST_CurveToLine(the_geom_2d), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine02', asewkt(snapToGrid(ST_CurveToLine(the_geom_3dm), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine03', asewkt(snapToGrid(ST_CurveToLine(the_geom_3dz), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
-SELECT 'ST_CurveToLine04', asewkt(snapToGrid(ST_CurveToLine(the_geom_4d), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine01', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_2d), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine02', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_3dm), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine03', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_3dz), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
+SELECT 'ST_CurveToLine04', ST_Asewkt(ST_SnapToGrid(ST_CurveToLine(the_geom_4d), 'POINT(0 0 0 0)'::geometry, 1e-8, 1e-8, 1e-8, 1e-8)) FROM public.multicurve;
 
 -- Removed due to descrepencies between hardware
 --SELECT 'box2d01', box2d(the_geom_2d) FROM public.multicurve;
@@ -244,39 +161,13 @@ SELECT 'ST_CurveToLine04', asewkt(snapToGrid(ST_CurveToLine(the_geom_4d), 'POINT
 --SELECT 'box3d02', box3d(the_geom_3dm) FROM public.multicurve;
 --SELECT 'box3d03', box3d(the_geom_3dz) FROM public.multicurve;
 --SELECT 'box3d04', box3d(the_geom_4d) FROM public.multicurve;
-
-SELECT 'isValid01', isValid(the_geom_2d) FROM public.multicurve;
-SELECT 'isValid02', isValid(the_geom_3dm) FROM public.multicurve;
-SELECT 'isValid03', isValid(the_geom_3dz) FROM public.multicurve;
-SELECT 'isValid04', isValid(the_geom_4d) FROM public.multicurve;
-
-SELECT 'dimension01', dimension(the_geom_2d) FROM public.multicurve;
-SELECT 'dimension02', dimension(the_geom_3dm) FROM public.multicurve;
-SELECT 'dimension03', dimension(the_geom_3dz) FROM public.multicurve;
-SELECT 'dimension04', dimension(the_geom_4d) FROM public.multicurve;
-
-SELECT 'numGeometries01', numGeometries(the_geom_2d) FROM public.multicurve;
-SELECT 'numGeometries02', numGeometries(the_geom_3dm) FROM public.multicurve;
-SELECT 'numGeometries03', numGeometries(the_geom_3dz) FROM public.multicurve;
-SELECT 'numGeometries04', numGeometries(the_geom_4d) FROM public.multicurve;
-
-SELECT 'geometryN-201', asEWKT(geometryN(the_geom_2d, 2)) FROM public.multicurve;
-SELECT 'geometryN-202', asEWKT(geometryN(the_geom_3dm, 2)) FROM public.multicurve;
-SELECT 'geometryN-203', asEWKT(geometryN(the_geom_3dz, 2)) FROM public.multicurve;
-SELECT 'geometryN-204', asEWKT(geometryN(the_geom_4d, 2)) FROM public.multicurve;
-
-SELECT 'geometryN-301', (asEWKT(geometryN(the_geom_2d, 3)) is null) FROM public.multicurve;
-SELECT 'geometryN-302', (asEWKT(geometryN(the_geom_3dm, 3)) is null) FROM public.multicurve;
-SELECT 'geometryN-303', (asEWKT(geometryN(the_geom_3dz, 3)) is null) FROM public.multicurve;
-SELECT 'geometryN-304', (asEWKT(geometryN(the_geom_4d, 3)) is null) FROM public.multicurve;
-
 -- TODO: ST_SnapToGrid is required to remove platform dependent precision
 -- issues.  Until ST_SnapToGrid is updated to work against curves, these
 -- tests cannot be run.
---SELECT 'ST_LineToCurve01', asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_2d))) FROM public.multicurve;
---SELECT 'ST_LineToCurve02', asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_3dm))) FROM public.multicurve;
---SELECT 'ST_LineToCurve03', asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_3dz))) FROM public.multicurve;
---SELECT 'ST_LineToCurve04', asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_4d))) FROM public.multicurve;
+--SELECT 'ST_LineToCurve01', ST_Asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_2d))) FROM public.multicurve;
+--SELECT 'ST_LineToCurve02', ST_Asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_3dm))) FROM public.multicurve;
+--SELECT 'ST_LineToCurve03', ST_Asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_3dz))) FROM public.multicurve;
+--SELECT 'ST_LineToCurve04', ST_Asewkt(ST_LineToCurve(ST_CurveToLine(the_geom_4d))) FROM public.multicurve;
 
 -- Repeat all tests with the new function names.
 SELECT 'astext01', ST_astext(the_geom_2d) FROM public.multicurve;
