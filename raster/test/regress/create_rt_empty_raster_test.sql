565ac3a2428d8d8cdb494a85050349bb2b936868
Jorge Arévalo <jorge.arevalo at deimos-space.com>
2011-01-03 16:17:36 +0000

    Added regress test files for ST_IsEmpty and ST_HasNoBand (they were missed in previous commit)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6556 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ raster/test/regress/create_rt_empty_raster_test.sql
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------
+-- $Id$
+--
+-- Copyright (c) 2011 Jorge Arevalo <jorge.arevalo@deimos-space.com>
+--
+-- This is free software; you can redistribute and/or modify it under
+-- the terms of the GNU General Public Licence. See the COPYING file.
+-----------------------------------------------------------------------
+
+CREATE TABLE empty_raster_test (
+	rid numeric,
+	rast raster
+);
+
+INSERT INTO empty_raster_test
+VALUES (1, ST_MakeEmptyRaster( 100, 100, 0.0005, 0.0005, 1, 1, 0, 0, 4326) );
---
+++ raster/test/regress/rt_isempty.sql
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------
+-- $Id$
+--
+-- Copyright (c) 2011 Jorge Arevalo <jorge.arevalo@deimos-space.com>
+--
+-- This is free software; you can redistribute and/or modify it under
+-- the terms of the GNU General Public Licence. See the COPYING file.
+-----------------------------------------------------------------------
+
+-------------------------------------------------------------------
+-- st_isempty
+-----------------------------------------------------------------------
+select st_isempty(rast) from empty_raster_test;
