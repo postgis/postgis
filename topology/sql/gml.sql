73bb371e4ad23348af90a12a3d7c0d0050dd8cf0
Sandro Santilli <strk@keybit.net>
2011-01-22 17:26:09 +0000

    Rename internal functions so to have an underscore prefix, document them as such. Drop wrappers for internal functions (they are not meant to be called by users so don't need to be friendly) [RT-SIGTA]
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6694 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/sql/gml.sql
+++ topology/sql/gml.sql
@@ -25,10 +25,11 @@
 
 --{
 --
--- text AsGMLNode(id, point, nsprefix)
+-- INTERNAL FUNCTION
+-- text _AsGMLNode(id, point, nsprefix)
 --
 -- 
-CREATE OR REPLACE FUNCTION topology.AsGMLNode(int, geometry, text)
+CREATE OR REPLACE FUNCTION topology._AsGMLNode(int, geometry, text)
   RETURNS text
 AS
 $$
@@ -63,26 +64,15 @@ BEGIN
 END
 $$
 LANGUAGE 'plpgsql';
---} AsGMLNode(id, point, nsprefix)
-
---{ AsGMLNode(id, point)
-CREATE OR REPLACE FUNCTION topology.AsGMLNode(int, geometry) RETURNS text
-AS $$ SELECT topology.AsGMLNode($1, $2, 'gml'); $$
-LANGUAGE 'sql';
---} AsGMLNode(id, point)
-
---{ AsGMLNode(id)
-CREATE OR REPLACE FUNCTION topology.AsGMLNode(int) RETURNS text
-AS $$ SELECT topology.AsGMLNode($1, NULL, 'gml'); $$
-LANGUAGE 'sql';
---} AsGMLNode(id)
+--} _AsGMLNode(id, point, nsprefix)
 
 --{
 --
--- text AsGMLEdge(edge_id, start_node, end_node, line, nsprefix)
+-- INTERNAL FUNCTION
+-- text _AsGMLEdge(edge_id, start_node, end_node, line, nsprefix)
 --
 -- 
-CREATE OR REPLACE FUNCTION topology.AsGMLEdge(int, int, int, geometry, text)
+CREATE OR REPLACE FUNCTION topology._AsGMLEdge(int, int, int, geometry, text)
   RETURNS text
 AS
 $$
@@ -110,13 +100,13 @@ BEGIN
   -- Start node
   -- TODO: optionally output the directedNode as xlink, using a visited map
   gml = gml || '<' || nsprefix || 'directedNode orientation="-">';
-  gml = gml || topology.AsGMLNode(start_node, NULL, nsprefix_in);
+  gml = gml || topology._AsGMLNode(start_node, NULL, nsprefix_in);
   gml = gml || '</' || nsprefix || 'directedNode>';
 
   -- End node
   -- TODO: optionally output the directedNode as xlink, using a visited map
   gml = gml || '<' || nsprefix || 'directedNode>';
-  --gml = gml || topology.AsGMLNode(end_node, NULL, nsprefix_in);
+  --gml = gml || topology._AsGMLNode(end_node, NULL, nsprefix_in);
   gml = gml || '</' || nsprefix || 'directedNode>';
 
   IF line IS NOT NULL THEN
@@ -131,18 +121,11 @@ BEGIN
 END
 $$
 LANGUAGE 'plpgsql';
---} AsGMLEdge(id, start_node, end_node, line, nsprefix)
-
---{ AsGMLEdge(id, start_node, end_node, line)
-CREATE OR REPLACE FUNCTION topology.AsGMLEdge(int, int, int, geometry)
-RETURNS text AS $$
-  SELECT topology.AsGMLEdge($1, $2, $3, $4, 'gml');
-$$ LANGUAGE 'sql';
---} AsGMLEdge(id, start_node, end_node, line)
-
-
+--} _AsGMLEdge(id, start_node, end_node, line, nsprefix)
 
 --{
+--
+-- API FUNCTION
 --
 -- text AsGML(TopoGeometry, nsprefix)
 --
@@ -187,7 +170,7 @@ BEGIN
       || ' AND r.topogeo_id = ' || tg.id
     LOOP
       gml = gml || '<' || nsprefix || 'directedNode>';
-      gml = gml || topology.AsGMLNode(rec.element_id, rec.geom, nsprefix_in);
+      gml = gml || topology._AsGMLNode(rec.element_id, rec.geom, nsprefix_in);
       gml = gml || '</' || nsprefix || 'directedNode>';
     END LOOP;
     gml = gml || '</' || nsprefix || 'TopoPoint>';
@@ -209,7 +192,7 @@ BEGIN
       ELSE
         gml = gml || '<' || nsprefix || 'directedEdge>';
       END IF;
-      gml = gml || topology.AsGMLEdge(rec.edge_id, rec.start_node,
+      gml = gml || topology._AsGMLEdge(rec.edge_id, rec.start_node,
                                       rec.end_node, rec.geom, nsprefix_in);
       gml = gml || '</' || nsprefix || 'directedEdge>';
     END LOOP;
@@ -244,7 +227,7 @@ BEGIN
         -- TODO: add left_face/right_face to the conditional, to reduce load ?
       LOOP
         -- TODO: pass the 'visited' table over
-        gml = gml || topology.AsGMLEdge(rec2.edge_id,
+        gml = gml || topology._AsGMLEdge(rec2.edge_id,
                                         rec2.start_node,
                                         rec2.end_node, rec2.geom,
                                         nsprefix_in);
