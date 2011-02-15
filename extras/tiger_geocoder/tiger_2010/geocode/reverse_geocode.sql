e84a603c34ec6e618bcd6d759fa2d513c7a7e785
Regina Obe <lr@pcorp.us>
2011-02-15 13:49:06 +0000

    Make work on PostgreSQL 8.4 (was using some syntax only allowed in 9.0+).  Also account for the odd / even side of street rule.    And of course that demonstrated there is a bug somewhere (most likely in the geocoder), but got to pull up some maps to see which is right.  One of these has the address orientations flipped (odd /even not right). Of course I have to be right :).  Boy do I hate when you program the reverse of something and it exposes a bug. Also fix some minor documentation.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@6823 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/geocode/reverse_geocode.sql
+++ extras/tiger_geocoder/tiger_2010/geocode/reverse_geocode.sql
@@ -20,6 +20,7 @@ DECLARE
   var_states text[];
   var_addy NORM_ADDY;
   var_strnum varchar;
+  var_nstrnum numeric(10);
   var_primary_line geometry := NULL;
   var_primary_dist numeric(10,2) ;
   var_pt geometry;
@@ -46,15 +47,15 @@ BEGIN
   FOR var_redge IN
         SELECT * 
         FROM (SELECT DISTINCT ON(fullname)  foo.fullname, foo.stusps, foo.zip, 
-               (SELECT z.place FROM zip_state_loc AS z WHERE z.zip = foo.zip and z.statefp = foo.statefp LIMIT 1) As place, foo.intpt,
+               (SELECT z.place FROM zip_state_loc AS z WHERE z.zip = foo.zip and z.statefp = foo.statefp LIMIT 1) As place, foo.center_pt,
               side, to_number(fromhn, '999999') As fromhn, to_number(tohn, '999999') As tohn, ST_GeometryN(ST_Multi(line),1) As line, foo.dist
         FROM 
-          (SELECT e.the_geom As line, e.fullname, a.zip, s.stusps, ST_ClosestPoint(e.the_geom, var_pt) As intpt, e.statefp, a.side, a.fromhn, a.tohn, ST_Distance_Sphere(e.the_geom, var_pt) As dist
+          (SELECT e.the_geom As line, e.fullname, a.zip, s.stusps, ST_ClosestPoint(e.the_geom, var_pt) As center_pt, e.statefp, a.side, a.fromhn, a.tohn, ST_Distance_Sphere(e.the_geom, var_pt) As dist
           		FROM edges AS e INNER JOIN state As s ON (e.statefp = s.statefp AND s.statefp = ANY(var_states) )
           			INNER JOIN faces As fl ON (e.tfidl = fl.tfid AND e.statefp = fl.statefp)
           			INNER JOIN faces As fr ON (e.tfidr = fr.tfid AND e.statefp = fr.statefp)
           			INNER JOIN addr As a ON ( e.tlid = a.tlid AND e.statefp = a.statefp AND  
-          			   ( ( ST_Contains(fl.the_geom, var_pt) AND a.side = 'L') OR ( ST_Contains(fr.the_geom, var_pt) AND a.side = 'R' ) ) )
+          			   ( ( ST_Covers(fl.the_geom, var_pt) AND a.side = 'L') OR ( ST_Covers(fr.the_geom, var_pt) AND a.side = 'R' ) ) )
           			-- INNER JOIN zip_state_loc As z ON (a.statefp =  z.statefp AND a.zip = z.zip) /** really slow with this join **/
           		WHERE e.statefp = ANY(var_states) AND a.statefp = ANY(var_states) AND ST_DWithin(e.the_geom, var_pt, 0.005)
           		ORDER BY ST_Distance_Sphere(e.the_geom, var_pt) LIMIT 4) As foo 
@@ -66,13 +67,18 @@ BEGIN
        END IF;
        -- We only consider other edges as matches if they intersect our primary edge -- that would mean we are at a corner place
        IF ST_Intersects(var_redge.line, var_primary_line) THEN
-           intpt := array_append(intpt,var_redge.intpt); 
+           intpt := array_append(intpt,var_redge.center_pt); 
            IF var_redge.fullname IS NOT NULL THEN
                 street := array_append(street, (CASE WHEN include_strnum_range THEN COALESCE(var_redge.fromhn::varchar, '')::varchar || ' - ' || COALESCE(var_redge.tohn::varchar,'')::varchar || ' '::varchar  ELSE '' END::varchar ||  var_redge.fullname::varchar)::varchar);
                 --interploate the number -- note that if fromhn > tohn we will be subtracting which is what we want
                 -- We only consider differential distances are reeally close from our primary pt
                 IF var_redge.dist < var_primary_dist*1.1 THEN 
-                    var_strnum := (var_redge.fromhn + ST_Line_Locate_Point(var_redge.line, var_pt)*(var_redge.tohn - var_redge.fromhn))::numeric(10)::varchar;
+                	var_nstrnum := (var_redge.fromhn + ST_Line_Locate_Point(var_redge.line, var_pt)*(var_redge.tohn - var_redge.fromhn))::numeric(10);
+                	-- The odd even street number side of street rule
+                	IF (var_nstrnum  % 2)  != (var_redge.tohn % 2) THEN
+                		var_nstrnum := CASE WHEN var_nstrnum + 1 NOT BETWEEN var_redge.fromhn AND var_redge.tohn THEN var_nstrnum - 1 ELSE var_nstrnum + 1 END;
+                	END IF;
+                    var_strnum := var_nstrnum::varchar;
                     var_addy := normalize_address( COALESCE(var_strnum::varchar || ' ', '') || var_redge.fullname || ', ' || var_redge.place || ', ' || var_redge.stusps || ' ' || var_redge.zip);
                     addy := array_append(addy, var_addy);
                 END IF;
@@ -82,7 +88,7 @@ BEGIN
           		
   RETURN;   
 END;
-$_$ LANGUAGE plpgsql;
+$_$ LANGUAGE plpgsql STABLE;
 
 CREATE OR REPLACE FUNCTION reverse_geocode(IN pt geometry, OUT intpt geometry[],
     OUT addy NORM_ADDY[],
@@ -92,4 +98,4 @@ $$
 -- default to not including street range in cross streets
 SELECT reverse_geocode($1,false);
 $$
-language sql;
+language sql STABLE;
