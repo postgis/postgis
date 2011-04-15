69833d53ca534426437e07f1bca5a03534a6836e
Regina Obe <lr@pcorp.us>
2011-04-15 11:00:57 +0000

    replace with linux breaks, minor changes to paths
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7033 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/create_geocode.sh
+++ extras/tiger_geocoder/tiger_2010/create_geocode.sh
@@ -4,8 +4,8 @@ PGHOST=localhost
 PGUSER=postgres
 PGPASSWORD=yourpasswordhere
 THEDB=geocoder
-PSQL_CMD=psql
-PGCONTRIB=/usr/share/contrib
+PSQL_CMD=/usr/bin/psql
+PGCONTRIB=/usr/share/postgresql/contrib
 ${PSQL_CMD} -d "${THEDB}" -f "${PGCONTRIB}/fuzzystrmatch.sql"
 ${PSQL_CMD} -d "${THEDB}" -c "CREATE SCHEMA tiger"
 ${PSQL_CMD} -d "${THEDB}" -f "tables/lookup_tables_2010.sql"
