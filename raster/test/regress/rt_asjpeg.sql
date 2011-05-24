ad0b84685abd60fa55b1436c2fc7e04f94a6cd68
Bborie Park <bkpark at ucdavis.edu>
2011-05-24 14:27:08 +0000

    - Added test testgdalraster.in which properly tests the output of ST_AsGDALRaster.  This test makes use of rtgdalraster.py found in raster/scripts/python, which itself requires psycopg2. - Refactored rt_asgdalraster.sql, rt_astiff.sql, rt_asjpeg.sql and rt_aspng.sql to no longer attempt to validate the output of the respective functions with an MD5 checksum but rather be a test of the functions' behaviors.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7235 b70326c6-7e19-0410-871a-916f4a2858ee

--- raster/test/regress/rt_asjpeg.sql
+++ raster/test/regress/rt_asjpeg.sql
@@ -1,81 +1,113 @@
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BSI', 123, NULL)
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BUI', 123, NULL)
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BSI', -123, NULL)
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BUI', 254, NULL)
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(
-			ST_AddBand(
-				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '8BUI', 1, 255
-				)
-				, 2, '8BUI', 11, 0
-			)
-			, 3, '8BUI', 111, 127
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
+			ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BSI', 123, NULL)
 		)
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
+			ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BUI', 123, NULL)
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
+			ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BSI', -123, NULL)
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
+			ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '8BUI', 254, NULL)
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '8BSI', 1, -1
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '8BUI', 1, 255
+					)
+					, 2, '8BUI', 11, 0
 				)
-				, 2, '8BSI', 11, -1
+				, 3, '8BUI', 111, 127
 			)
-			, 3, '8BSI', 111, -1
-		),
-		ARRAY['QUALITY=90','PROGRESSIVE=ON']
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '8BSI', 1, -1
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '8BSI', 1, -1
+					)
+					, 2, '8BSI', 11, -1
 				)
-				, 2, '8BSI', 11, -1
-			)
-			, 3, '8BSI', 111, -1
-		),
-		ARRAY[3,1],
-		50
-	)
-);
-SELECT md5(
-	ST_AsJPEG(
-		ST_AddBand(
+				, 3, '8BSI', 111, -1
+			),
+			ARRAY['QUALITY=90','PROGRESSIVE=ON']
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '8BSI', 1, -1
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '8BSI', 1, -1
+					)
+					, 2, '8BSI', 11, -1
 				)
-				, 2, '8BSI', 11, -1
-			)
-			, 3, '8BUI', 111, -1
-		),
-		ARRAY[3],
-		10
-	)
-);
+				, 3, '8BSI', 111, -1
+			),
+			ARRAY[3,1],
+			50
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsJPEG(
+			ST_AddBand(
+				ST_AddBand(
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '8BSI', 1, -1
+					)
+					, 2, '8BSI', 11, -1
+				)
+				, 3, '8BUI', 111, -1
+			),
+			ARRAY[3],
+			10
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
--- raster/test/regress/rt_astiff.sql
+++ raster/test/regress/rt_astiff.sql
@@ -1,91 +1,119 @@
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '64BF', 123.4567, NULL)
-	)
-);
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
+			ST_AddBand(ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1), 1, '64BF', 123.4567, NULL)
+		)
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0,-1)
-					, 1, '64BF', 1234.5678, NULL
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0,-1)
+						, 1, '64BF', 1234.5678, NULL
+					)
+					, '64BF', 987.654321, NULL
 				)
-				, '64BF', 987.654321, NULL
+				, '64BF', 9876.54321, NULL
 			)
-			, '64BF', 9876.54321, NULL
 		)
-	)
-);
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '64BF', 1234.5678, -9999
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '64BF', 1234.5678, -9999
+					)
+					, '64BF', 987.654321, -9999
 				)
-				, '64BF', 987.654321, -9999
+				, '64BF', 9876.54321, -9999
 			)
-			, '64BF', 9876.54321, -9999
 		)
-	)
-);
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '64BF', 1234.5678, -9999
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '64BF', 1234.5678, -9999
+					)
+					, '64BF', 987.654321, -9999
 				)
-				, '64BF', 987.654321, -9999
+				, '64BF', 9876.54321, -9999
 			)
-			, '64BF', 9876.54321, -9999
+			, ARRAY[3]
 		)
-		, ARRAY[3]
-	)
-);
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '64BF', 1234.5678, -9999
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '64BF', 1234.5678, -9999
+					)
+					, '64BF', 987.654321, -9999
 				)
-				, '64BF', 987.654321, -9999
+				, '64BF', 9876.54321, -1
 			)
-			, '64BF', 9876.54321, -1
 		)
-	)
-);
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '64BF', 1234.5678, -1
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '64BF', 1234.5678, -1
+					)
+					, '64BF', 987.654321, -9999
 				)
-				, '64BF', 987.654321, -9999
+				, '64BF', 9876.54321, -9999
 			)
-			, '64BF', 9876.54321, -9999
 		)
-	)
-);
-SELECT md5(
-	ST_AsTIFF(
-		ST_AddBand(
+	) > 0
+		THEN 1
+	ELSE 0
+END;
+SELECT CASE
+	WHEN length(
+		ST_AsTIFF(
 			ST_AddBand(
 				ST_AddBand(
-					ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
-					, 1, '64BF', 1234.5678, -9999
+					ST_AddBand(
+						ST_MakeEmptyRaster(200, 200, 10, 10, 2, 2, 0, 0, -1)
+						, 1, '64BF', 1234.5678, -9999
+					)
+					, '64BF', 987.654321, -9999
 				)
-				, '64BF', 987.654321, -9999
+				, '64BF', 9876.54321, -1
 			)
-			, '64BF', 9876.54321, -1
+			, 'JPEG90'
 		)
-		, 'JPEG90'
-	)
-);
+	) > 0
+		THEN 1
+	ELSE 0
+END;
