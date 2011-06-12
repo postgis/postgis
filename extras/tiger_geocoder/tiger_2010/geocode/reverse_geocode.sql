5d14666e26ce4843635a2c4c98059757d34a5632
Regina Obe <lr@pcorp.us>
2011-06-12 04:55:30 +0000

    make debug statement conditional
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7378 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/geocode/reverse_geocode.sql
+++ extras/tiger_geocoder/tiger_2010/geocode/reverse_geocode.sql
@@ -97,7 +97,10 @@ BEGIN
 				ORDER BY ST_Distance(e.the_geom, ' || quote_literal(var_pt::text) || '::geometry) LIMIT 4) As foo 
 				WHERE dist < 150 --less than 150 m
 				ORDER BY foo.fullname, foo.dist) As f ORDER BY f.dist ';
-	RAISE NOTICE 'Statement 1: %', var_stmt;
+				
+	IF var_debug THEN
+	    RAISE NOTICE 'Statement 1: %', var_stmt;
+	END IF;
 	/** FOR var_redge IN
 		SELECT * 
 		FROM (SELECT DISTINCT ON(fullname)  foo.fullname, foo.stusps, foo.zip, 
