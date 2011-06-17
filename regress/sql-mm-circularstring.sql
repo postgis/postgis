6cd1c12b957b8fbecbc5d9d33fb01593c9bf47ef
Paul Ramsey <pramsey@cleverelephant.ca>
2011-06-17 22:36:28 +0000

    Quiet regression failure for minor rounding difference.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7417 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/sql-mm-circularstring.sql
+++ regress/sql-mm-circularstring.sql
@@ -275,5 +275,5 @@ SELECT DropGeometryColumn('public', 'circularstring', 'the_geom_3dz');
 SELECT DropGeometryColumn('public', 'circularstring', 'the_geom_3dm');
 SELECT DropGeometryColumn('public', 'circularstring', 'the_geom_2d');
 DROP TABLE public.circularstring;
-SELECT ST_AsText(box2d('CIRCULARSTRING(220268.439465645 150415.359530563,220227.333322076 150505.561285879,220227.353105332 150406.434743975)'::geometry));
+SELECT ST_AsText(st_snaptogrid(box2d('CIRCULARSTRING(220268.439465645 150415.359530563,220227.333322076 150505.561285879,220227.353105332 150406.434743975)'::geometry),0.0001));
 SELECT 'npoints_is_null',ST_NumPoints(ST_GeomFromEWKT('CIRCULARSTRING(0 0,2 0, 2 1, 2 3, 4 3)'));
