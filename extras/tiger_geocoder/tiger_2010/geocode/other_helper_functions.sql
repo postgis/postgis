ed39f32b267ede06f7ee16ab9f1a48382fe1fc01
Regina Obe <lr@pcorp.us>
2011-06-18 07:29:07 +0000

    Found some indexes I missed -- added to tiger_loader, also #1036 create missing_indexes generation script to back install.  Still need to put in logic for some more indexes, but htis is a good start.  Also documented new function
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7422 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/geocode/other_helper_functions.sql
+++ extras/tiger_geocoder/tiger_2010/geocode/other_helper_functions.sql
@@ -7,9 +7,9 @@
 -- It also allows us to shorten and possibly better cache the repetitive pattern in the code 
 -- greatest(to_number(b.fromhn,''99999999''),to_number(b.tohn,''99999999'')) 
 -- and least(to_number(b.fromhn,''99999999''),to_number(b.tohn,''99999999''))
-CREATE OR REPLACE FUNCTION tiger.least_hn(fromhn varchar, tohn varchar)
+CREATE OR REPLACE FUNCTION least_hn(fromhn varchar, tohn varchar)
   RETURNS integer AS
-$$ SELECT least(to_number( CASE WHEN trim($1) ~ '^[0-9]+$' THEN $1 ELSE '0' END,'99999999'),to_number(CASE WHEN trim($2) ~ '^[0-9]+$' THEN $2 ELSE '0' END,'99999999') )::integer;  $$
+$$ SELECT least(to_number( CASE WHEN trim($1) ~ '^[0-9.]+$' THEN $1 ELSE '0' END,'99999999.9'),to_number(CASE WHEN trim($2) ~ '^[0-9.]+$' THEN $2 ELSE '0' END,'99999999.9') )::integer;  $$
   LANGUAGE sql IMMUTABLE
   COST 5;
   
@@ -17,6 +17,27 @@ $$ SELECT least(to_number( CASE WHEN trim($1) ~ '^[0-9]+$' THEN $1 ELSE '0' END,
 -- and thu useable in an index or cacheable for multiple calls
 CREATE OR REPLACE FUNCTION greatest_hn(fromhn varchar, tohn varchar)
   RETURNS integer AS
-$$ SELECT greatest(to_number( CASE WHEN trim($1) ~ '^[0-9]+$' THEN $1 ELSE '0' END,'99999999'),to_number(CASE WHEN trim($2) ~ '^[0-9]+$' THEN $2 ELSE '0' END,'99999999') )::integer;  $$
+$$ SELECT greatest(to_number( CASE WHEN trim($1) ~ '^[0-9.]+$' THEN $1 ELSE '0' END,'99999999.9'),to_number(CASE WHEN trim($2) ~ '^[0-9]+$' THEN $2 ELSE '0' END,'99999999.9') )::integer;  $$
   LANGUAGE sql IMMUTABLE
   COST 5;
+
+  
+-- Generate script to create missing indexes in tiger tables. 
+-- This will generate sql you can run to index commonly used join columns in geocoder --
+CREATE OR REPLACE FUNCTION missing_indexes_generate_script()
+RETURNS text AS
+$$
+SELECT array_to_string(ARRAY(SELECT 'CREATE INDEX idx_' || c.table_schema || '_' || c.table_name || '_' || c.column_name || ' ON ' || c.table_schema || '.' || c.table_name || ' USING btree(' || c.column_name || ')' As index
+FROM (SELECT * FROM 
+	information_schema.tables WHERE table_type = 'BASE TABLE') As t  INNER JOIN
+	(SELECT * FROM information_schema.columns WHERE column_name IN('countyfp','tlid', 'tfidl', 'tfidr', 'tfid', 'zip') ) AS c  
+		ON (t.table_name = c.table_name AND t.table_schema = c.table_schema)
+		LEFT JOIN pg_catalog.pg_indexes i ON 
+			(i.tablename = c.table_name AND i.schemaname = c.table_schema 
+				AND  indexdef LIKE '%' || c.column_name || '%') 
+WHERE i.tablename IS NULL
+ORDER BY c.table_schema, c.table_name), E';\r');
+
+
+$$
+LANGUAGE sql VOLATILE;
