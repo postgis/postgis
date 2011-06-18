be0c5e0fa3a64717ef8c4d0e3f160dbcf85f06d8
Regina Obe <lr@pcorp.us>
2011-06-18 07:34:47 +0000

    backout some code didn't mean to commit
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7423 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/geocode/other_helper_functions.sql
+++ extras/tiger_geocoder/tiger_2010/geocode/other_helper_functions.sql
@@ -9,7 +9,7 @@
 -- and least(to_number(b.fromhn,''99999999''),to_number(b.tohn,''99999999''))
 CREATE OR REPLACE FUNCTION least_hn(fromhn varchar, tohn varchar)
   RETURNS integer AS
-$$ SELECT least(to_number( CASE WHEN trim($1) ~ '^[0-9.]+$' THEN $1 ELSE '0' END,'99999999.9'),to_number(CASE WHEN trim($2) ~ '^[0-9.]+$' THEN $2 ELSE '0' END,'99999999.9') )::integer;  $$
+$$ SELECT least(to_number( CASE WHEN trim($1) ~ '^[0-9]+$' THEN $1 ELSE '0' END,'9999999'),to_number(CASE WHEN trim($2) ~ '^[0-9.]+$' THEN $2 ELSE '0' END,'9999999') )::integer;  $$
   LANGUAGE sql IMMUTABLE
   COST 5;
   
@@ -17,7 +17,7 @@ $$ SELECT least(to_number( CASE WHEN trim($1) ~ '^[0-9.]+$' THEN $1 ELSE '0' END
 -- and thu useable in an index or cacheable for multiple calls
 CREATE OR REPLACE FUNCTION greatest_hn(fromhn varchar, tohn varchar)
   RETURNS integer AS
-$$ SELECT greatest(to_number( CASE WHEN trim($1) ~ '^[0-9.]+$' THEN $1 ELSE '0' END,'99999999.9'),to_number(CASE WHEN trim($2) ~ '^[0-9]+$' THEN $2 ELSE '0' END,'99999999.9') )::integer;  $$
+$$ SELECT greatest(to_number( CASE WHEN trim($1) ~ '^[0-9]+$' THEN $1 ELSE '0' END,'99999999'),to_number(CASE WHEN trim($2) ~ '^[0-9]+$' THEN $2 ELSE '0' END,'99999999') )::integer;  $$
   LANGUAGE sql IMMUTABLE
   COST 5;
 
@@ -37,7 +37,5 @@ FROM (SELECT * FROM
 				AND  indexdef LIKE '%' || c.column_name || '%') 
 WHERE i.tablename IS NULL
 ORDER BY c.table_schema, c.table_name), E';\r');
-
-
 $$
 LANGUAGE sql VOLATILE;
