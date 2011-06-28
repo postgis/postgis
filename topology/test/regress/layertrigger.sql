763bb3b14598d9638c9e33fb7e25b6ede744e685
Sandro Santilli <strk@keybit.net>
2011-06-28 19:36:46 +0000

    Simplify layertrigger tests dropping unneeded operations. The reduction was verified to still trigger bug #950 prior to the fix for it.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7515 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/regress/layertrigger.sql
+++ topology/test/regress/layertrigger.sql
@@ -1,17 +1,14 @@
 \set VERBOSITY terse
 set client_min_messages to ERROR;
 
-select topology.DropTopology('t2');
-select topology.DropTopology('t1');
-
 select 'seq_reset', setval('topology.topology_id_seq', 1, false);
 select 't1', 'topology_id:' || topology.CreateTopology('t1');
-insert into t1.node(geom, containing_face) values ('POINT( 0 0)', 0);
-create table t1.l1 (id serial);
-select 't1.l1',topology.AddTopoGeometryColumn('t1', 't1', 'l1', 'g', 'POINT');
-insert INTO t1.l1(g) VALUES (topology.CreateTopoGeom('t1', 1, 1, '{{1,1}}'));
 
-select 't1.rel',* from t1.relation;
+--insert into t1.node(geom, containing_face) values ('POINT( 0 0)', 0);
+--create table t1.l1 (id serial);
+--select 't1.l1',topology.AddTopoGeometryColumn('t1', 't1', 'l1', 'g', 'POINT');
+--insert INTO t1.l1(g) VALUES (topology.CreateTopoGeom('t1', 1, 1, '{{1,1}}'));
+--select 't1.rel',* from t1.relation;
 
 select 't2', 'topology_id:' || topology.CreateTopology('t2');
 
@@ -35,22 +32,25 @@ select 'after update: topology.layer ',* from topology.layer;
 BEGIN;
   delete from t1.relation;
   delete from t2.relation;
+  select 't2.rel',* from t2.relation;
   delete from topology.layer;
-  select 'after delete n.1: topology.layer ',topology_id,layer_id from topology.layer;
+  select 'after delete n.1: topology.layer count', count(*) from topology.layer;
 ROLLBACK;
 
 -- the trigger must reject because the t2.relation is not empty.
+-- This test failed due to #950
+-- (there's no row in t2.relation where topogeo_id matches the topology id)
 BEGIN;
   delete from t2.l1;
   delete from topology.layer where topology_id=2;
-  select 'after delete n.2: topology.layer ',topology_id,layer_id from topology.layer;
+  select 'after delete n.2: topology.layer count', count(*) from topology.layer;
 ROLLBACK;
 
 -- the trigger must accept the delete 
 delete from t2.l1;
 delete from t2.relation;
 delete from topology.layer where topology_id=2;
-select 'after delete n.3: topology.layer ',topology_id,layer_id from topology.layer;
+select 'after delete n.3: topology.layer count', count(*) from topology.layer;
 
 select topology.DropTopology('t2');
 select topology.DropTopology('t1');
