95c6f70b40c16395344bd01e3384695f3cbf0749
Regina Obe <lr@pcorp.us>
2011-07-17 17:02:02 +0000

    more regress tests for reverse_geocode
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7649 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/regress/reverse_geocode_regress.sql
+++ extras/tiger_geocoder/tiger_2010/regress/reverse_geocode_regress.sql
@@ -3,4 +3,5 @@ SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.27593,42.338
 SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.85335,42.19262));  -- I 90 Exit 10, Worcester MA
 SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.057811,42.358274)); -- 1 Devonshire Place (washington st area)
 SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.123848,42.41115)); --30 capen
+SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.09436,42.35981)); -- 77 Massachusetts Ave
 \timing
