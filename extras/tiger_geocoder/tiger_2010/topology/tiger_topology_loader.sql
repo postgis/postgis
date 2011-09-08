c5002f489615204604f6fda50e2f061144d1164a
Regina Obe <lr@pcorp.us>
2011-09-08 19:18:26 +0000

    revise to transform to spatial reference system of target topology
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7826 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/topology/tiger_topology_loader.sql
+++ extras/tiger_geocoder/tiger_2010/topology/tiger_topology_loader.sql
@@ -19,9 +19,11 @@
  *	 place - plcidfp
  *	 county - cntyidfp 
  * USE CASE: 
- *	The following will create a topology called topo_boston and load Boston, MA
+ *  The following will create a topology called topo_boston 
+ *   in Mass State Plane feet and load Boston, MA tiger data
+ *  with tolerance of 1 foot
  * SELECT topology.DropTopology('topo_boston');
- * SELECT topology.CreateTopology('topo_boston', 4269);
+ * SELECT topology.CreateTopology('topo_boston', 2249,1);
  * SELECT tiger.topology_load_tiger('topo_boston', 'place', '2507000'); 
  * SELECT topology.TopologySummary('topo_boston');
  * SELECT topology.ValidateTopology('topo_boston');
@@ -36,6 +38,7 @@ DECLARE
  	var_statefp text;
  	var_rcnt bigint;
  	var_result text := '';
+ 	var_srid int := 4269;
 BEGIN
 	--$Id$
 	CASE region_type
@@ -46,23 +49,24 @@ BEGIN
 		ELSE
 			RAISE EXCEPTION 'Region type % IS NOT SUPPORTED', region_type;
 	END CASE;
-	
+	SELECT srid FROM topology.topology into var_srid
+                WHERE name = toponame;
 	var_sql := '
 	CREATE TEMPORARY TABLE tmp_edge
    				AS 
 	WITH te AS 
-   			(SELECT tlid,  ST_GeometryN(the_geom,1) As the_geom, tnidf, tnidt, tfidl, tfidr 
+   			(SELECT tlid,  ST_GeometryN(ST_LineMerge(ST_Transform(the_geom,$3)),1) As geom, tnidf, tnidt, tfidl, tfidr , the_geom As orig_geom
 									FROM tiger.edges 
 									WHERE statefp = $1 AND ST_Covers($2, the_geom)
 										)
-					SELECT DISTINCT ON (t.tlid) t.tlid As edge_id,t.the_geom As geom 
+					SELECT DISTINCT ON (t.tlid) t.tlid As edge_id,t.geom 
                         , t.tnidf As start_node, t.tnidt As end_node, t.tfidl As left_face
-                        , t.tfidr As right_face, tl.tlid AS next_left_edge,  tr.tlid As next_right_edge
+                        , t.tfidr As right_face, tl.tlid AS next_left_edge,  tr.tlid As next_right_edge, t.orig_geom
 						FROM 
 							te AS t LEFT JOIN te As tl ON (t.tnidf = tl.tnidt AND t.tfidl = tl.tfidl)
 							 LEFT JOIN te As tr ON (t.tnidt = tr.tnidf AND t.tfidr = tr.tfidr)				
 						';
-	EXECUTE var_sql USING var_statefp, var_rgeom;
+	EXECUTE var_sql USING var_statefp, var_rgeom, var_srid;
 	GET DIAGNOSTICS var_rcnt = ROW_COUNT;
 	var_result := var_rcnt::text || ' edges holding in temporary. ';
 	var_sql := 'ALTER TABLE tmp_edge ADD CONSTRAINT pk_tmp_edge PRIMARY KEY(edge_id );';
@@ -109,21 +113,21 @@ BEGIN
 	
 	-- start load in faces
 	var_sql := 'INSERT INTO ' || quote_ident(toponame) || '.face(face_id, mbr) 
-						SELECT f.tfid, ST_Envelope(f.the_geom) As mbr 
+						SELECT f.tfid, ST_Envelope(ST_Transform(f.the_geom,$2)) As mbr 
 							FROM tiger.faces AS f
 								WHERE statefp = $1 AND 
 								(  tfid IN(SELECT left_face FROM tmp_edge)
 									OR tfid IN(SELECT right_face FROM tmp_edge) )
 							AND tfid NOT IN(SELECT face_id FROM ' || quote_ident(toponame) || '.face) ';
-	EXECUTE var_sql USING var_statefp, var_rgeom;
+	EXECUTE var_sql USING var_statefp, var_srid;
 	GET DIAGNOSTICS var_rcnt = ROW_COUNT;
 	var_result := var_result || var_rcnt::text || ' faces added. ';
    -- end load in faces
    
    -- add remaining missing edges of present faces --
-   var_sql := 'INSERT INTO tmp_edge(edge_id, geom, start_node, end_node, left_face, right_face, next_left_edge, next_right_edge)	
+   var_sql := 'INSERT INTO tmp_edge(edge_id, geom, start_node, end_node, left_face, right_face, next_left_edge, next_right_edge, orig_geom)	
    			WITH te AS 
-   			(SELECT tlid,  ST_GeometryN(the_geom,1) As the_geom, tnidf, tnidt, tfidl, tfidr 
+   			(SELECT tlid,  ST_GeometryN(ST_LineMerge(ST_Transform(the_geom,$2)),1) As geom, tnidf, tnidt, tfidl, tfidr, the_geom As orig_geom 
 									FROM tiger.edges 
 									WHERE statefp = $1 AND
 									 tfidl IN(SELECT face_id FROM ' || quote_ident(toponame) || '.face)
@@ -131,15 +135,15 @@ BEGIN
 				AND tlid NOT IN(SELECT edge_id FROM tmp_edge)
 				 )
 				
-			SELECT DISTINCT ON (t.tlid) t.tlid As edge_id,t.the_geom As geom 
+			SELECT DISTINCT ON (t.tlid) t.tlid As edge_id,t.geom 
                         , t.tnidf As start_node, t.tnidt As end_node, t.tfidl As left_face
-                        , t.tfidr As right_face, tl.tlid AS next_left_edge,  tr.tlid As next_right_edge
+                        , t.tfidr As right_face, tl.tlid AS next_left_edge,  tr.tlid As next_right_edge, t.orig_geom
 				FROM 
 						te AS t LEFT JOIN te As tl 
 								ON (t.tnidf = tl.tnidt AND t.tfidl = tl.tfidl)
 			LEFT JOIN te As tr ON (t.tnidt = tr.tnidf AND t.tfidr = tr.tfidr)
 			';
-	EXECUTE var_sql USING var_statefp;
+	EXECUTE var_sql USING var_statefp, var_srid;
 	GET DIAGNOSTICS var_rcnt = ROW_COUNT;
 	var_result := var_result || var_rcnt::text || ' edges of faces added. ';
    	-- start load in nodes
@@ -167,7 +171,7 @@ BEGIN
 							FROM tiger.faces WHERE statefp = $1 
 							AND tfid IN(SELECT face_id FROM ' || quote_ident(toponame) || '.face) 
 							) As f
-						WHERE ST_Contains(f.the_geom, n.geom) ';
+						WHERE ST_Contains(f.the_geom, ST_Transform(n.geom,4269)) ';
 	EXECUTE var_sql USING var_statefp, var_rgeom;
 	GET DIAGNOSTICS var_rcnt = ROW_COUNT;
 	var_result := var_result || ' ' || var_rcnt::text || ' nodes contained in a face. ';
