eb48250b12e3ca22dd3641286a9894f1f2e68c07
Regina Obe <lr@pcorp.us>
2011-09-09 14:06:57 +0000

    revise to snap points to topology precision, also set edge left/right face to 0 if null (world face). ValidateTopology now reports 25 errors instead of 484 errors (what it did before)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7827 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2010/topology/tiger_topology_loader.sql
+++ extras/tiger_geocoder/tiger_2010/topology/tiger_topology_loader.sql
@@ -26,7 +26,7 @@
  * SELECT topology.CreateTopology('topo_boston', 2249,1);
  * SELECT tiger.topology_load_tiger('topo_boston', 'place', '2507000'); 
  * SELECT topology.TopologySummary('topo_boston');
- * SELECT topology.ValidateTopology('topo_boston');
+ * SELECT topology.ValidateTopology('topo_boston');  ("edge crosses node",81045651,85742215)
  ****/
 CREATE OR REPLACE FUNCTION tiger.topology_load_tiger(IN toponame varchar,  
 	region_type varchar, region_id varchar)
@@ -39,6 +39,7 @@ DECLARE
  	var_rcnt bigint;
  	var_result text := '';
  	var_srid int := 4269;
+ 	var_precision double precision := 0;
 BEGIN
 	--$Id$
 	CASE region_type
@@ -49,24 +50,24 @@ BEGIN
 		ELSE
 			RAISE EXCEPTION 'Region type % IS NOT SUPPORTED', region_type;
 	END CASE;
-	SELECT srid FROM topology.topology into var_srid
+	SELECT srid, precision FROM topology.topology into var_srid, var_precision
                 WHERE name = toponame;
 	var_sql := '
 	CREATE TEMPORARY TABLE tmp_edge
    				AS 
 	WITH te AS 
-   			(SELECT tlid,  ST_GeometryN(ST_LineMerge(ST_Transform(the_geom,$3)),1) As geom, tnidf, tnidt, tfidl, tfidr , the_geom As orig_geom
+   			(SELECT tlid,  ST_GeometryN(ST_SnapToGrid(ST_Transform(ST_LineMerge(the_geom),$3),$4),1) As geom, tnidf, tnidt, tfidl, tfidr , the_geom As orig_geom
 									FROM tiger.edges 
 									WHERE statefp = $1 AND ST_Covers($2, the_geom)
 										)
 					SELECT DISTINCT ON (t.tlid) t.tlid As edge_id,t.geom 
-                        , t.tnidf As start_node, t.tnidt As end_node, t.tfidl As left_face
-                        , t.tfidr As right_face, tl.tlid AS next_left_edge,  tr.tlid As next_right_edge, t.orig_geom
+                        , t.tnidf As start_node, t.tnidt As end_node, COALESCE(t.tfidl,0) As left_face
+                        , COALESCE(t.tfidr,0) As right_face, tl.tlid AS next_left_edge,  tr.tlid As next_right_edge, t.orig_geom
 						FROM 
 							te AS t LEFT JOIN te As tl ON (t.tnidf = tl.tnidt AND t.tfidl = tl.tfidl)
 							 LEFT JOIN te As tr ON (t.tnidt = tr.tnidf AND t.tfidr = tr.tfidr)				
 						';
-	EXECUTE var_sql USING var_statefp, var_rgeom, var_srid;
+	EXECUTE var_sql USING var_statefp, var_rgeom, var_srid, var_precision;
 	GET DIAGNOSTICS var_rcnt = ROW_COUNT;
 	var_result := var_rcnt::text || ' edges holding in temporary. ';
 	var_sql := 'ALTER TABLE tmp_edge ADD CONSTRAINT pk_tmp_edge PRIMARY KEY(edge_id );';
@@ -127,7 +128,7 @@ BEGIN
    -- add remaining missing edges of present faces --
    var_sql := 'INSERT INTO tmp_edge(edge_id, geom, start_node, end_node, left_face, right_face, next_left_edge, next_right_edge, orig_geom)	
    			WITH te AS 
-   			(SELECT tlid,  ST_GeometryN(ST_LineMerge(ST_Transform(the_geom,$2)),1) As geom, tnidf, tnidt, tfidl, tfidr, the_geom As orig_geom 
+   			(SELECT tlid,  ST_GeometryN(ST_SnapToGrid(ST_Transform(ST_LineMerge(the_geom),$2),$3),1) As geom, tnidf, tnidt, tfidl, tfidr, the_geom As orig_geom 
 									FROM tiger.edges 
 									WHERE statefp = $1 AND
 									 tfidl IN(SELECT face_id FROM ' || quote_ident(toponame) || '.face)
@@ -143,7 +144,7 @@ BEGIN
 								ON (t.tnidf = tl.tnidt AND t.tfidl = tl.tfidl)
 			LEFT JOIN te As tr ON (t.tnidt = tr.tnidf AND t.tfidr = tr.tfidr)
 			';
-	EXECUTE var_sql USING var_statefp, var_srid;
+	EXECUTE var_sql USING var_statefp, var_srid, var_precision;
 	GET DIAGNOSTICS var_rcnt = ROW_COUNT;
 	var_result := var_result || var_rcnt::text || ' edges of faces added. ';
    	-- start load in nodes
