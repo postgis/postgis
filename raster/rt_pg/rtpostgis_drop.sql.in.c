59993666e8c7014b90036d38278794b62c4ed2c3
Regina Obe <lr@pcorp.us>
2011-10-01 16:47:33 +0000

    itemize more removed functions for upgrade purposes
    git-svn-id: http://svn.osgeo.org/postgis/trunk@7928 b70326c6-7e19-0410-871a-916f4a2858ee

--- raster/rt_pg/rtpostgis_drop.sql.in.c
+++ raster/rt_pg/rtpostgis_drop.sql.in.c
@@ -25,9 +25,14 @@ DROP FUNCTION IF EXISTS ST_Intersects(raster,geometry);
 DROP FUNCTION IF EXISTS ST_Intersects(geometry,raster);
 DROP FUNCTION IF EXISTS ST_Intersects(raster rast , integer band , boolean exclude_nodata_value , geometry geommin);
 DROP FUNCTION IF EXISTS ST_Intersects(geometry geommin , raster rast , integer band , boolean exclude_nodata_value);
+DROP FUNCTION IF EXISTS ST_Intersection(raster rast1,raster rast2, integer nband1, integer nband2);
 
 --changed to take default args --
 DROP FUNCTION IF EXISTS ST_Intersection(geometry,raster);
+
+--these were renamed to ST_MapAlgebraExpr --
+ST_MapAlgebra(rast raster, band integer, pixeltype text, expression text, nodatavaluerepl text);
+ST_MapAlgebra(rast raster, pixeltype text, expression text, nodatavaluerepl text);
 
 --added extra parameter so these are obsolete --
 DROP FUNCTION IF EXISTS ST_AsRaster(geometry , integer , integer , double precision , double precision , text , double precision , double precision , double precision , double precision );
@@ -37,14 +42,4 @@ DROP FUNCTION IF EXISTS ST_AsRaster(geometry , integer , integer , double precis
 DROP FUNCTION IF EXISTS ST_AsRaster(geometry , integer , integer , double precision , double precision , text[] , double precision[] , double precision[] , double precision , double precision );
 FUNCTION ST_AsRaster(geometry , double precision , double precision , text , double precision , double precision , double precision , double precision , double precision , double precision )
 DROP FUNCTION IF EXISTS ST_AsRaster(geometry , raster , text , double precision , double precision );
-DROP FUNCTION IF EXISTS  _ST_AsRaster(
-	geom geometry,
-	scalex double precision , scaley double precision,
-	width integer , height integer,
-	pixeltype text[] ,
-	value double precision[] ,
-	nodataval double precision[] ,
-	upperleftx double precision, upperlefty double precision,
-	gridx double precision, gridy double precision,
-	skewx double precision, skewy double precision,
-	touched boolean);
+DROP FUNCTION IF EXISTS  _ST_AsRaster(geom geometry,scalex double precision , scaley double precision, width integer , height integer, pixeltype text[] , value double precision[] , nodataval double precision[] , upperleftx double precision, upperlefty double precision, gridx double precision, gridy double precision, skewx double precision, skewy double precision,touched boolean);
