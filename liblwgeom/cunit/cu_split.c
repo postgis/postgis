68a37aec98fdc75969ddbbeb7dfcf83561c02d54
Sandro Santilli <strk@keybit.net>
2011-11-08 08:55:09 +0000

    Add test for bug #1270
    git-svn-id: http://svn.osgeo.org/postgis/trunk@8113 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/cunit/cu_split.c
+++ liblwgeom/cunit/cu_split.c
@@ -81,15 +81,42 @@ static void test_lwline_split_by_point_to(void)
 #endif /* POSTGIS_GEOS_VERSION >= 33 */
 }
 
-/*
-** Used by test harness to register the tests in this file.
-*/
+static void test_lwgeom_split(void)
+{
+	LWGEOM *geom, *blade, *ret;
+	char *wkt;
+
+	geom = lwgeom_from_wkt(
+"MULTILINESTRING((-5 -2,0 0),(0 0,10 10))",
+	LW_PARSER_CHECK_NONE);
+	CU_ASSERT(geom != NULL);
+
+	blade = lwgeom_as_lwpoint(lwgeom_from_wkt(
+		"POINT(0 0)",
+		LW_PARSER_CHECK_NONE));
+	CU_ASSERT(blade != NULL);
+
+	ret = lwgeom_split(geom, blade);
+	CU_ASSERT(ret != NULL);
+
+	wkt = lwgeom_to_ewkt(geom);
+	/*printf("%s\n", wkt);*/
+	CU_ASSERT_STRING_EQUAL(wkt, "MULTILINESTRING((-5 -2,0 0),(0 0,10 10))");
+	lwfree(wkt);
+
+	lwgeom_free((LWGEOM*)ret);
+	lwgeom_free((LWGEOM*)geom);
+	lwgeom_free((LWGEOM*)blade);
+}
+
+
 /*
 ** Used by test harness to register the tests in this file.
 */
 CU_TestInfo split_tests[] =
 {
 	PG_TEST(test_lwline_split_by_point_to),
+	PG_TEST(test_lwgeom_split),
 	CU_TEST_INFO_NULL
 };
 CU_SuiteInfo split_suite = {"split",  NULL,  NULL, split_tests};
