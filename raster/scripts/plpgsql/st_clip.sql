4f6c2800f79cae1c4a3f0b99a0f104cafa0d50ad
Pierre Racine <Pierre.Racine@sbf.ulaval.ca>
2011-12-01 01:07:10 +0000

    Added some variants and tests
    git-svn-id: http://svn.osgeo.org/postgis/trunk@8268 b70326c6-7e19-0410-871a-916f4a2858ee

--- raster/scripts/plpgsql/st_clip.sql
+++ raster/scripts/plpgsql/st_clip.sql
@@ -46,6 +46,7 @@ CREATE OR REPLACE FUNCTION ST_Clip(rast raster, band int, geom geometry, nodata 
         bandend int;
         newextent text;
         newnodata float8;
+        newpixtype text;
         bandi int;
     BEGIN
         IF rast IS NULL THEN
@@ -64,36 +65,53 @@ CREATE OR REPLACE FUNCTION ST_Clip(rast raster, band int, geom geometry, nodata 
             bandstart := band;
             bandend := band;
         END IF;
-        IF nodata IS NULL THEN
-            newnodata := coalesce(ST_BandNodataValue(rast, bandstart), ST_MinPossibleVal(ST_BandPixelType(rast, bandstart)));
-        ELSE
-            newnodata := nodata;
-        END IF;
+        newpixtype := ST_BandPixelType(rast, bandstart);
+        newnodata := coalesce(nodata, ST_BandNodataValue(rast, bandstart), ST_MinPossibleVal(newpixtype));
         newextent := CASE WHEN trimraster THEN 'INTERSECTION' ELSE 'FIRST' END;
- RAISE NOTICE 'bandstart=%, bandend=%', bandstart, bandend;
+        
+--RAISE NOTICE 'newextent=%', newextent;
         -- Convert the geometry to a raster
         geomrast := ST_AsRaster(geom, rast, ST_BandPixelType(rast, band), 1, newnodata);  
 
-        -- Compute the first raster band
+        -- Set the newnodata
         sourceraster := ST_SetBandNodataValue(sourceraster, bandstart, newnodata);
-        newrast := ST_MapAlgebraExpr(sourceraster, bandstart, geomrast, 1, 'rast1');
+
+        -- Compute the first raster band
+        newrast := ST_MapAlgebraExpr(sourceraster, bandstart, geomrast, 1, 'rast1', newpixtype, newextent);
         
         FOR bandi IN bandstart+1..bandend LOOP
- RAISE NOTICE 'bandi=%', bandi;
+--RAISE NOTICE 'bandi=%', bandi;
             -- for each band we must determine the nodata value
-            IF nodata IS NULL THEN
-                newnodata := coalesce(ST_BandNodataValue(sourceraster, bandi), ST_MinPossibleVal(ST_BandPixelType(rast, bandi)));
-            ELSE
-                newnodata := nodata;
-            END IF;
+            newpixtype := ST_BandPixelType(rast, bandi);
+            newnodata := coalesce(nodata, ST_BandNodataValue(sourceraster, bandi), ST_MinPossibleVal(newpixtype));
             sourceraster := ST_SetBandNodataValue(sourceraster, bandi, newnodata);
-            newrast := ST_AddBand(newrast, ST_MapAlgebraExpr(sourceraster, bandi, geomrast, 1, 'rast1'));
+            newrast := ST_AddBand(newrast, ST_MapAlgebraExpr(sourceraster, bandi, geomrast, 1, 'rast1', newpixtype, newextent));
         END LOOP;
         RETURN newrast;
     END;
     $$
     LANGUAGE 'plpgsql';
 
+-- Variant defaulting to band 1
+CREATE OR REPLACE FUNCTION ST_Clip(rast raster, geom geometry, nodata float8 DEFAULT null, trimraster boolean DEFAULT false)
+    RETURNS raster 
+    AS $$
+        SELECT ST_Clip($1, 1, $2, $3, $4);
+    $$ LANGUAGE 'SQL';
+
+-- Variant defaulting nodata to the one of the raster or the min possible value
+CREATE OR REPLACE FUNCTION ST_Clip(rast raster, band int, geom geometry, trimraster boolean)
+    RETURNS raster 
+    AS $$
+        SELECT ST_Clip($1, $2, $3, null, $4);
+    $$ LANGUAGE 'SQL';
+
+-- Variant defaulting nodata to the one of the raster or the min possible value
+CREATE OR REPLACE FUNCTION ST_Clip(rast raster, geom geometry, trimraster boolean)
+    RETURNS raster 
+    AS $$
+        SELECT ST_Clip($1, 1, $2, null, $3);
+    $$ LANGUAGE 'SQL';
 
 -- Test
 
@@ -117,4 +135,36 @@ FROM ST_PixelAsPolygons(ST_Clip(ST_TestRaster(10, 10, 2), 1, ST_Buffer(ST_MakePo
 SELECT ST_AsBinary((gv).geom), (gv).val 
 FROM ST_PixelAsPolygons(ST_Clip(ST_AddBand(ST_TestRaster(10, 10, 2), '16BUI'::text, 4, 0), null, ST_Buffer(ST_MakePoint(8, 5), 4)), 2) gv
 
+-- Test one band raster with trimraster set to true
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_Clip(ST_TestRaster(10, 10, 2), 1, ST_Buffer(ST_MakePoint(8, 5), 4), null, true)) gv
 
+-- Test two bands raster with trimraster set to true
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_Clip(ST_AddBand(ST_TestRaster(10, 10, 2), '16BUI'::text, 4, 0), null, ST_Buffer(ST_MakePoint(8, 5), 4), null, true), 2) gv
+
+-- Test nodatavalue set by the first raster
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_SetBandNoDataValue(ST_Clip(ST_TestRaster(10, 10, 2), 1, ST_Buffer(ST_MakePoint(8, 5), 4)), null)) gv
+
+-- Test nodatavalue set by the parameter
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_SetBandNoDataValue(ST_Clip(ST_TestRaster(10, 10, 2), 1, ST_Buffer(ST_MakePoint(8, 5), 4), -10), null)) gv
+
+-- Test nodatavalue set by the min possible value
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_SetBandNoDataValue(ST_Clip(ST_SetBandNoDataValue(ST_TestRaster(10, 10, 2), null), 1, ST_Buffer(ST_MakePoint(8, 5), 4)), null)) gv
+
+-- Test the variant defaulting to band 1
+SELECT ST_Numbands(ST_Clip(ST_AddBand(ST_TestRaster(10, 10, 2), '16BUI'::text, 4, 0), ST_Buffer(ST_MakePoint(8, 5), 4))) gv
+
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_Clip(ST_AddBand(ST_TestRaster(10, 10, 2), '16BUI'::text, 4, 0), ST_Buffer(ST_MakePoint(8, 5), 4)), 2) gv
+
+-- Test defaulting to min possible value
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_SetBandNoDataValue(ST_Clip(ST_SetBandNoDataValue(ST_TestRaster(10, 10, 2), null), ST_Buffer(ST_MakePoint(8, 5), 4)), null)) gv
+
+-- Test defaulting to nodatavalue set by the first raster
+SELECT ST_AsBinary((gv).geom), (gv).val 
+FROM ST_PixelAsPolygons(ST_SetBandNoDataValue(ST_Clip(ST_TestRaster(10, 10, 2), ST_Buffer(ST_MakePoint(8, 5), 4)), null)) gv
