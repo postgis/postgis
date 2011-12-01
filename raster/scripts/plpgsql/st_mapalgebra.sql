84235f76a1e63baa146a17ffc56b0f8e031b9fc2
Pierre Racine <Pierre.Racine@sbf.ulaval.ca>
2011-12-01 16:06:40 +0000

    Removed ST_MinPossibleVal defined in another script
    git-svn-id: http://svn.osgeo.org/postgis/trunk@8285 b70326c6-7e19-0410-871a-916f4a2858ee

--- raster/scripts/plpgsql/st_mapalgebra.sql
+++ raster/scripts/plpgsql/st_mapalgebra.sql
@@ -1,4 +1,4 @@
-----------------------------------------------------------------------
+﻿----------------------------------------------------------------------
 -- $Id$
 --
 -- Copyright (c) 2009-2010 Pierre Racine <pierre.racine@sbf.ulaval.ca>
@@ -8,32 +8,6 @@
 -- Defining the plpgsql function below might overwrite the current C implementation and brake other functions dependent on it.
 -- Use with caution.
 ----------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION ST_MinPossibleVal(pixeltype text)
-    RETURNS float8 AS
-    $$
-    DECLARE
-        newval int := 0;
-    BEGIN
-        newval := CASE 
-            WHEN pixeltype = '1BB' THEN 0
-            WHEN pixeltype = '2BUI' THEN 0
-            WHEN pixeltype = '4BUI' THEN 0
-            WHEN pixeltype = '8BUI' THEN 0
-            WHEN pixeltype = '8BSI' THEN -128
-            WHEN pixeltype = '16BUI' THEN 0
-            WHEN pixeltype = '16BSI' THEN -32768
-            WHEN pixeltype = '32BUI' THEN 0
-            WHEN pixeltype = '32BSI' THEN -2147483648
-            WHEN pixeltype = '32BF' THEN -2147483648 -- Could not find a function returning the smallest real yet
-            WHEN pixeltype = '64BF' THEN -2147483648 -- Could not find a function returning the smallest float8 yet
-        END;
-        RETURN newval;
-    END;
-    $$
-    LANGUAGE 'plpgsql';
-
--- Tests
---SELECT ST_MinPossibleVal('32BF')
 
 --------------------------------------------------------------------
 -- ST_MapAlgebra - (one raster version) Return a raster which values 
