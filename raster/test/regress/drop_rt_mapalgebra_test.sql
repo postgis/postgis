f729c9a9b395e64ba2bae968813a5c5743664509
Sandro Santilli <strk@keybit.net>
2011-12-24 09:59:08 +0000

    Ensure mapalgebra test functions are dropped
    git-svn-id: http://svn.osgeo.org/postgis/trunk@8552 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ raster/test/regress/drop_rt_mapalgebra_test.sql
@@ -0,0 +1,5 @@
+DROP FUNCTION ST_TestRaster(ulx float8, uly float8, val float8);
+DROP FUNCTION raster_plus_twenty(pixel FLOAT, VARIADIC args TEXT[]);
+DROP FUNCTION raster_plus_arg1(pixel FLOAT, VARIADIC args TEXT[]);
+DROP FUNCTION raster_polynomial(pixel FLOAT, VARIADIC args TEXT[]);
+DROP FUNCTION raster_nullage(pixel FLOAT, VARIADIC args TEXT[]);
---
+++ raster/test/regress/drop_rt_mapalgebrafctngb_test.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION ST_Nullage(matrix float[][], nodatamode text, VARIADIC args text[]);
+DROP FUNCTION ST_TestRasterNgb(h integer, w integer, val float8);
