2bcde30d50f8b8640594edc68c2d648d01f44d29
Sandro Santilli <strk@keybit.net>
2011-12-24 09:58:39 +0000

    Move single-test data tables within the testfiles, cleanup after run
    git-svn-id: http://svn.osgeo.org/postgis/trunk@8548 b70326c6-7e19-0410-871a-916f4a2858ee

--- raster/test/regress/box3d.sql
+++ raster/test/regress/box3d.sql
@@ -7,6 +7,104 @@
 -- the terms of the GNU General Public Licence. See the COPYING file.
 -----------------------------------------------------------------------
 
+CREATE TABLE rt_box3d_test (
+    id numeric,
+    name text,
+    rast raster,
+    env box3d
+);
+
+-- 10x20, ip:0.5,0.5 scale:2,3
+INSERT INTO rt_box3d_test 
+VALUES ( 0, '10x20, ip:0.5,0.5 scale:2,3 skew:0,0',
+(
+'01' -- little endian (uint8 ndr)
+|| 
+'0000' -- version (uint16 0)
+||
+'0000' -- nBands (uint16 0)
+||
+'0000000000000040' -- scaleX (float64 2)
+||
+'0000000000000840' -- scaleY (float64 3)
+||
+'000000000000E03F' -- ipX (float64 0.5)
+||
+'000000000000E03F' -- ipY (float64 0.5)
+||
+'0000000000000000' -- skewX (float64 0)
+||
+'0000000000000000' -- skewY (float64 0)
+||
+'0A000000' -- SRID (int32 10)
+||
+'0A00' -- width (uint16 10)
+||
+'1400' -- height (uint16 20)
+)::raster
+,'BOX3D(0.5 0.5,20.5 60.5 0)' -- expected envelope (20x60) == (10*2 x 20*3)
+);
+
+INSERT INTO rt_box3d_test 
+VALUES ( 1, '1x1, ip:2.5,2.5 scale:5,5 skew:0,0',
+(
+'01' -- little endian (uint8 ndr)
+|| 
+'0000' -- version (uint16 0)
+||
+'0000' -- nBands (uint16 0)
+||
+'0000000000001440' -- scaleX (float64 5)
+||
+'0000000000001440' -- scaleY (float64 5)
+||
+'0000000000000440' -- ipX (float64 2.5)
+||
+'0000000000000440' -- ipY (float64 2.5)
+||
+'0000000000000000' -- skewX (float64 0)
+||
+'0000000000000000' -- skewY (float64 0)
+||
+'00000000' -- SRID (int32 0)
+||
+'0100' -- width (uint16 1)
+||
+'0100' -- height (uint16 1)
+)::raster
+,'BOX3D(2.5 2.5,7.5 7.5 0)' -- expected envelope 
+);
+
+INSERT INTO rt_box3d_test 
+VALUES ( 2, '1x1, ip:7.5,2.5 scale:5,5 skew:0,0',
+(
+'01' -- little endian (uint8 ndr)
+|| 
+'0000' -- version (uint16 0)
+||
+'0000' -- nBands (uint16 0)
+||
+'0000000000001440' -- scaleX (float64 5)
+||
+'0000000000001440' -- scaleY (float64 5)
+||
+'0000000000001E40' -- ipX (float64 7.5)
+||
+'0000000000000440' -- ipY (float64 2.5)
+||
+'0000000000000000' -- skewX (float64 0)
+||
+'0000000000000000' -- skewY (float64 0)
+||
+'00000000' -- SRID (int32 0)
+||
+'0100' -- width (uint16 1)
+||
+'0100' -- height (uint16 1)
+)::raster
+,'BOX3D(7.5 2.5,12.5 7.5 0)' -- expected envelope 
+);
+
 -----------------------------------------------------------------------
 -- test bounding box (2D)
 -----------------------------------------------------------------------
@@ -41,3 +139,6 @@ SELECT
 FROM rt_box3d_test
 WHERE
 	box3d(st_envelope(rast))::text != env::text;
+
+-- Cleanup
+DROP TABLE rt_box3d_test;
