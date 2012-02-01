af70a47eaa25fc1749238c8cd8545043d23df19b
Paul Ramsey <pramsey@cleverelephant.ca>
2012-02-01 17:39:13 +0000

    Also remove .gitignore files from tarbal distro
    git-svn-id: http://svn.osgeo.org/postgis/trunk@9004 b70326c6-7e19-0410-871a-916f4a2858ee

--- make_dist.sh
+++ make_dist.sh
@@ -41,6 +41,7 @@ fi
 # remove .cvsignore, make_dist.sh and HOWTO_RELEASE
 echo "Removing .cvsignore and make_dist.sh files"
 find "$outdir" -name .cvsignore -exec rm {} \;
+find "$outdir" -name .gitignore -exec rm {} \;
 rm -f "$outdir"/make_dist.sh "$outdir"/HOWTO_RELEASE
 
 # generating configure script and configuring
