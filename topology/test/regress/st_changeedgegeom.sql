982d30e9cd7eb0b2d7478ca4496fbc7fe14bacc1
Sandro Santilli <strk@keybit.net>
2012-02-17 08:39:55 +0000

    Add test numbers to ST_ChangeEdgeGeom regression cases
    git-svn-id: http://svn.osgeo.org/postgis/trunk@9223 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/regress/st_changeedgegeom.sql
+++ topology/test/regress/st_changeedgegeom.sql
@@ -4,7 +4,7 @@ set client_min_messages to ERROR;
 
 
 -- good one
-SELECT topology.ST_ChangeEdgeGeom('city_data', 25,
+SELECT 'T1', topology.ST_ChangeEdgeGeom('city_data', 25,
  'LINESTRING(9 35, 11 33, 13 35)');
 
 -- start/end points mismatch
@@ -26,15 +26,15 @@ SELECT topology.ST_ChangeEdgeGeom('city_data', 25,
  'LINESTRING(9 35, 11 40, 13 35)');
 
 -- Test change in presence of edges sharing node (#1428)
-SELECT topology.ST_ChangeEdgeGeom('city_data', 5,
+SELECT 'T2', topology.ST_ChangeEdgeGeom('city_data', 5,
  'LINESTRING(41 40, 57 33)');
 
 -- Change to edge crossing old self 
-SELECT topology.ST_ChangeEdgeGeom('city_data', 5,
+SELECT 'T3', topology.ST_ChangeEdgeGeom('city_data', 5,
  'LINESTRING(41 40, 49 40, 49 34, 57 33)');
 
 -- Change a closed edge
-SELECT topology.ST_ChangeEdgeGeom('city_data', 26,
+SELECT 'T4', topology.ST_ChangeEdgeGeom('city_data', 26,
  'LINESTRING(4 31, 7 31, 4 34, 4 31)');
 
 -- TODO: test reverse direction of closed edge
