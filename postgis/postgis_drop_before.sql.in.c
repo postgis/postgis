1b96be1de52f43051facc4c8ca8d1313b8107948
Sandro Santilli <strk@keybit.net>
2012-02-23 11:24:28 +0000

    Drop the UpdateGeometrySRID version with old param name (#1606)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@9275 b70326c6-7e19-0410-871a-916f4a2858ee

--- postgis/postgis_drop_before.sql.in.c
+++ postgis/postgis_drop_before.sql.in.c
@@ -5,3 +5,5 @@ DROP FUNCTION IF EXISTS AddGeometryColumn(varchar,varchar,varchar,varchar,intege
 DROP FUNCTION IF EXISTS ST_MakeEnvelope(float8, float8, float8, float8);
 --changed name of prec arg to be consistent with ST_AsGML/KML
 DROP FUNCTION IF EXISTS ST_AsX3D(geometry, integer, integer); 
+--changed name of arg: http://trac.osgeo.org/postgis/ticket/1606
+DROP FUNCTION IF EXISTS UpdateGeometrySRID(varchar,varchar,varchar,varchar,integer);
