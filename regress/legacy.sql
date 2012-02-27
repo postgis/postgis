25183b11f136365b1cb8fd68f8a114aa78ed0c2c
Paul Ramsey <pramsey@cleverelephant.ca>
2012-02-27 18:19:54 +0000

    Load the "right" legacy.sql file (warning: hard coding 00-regress-install into a sql file).
    git-svn-id: http://svn.osgeo.org/postgis/trunk@9322 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/legacy.sql
+++ regress/legacy.sql
@@ -8,7 +8,7 @@
 
 SET client_min_messages TO WARNING;
 
-\i ../postgis/legacy.sql
+\i 00-regress-install/share/contrib/postgis/legacy.sql
 
 INSERT INTO "spatial_ref_sys" ("srid","auth_name","auth_srid","srtext","proj4text") VALUES (4326,'EPSG',4326,'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]','+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ');
 
@@ -51,4 +51,4 @@ SELECT 'Done.';
 
 DELETE FROM spatial_ref_sys WHERE SRID = '4326';
 
-\i ../postgis/uninstall_legacy.sql
+\i 00-regress-install/share/contrib/postgis/uninstall_legacy.sql
