f509fae4abd43c164377febc51044f2b8a89b62c
Sandro Santilli <strk@keybit.net>
2012-03-22 15:07:11 +0000

    Add collapsed polygon case to ST_Simplify test (#1698)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@9529 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/simplify.sql
+++ regress/simplify.sql
@@ -8,4 +8,4 @@ SELECT '6', ST_astext(ST_Simplify('MULTILINESTRING((0 0 3 2, 0 10 6 1, 0 51 1 6,
 SELECT '7', ST_astext(ST_Simplify('POLYGON((0 0 3 2, 0 10 6 1, 0 51 1 6, 50 20 6 7, 30 20 9 9, 7 32 10 5, 0 0 3 2), (1 1 4 3, 1 3 2 3, 18 18 5 30, 1 1 4 3))', 20));
 SELECT '8', ST_astext(ST_Simplify('POLYGON((0 0 3 2, 0 10 6 1, 0 51 1 6, 50 20 6 7, 30 20 9 9, 7 32 10 5, 0 0 3 2), (1 1 4 3, 1 3 2 3, 18 18 5 30, 1 1 4 3))', 1));
 
-SELECT '9', ST_astext(ST_Simplify('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))', 20));
+SELECT '9', ST_astext(ST_Simplify('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 5, 5 6, 6 6, 8 5, 5 5))', 20));
