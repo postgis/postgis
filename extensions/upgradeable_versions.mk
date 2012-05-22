be0e427c52062bb18e977cf45cc5204d3034fcde
Paul Ramsey <pramsey@cleverelephant.ca>
2012-05-22 14:17:07 +0000

    Upgrade from 2.0 only
    git-svn-id: http://svn.osgeo.org/postgis/trunk@9781 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -1,14 +1,2 @@
 UPGRADEABLE_VERSIONS = \
-	2.0.0alpha1 \
-	2.0.0alpha2 \
-	2.0.0alpha3 \
-	2.0.0alpha4 \
-	2.0.0alpha5 \
-	2.0.0alpha6 \
-	2.0.0beta1 \
-	2.0.0beta2 \
-	2.0.0beta3 \
-	2.0.0beta4 \
-	2.0.0rc1 \
-	2.0.0rc2 \
 	2.0.0 
