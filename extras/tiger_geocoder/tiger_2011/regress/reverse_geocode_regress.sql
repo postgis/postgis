3def4a47b38e75376165318cde701a19cc4a5e66
Regina Obe <lr@pcorp.us>
2012-07-16 03:11:35 +0000

    #1913 regress check
    git-svn-id: http://svn.osgeo.org/postgis/trunk@10063 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/tiger_2011/regress/reverse_geocode_regress.sql
+++ extras/tiger_geocoder/tiger_2011/regress/reverse_geocode_regress.sql
@@ -4,4 +4,5 @@ SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.85335,42.192
 SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.057811,42.358274)); -- 1 Devonshire Place (washington st area)
 SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.123848,42.41115)); -- 30 capen, Medford, MA 02155
 SELECT pprint_addy(addy[1]), addy FROM reverse_geocode(ST_Point(-71.09436,42.35981)); -- 77 Massachusetts Ave, Cambridge, MA 02139
+SELECT '#1913' As ticket, pprint_addy(addy[1]) FROM reverse_geocode(ST_Point(-71.2248416, 42.30344833)); -- I- 95, Needham, MA 02494
 \timing
