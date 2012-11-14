50bc8405e646dbc62e865292a662c863ccefbc9e
Paul Ramsey <pramsey@cleverelephant.ca>
2012-11-14 23:01:22 +0000

    #2035, Strange behavior when using left (<<) and right (>>) operators
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.0@10676 b70326c6-7e19-0410-871a-916f4a2858ee

--- postgis/gserialized_gist_2d.c
+++ postgis/gserialized_gist_2d.c
@@ -840,6 +840,8 @@ static inline bool gserialized_gist_consistent_leaf_2d(BOX2DF *key, BOX2DF *quer
 
 	switch (strategy)
 	{
+
+	/* Basic overlaps */
 	case RTOverlapStrategyNumber:
 		retval = (bool) box2df_overlaps(key, query);
 		break;
@@ -854,6 +856,35 @@ static inline bool gserialized_gist_consistent_leaf_2d(BOX2DF *key, BOX2DF *quer
 	case RTOldContainedByStrategyNumber:
 		retval = (bool) box2df_contains(query, key);
 		break;
+		
+	/* To one side */
+	case RTAboveStrategyNumber:
+		retval = (bool) box2df_above(key, query);
+		break;
+	case RTBelowStrategyNumber:
+		retval = (bool) box2df_below(key, query);
+		break;
+	case RTRightStrategyNumber:
+		retval = (bool) box2df_right(key, query);
+		break;
+	case RTLeftStrategyNumber:
+		retval = (bool) box2df_left(key, query);
+		break;
+
+	/* Overlapping to one side */
+	case RTOverAboveStrategyNumber:
+		retval = (bool) box2df_overabove(key, query);
+		break;
+	case RTOverBelowStrategyNumber:
+		retval = (bool) box2df_overbelow(key, query);
+		break;
+	case RTOverRightStrategyNumber:
+		retval = (bool) box2df_overright(key, query);
+		break;
+	case RTOverLeftStrategyNumber:
+		retval = (bool) box2df_overleft(key, query);
+		break;		
+		
 	default:
 		retval = FALSE;
 	}
@@ -873,6 +904,8 @@ static inline bool gserialized_gist_consistent_internal_2d(BOX2DF *key, BOX2DF *
 
 	switch (strategy)
 	{
+		
+	/* Basic overlaps */
 	case RTOverlapStrategyNumber:
 		retval = (bool) box2df_overlaps(key, query);
 		break;
@@ -885,6 +918,35 @@ static inline bool gserialized_gist_consistent_internal_2d(BOX2DF *key, BOX2DF *
 	case RTOldContainedByStrategyNumber:
 		retval = (bool) box2df_overlaps(key, query);
 		break;
+		
+	/* To one side */
+	case RTAboveStrategyNumber:
+		retval = (bool)(!box2df_overbelow(key, query));
+		break;
+	case RTBelowStrategyNumber:
+		retval = (bool)(!box2df_overabove(key, query));
+		break;
+	case RTRightStrategyNumber:
+		retval = (bool)(!box2df_overleft(key, query));
+		break;
+	case RTLeftStrategyNumber:
+		retval = (bool)(!box2df_overright(key, query));
+		break;
+
+	/* Overlapping to one side */
+	case RTOverAboveStrategyNumber:
+		retval = (bool)(!box2df_below(key, query));
+		break;
+	case RTOverBelowStrategyNumber:
+		retval = (bool)(!box2df_above(key, query));
+		break;
+	case RTOverRightStrategyNumber:
+		retval = (bool)(!box2df_left(key, query));
+		break;
+	case RTOverLeftStrategyNumber:
+		retval = (bool)(!box2df_right(key, query));
+		break;
+		
 	default:
 		retval = FALSE;
 	}
