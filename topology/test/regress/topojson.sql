30284fe572e23abaf50bc356a6d35c8460a07af4
Sandro Santilli <strk@keybit.net>
2013-03-20 11:44:37 +0000

    Speedup areal TopoJSON output routine to use edge walking
    Now it takes 6% of the time to do the same thing !
    
    Tweak tests to expect new arcs numbering and order.
    Also fixes missing comma separating polygon ring arcs.
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@11187 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/regress/topojson.sql
+++ topology/test/regress/topojson.sql
@@ -5,25 +5,25 @@ set client_min_messages to WARNING;
 \i hierarchy.sql
 
 --- Lineal non-hierarchical 
-SELECT feature_name||'-vanilla', topology.AsTopoJSON(feature, NULL)
+SELECT 'L1-vanilla', feature_name, topology.AsTopoJSON(feature, NULL)
  FROM features.city_streets
  WHERE feature_name IN ('R3', 'R4', 'R1', 'R2' )
  ORDER BY feature_name;
 
 --- Lineal hierarchical 
-SELECT feature_name||'-vanilla', topology.AsTopoJSON(feature, NULL)
+SELECT 'L2-vanilla', feature_name, topology.AsTopoJSON(feature, NULL)
  FROM features.big_streets
  WHERE feature_name IN ('R4', 'R1R2' )
  ORDER BY feature_name;
 
 --- Areal non-hierarchical
-SELECT feature_name||'-vanilla', topology.AsTopoJSON(feature, NULL)
+SELECT 'A1-vanilla', feature_name, topology.AsTopoJSON(feature, NULL)
  FROM features.land_parcels
  WHERE feature_name IN ('P1', 'P2', 'P3', 'P4', 'P5' )
  ORDER BY feature_name;
 
 --- Areal hierarchical
-SELECT feature_name||'-vanilla', topology.AsTopoJSON(feature)
+SELECT 'A2-vanilla', feature_name, topology.AsTopoJSON(feature, NULL)
  FROM features.big_parcels
  WHERE feature_name IN ('P1P2', 'P3P4')
  ORDER BY feature_name;
@@ -32,25 +32,28 @@ SELECT feature_name||'-vanilla', topology.AsTopoJSON(feature)
 CREATE TEMP TABLE edgemap (arc_id serial, edge_id int unique);
 
 --- Lineal non-hierarchical 
-SELECT feature_name||'-edgemap', topology.AsTopoJSON(feature, 'edgemap')
+SELECT 'L1-edgemap', feature_name, topology.AsTopoJSON(feature, 'edgemap')
  FROM features.city_streets
  WHERE feature_name IN ('R3', 'R4', 'R1', 'R2' )
  ORDER BY feature_name;
 
 --- Lineal hierarchical 
-SELECT feature_name||'-edgemap', topology.AsTopoJSON(feature, 'edgemap')
+TRUNCATE edgemap; SELECT NULLIF(setval('edgemap_arc_id_seq', 1, false), 1);
+SELECT 'L2-edgemap', feature_name, topology.AsTopoJSON(feature, 'edgemap')
  FROM features.big_streets
  WHERE feature_name IN ('R4', 'R1R2' )
  ORDER BY feature_name;
 
 --- Areal non-hierarchical
-SELECT feature_name||'-edgemap', topology.AsTopoJSON(feature, 'edgemap')
+TRUNCATE edgemap; SELECT NULLIF(setval('edgemap_arc_id_seq', 1, false), 1);
+SELECT 'A1-edgemap', feature_name, topology.AsTopoJSON(feature, 'edgemap')
  FROM features.land_parcels
  WHERE feature_name IN ('P1', 'P2', 'P3', 'P4', 'P5' )
  ORDER BY feature_name;
 
 --- Areal hierarchical
-SELECT feature_name||'-edgemap', topology.AsTopoJSON(feature, 'edgemap')
+TRUNCATE edgemap; SELECT NULLIF(setval('edgemap_arc_id_seq', 1, false), 1);
+SELECT 'A2-edgemap', feature_name, topology.AsTopoJSON(feature, 'edgemap')
  FROM features.big_parcels
  WHERE feature_name IN ('P1P2', 'P3P4')
  ORDER BY feature_name;
