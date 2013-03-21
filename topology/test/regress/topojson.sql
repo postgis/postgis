59eba85d499b8b56f1f6ff4a4beb38c16e1047fa
Sandro Santilli <strk@keybit.net>
2013-03-21 16:15:51 +0000

    Fix areal TopoJSON output to group and order polygon rings (#2228)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@11193 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/regress/topojson.sql
+++ topology/test/regress/topojson.sql
@@ -4,7 +4,7 @@ set client_min_messages to WARNING;
 \i load_features.sql
 \i hierarchy.sql
 
--- This edge perturbates the topology so that walking around the boundaries
+-- This edges perturbate the topology so that walking around the boundaries
 -- of P1 and P2 may require walking on some of them
 SELECT 'E' || TopoGeo_addLinestring('city_data', 'LINESTRING(9 14, 15 10)');
 SELECT 'E' || TopoGeo_addLinestring('city_data', 'LINESTRING(21 14, 15 18)');
@@ -68,6 +68,29 @@ SELECT 'A2-edgemap', feature_name, topology.AsTopoJSON(feature, 'edgemap')
 DROP TABLE edgemap;
 -- End edge mapping }
 
+-- This edge splits an hole in two faces
+SELECT 'E' || TopoGeo_addLinestring('city_data', 'LINESTRING(4 31, 7 34)');
+
+-- This edge wraps a couple of faces, to test holes at 2 level distance from parent
+SELECT 'E' || TopoGeo_addLinestring('city_data', 'LINESTRING(0 25, 33 25, 33 44, 0 44, 0 25)');
+
+-- Now add a new polygon
+SELECT 'E' || TopoGeo_addLinestring('city_data', 'LINESTRING(3 47, 33 47, 33 52, 3 52, 3 47)');
+SELECT 'E' || TopoGeo_addLinestring('city_data', 'LINESTRING(10 48, 16 48, 16 50, 10 50, 10 48)');
+
+-- And this defines a new feature including both face 1and the new 
+-- wrapping face 11 plus the new (holed) face 12
+INSERT INTO features.land_parcels VALUES ('P6', 
+  topology.CreateTopoGeom(
+    'city_data', -- Topology name
+    3, -- Topology geometry type (polygon/multipolygon)
+    1, -- TG_LAYER_ID for this topology (from topology.layer)
+    '{{1,3},{11,3},{12,3}}'));
+
+SELECT 'A3-vanilla', feature_name, topology.AsTopoJSON(feature, null)
+ FROM features.land_parcels
+ WHERE feature_name IN ('P6')
+ ORDER BY feature_name;
 
 SELECT topology.DropTopology('city_data');
 DROP SCHEMA features CASCADE;
