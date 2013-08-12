4296e58da0fd1934a4929c44fc8f9468464ffb08
Sandro Santilli <strk@keybit.net>
2013-08-12 09:58:02 +0000

    Fix ticket reference (it's #2420, not 30)
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.1@11772 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/tickets.sql
+++ regress/tickets.sql
@@ -831,7 +831,7 @@ SELECT '#2415.2', ST_AsText(ST_Multi(
 
 SELECT '#2412', ST_AsText(ST_LineToCurve('LINESTRING(0 0,10 0,20 0)'));
 
-SELECT '#2430', ST_AsText(ST_LineToCurve('LINESTRING(0 0,10 0,10 10,0 10,0 0)'));
+SELECT '#2420', ST_AsText(ST_LineToCurve('LINESTRING(0 0,10 0,10 10,0 10,0 0)'));
 
 -- Clean up
 DELETE FROM spatial_ref_sys;
