00bb2a04a28ec07cc845e465fbf7f83b6e728f92
Nathaniel Hunter Clay <clay.nathaniel@gmail.com>
2013-12-01 10:52:42 -0800

    minor changes

--- raster/rt_core/librtcore.h
+++ raster/rt_core/librtcore.h
@@ -395,7 +395,7 @@ rt_errorstate rt_pixtype_compare_clamped_values(
  * @return ES_NONE on success, ES_ERROR on error
  */
 rt_errorstate rt_pixel_set_to_array(
-	rt_pixel npixel, rt_mask mask, int count,
+	rt_pixel npixel,rt_mask mask, int count,
 	int x, int y,
 	uint16_t distancex, uint16_t distancey,
 	double ***value,
