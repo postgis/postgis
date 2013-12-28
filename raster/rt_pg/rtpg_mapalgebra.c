ecd206621b77b087362c62bfea8356937dfed3bc
Nathaniel Hunter Clay <clay.nathaniel@gmail.com>
2013-12-28 14:18:43 -0500

    added Copyright Notice

--- raster/rt_pg/rtpg_mapalgebra.c
+++ raster/rt_pg/rtpg_mapalgebra.c
@@ -11,6 +11,7 @@
  * Copyright (C) 2009-2011 Pierre Racine <pierre.racine@sbf.ulaval.ca>
  * Copyright (C) 2009-2011 Mateusz Loskot <mateusz@loskot.net>
  * Copyright (C) 2008-2009 Sandro Santilli <strk@keybit.net>
+ * Copyright (C) 2013 Nathaniel Hunter Clay <clay.nathaniel@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
