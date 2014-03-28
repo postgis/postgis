ccd4ca5f5a8dd65ddcdc96c1cc08fe64c9bf6a0d
Paul Ramsey <pramsey@cleverelephant.ca>
2014-03-28 12:34:45 +0000

    #2684, upgrade extensions at the patch level
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.1@12371 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -8,4 +8,18 @@ UPGRADEABLE_VERSIONS = \
 	2.1.0rc2 \
 	2.1.0rc3 \
 	2.1.0 \
-	  2.1.1
+	2.1.1
+    
+UPGRADEABLE_VERSIONS_MINOR = \
+	2.0.0 \
+    2.0.1 \
+    2.0.2 \
+    2.0.3 \
+    2.0.4 
+
+UPGRADEABLE_VERSIONS_PATCH = \
+	2.1.0rc1 \
+	2.1.0rc2 \
+	2.1.0rc3 \
+	2.1.0 \
+	2.1.1
