6f49219756ef797cf70bf40b597996b4b6f732b4
Sandro Santilli <strk@keybit.net>
2014-03-28 20:11:10 +0000

    Also find uninstall_legacy.sql in :scriptdir, use \cd for 9.1 support
    git-svn-id: http://svn.osgeo.org/postgis/trunk@12382 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/legacy.sql
+++ regress/legacy.sql
@@ -8,7 +8,8 @@
 
 SET client_min_messages TO WARNING;
 
-\i :scriptdir/legacy.sql
+\cd :scriptdir
+\i legacy.sql
 
 TRUNCATE spatial_ref_sys;
 INSERT INTO "spatial_ref_sys" ("srid","auth_name","auth_srid","srtext","proj4text") VALUES (4326,'EPSG',4326,'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]','+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ');
@@ -55,4 +56,4 @@ SELECT 1869 As ticket_id, ST_AsText(ST_AsBinary('POINT(1 2)'));
 
 DELETE FROM spatial_ref_sys WHERE SRID = '4326';
 
-\i 00-regress-install/share/contrib/postgis/uninstall_legacy.sql
+\i uninstall_legacy.sql
