a4c715335c5cee4581ee78098aeb60383ca18470
Paul Ramsey <pramsey@cleverelephant.ca>
2014-08-01 23:04:44 +0000

    Make grep and sed swappable as well (Solaris again)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@12855 b70326c6-7e19-0410-871a-916f4a2858ee

--- autogen.sh
+++ autogen.sh
@@ -14,12 +14,32 @@ giveup()
 
 OSTYPE=`uname -s`
 
+#
+# Solaris has GNU versions of utils with a g prefix
+#
+for grep in ggrep grep; do
+    GREP=`which $grep 2>/dev/null`
+    if test -x "${GREP}"; then
+        break;
+    fi
+done
+
+for sed in gsed sed; do
+    SED=`which $sed 2>/dev/null`
+    if test -x "${SED}"; then
+        break;
+    fi
+done
+
+#
+# Find and run Autoconf
+#
 AUTOCONF=`which autoconf 2>/dev/null`
 if [ ! ${AUTOCONF} ]; then
     echo "Missing autoconf!"
     exit
 fi
-AUTOCONF_VER=`${AUTOCONF} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
+AUTOCONF_VER=`${AUTOCONF} --version | ${GREP} -E "^.*[0-9]$" | ${SED} 's/^.* //'`
 
 for aclocal in aclocal aclocal-1.10 aclocal-1.9; do
     ACLOCAL=`which $aclocal 2>/dev/null`
@@ -31,9 +51,9 @@ if [ ! ${ACLOCAL} ]; then
     echo "Missing aclocal!"
     exit
 fi
-ACLOCAL_VER=`${ACLOCAL} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
+ACLOCAL_VER=`${ACLOCAL} --version | ${GREP} -E "^.*[0-9]$" | ${SED} 's/^.* //'`
 
-for libtoolize in libtoolize glibtoolize; do
+for libtoolize in glibtoolize libtoolize; do
     LIBTOOLIZE=`which $libtoolize 2>/dev/null`
     if test -x "${LIBTOOLIZE}"; then
         break;
@@ -43,7 +63,7 @@ if [ ! ${LIBTOOLIZE} ]; then
     echo "Missing libtoolize!"
     exit
 fi
-LIBTOOLIZE_VER=`${LIBTOOLIZE} --version | grep -E "^.*[0-9]\.[0-9]" | sed 's/^.* //'`
+LIBTOOLIZE_VER=`${LIBTOOLIZE} --version | ${GREP} -E "^.*[0-9]\.[0-9]" | ${SED} 's/^.* //'`
 LIBTOOLIZE_MAJOR_VER=`echo ${LIBTOOLIZE_VER} | cut -f1 -d'.'`
 
 # TODO: Check libtool version and add --install option only for 1.9b+
