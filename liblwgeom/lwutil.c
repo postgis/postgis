34c27b7c3c053e7186c9e412c6391dea6c55b71b
Paul Ramsey <pramsey@cleverelephant.ca>
2014-09-19 21:24:37 +0000

    #2934, strcasestr support for those platforms that lack it
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.0@12991 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwutil.c
+++ liblwgeom/lwutil.c
@@ -388,3 +388,24 @@ clamp_srid(int srid)
 	
 	return newsrid;
 }
+
+#ifndef HAVE_STRCASESTR
+char *strcasestr(const char *s, const char *find)
+{
+  char c, sc;
+  size_t len;
+
+  if ((c = *find++) != 0) {
+    c = tolower((unsigned char)c);
+    len = strlen(find);
+    do {
+      do {
+        if ((sc = *s++) == 0)
+          return (NULL);
+      } while ((char)tolower((unsigned char)sc) != c);
+    } while (strncasecmp(s, find, len) != 0);
+    s--;
+  }
+  return ((char *)s);
+}
+#endif
