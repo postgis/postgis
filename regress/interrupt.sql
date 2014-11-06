f40855383f0f0faeff02dcd3a244650d7d44a502
Sandro Santilli <strk@keybit.net>
2014-11-06 17:22:20 +0000

    Add timing check to interrupt tests
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13120 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/interrupt.sql
+++ regress/interrupt.sql
@@ -1,12 +1,20 @@
+CREATE TEMPORARY TABLE _time AS SELECT now() t;
+
 SET statement_timeout TO 100;
 
 select ST_Buffer(g,100) from (
   select (st_dumppoints(st_buffer(st_makepoint(0,0),10000,100000))).geom g
 ) foo;
 
+SELECT 'buffer_interrupted_on_time', now()-t < '150ms'::interval
+FROM _time; UPDATE _time SET t = now();
+
 SELECT
 ST_Segmentize(ST_MakeLine(ST_MakePoint(4,39), ST_MakePoint(1,41)),
   1e-100);
+
+SELECT 'segmentize_interrupted_on_time', now()-t < '150ms'::interval
+FROM _time; UPDATE _time SET t = now();
 
 SET statement_timeout TO 0;
 
