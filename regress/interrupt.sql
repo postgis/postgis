b8291bfb140db884c59d5afed02f0bc0779b0720
Sandro Santilli <strk@keybit.net>
2014-11-06 18:41:43 +0000

    Allow interruption to take some more time.
    Also show the lateness when the time test fails.
    Hopefully fixes or helps fixing travis build.
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13121 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/interrupt.sql
+++ regress/interrupt.sql
@@ -6,15 +6,23 @@ select ST_Buffer(g,100) from (
   select (st_dumppoints(st_buffer(st_makepoint(0,0),10000,100000))).geom g
 ) foo;
 
-SELECT 'buffer_interrupted_on_time', now()-t < '150ms'::interval
-FROM _time; UPDATE _time SET t = now();
+-- it may take some more to interrupt st_buffer, see
+-- https://travis-ci.org/postgis/postgis/builds/40211116#L2222-L2223
+SELECT CASE WHEN now()-t < '200ms'::interval THEN
+    'buffer interrupted on time'
+  ELSE
+    'buffer interrupted late: ' || ( now()-t )::text
+  END FROM _time; UPDATE _time SET t = now();
 
 SELECT
 ST_Segmentize(ST_MakeLine(ST_MakePoint(4,39), ST_MakePoint(1,41)),
   1e-100);
 
-SELECT 'segmentize_interrupted_on_time', now()-t < '150ms'::interval
-FROM _time; UPDATE _time SET t = now();
+SELECT CASE WHEN now()-t < '150ms'::interval THEN
+    'segmentize interrupted on time'
+  ELSE
+    'segmentize interrupted late: ' || ( now()-t )::text
+  END FROM _time; UPDATE _time SET t = now();
 
 SET statement_timeout TO 0;
 
