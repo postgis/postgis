9a13442fe42f3ba680163b5674e6060c62bd8ea9
Sandro Santilli <strk@keybit.net>
2015-02-23 18:36:56 +0000

    Add test for use of GiST index with && operator
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.1@13273 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/regress_index.sql
+++ regress/regress_index.sql
@@ -3,14 +3,35 @@
 
 --- test some of the searching capabilities
 
+CREATE FUNCTION qnodes(q text) RETURNS text
+LANGUAGE 'plpgsql' AS
+$$
+DECLARE
+  exp TEXT;
+BEGIN
+  EXECUTE 'EXPLAIN ' || q INTO exp;
+  RETURN (regexp_matches(exp, ' *(.*Scan)'))[1];
+END;
+$$;
+
 -- GiST index
 
 CREATE INDEX quick_gist on test using gist (the_geom);
 
+set enable_indexscan = off;
+set enable_bitmapscan = off;
+set enable_seqscan = on;
+
+SELECT 'scan_idx', qnodes('select * from test where the_geom && ST_MakePoint(0,0)');
  select num,ST_astext(the_geom) from test where the_geom && 'BOX3D(125 125,135 135)'::box3d order by num;
 
+set enable_indexscan = on;
+set enable_bitmapscan = off;
 set enable_seqscan = off;
 
- select num,ST_astext(the_geom) from test where the_geom && 'BOX3D(125 125,135 135)'::box3d  order by num;
+SELECT 'scan_seq', qnodes('select * from test where the_geom && ST_MakePoint(0,0)');
+ select num,ST_astext(the_geom) from test where the_geom && 'BOX3D(125 125,135 135)'::box3d order by num;
 
 DROP TABLE test;
+
+DROP FUNCTION qnodes(text);
