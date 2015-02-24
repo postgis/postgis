7de3cd7ae8e270516ddfed70027426ae219aeca6
Sandro Santilli <strk@keybit.net>
2015-02-24 12:41:27 +0100

    Add N-dimensional distance operator with KNN support
    Includes docs and tests

--- regress/operators.sql
+++ regress/operators.sql
@@ -141,3 +141,41 @@ WITH v(i,g) AS ( VALUES
  )
 SELECT 'ndovm2', array_agg(i) FROM v WHERE g &&& 'POINTZ(0 0 1)'::geometry
 ORDER BY 1;
+
+-- nd box centroid distance  <<->>
+
+select 'ndcd1', 'LINESTRING(0 0,0 10,10 10)'::geometry <<->>
+                'LINESTRING(6 2,6 8)'::geometry; -- 1
+select 'ndcd2', 'LINESTRING(0 0,0 10,10 10)'::geometry <<->>
+                'LINESTRING(11 0,19 10)'::geometry; -- 10
+select 'ndcd3', 'POINTM(0 0 0)'::geometry <<->>
+                'POINTM(0 0 5)'::geometry; -- 5
+select 'ndcd4', 'POINTZ(0 0 15)'::geometry <<->>
+                'POINTZ(0 0 10)'::geometry; -- 5
+select 'ndcd5', 'POINTZM(1 2 3 4)'::geometry <<->>
+                'POINTZM(2 3 4 5)'::geometry; -- 2
+select 'ndcd6', 'POINTZM(9 9 3 4)'::geometry <<->>
+                'POINT(9 8)'::geometry; -- 1, higher dimensions overlapping
+
+-- nd box distance  <<#>>
+
+select 'ndbd1', 'LINESTRING(0 0,0 10,10 10)'::geometry <<#>>
+                'LINESTRING(6 2,6 8)'::geometry; -- 0, overlap
+select 'ndbd2', 'LINESTRING(0 0,0 10,10 10)'::geometry <<#>>
+                'LINESTRING(11 0,19 10)'::geometry; -- 1 on the right
+select 'ndbd3', 'LINESTRING(0 0,10 10)'::geometry <<#>>
+                'LINESTRING(-11 0,-2 10)'::geometry; -- 2 on the left
+select 'ndbd4', 'LINESTRING(0 0,10 10)'::geometry <<#>>
+                'LINESTRING(0 13,5 14)'::geometry; -- 3 above
+select 'ndbd5', 'LINESTRING(0 0,10 10)'::geometry <<#>>
+                'LINESTRING(0 -20,5 -4)'::geometry; -- 4 below
+select 'ndbd6', 'LINESTRINGM(0 0 0,1 1 1)'::geometry <<#>>
+                'LINESTRING(0 0,1 1)'::geometry; -- 0 overlap, mixed
+select 'ndbd7', 'LINESTRINGM(0 0 0,1 1 1)'::geometry <<#>>
+                'LINESTRINGM(1 1 2,1 1 3)'::geometry; -- 1
+select 'ndbd8', 'LINESTRINGZ(0 0 0,1 1 1)'::geometry <<#>>
+                'LINESTRINGZ(1 1 3,1 1 5)'::geometry; -- 2
+select 'ndbd9', 'LINESTRINGZ(0 0 0,1 1 1)'::geometry <<#>>
+                'LINESTRINGM(1 1 3,1 1 5)'::geometry; -- 0, overlap, mixed
+select 'ndbd10', 'LINESTRINGZM(0 0 0 0,1 2 3 4)'::geometry <<#>>
+                 'LINESTRINGZM(3 4 5 6,4 5 6 7)'::geometry; -- 4
