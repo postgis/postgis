68c4ad48d7bcbb4d36f75e9ead918f91cb3622b3
Sandro Santilli <strk@keybit.net>
2015-02-24 17:00:08 +0000

    Include the LIMIT clause in KNN scan test queries
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13284 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/knn.sql
+++ regress/knn.sql
@@ -25,14 +25,14 @@ CREATE INDEX on test using gist (the_geom);
 
 -- Index-supported KNN query
 
-SELECT '<-> idx', qnodes('select * from test order by the_geom <-> ST_MakePoint(0,0)');
+SELECT '<-> idx', qnodes('select * from test order by the_geom <-> ST_MakePoint(0,0) LIMIT 1');
 SELECT '<-> res1',num,
   (the_geom <-> 'LINESTRING(0 0,5 5)'::geometry)::numeric(10,2),
   ST_astext(the_geom) from test
   order by the_geom <-> 'LINESTRING(0 0,5 5)'::geometry LIMIT 1;
 
 -- Full table extent: BOX(0.0439142361 0.0197799355,999.955261 999.993652)
-SELECT '<#> idx', qnodes('select * from test order by the_geom <#> ST_MakePoint(0,0)');
+SELECT '<#> idx', qnodes('select * from test order by the_geom <#> ST_MakePoint(0,0) LIMIT 1');
 SELECT '<#> res1',num,
   (the_geom <#> 'LINESTRING(1000 0,1005 5)'::geometry)::numeric(10,2),
   ST_astext(the_geom) from test
