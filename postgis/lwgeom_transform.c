9e5b76f5ee5326d07f02b01f2176f3546b044bd7
mwtoews <mwtoews@gmail.com>
2015-03-29 21:53:17 +1300

    Initial work on ST_Transform for arbitrary PROJ.4 text in/outputs

--- postgis/lwgeom_transform.c
+++ postgis/lwgeom_transform.c
@@ -19,10 +19,13 @@
 
 
 Datum transform(PG_FUNCTION_ARGS);
-Datum transform_geom(PG_FUNCTION_ARGS);
+Datum transform_geom(PG_FUNCTION_ARGS); /* Deprecation in 2.2.0 */
 Datum postgis_proj_version(PG_FUNCTION_ARGS);
 
-
+/* Availability: 2.2.0 */
+Datum transform_to_proj(PG_FUNCTION_ARGS);
+Datum transform_from_proj_to_proj(PG_FUNCTION_ARGS);
+Datum transform_from_proj_to_srid(PG_FUNCTION_ARGS);
 
 /**
  * transform( GEOMETRY, INT (output srid) )
@@ -95,6 +98,8 @@ Datum transform(PG_FUNCTION_ARGS)
  * tmpPts - if there is a nadgrid error (-38), we re-try the transform
  * on a copy of points.  The transformed points
  * are in an indeterminate state after the -38 error is thrown.
+ *
+ * Deprecation in 2.2.0
  */
 PG_FUNCTION_INFO_V1(transform_geom);
 Datum transform_geom(PG_FUNCTION_ARGS)
@@ -206,4 +211,275 @@ Datum postgis_proj_version(PG_FUNCTION_ARGS)
 	const char *ver = pj_get_release();
 	text *result = cstring2text(ver);
 	PG_RETURN_POINTER(result);
+}
+
+/**
+ * ST_Transform(geom geometry, to_proj text)
+ *
+ * Availability: 2.2.0
+ */
+PG_FUNCTION_INFO_V1(transform_to_proj);
+Datum transform_to_proj(PG_FUNCTION_ARGS)
+{
+	GSERIALIZED *geom;
+	GSERIALIZED *result=NULL;
+	LWGEOM *lwgeom;
+	projPJ input_pj, output_pj;
+	char *input_proj4, *output_proj4;
+	text *output_proj4_text;
+	int32 input_srid;
+
+	char *pj_errstr;
+
+	/* Set the search path if we haven't already */
+	SetPROJ4LibPath();
+
+	/* Read the arguments */
+	geom = PG_GETARG_GSERIALIZED_P_COPY(0);
+	input_srid = gserialized_get_srid(geom);
+	if ( input_srid == SRID_UNKNOWN )
+	{
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR, "Input geometry has unknown (%d) SRID", SRID_UNKNOWN);
+		PG_RETURN_NULL();
+	}
+	input_proj4 = GetProj4StringSPI(input_srid);
+
+    output_proj4_text = PG_GETARG_TEXT_P(1);
+	output_proj4 = text2cstring(output_proj4_text);
+
+	/* make input and output projection objects */
+	input_pj = lwproj_from_string(input_proj4);
+	if ( input_pj == NULL )
+	{
+		pj_errstr = pj_strerrno(*pj_get_errno_ref());
+		if ( ! pj_errstr ) pj_errstr = "";
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR,
+		    "transform_to_proj: could not parse SRID=%d for PROJ.4: '%s' %s",
+		    input_srid, input_proj4, pj_errstr);
+		pfree(input_proj4);
+		PG_RETURN_NULL();
+	}
+	pfree(input_proj4);
+
+	output_pj = lwproj_from_string(output_proj4);
+	if ( output_pj == NULL )
+	{
+		pj_errstr = pj_strerrno(*pj_get_errno_ref());
+		if ( ! pj_errstr ) pj_errstr = "";
+		pj_free(input_pj);
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR,
+			"transform_to_proj: could not parse 'to_proj' for PROJ.4: '%s': %s",
+			output_proj4, pj_errstr);
+		pfree(output_proj4);
+		PG_RETURN_NULL();
+	}
+	pfree(output_proj4);
+
+	/* now we have a geometry, and input/output PJ structs. */
+	lwgeom = lwgeom_from_gserialized(geom);
+	lwgeom_transform(lwgeom, input_pj, output_pj);
+	/* the geometry does not have an SRID */
+	lwgeom->srid = SRID_UNKNOWN;
+
+	/* clean up */
+	pj_free(input_pj);
+	pj_free(output_pj);
+
+	/* Re-compute bbox if input had one (COMPUTE_BBOX TAINTING) */
+	if ( lwgeom->bbox )
+	{
+		lwgeom_drop_bbox(lwgeom);
+		lwgeom_add_bbox(lwgeom);
+	}
+
+	result = geometry_serialize(lwgeom);
+
+	lwgeom_free(lwgeom);
+	PG_FREE_IF_COPY(geom, 0);
+
+	PG_RETURN_POINTER(result); /* new geometry */
+}
+
+/**
+ * ST_Transform(geom geometry, from_proj text, to_proj text)
+ *
+ * Availability: 2.2.0
+ */
+PG_FUNCTION_INFO_V1(transform_from_proj_to_proj);
+Datum transform_from_proj_to_proj(PG_FUNCTION_ARGS)
+{
+	GSERIALIZED *geom;
+	GSERIALIZED *result=NULL;
+	LWGEOM *lwgeom;
+	projPJ input_pj, output_pj;
+	char *input_proj4, *output_proj4;
+	text *input_proj4_text;
+	text *output_proj4_text;
+	int32 geom_srid;
+	char *pj_errstr;
+
+
+	geom = PG_GETARG_GSERIALIZED_P_COPY(0);
+    input_proj4_text = PG_GETARG_TEXT_P(1);
+    output_proj4_text = PG_GETARG_TEXT_P(2);
+
+    /* Convert from text to cstring for libproj */
+
+	geom_srid = gserialized_get_srid(geom);
+	if (geom_srid != SRID_UNKNOWN)
+		elog(WARNING,
+				"transform_from_proj_to_proj: ignoring geometry SRID=%d",
+				geom_srid);
+
+	/* make input and output projection objects */
+	SetPROJ4LibPath();
+	input_proj4 = text2cstring(input_proj4_text);
+	input_pj = lwproj_from_string(input_proj4);
+	if ( input_pj == NULL )
+	{
+		pj_errstr = pj_strerrno(*pj_get_errno_ref());
+		if ( ! pj_errstr ) pj_errstr = "";
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR,
+		    "transform_from_proj_to_proj: could not parse 'from_proj' for PROJ.4: '%s' %s",
+		    input_proj4, pj_errstr);
+		pfree(input_proj4);
+		PG_RETURN_NULL();
+	}
+	pfree(input_proj4);
+
+	output_proj4 = text2cstring(output_proj4_text);
+	output_pj = lwproj_from_string(output_proj4);
+	if ( output_pj == NULL )
+	{
+		pj_errstr = pj_strerrno(*pj_get_errno_ref());
+		if ( ! pj_errstr ) pj_errstr = "";
+		pj_free(input_pj);
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR,
+			"transform_from_proj_to_proj: could not parse 'to_proj' for PROJ.4: '%s': %s",
+			output_proj4, pj_errstr);
+		pfree(output_proj4);
+		PG_RETURN_NULL();
+	}
+	pfree(output_proj4);
+
+	/* now we have a geometry, and input/output PJ structs. */
+	lwgeom = lwgeom_from_gserialized(geom);
+	lwgeom_transform(lwgeom, input_pj, output_pj);
+	/* the geometry does not have an SRID */
+	lwgeom->srid = SRID_UNKNOWN;
+
+	/* clean up */
+	pj_free(input_pj);
+	pj_free(output_pj);
+
+	/* Re-compute bbox if input had one (COMPUTE_BBOX TAINTING) */
+	if ( lwgeom->bbox )
+	{
+		lwgeom_drop_bbox(lwgeom);
+		lwgeom_add_bbox(lwgeom);
+	}
+
+	result = geometry_serialize(lwgeom);
+
+	lwgeom_free(lwgeom);
+	PG_FREE_IF_COPY(geom, 0);
+
+	PG_RETURN_POINTER(result); /* new geometry */
+}
+
+/**
+ * ST_Transform(geom geometry, from_proj text, to_srid integer)
+ *
+ * Availability: 2.2.0
+ */
+PG_FUNCTION_INFO_V1(transform_from_proj_to_srid);
+Datum transform_from_proj_to_srid(PG_FUNCTION_ARGS)
+{
+	GSERIALIZED *geom;
+	GSERIALIZED *result=NULL;
+	LWGEOM *lwgeom;
+	projPJ input_pj, output_pj;
+	char *input_proj4, *output_proj4;
+	text *input_proj4_text;
+	int32 output_srid, geom_srid;
+
+	char *pj_errstr;
+
+	output_srid = PG_GETARG_INT32(2);
+	if (output_srid == SRID_UNKNOWN)
+	{
+		elog(ERROR, "%d is an invalid target SRID", SRID_UNKNOWN);
+		PG_RETURN_NULL();
+	}
+
+	geom = PG_GETARG_GSERIALIZED_P_COPY(0);
+	geom_srid = gserialized_get_srid(geom);
+	if (geom_srid != SRID_UNKNOWN)
+		elog(WARNING,
+				"transform_from_proj_to_srid: ignoring geometry SRID=%d",
+				geom_srid);
+
+    input_proj4_text = PG_GETARG_TEXT_P(1);
+	input_proj4 = text2cstring(input_proj4_text);
+
+	SetPROJ4LibPath();
+	output_proj4 = GetProj4StringSPI((int)output_srid);
+
+	/* make input and output projection objects */
+	input_pj = lwproj_from_string(input_proj4);
+	if ( input_pj == NULL )
+	{
+		pj_errstr = pj_strerrno(*pj_get_errno_ref());
+		if ( ! pj_errstr ) pj_errstr = "";
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR,
+		    "transform_from_proj_to_srid: could not parse 'from_proj' for PROJ.4: '%s' %s",
+		    input_proj4, pj_errstr);
+		pfree(input_proj4);
+		PG_RETURN_NULL();
+	}
+	pfree(input_proj4);
+
+	output_pj = lwproj_from_string(output_proj4);
+	if ( output_pj == NULL )
+	{
+		pj_errstr = pj_strerrno(*pj_get_errno_ref());
+		if ( ! pj_errstr ) pj_errstr = "";
+		pj_free(input_pj);
+		PG_FREE_IF_COPY(geom, 0);
+		elog(ERROR,
+			"transform_from_proj_to_srid: could not parse SRID=%d 'to_proj' for PROJ.4: '%s': %s",
+			output_srid, output_proj4, pj_errstr);
+		pfree(output_proj4);
+		PG_RETURN_NULL();
+	}
+	pfree(output_proj4);
+
+	/* now we have a geometry, and input/output PJ structs. */
+	lwgeom = lwgeom_from_gserialized(geom);
+	lwgeom_transform(lwgeom, input_pj, output_pj);
+	lwgeom->srid = output_srid;
+
+	/* clean up */
+	pj_free(input_pj);
+	pj_free(output_pj);
+
+	/* Re-compute bbox if input had one (COMPUTE_BBOX TAINTING) */
+	if ( lwgeom->bbox )
+	{
+		lwgeom_drop_bbox(lwgeom);
+		lwgeom_add_bbox(lwgeom);
+	}
+
+	result = geometry_serialize(lwgeom);
+
+	lwgeom_free(lwgeom);
+	PG_FREE_IF_COPY(geom, 0);
+
+	PG_RETURN_POINTER(result); /* new geometry */
 }
