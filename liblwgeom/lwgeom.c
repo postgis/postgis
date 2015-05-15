f48830ef0db936c32268df62dfb8ab3cb3cf1d34
Sandro Santilli <strk@keybit.net>
2015-05-15 08:04:02 +0000

    Remove unused assignment
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13505 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwgeom.c
+++ liblwgeom/lwgeom.c
@@ -2024,10 +2024,9 @@ lwgeom_subdivide_recursive(const LWGEOM *geom, int maxvertices, LWCOLLECTION *co
 LWCOLLECTION *
 lwgeom_subdivide(const LWGEOM *geom, int maxvertices)
 {
-	int n = 0;
 	LWCOLLECTION *col;
 	col = lwcollection_construct_empty(COLLECTIONTYPE, geom->srid, lwgeom_has_z(geom), lwgeom_has_m(geom));
-	n = lwgeom_subdivide_recursive(geom, maxvertices, col, NULL);
+	lwgeom_subdivide_recursive(geom, maxvertices, col, NULL);
 	return col;
 }
 
