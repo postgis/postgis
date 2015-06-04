bc678db2c0c6f015af3a3f25087c3b24d169bfd8
Sandro Santilli <strk@keybit.net>
2015-06-04 21:03:53 +0000

    Move ST_ClosestPointOfApproach tests under temporal.sql
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13622 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/regress_lrs.sql
+++ regress/regress_lrs.sql
@@ -63,29 +63,6 @@ select 'line_substring_12', ST_AsText(ST_LineSubstring('LINESTRING(0 0 10, 1 1 5
 select 'line_interpolate_point', ST_AsText(ST_LineInterpolatePoint('LINESTRING(0 0, 1 1)', 0));
 select 'line_interpolate_point', ST_AsText(ST_LineInterpolatePoint('LINESTRING(0 0 10, 1 1 5)', 0.5));
 
---
---- ST_PointOfClosestApproach
---
-
--- Converging
-select 'pca1', ST_ClosestPointOfApproach(
-  'LINESTRINGZM(0 0 0 0, 10 10 10 10)',
-  'LINESTRINGZM(0 0 0 1, 10 10 10 10)');
--- Following
-select 'pca2', ST_ClosestPointOfApproach(
-  'LINESTRINGZM(0 0 0 0, 10 10 10 10)',
-  'LINESTRINGZM(0 0 0 5, 10 10 10 15)');
--- Crossing
-select 'pca3', ST_ClosestPointOfApproach(
-  'LINESTRINGZM(0 0 0 0, 0 0 0 10)',
-  'LINESTRINGZM(-30 0 5 4, 10 0 5 6)');
--- Ticket #3136
-WITH inp as ( SELECT
- 'LINESTRING M (0 0 80000002,1 0 80000003)'::geometry g1,
- 'LINESTRING M (2 2 80000000,1 1 80000001,0 0 80000002)'::geometry g2 )
-SELECT 'pca#3136',
-ST_ClosestPointOfApproach(g2,g1), ST_ClosestPointOfApproach(g1,g2)
-FROM inp;
 
 --
 -- ST_AddMeasure
--- regress/temporal.sql
+++ regress/temporal.sql
@@ -1,3 +1,9 @@
+----------------------------------------
+--
+-- ST_IsValidTrajectory
+--
+----------------------------------------
+
 SELECT 'invalidTrajectory1', ST_IsValidTrajectory('POINTM(0 0 0)'::geometry);
 SELECT 'invalidTrajectory2', ST_IsValidTrajectory('LINESTRINGZ(0 0 0,1 1 1)'::geometry);
 SELECT 'invalidTrajectory3', ST_IsValidTrajectory('LINESTRINGM(0 0 0,1 1 0)'::geometry);
@@ -7,3 +13,29 @@ SELECT 'invalidTrajectory8', ST_IsValidTrajectory('LINESTRINGM(0 0 0,1 1 1,1 1 2
 SELECT 'validTrajectory1', ST_IsValidTrajectory('LINESTRINGM(0 0 0,1 1 1)'::geometry);
 SELECT 'validTrajectory2', ST_IsValidTrajectory('LINESTRINGM EMPTY'::geometry);
 SELECT 'validTrajectory3', ST_IsValidTrajectory('LINESTRINGM(0 0 0,1 1 1,1 1 2)'::geometry);
+
+----------------------------------------
+--
+-- ST_PointOfClosestApproach
+--
+----------------------------------------
+
+-- Converging
+select 'pca1', ST_ClosestPointOfApproach(
+  'LINESTRINGZM(0 0 0 0, 10 10 10 10)',
+  'LINESTRINGZM(0 0 0 1, 10 10 10 10)');
+-- Following
+select 'pca2', ST_ClosestPointOfApproach(
+  'LINESTRINGZM(0 0 0 0, 10 10 10 10)',
+  'LINESTRINGZM(0 0 0 5, 10 10 10 15)');
+-- Crossing
+select 'pca3', ST_ClosestPointOfApproach(
+  'LINESTRINGZM(0 0 0 0, 0 0 0 10)',
+  'LINESTRINGZM(-30 0 5 4, 10 0 5 6)');
+-- Ticket #3136
+WITH inp as ( SELECT
+ 'LINESTRING M (0 0 80000002,1 0 80000003)'::geometry g1,
+ 'LINESTRING M (2 2 80000000,1 1 80000001,0 0 80000002)'::geometry g2 )
+SELECT 'pca#3136',
+ST_ClosestPointOfApproach(g2,g1), ST_ClosestPointOfApproach(g1,g2)
+FROM inp;
