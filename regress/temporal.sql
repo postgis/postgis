f4f19b8fe678786f4c911cbb665428916e4e56f5
Sandro Santilli <strk@keybit.net>
2015-06-04 21:13:12 +0000

    Get "Closest Point of Approach" name straight ...
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13623 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/temporal.sql
+++ regress/temporal.sql
@@ -16,26 +16,26 @@ SELECT 'validTrajectory3', ST_IsValidTrajectory('LINESTRINGM(0 0 0,1 1 1,1 1 2)'
 
 ----------------------------------------
 --
--- ST_PointOfClosestApproach
+-- ST_ClosestPointOfApproach
 --
 ----------------------------------------
 
 -- Converging
-select 'pca1', ST_ClosestPointOfApproach(
+select 'cpa1', ST_ClosestPointOfApproach(
   'LINESTRINGZM(0 0 0 0, 10 10 10 10)',
   'LINESTRINGZM(0 0 0 1, 10 10 10 10)');
 -- Following
-select 'pca2', ST_ClosestPointOfApproach(
+select 'cpa2', ST_ClosestPointOfApproach(
   'LINESTRINGZM(0 0 0 0, 10 10 10 10)',
   'LINESTRINGZM(0 0 0 5, 10 10 10 15)');
 -- Crossing
-select 'pca3', ST_ClosestPointOfApproach(
+select 'cpa3', ST_ClosestPointOfApproach(
   'LINESTRINGZM(0 0 0 0, 0 0 0 10)',
   'LINESTRINGZM(-30 0 5 4, 10 0 5 6)');
 -- Ticket #3136
 WITH inp as ( SELECT
  'LINESTRING M (0 0 80000002,1 0 80000003)'::geometry g1,
  'LINESTRING M (2 2 80000000,1 1 80000001,0 0 80000002)'::geometry g2 )
-SELECT 'pca#3136',
+SELECT 'cpa#3136',
 ST_ClosestPointOfApproach(g2,g1), ST_ClosestPointOfApproach(g1,g2)
 FROM inp;
