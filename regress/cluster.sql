1e9c62af1e0cda6c257016bcead1f8c5e073e858
Daniel Baston <dbaston@gmail.com>
2015-06-28 12:31:23 -0400

    add null and empty geometries to regression test

--- regress/cluster.sql
+++ regress/cluster.sql
@@ -1,12 +1,14 @@
--- tests for ST_ClusterIntersecting
+-- tests for ST_ClusterIntersecting and ST_ClusterWithin
 
 CREATE TEMPORARY TABLE cluster_inputs (id int, geom geometry);
 INSERT INTO cluster_inputs VALUES
 (1, 'LINESTRING (0 0, 1 1)'),
 (2, 'LINESTRING (5 5, 4 4)'),
-(3, 'LINESTRING (0 0, -1 -1)'),
-(4, 'LINESTRING (6 6, 7 7)'),
-(5, 'POLYGON ((0 0, 4 0, 4 4, 0 4, 0 0))');
+(3, NULL),
+(4, 'LINESTRING (0 0, -1 -1)'),
+(5, 'LINESTRING (6 6, 7 7)'),
+(6, 'POLYGON EMPTY'),
+(7, 'POLYGON ((0 0, 4 0, 4 4, 0 4, 0 0))');
 
 SELECT 't1', ST_AsText(unnest(ST_ClusterIntersecting(geom ORDER BY id))) FROM cluster_inputs;
 SELECT 't2', ST_AsText(unnest(ST_ClusterIntersecting(ST_Accum(geom ORDER BY id)))) FROM cluster_inputs;
