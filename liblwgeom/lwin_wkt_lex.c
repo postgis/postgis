789793ce1de232b6d6391b473de470832295fa18
Paul Ramsey <pramsey@cleverelephant.ca>
2015-06-29 17:44:21 +0000

    Remove comment
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13748 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwin_wkt_lex.c
+++ liblwgeom/lwin_wkt_lex.c
@@ -2024,7 +2024,6 @@ static int yy_flex_strlen (yyconst char * s )
 
 
 
-
 void *wkt_yyalloc (yy_size_t  size )
 {
 	return (void *) lwalloc( size );
@@ -2032,13 +2031,6 @@ void *wkt_yyalloc (yy_size_t  size )
 
 void *wkt_yyrealloc  (void * ptr, yy_size_t  size )
 {
-	/* The cast to (char *) in the following accommodates both
-	 * implementations that use char* generic pointers, and those
-	 * that use void* generic pointers.  It works with the latter
-	 * because both ANSI C and C++ allow castless assignment from
-	 * any pointer type to void*, and deal with argument conversions
-	 * as though doing an assignment.
-	 */
 	return (void *) lwrealloc( (char *) ptr, size );
 }
 
