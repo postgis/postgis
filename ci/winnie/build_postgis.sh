2f865f8370ffe030b32e0673c5a9480f5452350f
Regina Obe <lr@pcorp.us>
2015-08-04 22:40:00 +0000

    add build and package scripts and update all to copy new postgis topology .dll to install and package folder
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13879 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/winnie/build_postgis.sh
+++ ci/winnie/build_postgis.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 set -e
-#winnie passed in variables
-#export OS_BUILD=64
-#export GCC_TYPE=gcc48
-#export PG_VER=9.3
+#### $Id:build_postgis210.sh 10208 2012-08-30 05:38:22Z robe $
+#all these get passed in by jenkins
+#export OS_BUILD=64 
+#export PG_VER=9.2beta2
+#export PGHOST=localhost
+#export PGPORT=8442
+#export PGUSER=postgres
+#export GEOS_VER=3.4.0dev
+#export GDAL_VER=1.9.1
+#POSTGIS_SVN_REVISION=passed_in_by_buildbot
+#POSTGIS_MAJOR_VERSION=2
+#POSTGIS_MINOR_VERSION=1
+#POSTGIS_MICRO_VERSION=0SVN
+#export GCC_TYPE=gcc48  #for pre-4.8.0 compiles this is blank
+#export JSON_VER=0.9
+#export SFCGAL_VER=1.0.4
 #export PROJ_VER=4.8.0
-#export PROJSO=libproj-0.dll
-#export POSTGIS_MAJOR_VERSION=2
-#export POSTGIS_MINOR_VERSION=2
-#export POSTGIS_MICRO_VERSION=0dev
 
 
-export PROJECTS=/projects
-export MINGPROJECTS=/projects
+export LIBXML_VER=2.7.8
+
+if [[ "${GCC_TYPE}" == *gcc48* ]] ; then
+	export PROJECTS=/projects
+	export MINGPROJECTS=/projects
+	export PATHOLD=$PATH
+else
+	export PROJECTS=/projects
+	export MINGPROJECTS=/projects
+	export PATHOLD=$PATH
+	#export JSON_VER=0.9
+fi;
 export PATHOLD=$PATH
 
 
@@ -22,22 +40,17 @@ else
 	export MINGHOST=i686-w64-mingw32
 fi;
 
-export PATHOLD="/mingw/bin:/mingw/include:/mingw/lib:/c/Windows/system32:/c/Windows:.:/bin:/include:/usr/local/bin:/c/ming${OS_BUILD}/svn"
-#export PG_VER=9.2beta2
+export PATHOLD="/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows:.:/bin:/include:/usr/local/bin:/c/ming${OS_BUILD}/svn"
+
 export PGWINVER=${PG_VER}edb
-export WORKSPACE=`pwd`
 
 echo PATH BEFORE: $PATH
 
-#export PGHOST=localhost
-#export PGPORT=8442
-export PGUSER=postgres
-#export GEOS_VER=3.4.0dev
-#export GDAL_VER=1.9.1
 export PGPATH=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}
-export PGPATHEDB=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}edb
+#export PROJSO=libproj-0.dll
 
 export POSTGIS_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}
+
 export POSTGIS_MICRO_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}.${POSTGIS_MICRO_VERSION}
 if [[ "$POSTGIS_MICRO_VERSION"  == *SVN* || "$POSTGIS_MICRO_VERSION"  == *dev* ]] ; then
 	export POSTGIS_SRC=${PROJECTS}/postgis/branches/${POSTGIS_VER}
@@ -45,36 +58,33 @@ else
 	#tagged version -- official release
 	export POSTGIS_SRC=${PROJECTS}/postgis/tags/${POSTGIS_VER}.${POSTGIS_MICRO_VERSION}
 fi;
-export LIBXML_VER=2.7.8
+
 #export POSTGIS_SRC=${PROJECTS}/postgis/trunk
 export GDAL_DATA="${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/share/gdal"
+# export REL_PGVER=$(echo $PG_VER | tr '.' '') 
+# echo $REL_PGVER
+# export RELDIR=${MINGPROJECTS}/postgis/builds/postgis-21
+# export RELVERDIR=postgis-pg${REL_PGVER}-binaries-${POSTGIS_MICRO_VER}w64
 
-export RELVERDIR=postgis-pg${REL_PGVER}-binaries-${POSTGIS_MICRO_VER}w${OS_BUILD}
-
-export PATH="${PATHOLD}:${PGPATH}/bin:${PGPATH}/lib"
-#PATH="${MINGPROJECTS}/gettext/rel-gettext-0.18.1/bin:${MINGPROJECTS}/xsltproc:${MINGPROJECTS}/gtk/bin:${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}/bin:${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}/bin:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}/include:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}/bin:${MINGPROJECTS}/proj/rel-${PROJ_VER}w${OS_BUILD}/bin:${MINGPROJECTS}/libxml/rel-libxml2-${LIBXML_VER}w${OS_BUILD}/bin:${PATH}"
-PATH="${MINGPROJECTS}/xsltproc:${MINGPROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/lib:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/proj/rel-${PROJ_VER}w${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/libxml/rel-libxml2-${LIBXML_VER}w${OS_BUILD}${GCC_TYPE}/bin:${PATH}"
+#export PATH="${PATHOLD}:${PGPATH}/bin:${PGPATH}/lib"
+export PATH="${PGPATH}/bin:${PGPATH}/lib:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/gtk/bin:${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/xsltproc:${PATH}"
 echo PATH AFTER: $PATH
 
-echo WORKSPACE IS $WORKSPACE
-#mkdir ${PROJECTS}/postgis/tmp
-export PGIS_REG_TMPDIR=${PROJECTS}/postgis/tmp/${POSTGIS_MICRO_VER}_pg${PG_VER}_geos${GEOS_VER}_gdal${GDAL_VER}w${OS_BUILD}
-rm -rf ${PGIS_REG_TMPDIR}
-mkdir ${PGIS_REG_TMPDIR}
-export TMPDIR=${PGIS_REG_TMPDIR}
-
-#rm -rf ${PGIS_REG_TMPDIR}
-#TMPDIR=${PROJECTS}/postgis/tmp/${POSTGIS_VER}_${PG_VER}_${GEOS_VERSION}_${PROJ_VER}
-echo PORT IS $PGPORT
-echo PGIS_REG_TMPDIR IS $PGIS_REG_TMPDIR
-export XSLTPROCFLAGS=
 cd ${POSTGIS_SRC}
 if [ -e ./GNUMakefile ]; then
 	make distclean
 fi
-
+echo ${POSTGIS_SRC}
 sh autogen.sh
 
+#hack to get around boolean incompatibility
+#-D__USE_MINGW_ANSI_STDIO=1
+if [ "$JSON_VER" == "0.9" ] ; then
+	cp ${MINGPROJECTS}/json-c/rel-${JSON_VER}w${OS_BUILD}${GCC_TYPE}/include/json/json_object.h.for_configure ${MINGPROJECTS}/json-c/rel-${JSON_VER}w${OS_BUILD}${GCC_TYPE}/include/json/json_object.h
+fi
+export XSLTPROCFLAGS=
+
+#add  PCRE for address standardizer
 if [ -n "$PCRE_VER" ]; then
     export PATH="${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}/include:${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}/lib:${PATH}"
 fi
@@ -87,8 +97,6 @@ if [ -n "$SFCGAL_VER" ]; then
 	export BOOST_VER_WU=1_53_0
 	export PATH="${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/boost/rel-${BOOST_VER_WU}w${OS_BUILD}${GCC_TYPE}/lib:${PATH}"
 	
-	#this is a major cludge but only building with pcre support if SFCGAL is built - can't figure out how to do this without passing PCRE
-	#TODO: turn back on topology regression testing once topology issues resolved
 	CPPFLAGS="-I${PGPATH}/include -I${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include" \
 LDFLAGS="-L${PGPATH}/lib -L${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/lib" ./configure \
   --host=${MINGHOST} --with-xml2config=${PROJECTS}/libxml/rel-libxml2-${LIBXML_VER}w${OS_BUILD}${GCC_TYPE}/bin/xml2-config \
@@ -100,7 +108,7 @@ LDFLAGS="-L${PGPATH}/lib -L${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYP
   --with-libiconv=${PROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE} \
   --with-xsldir=${PROJECTS}/docbook/docbook-xsl-1.76.1 \
   --with-gui --with-gettext=no --with-sfcgal=${PROJECTS}/CGAL/rel-sfcgal-${SFCGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin/sfcgal-config \
-  --with-pcredir=${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}  --without-interrupt-tests
+  --with-pcredir=${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}
 elif [ "$POSTGIS_MAJOR_VERSION" == "2" ] ; then
 CPPFLAGS="-I${PGPATH}/include -I${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include" \
 LDFLAGS="-L${PGPATH}/lib -L${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/lib" ./configure \
@@ -126,38 +134,10 @@ CPPFLAGS="-I${PGPATH}/include  -I${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}
   --with-xsldir=${PROJECTS}/docbook/docbook-xsl-1.76.1 \
   --with-gettext=no
 fi;
-
-make clean
-
-##hack to get around boolean incompatibility
+#make clean
 ##hack to get around boolean incompatibility now only needed for 2.0 (no longer for 2.1)
 if [ "$POSTGIS_MINOR_VERSION" == "0" ] ; then
 cp ${MINGPROJECTS}/json-c/rel-${JSON_VER}w${OS_BUILD}${GCC_TYPE}/include/json/json_object.h.for_compile ${MINGPROJECTS}/json-c/rel-${JSON_VER}w${OS_BUILD}${GCC_TYPE}/include/json/json_object.h
 fi;
-make
-make install
-make check RUNTESTFLAGS=-v
+make && make install
 
-if [ "$MAKE_EXTENSION" == "1" ]; then
- export PGUSER=postgres
- #need to copy install files to EDB install (since not done by make install
- cd ${POSTGIS_SRC}
- echo "Postgis src dir is ${POSTGIS_SRC}"
- strip postgis/postgis-*.dll
- strip raster/rt_pg/rtpostgis-*.dll
- cp topology/*.dll ${PGPATHEDB}/lib
- cp postgis/postgis*.dll ${PGPATHEDB}/lib
- cp raster/rt_pg/rtpostgis-*.dll ${PGPATHEDB}/lib
- cp -r extensions/*/sql/* ${PGPATHEDB}/share/extension
- cp -r extensions/*/*.control ${PGPATHEDB}/share/extension
- 
- make check RUNTESTFLAGS=--extension
- if [ "$?" != "0" ]; then
-  exit $?
- fi
-fi
-
-if [ "$MAKE_GARDEN" == "1" ]; then
- export PGUSER=postgres
- make garden
-fi
---
+++ ci/winnie/package_postgis.sh
@@ -0,0 +1,197 @@
+#### $Id: package_postgis210.sh 10291 2012-09-15 05:37:23Z robe $
+### this script is used to zip up the compiled binaries
+## PostgreSQL, OS_BUILD denote the last build to be packaged
+## and are passed in by the jenkins job process
+## the scp to postgis website is done using Jenkins scp plugin as a final step
+###
+#export OS_BUILD=64
+#export PGPORT=8442
+#POSTGIS_MAJOR_VERSION=2
+#POSTGIS_MINOR_VERSION=1
+#POSTGIS_MICRO_VERSION=0dev
+#export GEOS_VER=3.4.0dev
+#export GDAL_VER=2.0.0
+#export OS_BUILD=32
+#export PROJ_VER=4.9.1
+#export GCC_TYPE=
+#export SFCGAL_VER=1.1.0
+#export PCRE_VER
+if [[ "${GCC_TYPE}" == *gcc48* ]] ; then
+	export PROJECTS=/projects
+	export MINGPROJECTS=/projects
+	export PATHOLD=$PATH
+else
+	export PROJECTS=/projects
+	export MINGPROJECTS=/projects
+	export PATHOLD=$PATH
+	#export JSON_VER=0.9
+fi;
+
+export PGHOST=localhost
+
+export PGUSER=postgres
+
+export PATHOLD=$PATH
+WEB=/home/www/postgis/htdocs
+DWN=${WEB}/download
+
+export PATHOLD="/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows:.:/bin:/include:/usr/local/bin:/c/ming${OS_BUILD}/svn"
+#export PG_VER=9.2beta2
+
+echo PATH BEFORE: $PATH
+
+export PGPATH=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}
+export PGPATHEDB=${PGPATH}edb
+export PROJSO=libproj-0.dll
+export POSTGIS_MINOR_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}
+export POSTGIS_MICRO_VER=${POSTGIS_MICRO_VERSION}
+
+if [[ "$POSTGIS_MICRO_VERSION"  == *SVN* || "$POSTGIS_MICRO_VERSION"  == *dev* ]] ; then
+	export POSTGIS_SRC=${PROJECTS}/postgis/branches/${POSTGIS_MINOR_VER}
+	export svnurl="http://svn.osgeo.org/postgis/branches/${POSTGIS_MINOR_VER}"
+else
+	#tagged version -- official release
+	export POSTGIS_SRC=${PROJECTS}/postgis/tags/${POSTGIS_MINOR_VER}.${POSTGIS_MICRO_VERSION}
+	export svnurl="http://svn.osgeo.org/postgis/tags/${POSTGIS_MINOR_VER}.${POSTGIS_MICRO_VERSION}"
+fi;
+
+if [[ "$POSTGIS_MINOR_VER"  == 2.2 ]] ; then
+	export svnurl="http://svn.osgeo.org/postgis/branches/trunk"
+fi;
+#export POSTGIS_SRC=${PROJECTS}/postgis/trunk
+#POSTGIS_SVN_REVISION=will_be_passed_in_by_bot
+export GDAL_DATA="${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/share/gdal"
+
+
+export REL_PGVER=${PG_VER//./} #strip the period
+
+
+export RELDIR=${PROJECTS}/postgis/builds/${POSTGIS_MINOR_VER}
+export RELVERDIR=postgis-pg${REL_PGVER}-binaries-${POSTGIS_MINOR_VER}.${POSTGIS_MICRO_VER}w${OS_BUILD}${GCC_TYPE}
+export PATH="${PATHOLD}:${PGPATH}/bin:${PGPATH}/lib"
+export PCRE_VER=8.33 #PATH="${PGPATH}/bin:${PGPATH}/lib:${MINGPROJECTS}/xsltproc:${MINGPROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/gtkw${OS_BUILD}/bin:${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE}/bin:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include:${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/bin:${PATH}"
+#echo PATH AFTER: $PATH
+outdir="${RELDIR}/${RELVERDIR}"
+package="${RELDIR}/${RELVERDIR}.zip"
+verfile="${RELDIR}/${RELVERDIR}/version.txt"
+#svnurl="http://svn.osgeo.org/postgis/trunk"
+rm -rf $outdir
+rm $package
+mkdir -p $outdir
+mkdir -p $outdir/share/contrib/postgis-${POSTGIS_MINOR_VER}
+mkdir -p $outdir/share/contrib/postgis-${POSTGIS_MINOR_VER}/proj
+mkdir -p $outdir/share/extension
+mkdir $outdir/bin
+mkdir $outdir/lib
+mkdir $outdir/bin/postgisgui
+mkdir $outdir/bin/postgisgui/share
+mkdir $outdir/bin/postgisgui/lib
+mkdir $outdir/utils
+cp ${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}/bin/*.dll  $outdir/bin/postgisgui
+# it seems 9.2 and 9.3 doesn't come with its own libiconv good grief
+# and trying to use their libiconv2.dll makes shp2pgsql crash
+if [[ "$PG_VER" == *9.2* || "$PG_VER" == *9.3* ]]; then
+	cp ${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/bin/*.dll  $outdir/bin 
+fi;
+cp ${PGPATHEDB}/bin/libpq.dll  $outdir/bin/postgisgui 
+#cp ${PGPATHEDB}/bin/libiconv2.dll  $outdir/bin/postgisgui 
+cp ${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/bin/libicon*.dll $outdir/bin/postgisgui
+cp ${PGPATHEDB}/bin/libintl.dll $outdir/bin/postgisgui 
+cp ${PGPATHEDB}/bin/ssleay32.dll $outdir/bin/postgisgui
+cp ${PGPATHEDB}/bin/libeay32.dll $outdir/bin/postgisgui
+
+cp /c/ming${OS_BUILD}${GCC_TYPE}/mingw${OS_BUILD}/bin/libstdc++-6.dll $outdir/bin
+cp /c/ming${OS_BUILD}${GCC_TYPE}/mingw${OS_BUILD}/bin/libgcc*.dll $outdir/bin
+cp -r ${PROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/bin/*.dll $outdir/bin/postgisgui
+cp -r ${PROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/etc $outdir/bin/postgisgui
+cp -r ${PROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/share/themes $outdir/bin/postgisgui/share
+cp -r ${PROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/lib/gtk-2.0 $outdir/bin/postgisgui/lib
+cp -r ${PROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/lib/*.dll $outdir/bin/postgisgui/lib
+cp -r ${PROJECTS}/gtkw${OS_BUILD}${GCC_TYPE}/lib/gdk-pixbuf-2.0 $outdir/bin/postgisgui/lib
+
+cp ${MINGPROJECTS}/proj/rel-${PROJ_VER}w${OS_BUILD}${GCC_TYPE}/share/proj/* $outdir/share/contrib/postgis-${POSTGIS_MINOR_VER}/proj
+cp ${MINGPROJECTS}/proj/rel-${PROJ_VER}w${OS_BUILD}${GCC_TYPE}/bin/*.dll $outdir/bin
+cp -p ${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE}/bin/*.dll $outdir/bin
+
+echo "POSTGIS: ${POSTGIS_MINOR_VER} r${POSTGIS_SVN_REVISION} http://postgis.net/source" > $verfile
+
+if [ "$POSTGIS_MAJOR_VERSION" == "2" ] ; then
+  ## only copy gdal components if 2+.  1.5 doesn't have raster support
+  cp -p ${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/bin/*.dll $outdir/bin
+  cp -rp  ${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/share/gdal $outdir/gdal-data
+  
+  if [ "$POSTGIS_MINOR_VERSION" > "0" ] ; then
+    ## only copy pagc standardizer components for 2.1+
+    cp -p ${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}/bin/libpcre-1*.dll $outdir/bin
+    cp -p ${PGPATH}/lib/address*.dll $outdir/lib
+    # cp -p ${PGPATH}/share/extension/address*.* $outdir/share/extension
+    # cp -p ${PGPATH}/share/extension/us-*.sql $outdir/share/extension
+  fi;
+fi;
+
+if [ -n "$SFCGAL_VER"  ]; then
+	## only copy cgal and sfcgal stuff if sfcgal is packaged
+	export CGAL_VER=4.2
+	export BOOST_VER=1.53.0
+	export BOOST_VER_WU=1_53_0
+	export GMP_VER=5.1.2
+	export MPFR_VER=3.1.2
+	echo "CGAL VERSION: ${CGAL_VER} http://www.cgal.org" >> $verfile
+	echo "Boost VERSION: ${BOOST_VER} http://www.boost.org" >> $verfile
+	echo "GMP VERSION: ${GMP_VER} https://gmplib.org" >> $verfile
+	echo "MPFR VERSION: ${MPFR_VER} http://www.mpfr.org" >> $verfile
+	
+	cp -p ${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin/*.dll $outdir/bin
+	cp -p ${PROJECTS}/CGAL/rel-sfcgal-${SFCGAL_VER}w${OS_BUILD}${GCC_TYPE}/lib/*.dll $outdir/bin
+fi;
+#cp ${MINGPROJECTS}/libxml/rel-libxml2-2.7.8w${OS_BUILD}/bin/*.dll  $outdir/bin/
+cp ${PGPATHEDB}/bin/libxml2-2.dll   $outdir/bin/
+
+cd ${POSTGIS_SRC}
+strip postgis/postgis-${POSTGIS_MINOR_VER}.dll
+strip raster/rt_pg/rtpostgis-${POSTGIS_MINOR_VER}.dll
+strip liblwgeom/.libs/*.dll
+
+cp postgis/postgis-${POSTGIS_MINOR_VER}.dll ${RELDIR}/${RELVERDIR}/lib
+cp topology/*.dll ${RELDIR}/${RELVERDIR}/lib
+cp raster/rt_pg/rtpostgis-${POSTGIS_MINOR_VER}.dll ${RELDIR}/${RELVERDIR}/lib
+cp doc/*_comments.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+cp postgis/*.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+cp raster/rt_pg/*.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+cp utils/*.pl ${RELDIR}/${RELVERDIR}/utils
+#add extras
+svn export "${svnurl}/extras" ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}/extras
+#cp raster/rt_pg/rtpostgis.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+#cp raster/rt_pg/rtpostgis_upgrade_20_minor.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+
+cp raster/loader/.libs/raster2pgsql.exe ${RELDIR}/${RELVERDIR}/bin
+cp liblwgeom/.libs/*.dll ${RELDIR}/${RELVERDIR}/bin
+cp loader/.libs/shp2pgsql.exe ${RELDIR}/${RELVERDIR}/bin
+cp loader/.libs/pgsql2shp.exe ${RELDIR}/${RELVERDIR}/bin
+cp loader/.libs/shp2pgsql-gui.exe ${RELDIR}/${RELVERDIR}/bin/postgisgui
+cp liblwgeom/.libs/*.dll ${RELDIR}/${RELVERDIR}/bin/postgisgui
+
+#shp2pgsql-gui now has dependency on geos (though in theory it shouldn't)
+cp -p ${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}/bin/*.dll ${RELDIR}/${RELVERDIR}/bin/postgisgui
+cp spatial_ref_sys.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+cp topology/topology.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+cp topology/topology_upgrade_*.sql ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+#cp topology/README* ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}
+#cp utils/* ${RELDIR}/${RELVERDIR}/utils
+#cp extras/* ${RELDIR}/${RELVERDIR}/share/contrib/postgis-${POSTGIS_MINOR_VER}/extras
+cp -r extensions/*/sql/* ${RELDIR}/${RELVERDIR}/share/extension
+cp -r extensions/*/*.control ${RELDIR}/${RELVERDIR}/share/extension
+#cp extensions/postgis_topology/sql/* ${RELDIR}/${RELVERDIR}/share/extension
+#cp extensions/postgis_topology/*.control ${RELDIR}/${RELVERDIR}/share/extension
+cp -r ${RELDIR}/packaging_notes/* ${RELDIR}/${RELVERDIR}/
+
+
+echo "GEOS VERSION: ${GEOS_VER} http://trac.osgeo.org/geos" >> $verfile
+echo "GDAL VERSION: ${GDAL_VER} http://trac.osgeo.org/gdal" >> $verfile
+echo "PROJ VERSION: ${PROJ_VER} http://trac.osgeo.org/proj" >> $verfile
+echo "PAGC ADDRESS STANDARDIZER: http://sourceforge.net/p/pagc/code/HEAD/tree/branches/sew-refactor/postgresql " >> $verfile
+cd ${RELDIR}
+zip -r $package ${RELVERDIR}
+#scp $package robe@www.refractions.net:${DWN}/${REL_PGVER}/buildbot/${RELVERDIR}.zip
+cp $package ${PROJECTS}/postgis/win_web/download/windows/pg${REL_PGVER}/buildbot
+cd ${POSTGIS_SRC}
