0309e04a3043e0c48d4011afc644ef9a7d5a165d
Regina Obe <lr@pcorp.us>
2015-08-21 05:09:18 +0000

    missed changes per #3247 highlighted.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@13966 b70326c6-7e19-0410-871a-916f4a2858ee

--- extras/tiger_geocoder/create_geocode.sh
+++ extras/tiger_geocoder/create_geocode.sh
@@ -12,7 +12,7 @@ ${PSQL_CMD} -d "${THEDB}" -f "${PGCONTRIB}/fuzzystrmatch.sql"
 ${PSQL_CMD} -d "${THEDB}" -c "CREATE SCHEMA tiger"
 #unremark this next line and edit if you want the search paths set as part of the install
 #${PSQL_CMD} -d "${THEDB}" -c "ALTER DATABASE ${THEDB} SET search_path=public, tiger;"
-#${PSQL_CMD} -d "${THEDB}" -f "tables/lookup_tables_2010.sql"
+#${PSQL_CMD} -d "${THEDB}" -f "tables/lookup_tables_2011.sql"
 ${PSQL_CMD} -d "${THEDB}" -c "CREATE SCHEMA tiger_data"
 ${PSQL_CMD} -d "${THEDB}" -f "create_geocode.sql"
 ${PSQL_CMD} -d "${THEDB}" -f "tiger_loader_2015.sql"
--- extras/tiger_geocoder/upgrade_geocoder.bat
+++ extras/tiger_geocoder/upgrade_geocoder.bat
@@ -3,13 +3,16 @@ set PGHOST=localhost
 set PGUSER=postgres
 set PGPASSWORD=yourpasswordhere
 set THEDB=geocoder
-set PGBIN=C:\Program Files\PostgreSQL\8.4\bin
-set PGCONTRIB=C:\Program Files\PostgreSQL\8.4\share\contrib
+set PGBIN=C:\Program Files\PostgreSQL\9.4\bin
+set PGCONTRIB=C:\Program Files\PostgreSQL\9.4\share\contrib
+
 "%PGBIN%\psql"  -d "%THEDB%" -f "upgrade_geocode.sql"
+
+REM "%PGBIN%\psql" -d "%THEDB%" -c "ALTER EXTENSION postgis_tiger_geocoder UPDATE;"
 
 REM unremark the loader line to update your loader scripts
 REM note this wipes out your custom settings in loader_* tables
-REM "%PGBIN%\psql"  -d "%THEDB%" -f "tiger_loader_2013.sql"
+REM "%PGBIN%\psql"  -d "%THEDB%" -f "tiger_loader_2015.sql"
 cd regress
 REM "%PGBIN%\psql"  -d "%THEDB%" -t -f regress.sql
 pause
