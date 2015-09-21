2c70f6381f07791ad0ed23d2fb91d47c86868fed
Daniel Baston <dbaston@gmail.com>
2015-09-21 14:42:07 -0400

    Add missing header

---
+++ liblwgeom/lwboundingcircle.h
@@ -0,0 +1,23 @@
+/**********************************************************************
+ *
+ * PostGIS - Spatial Types for PostgreSQL
+ * http://postgis.net
+ *
+ * Copyright 2015 Daniel Baston <dbaston@gmail.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public Licence. See the COPYING file.
+ *
+ **********************************************************************/
+
+#ifndef _LWBOUNDINGCIRCLE
+#define _LWBOUNDINGCIRCLE 1
+
+typedef struct {
+	POINT2D centre;
+	double radius;
+} LW_BOUNDINGCIRCLE;
+
+int lwgeom_calculate_mbc(const LWGEOM* g, LW_BOUNDINGCIRCLE* result);
+
+#endif
