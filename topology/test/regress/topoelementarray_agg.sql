0d0cd2612463ba81180a3b7ce07055b49d2015b7
Sandro Santilli <strk@keybit.net>
2015-11-20 19:00:04 +0000

    Add test for (non-literal) NULL TopoElementArray
    git-svn-id: http://svn.osgeo.org/postgis/trunk@14410 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/regress/topoelementarray_agg.sql
+++ topology/test/regress/topoelementarray_agg.sql
@@ -1,8 +1,13 @@
 set client_min_messages to WARNING;
 
-SELECT 1,topology.TopoElementArray_agg('{1,3}'::topology.TopoElement);
+SELECT 1, topology.TopoElementArray_agg('{1,3}'::topology.TopoElement);
 
 SELECT 2, topology.TopoElementArray_agg(e) from (
- select '{4,1}'::topology.TopoElement as e union
- select '{5,2}'
-) as foo;
+  VALUES ('{4,1}'::TopoElement),
+         ('{5,2}')
+) as foo(e);
+
+-- See https://trac.osgeo.org/postgis/ticket/3372
+SELECT 3, topology.TopoElementArray_agg(e) from (
+  SELECT '{4,1}'::TopoElement WHERE false
+) as foo(e);
