4b3a6571fa851574f63e0abf7b27fc10adee8745
Sandro Santilli <strk@keybit.net>
2015-11-20 09:49:31 +0000

    Allow existance of databases including name of target test db
    .. otherwise you get a weird "database postgis_reg already exists"
    when only having a database like "postgis_reg_22" (for example) ..
    
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.1@14403 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/run_test.pl
+++ regress/run_test.pl
@@ -191,7 +191,7 @@ print "TMPDIR is $TMPDIR\n";
 # Prepare the database
 ##################################################################
 
-my @dblist = grep(/$DB/, split(/\n/, `psql -Xl`));
+my @dblist = grep(/\b$DB\b/, split(/\n/, `psql -Xl`));
 my $dbcount = @dblist;
 
 if ( $dbcount == 0 )
