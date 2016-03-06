e1e448f7948b9aeae68fe07b880482f8e1e5f6a0
Regina Obe <lr@pcorp.us>
2016-03-06 00:30:01 +0000

    Use Tomvantilburg suggestion and just hard-code convex='false' for all IndexedFaceSets. Corrects the output of wrongly rendered without affecting true convex References #3435 for PostGIS 2.2 (fix).
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.2@14755 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwout_x3d.c
+++ liblwgeom/lwout_x3d.c
@@ -445,7 +445,7 @@ asx3d3_multi_buf(const LWCOLLECTION *col, char *srs, char *output, int precision
             break;
         case MULTIPOLYGONTYPE:
             x3dtype = "IndexedFaceSet";
-            ptr += sprintf(ptr, "<%s %s coordIndex='", x3dtype, defid);
+            ptr += sprintf(ptr, "<%s %s convex='false' coordIndex='", x3dtype, defid);
             ptr += asx3d3_mpoly_coordindex((const LWMPOLY *)col, ptr);
             ptr += sprintf(ptr, "'>");
             break;
@@ -511,8 +511,8 @@ asx3d3_psurface_size(const LWPSURFACE *psur, char *srs, int precision, int opts,
 	size_t size;
 	size_t defidlen = strlen(defid);
 
-	if ( X3D_USE_GEOCOORDS(opts) ) size = sizeof("<IndexedFaceSet coordIndex=''><GeoCoordinate geoSystem='\"GD\" \"WE\" \"longitude_first\"' point='' />") + defidlen;
-	else size = sizeof("<IndexedFaceSet coordIndex=''><Coordinate point='' />") + defidlen;
+	if ( X3D_USE_GEOCOORDS(opts) ) size = sizeof("<IndexedFaceSet convex='false' coordIndex=''><GeoCoordinate geoSystem='\"GD\" \"WE\" \"longitude_first\"' point='' />") + defidlen;
+	else size = sizeof("<IndexedFaceSet convex='false' coordIndex=''><Coordinate point='' />") + defidlen;
 	
 
 	for (i=0; i<psur->ngeoms; i++)
@@ -540,7 +540,7 @@ asx3d3_psurface_buf(const LWPSURFACE *psur, char *srs, char *output, int precisi
 	ptr = output;
 
 	/* Open outmost tag */
-	ptr += sprintf(ptr, "<IndexedFaceSet %s coordIndex='",defid);
+	ptr += sprintf(ptr, "<IndexedFaceSet convex='false' %s coordIndex='",defid);
 
 	j = 0;
 	for (i=0; i<psur->ngeoms; i++)
