0dcd5b0287eca8804c6ef457b296eb120d5b9e76
Regina Obe <lr@pcorp.us>
2016-03-06 08:06:21 +0000

    fix compile error introduced in X3D commeit references #3435
    git-svn-id: http://svn.osgeo.org/postgis/trunk@14758 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwout_x3d.c
+++ liblwgeom/lwout_x3d.c
@@ -1,7 +1,8 @@
-**********************************************************************
+/**********************************************************************
  *
  * PostGIS - Spatial Types for PostgreSQL
  * http://postgis.net
+ *
  * PostGIS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
