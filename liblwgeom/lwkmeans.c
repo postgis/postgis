postgis/postgis

commit 392263f4b12b810dc7accb91f5a2d545e2bc6f1c

#3523, kmeans compile warnings
git-svn-id: http://svn.osgeo.org/postgis/trunk@14890 b70326c6-7e19-0410-871a-916f4a2858ee

liblwgeom/lwkmeans.c
@@ -81,13 +81,13 @@ lwgeom_cluster_2d_kmeans(const LWGEOM **geoms, int ngeoms, int k)
 	LWGEOM **centroids;
 	POINT2D *centers_raw;
 	const POINT2D *cp;
-	POINT2D min, max;
+	POINT2D min = { FLT_MAX, FLT_MAX };
+	POINT2D max = { FLT_MIN, FLT_MIN };
 	double dx, dy;
 	kmeans_config config;
 	kmeans_result result;
 	int *seen;
 	int sidx = 0;
-	int initialized = 0;
 
 	assert(k>0);
 	assert(ngeoms>0);
@@ -157,19 +157,10 @@ lwgeom_cluster_2d_kmeans(const LWGEOM **geoms, int ngeoms, int k)
 		config.objs[i] = (Pointer)cp;
 
 		/* Since we're already here, let's calculate the extrema of the set */
-		if (!initialized)
-		{
-			initialized = 1;
-			min = *cp;
-			max = *cp;
-		}
-		else
-		{
-			if (cp->x < min.x) min.x = cp->x;
-			if (cp->y < min.y) min.y = cp->y;
-			if (cp->x > max.x) max.x = cp->x;
-			if (cp->y > max.y) max.y = cp->y;
-		}
+		if (cp->x < min.x) min.x = cp->x;
+		if (cp->y < min.y) min.y = cp->y;
+		if (cp->x > max.x) max.x = cp->x;
+		if (cp->y > max.y) max.y = cp->y;
 	}
 
 	/*

commit 73517d81acea9ee67a4497ffa439b4038e739e7e

Fix logic error introduced in r14890
git-svn-id: http://svn.osgeo.org/postgis/trunk@14893 b70326c6-7e19-0410-871a-916f4a2858ee

liblwgeom/lwkmeans.c
@@ -81,8 +81,8 @@ lwgeom_cluster_2d_kmeans(const LWGEOM **geoms, int ngeoms, int k)
 	LWGEOM **centroids;
 	POINT2D *centers_raw;
 	const POINT2D *cp;
-	POINT2D min = { DBL_MAX, DBL_MAX };
-	POINT2D max = { DBL_MIN, DBL_MIN };
+	POINT2D min = { DBL_MAX,   DBL_MAX };
+	POINT2D max = { -DBL_MAX, -DBL_MAX };
 	double dx, dy;
 	kmeans_config config;
 	kmeans_result result;