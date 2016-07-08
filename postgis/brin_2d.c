82f689be4d9209a2d83eb9fccc2b12004d4c2140
Giuseppe Broccolo <giuseppe.broccolo@2ndquadrant.it>
2016-07-08 12:51:19 +0000

    Revert "Add support for empty 2D geometries - it's a prototype, need more testing" The idea is to add a new branch, "brin_support_dev", rebased on the original commit and used for the changes suggested during the review phase by the PostGIS team.
    This reverts commit 8ad5c7500002828fa376f3a1eea1cfc407a777f0.

--- postgis/brin_2d.c
+++ postgis/brin_2d.c
@@ -11,23 +11,14 @@
  * FunctionCallInvoke machinery for each heap tuple.
  */
 
-static FmgrInfo *inclusion_get_procinfo(BrinDesc *bdesc, uint16 attno,
-uint16 procnum);
-
-static bool isempty(Datum the_datum);
-
 PG_FUNCTION_INFO_V1(geom2d_brin_inclusion_add_value);
 Datum
 geom2d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
 {
-        BrinDesc   *bdesc = (BrinDesc *) PG_GETARG_POINTER(0);
 	BrinValues *column = (BrinValues *) PG_GETARG_POINTER(1);
-	Datum      newval = PG_GETARG_DATUM(2);
-	bool	   isnull = PG_GETARG_BOOL(3);
-        Oid        colloid = PG_GET_COLLATION();
-        FmgrInfo   *finfo;
-        AttrNumber attno;
-	BOX2DF     box_geom, *box_key;
+	Datum newval = PG_GETARG_DATUM(2);
+	bool		isnull = PG_GETARG_BOOL(3);
+	BOX2DF box_geom, *box_key;
 
 	/*
 	 * If the new value is null, we record that we saw it if it's the first
@@ -42,32 +33,8 @@ geom2d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
 		PG_RETURN_BOOL(true);
 	}
 
-
-        /*
-         * If the opclass supports the concept of empty values, test the passed
-         * new value for emptiness; if it returns true, we need to set the
-         * "contains empty" flag in the element (unless already set).
-         */
-        if (gserialized_datum_get_box2df_p(newval, &box_geom) == LW_FAILURE) {
-             if (isempty(newval)) {
-                  attno = column->bv_attno;
-  
-                  finfo = inclusion_get_procinfo(bdesc, attno, PROCNUM_EMPTY);
-                  if (finfo != NULL && DatumGetBool(FunctionCall1Coll(finfo, colloid, newval)))
-                  {
-                          if (!DatumGetBool(column->bv_values[INCLUSION_CONTAINS_EMPTY]))
-                          {
-                                 column->bv_values[INCLUSION_CONTAINS_EMPTY] = BoolGetDatum(true);
-                                 PG_RETURN_BOOL(true);
-                          }
-
-                          PG_RETURN_BOOL(false);
-                  }
-             } else {
-                  /* check other cases where it is not possible to retrieve a box*/
-                  elog(ERROR, "Error while extracting the box2df from the geom");
-             }
-        }
+	if (gserialized_datum_get_box2df_p(newval, &box_geom) == LW_FAILURE)
+		elog(ERROR, "Error while extracting the box2df from the geom");
 
 	/* if the recorded value is null, we just need to store the box2df */
 	if (column->bv_allnulls)
@@ -93,59 +60,4 @@ geom2d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
 	box_key->ymax = Max(box_key->ymax, box_geom.ymax);
 
 	PG_RETURN_BOOL(false);
-}
-
-/*
- * Cache and return inclusion opclass support procedure
- *
- * Return the procedure corresponding to the given function support number
- * or null if it is not exists.
- */
-static FmgrInfo *
-inclusion_get_procinfo(BrinDesc *bdesc, uint16 attno, uint16 procnum)
-{
-        InclusionOpaque *opaque;
-        uint16          basenum = procnum - PROCNUM_BASE;
-
-        /*
-         * We cache these in the opaque struct, to avoid repetitive syscache
-         * lookups.
-         */
-        opaque = (InclusionOpaque *) bdesc->bd_info[attno - 1]->oi_opaque;
-
-        /*
-         * If we already searched for this proc and didn't find it, don't bother
-         * searching again.
-         */
-        if (opaque->extra_proc_missing[basenum])
-                return NULL;
-
-        if (opaque->extra_procinfos[basenum].fn_oid == InvalidOid)
-        {
-                if (RegProcedureIsValid(index_getprocid(bdesc->bd_index, attno,
-                                                                                                procnum)))
-                {
-                        fmgr_info_copy(&opaque->extra_procinfos[basenum],
-                                                   index_getprocinfo(bdesc->bd_index, attno, procnum),
-                                                   bdesc->bd_context);
-                }
-                else
-                {
-                        opaque->extra_proc_missing[basenum] = true;
-                        return NULL;
-                }
-        }
-
-        return &opaque->extra_procinfos[basenum];
-}
-
-static bool
-isempty(Datum the_datum)
-{
-        GSERIALIZED *geom = (GSERIALIZED*)PG_DETOAST_DATUM(the_datum);
-        LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
-        bool empty = lwgeom_is_empty(lwgeom);
-
-        lwgeom_free(lwgeom);
-        return empty;
 }
--- postgis/postgis_brin.h
+++ postgis/postgis_brin.h
@@ -12,24 +12,11 @@
 #include <math.h>
 #include <float.h>
 #include <string.h>
-#include "access/genam.h"
-#include "access/stratnum.h"
 #include "access/brin_tuple.h"
 #include "utils/datum.h"
 #include "gserialized_gist.h"
 
-#define	INCLUSION_MAX_PROCNUMS	        4	/* maximum support procs we need */
-#define	PROCNUM_BASE	               11
-
-#define INCLUSION_UNION			0
+#define INCLUSION_UNION				0
 #define INCLUSION_UNMERGEABLE		1
 #define INCLUSION_CONTAINS_EMPTY	2
-#define PROCNUM_EMPTY                  14      /* optional support function to manage empty elements */
 
-typedef struct InclusionOpaque
-{
-	FmgrInfo	extra_procinfos[INCLUSION_MAX_PROCNUMS];
-	bool		extra_proc_missing[INCLUSION_MAX_PROCNUMS];
-	Oid		cached_subtype;
-	FmgrInfo	strategy_procinfos[RTMaxStrategyNumber];
-} InclusionOpaque;
