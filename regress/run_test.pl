2ca181c00e64f749f0ef10f6ac376a126244b6be
Sandro Santilli <strk@kbt.io>
2016-09-07 18:36:05 +0000

    Do not require expecting DISCARD ALL
    .. actually it may be a good idea to run it always after each test ..
    
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.2@15082 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/run_test.pl
+++ regress/run_test.pl
@@ -712,7 +712,7 @@ sub run_simple_test
 	@lines = grep(!/^(INSERT|DELETE|UPDATE|SELECT|COPY|DO)/, @lines);
 	@lines = grep(!/^(CONTEXT|RESET|ANALYZE)/, @lines);
 	@lines = grep(!/^(DROP|CREATE|ALTER|VACUUM)/, @lines);
-	@lines = grep(!/^(LOG|SET|TRUNCATE)/, @lines);
+	@lines = grep(!/^(LOG|SET|TRUNCATE|DISCARD)/, @lines);
 	@lines = grep(!/^LINE \d/, @lines);
 	@lines = grep(!/^\s+$/, @lines);
 
