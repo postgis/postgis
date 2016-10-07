1d74af038d5b86da0c82370409434bf3bee3389a
Sandro Santilli <strk@kbt.io>
2016-10-07 19:49:32 +0000

    Properly signal check errors, stopping on first error
    git-svn-id: http://svn.osgeo.org/postgis/trunk@15188 b70326c6-7e19-0410-871a-916f4a2858ee

--- utils/check_all_upgrades.sh
+++ utils/check_all_upgrades.sh
@@ -8,7 +8,6 @@ to_version="$1"
 
 BUILDDIR=$PWD
 EXTDIR=`pg_config --sharedir`/extension/
-RET=0
 
 cd $EXTDIR
 'ls' postgis--* | grep -v -- '--.*--' |
@@ -18,17 +17,8 @@ sed 's/^postgis--\(.*\)\.sql/\1/' | while read fname; do
   if test -e postgis--${UPGRADE_PATH}.sql; then
     echo "Testing upgrade $UPGRADE_PATH"
     export RUNTESTFLAGS="-v --extension --upgrade-path=${UPGRADE_PATH}"
-    make -C ${BUILDDIR}/regress check
-    ret=$?
-    echo "UPGRADE TEST EXIT CODE: $ret"
-    if test $ret -ne 0; then
-      cat /tmp/pgis_reg/regress_log
-    fi
-    RET=$((RET+$ret))
-    dropdb postgis_reg
+    make -C ${BUILDDIR}/regress check || exit 1
   else
     echo "Missing script for $UPGRADE_PATH upgrade" >&2
   fi
 done
-
-exit $RET
