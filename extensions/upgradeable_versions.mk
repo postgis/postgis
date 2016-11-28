8a4a58409601e721356c95cc5ca84d91a0b0c376
Regina Obe <lr@pcorp.us>
2016-11-28 15:10:35 +0000

    More minor corrections to HOW_TO_RELEASE, bump version to 2.3.2dev
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.3@15265 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -22,7 +22,8 @@ UPGRADEABLE_VERSIONS = \
 	2.2.2 \
 	2.2.3 \
 	2.2.4 \
-	2.3.0
+	2.3.0 \
+	2.3.1
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
