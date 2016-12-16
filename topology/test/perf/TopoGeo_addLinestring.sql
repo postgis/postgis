84da02a2900095538c637f3670b2e77cf107ea71
Sandro Santilli <strk@kbt.io>
2016-12-16 18:55:39 +0000

    Fully qualify performance test script
    ... helps running it from single-mode postgres
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@15275 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/perf/TopoGeo_addLinestring.sql
+++ topology/test/perf/TopoGeo_addLinestring.sql
@@ -1,8 +1,8 @@
-SELECT DropTopology('topoperf');
-SELECT CreateTopology('topoperf');
+SELECT topology.DropTopology('topoperf');
+SELECT topology.CreateTopology('topoperf');
 \timing
 SELECT count(*) FROM (
-  SELECT TopoGeo_addLinestring('topoperf', g) FROM (
+  SELECT topology.TopoGeo_addLinestring('topoperf', g) FROM (
     SELECT ST_ExteriorRing(ST_Buffer(ST_MakePoint(x, y), 10, 10)) g FROM
       generate_series(-15,15,5) x,
       generate_series(-15,15,5) y
