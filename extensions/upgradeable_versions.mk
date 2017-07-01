a8467f716afd7d7012553e0034f27cdc8008bea7
Regina Obe <lr@pcorp.us>
2017-07-01 06:49:42 +0000

    bump to 2.3.4dev
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.3@15474 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -25,7 +25,8 @@ UPGRADEABLE_VERSIONS = \
 	2.2.5 \
 	2.3.0 \
 	2.3.1 \
-    2.3.2
+  2.3.2 \
+	2.3.3
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
