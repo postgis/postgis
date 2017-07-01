b4d03f0b1b3f340c2cddbdb50fc848d370f9d0f0
Regina Obe <lr@pcorp.us>
2017-07-01 04:01:25 +0000

    Update news to include 2.3 and add missing upgrade paths
    git-svn-id: http://svn.osgeo.org/postgis/trunk@15471 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -22,8 +22,11 @@ UPGRADEABLE_VERSIONS = \
 	2.2.2 \
 	2.2.3 \
 	2.2.4 \
+	2.2.5 \
 	2.3.0 \
-	2.3.1
+	2.3.1 \
+	2.3.2 \
+	2.3.3
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
