bb2b85a263efacc513473bf1bb97af16ded69ae9
Even Rouault <even.rouault@spatialys.com>
2017-07-02 17:44:28 +0200

    Add fuzzers infrastructure

---
+++ fuzzers/build_google_oss_fuzzers.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+if [ "$SRC" == "" ]; then
+    echo "SRC env var not defined"
+    exit 1
+fi
+
+if [ "$OUT" == "" ]; then
+    echo "OUT env var not defined"
+    exit 1
+fi
+
+if [ "$CXX" == "" ]; then
+    echo "CXX env var not defined"
+    exit 1
+fi
+
+SRC_DIR=$(dirname $0)/..
+
+build_fuzzer()
+{
+    fuzzerName=$1
+    sourceFilename=$2
+    shift
+    shift
+    echo "Building fuzzer $fuzzerName"
+    $CXX $CXXFLAGS -std=c++11 -I$SRC_DIR/liblwgeom \
+        $sourceFilename $* -o $OUT/$fuzzerName \
+        -lFuzzingEngine -lstdc++ $SRC_DIR/liblwgeom/.libs/liblwgeom.a
+}
+
+fuzzerFiles=$(dirname $0)/*.cpp
+for F in $fuzzerFiles; do
+    fuzzerName=$(basename $F .cpp)
+    build_fuzzer $fuzzerName $F
+done
+
+cp $(dirname $0)/*.dict $(dirname $0)/*.options $OUT/
