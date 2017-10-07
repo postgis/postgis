c7725e150da135db1af50ff9a1c9659a0bb9a3cb
Sandro Santilli <strk@kbt.io>
2017-10-07 13:39:45 +0000

    Keep running on failure, report all pass/fail/skip at the end
    git-svn-id: http://svn.osgeo.org/postgis/trunk@15930 b70326c6-7e19-0410-871a-916f4a2858ee

--- utils/check_all_upgrades.sh
+++ utils/check_all_upgrades.sh
@@ -10,18 +10,23 @@ BUILDDIR=$PWD
 EXTDIR=`pg_config --sharedir`/extension/
 
 cd $EXTDIR
-'ls' postgis--* | grep -v -- '--.*--' |
-sed 's/^postgis--\(.*\)\.sql/\1/' | while read fname; do
+failures=0
+files=`'ls' postgis--* | grep -v -- '--.*--' | sed 's/^postgis--\(.*\)\.sql/\1/'`
+for fname in $files; do
   from_version="$fname"
   UPGRADE_PATH="${from_version}--${to_version}"
   if test -e postgis--${UPGRADE_PATH}.sql; then
     echo "Testing upgrade $UPGRADE_PATH"
     export RUNTESTFLAGS="-v --extension --upgrade-path=${UPGRADE_PATH}"
-    make -C ${BUILDDIR}/regress check || {
-      echo "Upgrade $UPGRADE_PATH failed" >&2
-      exit 1
+    make -C ${BUILDDIR}/regress check && {
+      echo "PASS: upgrade $UPGRADE_PATH"
+    } || {
+      echo "FAIL: upgrade $UPGRADE_PATH"
+      failures=$((failures+1))
     }
   else
-    echo "Missing script for $UPGRADE_PATH upgrade" >&2
+    echo "SKIP: upgrade $UPGRADE_PATH is missing"
   fi
 done
+
+exit $failures
