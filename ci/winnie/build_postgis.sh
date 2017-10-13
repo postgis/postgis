3f173362fca9eebc74452a940806714957360e3f
Regina Obe <lr@pcorp.us>
2017-10-13 05:40:27 +0000

    Fix winnie build, package, and regress scripts to copy from mingw .sql from mingw install location Closes #3901
    git-svn-id: http://svn.osgeo.org/postgis/trunk@15979 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/winnie/build_postgis.sh
+++ ci/winnie/build_postgis.sh
@@ -2,7 +2,7 @@
 set -e
 #### $Id:build_postgis210.sh 10208 2012-08-30 05:38:22Z robe $
 #all these get passed in by jenkins
-#export OS_BUILD=64 
+#export OS_BUILD=64
 #export PG_VER=9.2beta2
 #export PGHOST=localhost
 #export PGPORT=8442
@@ -61,7 +61,7 @@ fi;
 
 #export POSTGIS_SRC=${PROJECTS}/postgis/trunk
 export GDAL_DATA="${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/share/gdal"
-# export REL_PGVER=$(echo $PG_VER | tr '.' '') 
+# export REL_PGVER=$(echo $PG_VER | tr '.' '')
 # echo $REL_PGVER
 # export RELDIR=${MINGPROJECTS}/postgis/builds/postgis-21
 # export RELVERDIR=postgis-pg${REL_PGVER}-binaries-${POSTGIS_MICRO_VER}w64
@@ -102,7 +102,7 @@ if [ -n "$SFCGAL_VER" ]; then
 	#BOOST_VER_WU=1_49_0
 	export BOOST_VER_WU=1_53_0
 	export PATH="${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/boost/rel-${BOOST_VER_WU}w${OS_BUILD}${GCC_TYPE}/lib:${PATH}"
-	
+
 CPPFLAGS="-I${PGPATH}/include -I${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include" \
 LDFLAGS="-L${PGPATH}/lib -L${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/lib" ./configure \
   --host=${MINGHOST} --with-xml2config=${PROJECTS}/libxml/rel-libxml2-${LIBXML_VER}w${OS_BUILD}${GCC_TYPE}/bin/xml2-config \
@@ -143,6 +143,7 @@ CPPFLAGS="-I${PGPATH}/include  -I${MINGPROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}
   --with-xsldir=${PROJECTS}/docbook/docbook-xsl-1.76.1 \
   --with-gettext=no
 fi;
+
 #make clean
 ##hack to get around boolean incompatibility now only needed for 2.0 (no longer for 2.1)
 if [ "$POSTGIS_MINOR_VERSION" == "0" ] ; then
@@ -152,6 +153,8 @@ fi;
 
 #patch liblwgeom generated make to get rid of dynamic linking
 sed -i 's/LDFLAGS += -no-undefined//g' liblwgeom/Makefile
+
+make uninstall
 
 make && make install
 
