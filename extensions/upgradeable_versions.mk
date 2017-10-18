fec5e14db693b3e33adf39a9cbf67d90d2123598
Paul Ramsey <pramsey@cleverelephant.ca>
2017-10-18 18:19:58 +0000

    Push versions to dev
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.3@16010 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -26,7 +26,8 @@ UPGRADEABLE_VERSIONS = \
 	2.3.0 \
 	2.3.1 \
 	2.3.2 \
-	2.3.3
+	2.3.3 \
+    2.3.4
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
