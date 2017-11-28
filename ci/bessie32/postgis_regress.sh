501efc3d52a835f63369db1278b4f16b863627b1
Regina Obe <lr@pcorp.us>
2017-11-28 22:53:15 +0000

    create script for bessie32 duplicate of bessie for now, but may change in future
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16121 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ ci/bessie32/postgis_regress.sh
@@ -0,0 +1,8 @@
+export PATH=${PATH}:/usr/local:/usr/local/lib:/usr/local/bin
+sh autogen.sh
+./configure --with-projdir=/usr/local --with-libiconv=/usr/local --without-interrupt-tests
+make clean
+make
+export PGUSER=postgres
+export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${TAG}
+make check
