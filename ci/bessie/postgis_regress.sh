343aac8f56cecdaf811c53989c731e7dd19da83d
Regina Obe <lr@pcorp.us>
2017-11-29 22:53:00 +0000

    revise bessie scripts to make install and then test extension install
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16126 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/bessie/postgis_regress.sh
+++ ci/bessie/postgis_regress.sh
@@ -1,8 +1,11 @@
+#BRANCH is passed in via jenkins which is set via svn hook
 export PATH=${PATH}:/usr/local:/usr/local/lib:/usr/local/bin
 sh autogen.sh
 ./configure --with-projdir=/usr/local --with-libiconv=/usr/local --without-interrupt-tests
 make clean
 make
 export PGUSER=postgres
-export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${TAG}
+export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${BRANCH}
 make check
+sudo make install
+make check RUNTESTFLAGS=--extension
--- ci/bessie32/postgis_regress.sh
+++ ci/bessie32/postgis_regress.sh
@@ -4,5 +4,7 @@ sh autogen.sh
 make clean
 make
 export PGUSER=postgres
-export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${TAG}
+export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${BRANCH}
 make check
+sudo make install
+make check RUNTESTFLAGS=--extension
