65d1597d570ee753112ea7a363779e2c99825f26
Regina Obe <lr@pcorp.us>
2017-12-11 21:14:31 +0000

    force drop of postgis_reg database if it exists.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16144 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/bessie/postgis_regress.sh
+++ ci/bessie/postgis_regress.sh
@@ -6,6 +6,7 @@ make clean
 make
 export PGUSER=postgres
 export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${BRANCH}
+psql -c "DROP DATABASE IF EXISTS postgis_reg;"
 make check
 sudo make install
 make check RUNTESTFLAGS=--extension
