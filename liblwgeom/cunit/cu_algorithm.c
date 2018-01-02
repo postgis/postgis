b48024e06106c74d9abd71aab09ee2fb6ef90f94
Darafei Praliaskouski <me@komzpa.net>
2018-01-03 00:16:49 +0300

    Update cu_algorithm.c

--- liblwgeom/cunit/cu_algorithm.c
+++ liblwgeom/cunit/cu_algorithm.c
@@ -1209,7 +1209,7 @@ static void test_median_robustness(void)
 	do_median_test("MULTIPOINT ((1 -1 3 1), (1 0 2 7), (2 1 1 2), (2 1 1 -1))", NULL, LW_TRUE, 1000);
 	do_median_test("MULTIPOINT ((1 -1 3 1), (1 0 2 7), (2 1 1 2), (2 1 1 -1))", NULL, LW_FALSE, 1000);
 	/* Weightless input is empty */
-	do_median_test("MULTIPOINT ((0 -1 0 0), (0 0 0 0), (0 0 0 0), (0 1 0 0))", "POINT EMPTY", LW_FALSE, 1000);
+	do_median_test("MULTIPOINT ((0 -1 0 0), (0 0 0 0), (0 0 0 0), (0 1 0 0))", NULL, LW_FALSE, 1000);
 	do_median_test("MULTIPOINT ((0 -1 0 0), (0 0 0 0), (0 0 0 0), (0 1 0 0))", NULL, LW_TRUE, 1000);
 	/* Negative weight won't converge */
 	do_median_test("MULTIPOINT ((0 -1 0 -1), (0 0 0 -1), (0 1 0 -1))", NULL, LW_FALSE, 1000);
