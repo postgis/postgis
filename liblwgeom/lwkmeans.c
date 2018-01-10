073b5ffb114893f93b63fbc7deed370aa6b96eca
Darafei Praliaskouski <komzpa@gojuno.com>
2018-01-10 17:01:34 +0300

    get rid of some more variables

--- liblwgeom/lwkmeans.c
+++ liblwgeom/lwkmeans.c
@@ -15,8 +15,6 @@
  */
 #define KMEANS_MAX_ITERATIONS 1000
 
-typedef enum { KMEANS_OK, KMEANS_EXCEEDED_MAX_ITERATIONS, KMEANS_ERROR } kmeans_result;
-
 typedef struct kmeans_config
 {
 	/* An array of objects to be analyzed. User allocates this array */
@@ -41,15 +39,132 @@ typedef struct kmeans_config
 	/* Number of means we are calculating, length of preceding array */
 	unsigned int k;
 
-	/* Iteration counter */
-	unsigned int total_iterations;
-
 	/* Array to fill in with cluster numbers. User allocates and frees. */
 	int* clusters;
 
 } kmeans_config;
 
-kmeans_result kmeans(kmeans_config* config);
+static void
+update_r(kmeans_config* config)
+{
+	unsigned int i;
+	assert(config->objs != NULL);
+	assert(config->num_objs > 0);
+	assert(config->centers);
+	assert(config->clusters);
+	for (i = 0; i < config->num_objs; i++)
+	{
+		double distance, curr_distance;
+		int cluster, curr_cluster;
+		POINT2D* obj;
+
+		obj = config->objs[i];
+
+		/*
+		 * Don't try to cluster NULL objects, just add them
+		 * to the "unclusterable cluster"
+		 */
+		if (!obj)
+		{
+			config->clusters[i] = KMEANS_NULL_CLUSTER;
+			continue;
+		}
+
+		/* Initialize with distance to first cluster */
+		curr_distance = distance2d_sqr_pt_pt(obj, config->centers[0]);
+		curr_cluster = 0;
+
+		/* Check all other cluster centers and find the nearest */
+		for (cluster = 1; cluster < config->k; cluster++)
+		{
+			distance = distance2d_sqr_pt_pt(obj, config->centers[cluster]);
+			if (distance < curr_distance)
+			{
+				curr_distance = distance;
+				curr_cluster = cluster;
+			}
+		}
+
+		/* Store the nearest cluster this object is in */
+		config->clusters[i] = curr_cluster;
+	}
+}
+
+static void
+update_means(kmeans_config* config)
+{
+	unsigned int i;
+	unsigned int* weights;
+
+	weights = lwalloc(sizeof(int) * config->k);
+	memset(weights, 0, sizeof(int) * config->k);
+
+	for (i = 0; i < config->k; i++)
+	{
+		config->centers[i]->x = 0.0;
+		config->centers[i]->y = 0.0;
+	}
+	for (i = 0; i < config->num_objs; i++)
+	{
+		config->centers[config->clusters[i]]->x += config->objs[i]->x;
+		config->centers[config->clusters[i]]->y += config->objs[i]->y;
+		weights[config->clusters[i]] += 1;
+	}
+	for (i = 0; i < config->k; i++)
+	{
+		config->centers[i]->x /= weights[i];
+		config->centers[i]->y /= weights[i];
+	}
+	lwfree(weights);
+}
+
+static int
+kmeans(kmeans_config* config)
+{
+	unsigned int i = 0;
+	int* clusters_last;
+	int converged = LW_FALSE;
+	size_t clusters_sz = sizeof(int) * config->num_objs;
+
+	assert(config);
+	assert(config->objs);
+	assert(config->num_objs);
+	assert(config->centers);
+	assert(config->k);
+	assert(config->clusters);
+	assert(config->k <= config->num_objs);
+
+	/* Zero out cluster numbers, just in case user forgets */
+	memset(config->clusters, 0, clusters_sz);
+
+	/*
+	 * Previous cluster state array. At this time, r doesn't mean anything
+	 * but it's ok
+	 */
+	clusters_last = lwalloc(clusters_sz);
+
+	for (i = 0; i < KMEANS_MAX_ITERATIONS && !converged; i++)
+	{
+		LW_ON_INTERRUPT(break);
+
+		/* Store the previous state of the clustering */
+		memcpy(clusters_last, config->clusters, clusters_sz);
+
+		update_r(config);
+		update_means(config);
+
+		/*
+		 * if all the cluster numbers are unchanged since last time,
+		 * we are at a stable solution, so we can stop here
+		 */
+		converged = memcmp(clusters_last, config->clusters, clusters_sz) == 0;
+	}
+
+	lwfree(clusters_last);
+	if (!converged)
+		lwerror("%s did not converge after %d iterations", __func__, i);
+	return converged;
+}
 
 int*
 lwgeom_cluster_2d_kmeans(const LWGEOM** geoms, int ngeoms, int k)
@@ -61,7 +176,7 @@ lwgeom_cluster_2d_kmeans(const LWGEOM** geoms, int ngeoms, int k)
 	double* distances;
 	const POINT2D* cp;
 	kmeans_config config;
-	kmeans_result result;
+	int result = LW_FALSE;
 	unsigned int boundary_point_idx = 0;
 	double max_norm = -DBL_MAX;
 	double curr_norm;
@@ -72,12 +187,9 @@ lwgeom_cluster_2d_kmeans(const LWGEOM** geoms, int ngeoms, int k)
 
 	/* Initialize our static structs */
 	memset(&config, 0, sizeof(kmeans_config));
-	memset(&result, 0, sizeof(kmeans_result));
 
 	if (ngeoms < k)
-	{
 		lwerror("%s: number of geometries is less than the number of clusters requested", __func__);
-	}
 
 	/* We'll hold the temporary centroid objects here */
 	centroids = lwalloc(sizeof(LWGEOM*) * ngeoms);
@@ -211,149 +323,10 @@ lwgeom_cluster_2d_kmeans(const LWGEOM** geoms, int ngeoms, int k)
 	lwfree(distances);
 
 	/* Good result */
-	if (result == KMEANS_OK)
+	if (result)
 		return config.clusters;
 
 	/* Bad result, not going to need the answer */
 	lwfree(config.clusters);
-	if (result == KMEANS_EXCEEDED_MAX_ITERATIONS)
-	{
-		lwerror("%s did not converge after %d iterations", __func__, KMEANS_MAX_ITERATIONS);
-		return NULL;
-	}
-
-	/* Unknown error */
 	return NULL;
-}
-
-static void
-update_r(kmeans_config* config)
-{
-	unsigned int i;
-	assert(config->objs != NULL);
-	assert(config->num_objs > 0);
-	assert(config->centers);
-	assert(config->clusters);
-	for (i = 0; i < config->num_objs; i++)
-	{
-		double distance, curr_distance;
-		int cluster, curr_cluster;
-		POINT2D* obj;
-
-		obj = config->objs[i];
-
-		/*
-		 * Don't try to cluster NULL objects, just add them
-		 * to the "unclusterable cluster"
-		 */
-		if (!obj)
-		{
-			config->clusters[i] = KMEANS_NULL_CLUSTER;
-			continue;
-		}
-
-		/* Initialize with distance to first cluster */
-		curr_distance = distance2d_sqr_pt_pt(obj, config->centers[0]);
-		curr_cluster = 0;
-
-		/* Check all other cluster centers and find the nearest */
-		for (cluster = 1; cluster < config->k; cluster++)
-		{
-			distance = distance2d_sqr_pt_pt(obj, config->centers[cluster]);
-			if (distance < curr_distance)
-			{
-				curr_distance = distance;
-				curr_cluster = cluster;
-			}
-		}
-
-		/* Store the nearest cluster this object is in */
-		config->clusters[i] = curr_cluster;
-	}
-}
-
-static void
-update_means(kmeans_config* config)
-{
-	unsigned int i;
-	unsigned int* weights;
-
-	weights = lwalloc(sizeof(int) * config->k);
-	memset(weights, 0, sizeof(int) * config->k);
-
-	for (i = 0; i < config->k; i++)
-	{
-		config->centers[i]->x = 0.0;
-		config->centers[i]->y = 0.0;
-	}
-	for (i = 0; i < config->num_objs; i++)
-	{
-		config->centers[config->clusters[i]]->x += config->objs[i]->x;
-		config->centers[config->clusters[i]]->y += config->objs[i]->y;
-		weights[config->clusters[i]] += 1;
-	}
-	for (i = 0; i < config->k; i++)
-	{
-		config->centers[i]->x /= weights[i];
-		config->centers[i]->y /= weights[i];
-	}
-	lwfree(weights);
-}
-
-kmeans_result
-kmeans(kmeans_config* config)
-{
-	unsigned int iterations = 0;
-	int* clusters_last;
-	size_t clusters_sz = sizeof(int) * config->num_objs;
-
-	assert(config);
-	assert(config->objs);
-	assert(config->num_objs);
-	assert(config->centers);
-	assert(config->k);
-	assert(config->clusters);
-	assert(config->k <= config->num_objs);
-
-	/* Zero out cluster numbers, just in case user forgets */
-	memset(config->clusters, 0, clusters_sz);
-
-	/*
-	 * Previous cluster state array. At this time, r doesn't mean anything
-	 * but it's ok
-	 */
-	clusters_last = lwalloc(clusters_sz);
-
-	while (1)
-	{
-		LW_ON_INTERRUPT(lwfree(clusters_last); return KMEANS_ERROR);
-
-		/* Store the previous state of the clustering */
-		memcpy(clusters_last, config->clusters, clusters_sz);
-
-		update_r(config);
-		update_means(config);
-
-		/*
-		 * if all the cluster numbers are unchanged since last time,
-		 * we are at a stable solution, so we can stop here
-		 */
-		if (memcmp(clusters_last, config->clusters, clusters_sz) == 0)
-		{
-			lwfree(clusters_last);
-			config->total_iterations = iterations;
-			return KMEANS_OK;
-		}
-
-		if (iterations++ > KMEANS_MAX_ITERATIONS)
-		{
-			lwfree(clusters_last);
-			config->total_iterations = iterations;
-			return KMEANS_EXCEEDED_MAX_ITERATIONS;
-		}
-	}
-
-	lwfree(clusters_last);
-	config->total_iterations = iterations;
-	return KMEANS_ERROR;
 }
