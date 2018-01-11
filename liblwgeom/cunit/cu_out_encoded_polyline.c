fdf42e94f63b504e7d66b821d44e68e018cadae9
Darafei Praliaskouski <me@komzpa.net>
2018-01-11 12:46:27 +0000

    Formatting only before toucing Encoded Polyline logic.
    References #3982
    
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16254 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/cunit/cu_out_encoded_polyline.c
+++ liblwgeom/cunit/cu_out_encoded_polyline.c
@@ -1,15 +1,15 @@
 /**********************************************************************
-*
-* PostGIS - Spatial Types for PostgreSQL
-* http://postgis.net
-*
-* Copyright 2014 Kashif Rasul <kashif.rasul@gmail.com> and
-*                Shoaib Burq <saburq@gmail.com>
-*
-* This is free software; you can redistribute and/or modify it under
-* the terms of the GNU General Public Licence. See the COPYING file.
-*
-**********************************************************************/
+ *
+ * PostGIS - Spatial Types for PostgreSQL
+ * http://postgis.net
+ *
+ * Copyright 2014 Kashif Rasul <kashif.rasul@gmail.com> and
+ *                Shoaib Burq <saburq@gmail.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public Licence. See the COPYING file.
+ *
+ **********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,16 +19,16 @@
 #include "liblwgeom_internal.h"
 #include "cu_tester.h"
 
-static void do_encoded_polyline_test(char * in, int precision, char * out)
+static void
+do_encoded_polyline_test(char* in, int precision, char* out)
 {
-	LWGEOM *g;
-	char * h;
+	LWGEOM* g;
+	char* h;
 
 	g = lwgeom_from_wkt(in, LW_PARSER_CHECK_NONE);
 	h = lwgeom_to_encoded_polyline(g, precision);
 
-	if (strcmp(h, out))
-		fprintf(stderr, "\nIn:   %s\nOut:  %s\nTheo: %s\n", in, h, out);
+	if (strcmp(h, out)) fprintf(stderr, "\nIn:   %s\nOut:  %s\nTheo: %s\n", in, h, out);
 
 	CU_ASSERT_STRING_EQUAL(h, out);
 
@@ -36,66 +36,54 @@ static void do_encoded_polyline_test(char * in, int precision, char * out)
 	lwfree(h);
 }
 
-
-static void out_encoded_polyline_test_geoms(void)
+static void
+out_encoded_polyline_test_geoms(void)
 {
 	/* Magic Linestring */
-	do_encoded_polyline_test(
-	    "SRID=4326;LINESTRING(33.6729 38.7071,33.6692 38.701,33.6673 38.6972,33.6626 38.6871)",
-	    5,
-	    "k~fkFsvolEbe@bVvVzJb~@j\\");
-		return;
+	do_encoded_polyline_test("SRID=4326;LINESTRING(33.6729 38.7071,33.6692 38.701,33.6673 38.6972,33.6626 38.6871)",
+				 5,
+				 "k~fkFsvolEbe@bVvVzJb~@j\\");
+	return;
 
 	/* Linestring */
 	do_encoded_polyline_test(
-	    "LINESTRING(-120.2 38.5,-120.95 40.7,-126.453 43.252)",
-	    5,
-	    "_p~iF~ps|U_ulLnnqC_mqNvxq`@");
+	    "LINESTRING(-120.2 38.5,-120.95 40.7,-126.453 43.252)", 5, "_p~iF~ps|U_ulLnnqC_mqNvxq`@");
 
 	/* MultiPoint */
-	do_encoded_polyline_test(
-	    "MULTIPOINT(-120.2 38.5,-120.95 40.7)",
-	    5,
-	    "_p~iF~ps|U_ulLnnqC");
+	do_encoded_polyline_test("MULTIPOINT(-120.2 38.5,-120.95 40.7)", 5, "_p~iF~ps|U_ulLnnqC");
 }
 
-static void out_encoded_polyline_test_srid(void)
+static void
+out_encoded_polyline_test_srid(void)
 {
 
 	/* SRID - with PointArray */
-	do_encoded_polyline_test(
-	    "SRID=4326;LINESTRING(0 1,2 3)",
-	    5,
-	    "_ibE?_seK_seK");
+	do_encoded_polyline_test("SRID=4326;LINESTRING(0 1,2 3)", 5, "_ibE?_seK_seK");
 
 	/* wrong SRID */
-	do_encoded_polyline_test(
-	    "SRID=4327;LINESTRING(0 1,2 3)",
-	    5,
-	    "_ibE?_seK_seK");
+	do_encoded_polyline_test("SRID=4327;LINESTRING(0 1,2 3)", 5, "_ibE?_seK_seK");
 }
 
-static void out_encoded_polyline_test_precision(void)
+static void
+out_encoded_polyline_test_precision(void)
 {
 
 	/* Linestring */
-	do_encoded_polyline_test(
-	    "LINESTRING(-0.250691 49.283048,-0.250633 49.283376,-0.250502 49.283972,-0.251245 49.284028,-0.251938 49.284232,-0.251938 49.2842)",
-	    6,
-	    "o}~~|AdshNoSsBgd@eGoBlm@wKhj@~@?");
+	do_encoded_polyline_test("LINESTRING(-0.250691 49.283048,-0.250633 49.283376,-0.250502 49.283972,-0.251245 "
+				 "49.284028,-0.251938 49.284232,-0.251938 49.2842)",
+				 6,
+				 "o}~~|AdshNoSsBgd@eGoBlm@wKhj@~@?");
 
 	/* MultiPoint */
-	do_encoded_polyline_test(
-	    "MULTIPOINT(-120.2 38.5,-120.95 40.7)",
-	    3,
-	    "gejAnwiFohCzm@");
+	do_encoded_polyline_test("MULTIPOINT(-120.2 38.5,-120.95 40.7)", 3, "gejAnwiFohCzm@");
 }
 
 /*
 ** Used by test harness to register the tests in this file.
 */
 void out_encoded_polyline_suite_setup(void);
-void out_encoded_polyline_suite_setup(void)
+void
+out_encoded_polyline_suite_setup(void)
 {
 	CU_pSuite suite = CU_add_suite("encoded_polyline_output", NULL, NULL);
 	PG_ADD_TEST(suite, out_encoded_polyline_test_geoms);
