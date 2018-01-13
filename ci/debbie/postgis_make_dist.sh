b34e701ac0cce3ab0afe9a3307e86e2cfa6bf4fb
Regina Obe <lr@pcorp.us>
2018-01-13 07:05:29 +0000

    no need to cd down, already in root
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16282 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/debbie/postgis_make_dist.sh
+++ ci/debbie/postgis_make_dist.sh
@@ -15,7 +15,6 @@ export WEB_DIR=/var/www/postgis_stuff/
 export PATH="${PGPATH}/bin:$PATH"
 export LD_LIBRARY_PATH="${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}/lib:${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}/lib:${PGPATH}/lib"
 
-cd ../../
 $POSTGIS_MAJOR_VERSION=`grep ^POSTGIS_MAJOR_VERSION Version.config | cut -d= -f2`
 $POSTGIS_MINOR_VERSION=`grep ^POSTGIS_MINOR_VERSION Version.config | cut -d= -f2`
 $POSTGIS_MICRO_VERSION=`grep ^POSTGIS_MICRO_VERSION Version.config | cut -d= -f2`
