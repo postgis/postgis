08e45cff2d3d9d3c55b59acf01f783b20fec3177
Regina Obe <lr@pcorp.us>
2018-01-14 16:05:45 +0000

    minor edits to HOWTO_RELEASE Add 2.3.5, 2.3.6dev to upgradeable_versions.mk Update release_notes.xml to reflect impending release Flip version to 2.3.6rc1 (in prep for rc1 tagging)
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.3@16295 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -28,7 +28,9 @@ UPGRADEABLE_VERSIONS = \
 	2.3.1 \
 	2.3.2 \
 	2.3.3 \
-	2.3.4
+	2.3.4 \
+	2.3.5 \
+	2.3.6dev
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
