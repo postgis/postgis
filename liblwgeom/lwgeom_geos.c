53302adbee0efce0454e76785f4f671004b04169
Darafei Praliaskouski <komzpa@gojuno.com>
2018-02-13 18:40:45 +0300

    clipping stub should not emit lines and points for polygons

--- liblwgeom/lwgeom_geos.c
+++ liblwgeom/lwgeom_geos.c
@@ -1080,11 +1080,27 @@ lwgeom_union(const LWGEOM *geom1, const LWGEOM *geom2)
 LWGEOM*
 lwgeom_clip_by_rect(const LWGEOM* geom1, double x1, double y1, double x2, double y2)
 {
-	return lwgeom_intersection(geom1, (LWGEOM*)lwpoly_construct_envelope(geom1->srid, x1, y1, x2, y2));
+	LWGEOM* result;
+	LWGEOM* tmp;
 
-	/* POSTGIS_GEOS_VERSION >= 35, enable only after bugs in geos are fixed */
-#if 0
+	result = lwgeom_intersection(geom1, (LWGEOM*)lwpoly_construct_envelope(geom1->srid, x1, y1, x2, y2));
 
+	/* clipping should not produce lower dimension objects */
+	if (
+		/* input has exact dimensionality */
+		geom1->type != COLLECTIONTYPE && lwgeom_is_collection(geom1) &&
+		/* output may have different things inside */
+		result->type == COLLECTIONTYPE
+	)
+	{
+		tmp = lwcollection_as_lwgeom(lwcollection_extract(lwgeom_as_lwcollection(result), lwgeom_dimension(geom1)));
+		lwfree(result);
+		result = tmp;
+	}
+	return result;
+
+
+#if 0 /* POSTGIS_GEOS_VERSION >= 35, enable only after bugs in geos are fixed */
 	GEOSGeometry *g1, *g3;
 	int is3d;
 
