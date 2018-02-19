cc08d6c6bf1dc738dd7c0596468c84ae545a3efe
Regina Obe <lr@pcorp.us>
2018-02-19 17:39:56 +0000

    move library version declare into shared script -- with plans to eventually have postgres start/stop from script as well.
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16384 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/winnie/package_postgis.sh
+++ ci/winnie/package_postgis.sh
@@ -12,14 +12,7 @@
 #export OS_BUILD=32
 
 #export GCC_TYPE=
-export GEOS_VER=3.7.0dev
-export GDAL_VER=2.2.3
-export PROJ_VER=4.9.3
-export SFCGAL_VER=1.3.2
-export PCRE_VER=8.33
-export PROTOBUF_VER=3.2.0
-export PROTOBUFC_VER=1.2.1
-export CGAL_VER=4.11
+bash lib_envs.sh
 
 if [[ "${GCC_TYPE}" == *gcc48* ]] ; then
 	export PROJECTS=/projects
