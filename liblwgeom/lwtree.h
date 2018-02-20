5ad6e06a617eaa48a91631ef74e5e42caed75b3a
Paul Ramsey <pramsey@cleverelephant.ca>
2018-02-20 20:40:00 +0000

    Edge tree index on geometries makes distance calculations on large geometries go much faster. Needs to be used selectively and potentially with a caching component for maximum effect. Test it out using _ST_DistanceRectTree(geom, geom) for now. References #2614
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16393 b70326c6-7e19-0410-871a-916f4a2858ee

--- liblwgeom/lwtree.h
+++ liblwgeom/lwtree.h
@@ -22,21 +22,93 @@
  *
  **********************************************************************/
 
+#define RECT_NODE_SIZE 8
+
+typedef enum
+{
+	RECT_NODE_INTERNAL_TYPE,
+	RECT_NODE_LEAF_TYPE
+} RECT_NODE_TYPE;
+
+typedef enum
+{
+	RECT_NODE_RING_NONE = 0,
+	RECT_NODE_RING_EXTERIOR,
+	RECT_NODE_RING_INTERIOR
+} RECT_NODE_RING_TYPE;
+
+typedef enum
+{
+	RECT_NODE_SEG_UNKNOWN = 0,
+	RECT_NODE_SEG_POINT,
+	RECT_NODE_SEG_LINEAR,
+	RECT_NODE_SEG_CIRCULAR
+} RECT_NODE_SEG_TYPE;
+
+typedef struct
+{
+	const POINTARRAY *pa;
+	RECT_NODE_SEG_TYPE seg_type;
+	int seg_num;
+} RECT_NODE_LEAF;
+
+struct rect_node;
+
+typedef struct
+{
+	int num_nodes;
+	RECT_NODE_RING_TYPE ring_type;
+	struct rect_node *nodes[RECT_NODE_SIZE];
+	int sorted;
+} RECT_NODE_INTERNAL;
+
 typedef struct rect_node
 {
+	RECT_NODE_TYPE type;
+	unsigned char geom_type;
 	double xmin;
 	double xmax;
 	double ymin;
 	double ymax;
-	struct rect_node *left_node;
-	struct rect_node *right_node;
-	POINT2D *p1;
-	POINT2D *p2;
+	double d;
+	union {
+		RECT_NODE_INTERNAL i;
+		RECT_NODE_LEAF l;
+	};
 } RECT_NODE;
 
-int rect_tree_contains_point(const RECT_NODE *tree, const POINT2D *pt, int *on_boundary);
-int rect_tree_intersects_tree(const RECT_NODE *tree1, const RECT_NODE *tree2);
+typedef struct rect_tree_distance_state
+{
+	double threshold;
+	double min_dist;
+	double max_dist;
+	POINT2D p1;
+	POINT2D p2;
+} RECT_TREE_DISTANCE_STATE;
+
+/**
+* Create a tree index on top an LWGEOM. Do not free the LWGEOM
+* until the tree is freed.
+*/
+RECT_NODE * rect_tree_from_lwgeom(const LWGEOM *geom);
+
+/**
+* Test if two RECT_NODE trees intersect one another.
+*/
+int rect_tree_intersects_tree(RECT_NODE *tree1, RECT_NODE *tree2);
+
+/**
+* Return the distance between two RECT_NODE trees.
+*/
+double rect_tree_distance_tree(RECT_NODE *n1, RECT_NODE *n2, double threshold);
+
+/**
+* Free the rect-tree memory
+*/
 void rect_tree_free(RECT_NODE *node);
-RECT_NODE* rect_node_leaf_new(const POINTARRAY *pa, int i);
-RECT_NODE* rect_node_internal_new(RECT_NODE *left_node, RECT_NODE *right_node);
-RECT_NODE* rect_tree_new(const POINTARRAY *pa);
+
+int rect_tree_contains_point(RECT_NODE *tree, const POINT2D *pt);
+RECT_NODE * rect_tree_from_ptarray(const POINTARRAY *pa, int geom_type);
+LWGEOM * rect_tree_to_lwgeom(const RECT_NODE *tree);
+char * rect_tree_to_wkt(const RECT_NODE *node);
+void rect_tree_printf(const RECT_NODE *node, int depth);
