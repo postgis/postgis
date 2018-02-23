d1a1fdc4992b56ad68ebe3577134c0026dbae506
Darafei Praliaskouski <komzpa@gojuno.com>
2018-02-23 11:55:27 +0300

    also format *.h

--- astyle.sh
+++ astyle.sh
@@ -20,7 +20,7 @@ fi
 # Find all "pure" C files in the codebase
 #   - not .in.c used for .sql generation
 #   - not lex.yy.c or wktparse.tab.c as these are generated files
-CFILES=`find . -name '*.c' -not \( -name '*_parse.c' -o -name '*_lex.c' \)`
+CFILES=`find . -name '*.c' -o -name '*.h' -not \( -name '*_parse.c' -o -name '*_lex.c' \)`
 
 # Run the standard format on the files, and do not
 # leave .orig files around for altered files.
