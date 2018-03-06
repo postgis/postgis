d4cd4f6a266df2d6eecbb7f6ae1d08849e1da7ee
Darafei Praliaskouski <komzpa@gojuno.com>
2018-03-06 17:25:25 +0300

    data copyright

--- regress/regress_big_polygon.sql
+++ regress/regress_big_polygon.sql
@@ -3,6 +3,7 @@ SET client_min_messages TO WARNING;
 drop table if exists big_polygon;
 create table big_polygon(geom geometry);
 alter table big_polygon alter column geom set storage main;
+-- Data (c) OpenStreetMap contributors, ODbL / http://osm.org/
 \copy big_polygon from 'big_polygon.wkb'
 \set QUIET off
 SET client_min_messages TO NOTICE;
