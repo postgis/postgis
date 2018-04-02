d7c9affc5b03b812ad9b40044f847952fbbd41ab
Regina Obe <lr@pcorp.us>
2018-04-02 03:01:59 +0000

    Update to 2.3.7 and get everything ready in prep for official tagging on Friday (2018-04-06)
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.3@16512 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -24,13 +24,15 @@ UPGRADEABLE_VERSIONS = \
 	2.2.4 \
 	2.2.5 \
 	2.2.6 \
+	2.2.7 \
 	2.3.0 \
 	2.3.1 \
 	2.3.2 \
 	2.3.3 \
 	2.3.4 \
 	2.3.5 \
-	2.3.6
+	2.3.6 \
+	2.3.7dev
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
