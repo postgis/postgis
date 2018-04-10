41c3da15ae360f1284f7c8fba6ce3b7bf4ac42e9
Regina Obe <lr@pcorp.us>
2018-04-10 04:06:01 +0000

    Disable sfcgal in make dist and regress testing until sfcgal compile issue  for CGAL 4.11 / SFCGAL 1.3.3 against PostGIS is fixed References #4068 for PostGIS 2.5
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16537 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/debbie/postgis_regress.sh
+++ ci/debbie/postgis_regress.sh
@@ -61,7 +61,7 @@ fi
 ./configure \
     --with-pgconfig=${PROJECTS}/pg/rel/pg${PG_VER}w${OS_BUILD}/bin/pg_config \
     --with-geosconfig=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}/bin/geos-config \
-    --with-gdalconfig=${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}/bin/gdal-config --with-sfcgal=/usr/bin/sfcgal-config \
+    --with-gdalconfig=${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}/bin/gdal-config \
     --without-interrupt-tests \
     --prefix=${PROJECTS}/pg/rel/pg${PG_VER}w${OS_BUILD}
 make clean
