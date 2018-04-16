414e9d2ce439c16eccf4259dd20ba1f2e6ff4673
Regina Obe <lr@pcorp.us>
2018-04-16 08:20:29 +0000

    Add doc notes, NEWS, and regress tests for json/jsonb (also add missing credits to NEWS) Closes #4006 for 2.5.0
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16540 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/in_geojson.sql
+++ regress/in_geojson.sql
@@ -5,7 +5,8 @@ select 'geomfromgeojson_03',st_astext(st_geomfromgeojson(st_asgeojson('POINT(1 1
 select 'geomfromgeojson_04',st_astext(st_geomfromgeojson(st_asgeojson('LINESTRING(0 0,1 1)')));
 select 'geomfromgeojson_05',st_astext(st_geomfromgeojson(st_asgeojson('POLYGON((0 0,1 1,1 0,0 0))')));
 select 'geomfromgeojson_06',st_astext(st_geomfromgeojson(st_asgeojson('MULTIPOLYGON(((0 0,1 1,1 0,0 0)))')));
-
+select 'geomfromgeojson_07',st_astext(st_geomfromgeojson(st_asgeojson('MULTIPOLYGON(((0 0,1 1,1 0,0 0)))')::json));
+select 'geomfromgeojson_08',st_astext(st_geomfromgeojson(st_asgeojson('MULTIPOLYGON(((0 0,1 1,1 0,0 0)))')::jsonb));
 -- #1434
 select '#1434: Next two errors';
 select '#1434.1',ST_GeomFromGeoJSON('{ "type": "Point", "crashme": [100.0, 0.0] }');
