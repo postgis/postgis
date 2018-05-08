aa71fec2a6936e37179c41c52d938c62933893c8
Even Rouault <even.rouault@spatialys.com>
2018-05-08 23:18:01 +0200

    Extend testing of ST_BandFileSize() and ST_BandFileTimestamp() to error cases

--- raster/test/regress/load_outdb.sql
+++ raster/test/regress/load_outdb.sql
@@ -4,3 +4,14 @@ SELECT DISTINCT ST_BandFileTimestamp(rast) FROM raster_outdb_template;
 SET postgis.enable_outdb_rasters = true;
 SELECT DISTINCT ST_BandFileSize(rast) FROM raster_outdb_template;
 SELECT DISTINCT ST_BandFileTimestamp(rast) != 0 FROM raster_outdb_template;
+
+-- error cases
+SELECT ST_BandFileSize(''::raster);
+SELECT ST_BandFileTimestamp(''::raster);
+SELECT ST_BandFileSize(rast,-1) FROM raster_outdb_template WHERE rid = 1;
+SELECT ST_BandFileTimestamp(rast,-1) FROM raster_outdb_template WHERE rid = 1;
+SELECT ST_BandFileSize(rast,10) FROM raster_outdb_template WHERE rid = 1;
+SELECT ST_BandFileTimestamp(rast,10) FROM raster_outdb_template WHERE rid = 1;
+-- valid raster, but file does not exist
+SELECT ST_BandFileSize('0100000100000000000000F03F000000000000F0BF0000000000000000000000000000000000000000000000000000000000000000000000005A003200840000DEADBEEF00'::raster);
+SELECT ST_BandFileTimestamp('0100000100000000000000F03F000000000000F0BF0000000000000000000000000000000000000000000000000000000000000000000000005A003200840000DEADBEEF00'::raster);
