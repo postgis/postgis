3b5f4ad36508eb0034f15227a0a6dc9007606f94
Arthur Lesuisse <root@cs.ulb.ac.be>
2018-06-08 11:52:11 +0200

    more deterministic regression tests

--- regress/regress_spgist_index_2d.sql
+++ regress/regress_spgist_index_2d.sql
@@ -1,96 +1,4 @@
 
-CREATE SCHEMA testschema ;
-SET search_path TO testschema,public ;
-
-CREATE OR REPLACE FUNCTION random_int(low int, high int)
-	RETURNS int AS $$
-BEGIN
-	RETURN floor(random()* (high-low + 1) + low);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_float(low float, high float)
-	RETURNS float AS $$
-BEGIN
-	RETURN random()* (high-low + 1) + low;
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geompoint(lowx float, highx float, lowy float, highy float)
-	RETURNS geometry AS $$
-BEGIN
-	RETURN st_point(random_float(lowx, highx), random_float(lowy, highy));
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geomlinestring(lowx float, highx float,
-		lowy float, highy float, maxvertices int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geompoint(lowx, highx, lowy, highy)) INTO result
-	FROM generate_series (1, random_int(2, maxvertices)) AS x;
-	RETURN st_geometryn(st_unaryunion(ST_LineFromMultiPoint(st_collect(result))),1);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geompolygon(lowx float, highx float,
-	lowy float, highy float, maxvertices int)
-	RETURNS geometry AS $$
-DECLARE
-	pointarr geometry[];
-	noVertices int;
-	t timestamptz;
-	valid geometry;
-BEGIN
-	noVertices = random_int(3, maxvertices);
-	for i in 1..noVertices
-	loop
-		pointarr[i] = random_geompoint(lowx, highx, lowy, highy);
-	end loop;
-	pointarr[noVertices+1] = pointarr[1];
-	return st_geometryn(st_makevalid(st_makepolygon(st_makeline(pointarr))),1);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geommultipoint(lowx float, highx float,
-		lowy float, highy float, maxcard int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geompoint(lowx, highx, lowy, highy)) INTO result
-	FROM generate_series (1, random_int(1, maxcard)) AS x;
-	RETURN st_collect(result);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geommultilinestring(lowx float, highx float,
-		lowy float, highy float, maxvertices int, maxcard int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geomlinestring(lowx, highx, lowy, highy, maxvertices)) INTO result
-	FROM generate_series (1, random_int(2, maxcard)) AS x;
-	RETURN st_unaryunion(st_collect(result));
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-
-CREATE OR REPLACE FUNCTION random_geommultipolygon(lowx float, highx float,
-		lowy float, highy float, maxvertices int, maxcard int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geompolygon(lowx, highx, lowy, highy, maxvertices)) INTO result
-	FROM generate_series (1, random_int(2, maxcard)) AS x;
-	RETURN st_makevalid(st_collect(result));
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
 CREATE OR REPLACE FUNCTION qnodes(q text) RETURNS text
 LANGUAGE 'plpgsql' AS
 $$
@@ -113,46 +21,12 @@ BEGIN
 END;
 $$;
 
--------------------------------------------------------------------------------
-/* Create random geometries of various types */
-
-select setseed(0.77777) ;
-
-create table tbl_geompoint as
-select k, random_geompoint(0, 100, 0, 100) as g
-from generate_series(1,50) k;
-
-create table tbl_geomlinestring as
-select k, random_geomlinestring(0, 100, 0, 100, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geompolygon as
-select k, random_geompolygon(0, 100, 0, 100, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geommultipoint as
-select k, random_geommultipoint(0, 100, 0, 100, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geommultilinestring as
-select k, random_geommultilinestring(0, 100, 0, 100, 10, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geommultipolygon as
-select k, random_geommultipolygon(0, 100, 0, 100, 10, 10) as g
-from generate_series(1,50) k;
-
 create table tbl_geomcollection (
 	k serial,
 	g geometry
 );
-insert into tbl_geomcollection (g)
-(select g from tbl_geompoint) union
-(select g from tbl_geomlinestring)  union
-(select g from tbl_geompolygon) union
-(select g from tbl_geommultipoint)  union
-(select g from tbl_geommultilinestring) union
-(select g from tbl_geommultipolygon);
+
+\copy tbl_geomcollection from 'regress_spgist_index_2d.data' ;
 
 create table test_spgist_idx_2d(
 	op char(3),
@@ -255,5 +129,6 @@ select * from test_spgist_idx_2d;
 
 -------------------------------------------------------------------------------
 
-SET search_path TO "$user",public ;
-DROP SCHEMA testschema CASCADE ;
+DROP TABLE tbl_geomcollection CASCADE;
+DROP TABLE test_spgist_idx_2d CASCADE;
+DROP FUNCTION qnodes;
--- regress/regress_spgist_index_3d.sql
+++ regress/regress_spgist_index_3d.sql
@@ -1,97 +1,4 @@
 
-CREATE SCHEMA testschema ;
-SET search_path TO testschema,public ;
-
-CREATE OR REPLACE FUNCTION random_int(low int, high int)
-	RETURNS int AS $$
-BEGIN
-	RETURN floor(random()* (high-low + 1) + low);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_float(low float, high float)
-	RETURNS float AS $$
-BEGIN
-	RETURN random()* (high-low + 1) + low;
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geompoint3D(lowx float, highx float,
-	lowy float, highy float, lowz float, highz float)
-	RETURNS geometry AS $$
-BEGIN
-	RETURN st_makepoint(random_float(lowx, highx), random_float(lowy, highy),
-		random_float(lowz, highz));
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geomlinestring3D(lowx float, highx float,
-		lowy float, highy float, lowz float, highz float, maxvertices int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geompoint3D(lowx, highx, lowy, highy, lowz, highz)) INTO result
-	FROM generate_series (1, random_int(2, maxvertices)) AS x;
-	RETURN st_geometryn(st_unaryunion(ST_LineFromMultiPoint(st_collect(result))),1);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geompolygon3D(lowx float, highx float,
-	lowy float, highy float, lowz float, highz float, maxvertices int)
-	RETURNS geometry AS $$
-DECLARE
-	pointarr geometry[];
-	noVertices int;
-	t timestamptz;
-	valid geometry;
-BEGIN
-	noVertices = random_int(3, maxvertices);
-	for i in 1..noVertices
-	loop
-		pointarr[i] = random_geompoint3D(lowx, highx, lowy, highy, lowz, highz);
-	end loop;
-	pointarr[noVertices+1] = pointarr[1];
-	return st_geometryn(st_makevalid(st_makepolygon(st_makeline(pointarr))),1);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geommultipoint3D(lowx float, highx float,
-		lowy float, highy float, lowz float, highz float, maxcard int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geompoint3D(lowx, highx, lowy, highy, lowz, highz)) INTO result
-	FROM generate_series (1, random_int(1, maxcard)) AS x;
-	RETURN st_collect(result);
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geommultilinestring3D(lowx float, highx float,
-		lowy float, highy float, lowz float, highz float, maxvertices int, maxcard int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geomlinestring3D(lowx, highx, lowy, highy, lowz, highz, maxvertices)) INTO result
-	FROM generate_series (1, random_int(2, maxcard)) AS x;
-	RETURN st_unaryunion(st_collect(result));
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
-CREATE OR REPLACE FUNCTION random_geommultipolygon3D(lowx float, highx float,
-		lowy float, highy float, lowz float, highz float, maxvertices int, maxcard int)
-	RETURNS geometry AS $$
-DECLARE
-	result geometry[];
-BEGIN
-	SELECT array_agg(random_geompolygon3D(lowx, highx, lowy, highy, lowz, highz, maxvertices)) INTO result
-	FROM generate_series (1, random_int(2, maxcard)) AS x;
-	RETURN st_makevalid(st_collect(result));
-END;
-$$ LANGUAGE 'plpgsql' STRICT;
-
 CREATE OR REPLACE FUNCTION qnodes(q text) RETURNS text
 LANGUAGE 'plpgsql' AS
 $$
@@ -115,45 +22,13 @@ END;
 $$;
 
 -------------------------------------------------------------------------------
-/* Create random geometries of various types */
-
-select setseed(0.77777) ;
-
-create table tbl_geompoint3D as
-select k, random_geompoint3D(0, 1000, 0, 1000, 0, 1000) as g
-from generate_series(1,50) k;
-
-create table tbl_geomlinestring3D as
-select k, random_geomlinestring3D(0, 1000, 0, 1000, 0, 1000, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geompolygon3D as
-select k, random_geompolygon3D(0, 1000, 0, 1000, 0, 1000, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geommultipoint3D as
-select k, random_geommultipoint3D(0, 1000, 0, 1000, 0, 1000, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geommultilinestring3D as
-select k, random_geommultilinestring3D(0, 1000, 0, 1000, 0, 1000, 10, 10) as g
-from generate_series(1,50) k;
-
-create table tbl_geommultipolygon3D as
-select k, random_geommultipolygon3D(0, 1000, 0, 1000, 0, 1000, 10, 10) as g
-from generate_series(1,50) k;
 
 create table tbl_geomcollection (
 	k serial,
 	g geometry
 );
-insert into tbl_geomcollection(g)
-(select g from tbl_geompoint3D)  union
-(select g from tbl_geomlinestring3D) union
-(select g from tbl_geompolygon3D) union
-(select g from tbl_geommultipoint3D) union
-(select g from tbl_geommultilinestring3D) union
-(select g from tbl_geommultipolygon3D);
+
+\copy tbl_geomcollection from 'regress_spgist_index_3d.data';
 
 create table test_spgist_idx_3d(
 	op char(3),
@@ -208,5 +83,7 @@ select * from test_spgist_idx_3d;
 
 -------------------------------------------------------------------------------
 
-SET search_path TO "$user",public ;
-DROP SCHEMA testschema CASCADE ;
+DROP TABLE tbl_geomcollection CASCADE;
+DROP TABLE test_spgist_idx_3d CASCADE;
+DROP FUNCTION qnodes;
+
