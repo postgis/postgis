28bee0c3e620181564a8cf65facbb4f5d6325226
Raul Marin <git@rmr.ninja>
2018-06-12 17:33:40 +0200

    MVT tests: Remove ST_Normalize calls and check the orientation
    From the spec (https://git.io/vhrK4):
    ```
    An exterior ring is DEFINED as a linear ring having a positive area
    as calculated by applying the surveyor's formula to the vertices of
    the polygon in tile coordinates. In the tile coordinate system
    (with the Y axis positive down and X axis positive to the right)
    this makes the exterior ring's winding order appear clockwise.
    
    An interior ring is DEFINED as a linear ring having a negative area
    as calculated by applying the surveyor's formula to the vertices of
    the polygon in tile coordinates. In the tile coordinate system (with
    the Y axis positive down and X axis positive to the right) this makes
    the interior ring's winding order appear counterclockwise.
    ```

--- regress/mvt.sql
+++ regress/mvt.sql
@@ -1,47 +1,99 @@
 -- geometry preprocessing tests
-select 'PG1', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+select 'PG1', ST_AsText(ST_AsMVTGeom(
 	ST_Point(1, 2),
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
-	4096, 0, false)));
-select 'PG2', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+	4096, 0, false));
+select 'PG2', ST_AsText(ST_AsMVTGeom(
 	ST_Point(1, 2),
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096*2, 4096*2)),
-	4096, 0, false)));
-select 'PG3', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+	4096, 0, false));
+select 'PG3', ST_AsText(ST_AsMVTGeom(
 	ST_Point(1, 2),
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096/2, 4096/2)),
-	4096, 0, false)));
-select 'PG4', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+	4096, 0, false));
+select 'PG4', ST_AsText(ST_AsMVTGeom(
 	ST_GeomFromText('POLYGON ((0 0, 10 0, 10 5, 0 -5, 0 0))'),
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
-	4096, 0, false)));
-select 'PG5', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+	4096, 0, false));
+select 'PG5', ST_AsText(ST_AsMVTGeom(
 	ST_GeomFromText('POLYGON ((0 0, 10 0, 10 5, 0 -5, 0 0))'),
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096*4096, 4096*4096)),
-	4096, 0, false)));
-select 'PG6', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+	4096, 0, false));
+select 'PG6', ST_AsText(ST_AsMVTGeom(
 	ST_GeomFromText('POLYGON ((762780 6474467, 717821 6797045, 1052826 6797045, 762780 6474467))'),
 	ST_MakeBox2D(ST_Point(626172.135625, 6261721.35625), ST_Point(1252344.27125, 6887893.49188)),
-	4096, 0, false)));
-select 'PG7', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+	4096, 0, false));
+select 'PG7', ST_AsText(ST_AsMVTGeom(
     ST_GeomFromText('POLYGON((-7792023.4539488 1411512.60791779,-7785283.40665468 1406282.69482469,-7783978.88137195 1404858.20373788,-7782986.89858399 1402324.91434802,-7779028.02672366 1397370.31802772,
         -7778652.06985644 1394387.75452545,-7779906.76953697 1393279.22658385,-7782212.33678782 1393293.14086794,-7784631.14401331 1394225.4151684,-7786257.27108231 1395867.40241344,-7783978.88137195 1395867.40241344,
         -7783978.88137195 1396646.68250521,-7787752.03959369 1398469.72134299,-7795443.30325373 1405280.43988858,-7797717.16326269 1406217.73286975,-7798831.44531677 1406904.48130551,-7799311.5830898 1408004.24038921,
         -7799085.10302919 1409159.72782477,-7798052.35381919 1411108.84582812,-7797789.63692662 1412213.40365339,-7798224.47868753 1414069.89725829,-7799003.5701851 1415694.42577482,-7799166.63587328 1416966.26267896,
         -7797789.63692662 1417736.81850415,-7793160.38395328 1412417.61222784,-7792023.4539488 1411512.60791779))'),
         ST_MakeBox2D(ST_Point(-20037508.34, -20037508.34), ST_Point(20037508.34, 20037508.34)),
-        4096, 10, true)));
-select 'PG8', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+        4096, 10, true));
+select 'PG8', ST_AsText(ST_AsMVTGeom(
     ST_GeomFromText('GEOMETRYCOLLECTION(MULTIPOLYGON (((0 0, 10 0, 10 5, 0 -5, 0 0))))'),
     ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
-    4096, 0, false)));
-select 'PG9', ST_AsText(ST_Normalize(ST_AsMVTGeom(
+    4096, 0, false));
+select 'PG9', ST_AsText(ST_AsMVTGeom(
 	ST_GeomFromText('POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))'),
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(5, 5)),
-	4096, 0, true)));
+	4096, 0, true));
 SELECT 'PG10', ST_AsText(ST_AsMVTGeom(
 	'POINT EMPTY'::geometry,
 	'BOX(0 0,2 2)'::box2d));
+
+-- Clockwise Polygon
+SELECT 'PG11', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
+
+-- Same as PG11 but CCW
+SELECT 'PG12', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
+
+--- POLYGONS WITH INTERIOR RINGS
+-- Input: Exterior CW, interior CW
+-- Output: CW, CCW
+SELECT 'PG13', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0), (1 1, 1 9, 9 9, 9 1, 1 1))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
+
+-- Input: Exterior CW, interior CCW
+-- Output: CW, CCW
+SELECT 'PG14', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0), (1 1, 9 1, 9 9, 1 9, 1 1))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
+
+-- Input: Exterior CCW, interior CW
+-- Output: CW, CCW
+SELECT 'PG15', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (1 1, 1 9, 9 9, 9 1, 1 1))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
+
+-- Input: Exterior CCW, interior CW
+-- Output: CW, CCW
+SELECT 'PG16', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (1 1, 1 9, 9 9, 9 1, 1 1))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
+
+-- Input: CW, CW, CW, CW
+-- Output: CW, CCW, CW, CCW
+SELECT 'PG17', ST_AsText(ST_AsMVTGeom(
+	ST_GeomFromText('POLYGON(
+            (0 0, 0 10, 10 10, 10 0, 0 0),
+            (1 1, 1  9,  9  9,  9 1, 1 1),
+            (2 2, 2  8,  8  8,  8 2, 2 2),
+            (3 3, 3  7,  7  7,  7 3, 3 3))'),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 0, false));
 
 -- geometry encoding tests
 SELECT 'TG1', encode(ST_AsMVT(q, 'test', 4096, 'geom'), 'base64') FROM (SELECT 1 AS c1,
