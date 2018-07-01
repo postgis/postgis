d13a99e1640336909d9ec55473c63cc1ff505169
Regina Obe <lr@pcorp.us>
2018-07-01 16:30:51 +0000

    Get rid of the ifdefs around the functions, these have always existed but were static before. Closes #4114
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16629 b70326c6-7e19-0410-871a-916f4a2858ee

--- libpgcommon/gserialized_gist.h
+++ libpgcommon/gserialized_gist.h
@@ -131,22 +131,21 @@ GSERIALIZED* gserialized_drop_gidx(GSERIALIZED *g);
 
 bool box2df_contains(const BOX2DF *a, const BOX2DF *b);
 
-/** needed for sp-gist support PostgreSQL 11+ **/
-#if POSTGIS_PGSQL_VERSION > 100
-	void box2df_set_empty(BOX2DF *a);
-	void box2df_set_finite(BOX2DF *a);
-	void box2df_validate(BOX2DF *b);
-	bool box2df_overlaps(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_overleft(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_equals(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_left(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_right(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_overright(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_overbelow(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_below(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_above(const BOX2DF *a, const BOX2DF *b);
-	bool box2df_overabove(const BOX2DF *a, const BOX2DF *b);
-#endif
+
+void box2df_set_empty(BOX2DF *a);
+void box2df_set_finite(BOX2DF *a);
+void box2df_validate(BOX2DF *b);
+bool box2df_overlaps(const BOX2DF *a, const BOX2DF *b);
+bool box2df_overleft(const BOX2DF *a, const BOX2DF *b);
+bool box2df_equals(const BOX2DF *a, const BOX2DF *b);
+bool box2df_left(const BOX2DF *a, const BOX2DF *b);
+bool box2df_right(const BOX2DF *a, const BOX2DF *b);
+bool box2df_overright(const BOX2DF *a, const BOX2DF *b);
+bool box2df_overbelow(const BOX2DF *a, const BOX2DF *b);
+bool box2df_below(const BOX2DF *a, const BOX2DF *b);
+bool box2df_above(const BOX2DF *a, const BOX2DF *b);
+bool box2df_overabove(const BOX2DF *a, const BOX2DF *b);
+
 
 bool gidx_contains(GIDX *a, GIDX *b);
 int gserialized_datum_get_box2df_p(Datum gsdatum, BOX2DF *box2df);
