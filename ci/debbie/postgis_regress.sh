5580d1aa2bd9d30e21062cd9342af1acf57e138a
Regina Obe <lr@pcorp.us>
2018-07-04 20:41:42 +0000

    Reenable debbie's SFCGAL References #4068
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16634 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/debbie/postgis_regress.sh
+++ ci/debbie/postgis_regress.sh
@@ -62,7 +62,7 @@ fi
     --with-pgconfig=${PROJECTS}/pg/rel/pg${PG_VER}w${OS_BUILD}/bin/pg_config \
     --with-geosconfig=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}/bin/geos-config \
     --with-gdalconfig=${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}/bin/gdal-config \
-    --without-interrupt-tests \
+    --with-sfcgal=/usr/bin/sfcgal-config --without-interrupt-tests \
     --prefix=${PROJECTS}/pg/rel/pg${PG_VER}w${OS_BUILD}
 make clean
 make
