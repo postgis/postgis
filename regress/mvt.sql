d79e3d3f3e86e266826f70cb80a380d2ad9bf45e
Raul Marin <git@rmr.ninja>
2018-07-10 12:58:15 +0200

    MVT_geom: Clip using tile units

--- regress/mvt.sql
+++ regress/mvt.sql
@@ -138,6 +138,72 @@ SELECT 'PG25', ST_AsText(ST_AsMVTGeom(
 	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
 	4096, 0, false));
 
+-- Clipping right in the borders
+SELECT 'PG26', ST_AsText(ST_AsMVTGeom(
+	ST_Point(-1, -1),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG27', ST_AsText(ST_AsMVTGeom(
+	ST_Point(-1, 11),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG28', ST_AsText(ST_AsMVTGeom(
+	ST_Point(11, -1),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG29', ST_AsText(ST_AsMVTGeom(
+	ST_Point(11, 11),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG30', ST_AsText(ST_AsMVTGeom(
+	ST_Point(11.5, 11.5),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG31', ST_AsText(ST_AsMVTGeom(
+	ST_Point(11.49, 11.49),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG32', ST_AsText(ST_AsMVTGeom(
+	ST_Point(-1.5, -1.5),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG33', ST_AsText(ST_AsMVTGeom(
+	ST_Point(-1.49, -1.49),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG34', ST_AsText(ST_AsMVTGeom(
+	ST_Point(-1.5, 11.5),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(10, 10)),
+	10, 1, true));
+
+SELECT 'PG35', ST_AsText(ST_AsMVTGeom(
+	ST_Point(4352.49, -256.49),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
+	4096, 256, true));
+
+SELECT 'PG36', ST_AsText(ST_AsMVTGeom(
+	ST_Point(4352.49, -256.50),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
+	4096, 256, true));
+
+SELECT 'PG37', ST_AsText(ST_AsMVTGeom(
+	ST_Point(4352.50, -256.49),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
+	4096, 256, true));
+
+SELECT 'PG38', ST_AsText(ST_AsMVTGeom(
+	ST_Point(4352.50, -256.50),
+	ST_MakeBox2D(ST_Point(0, 0), ST_Point(4096, 4096)),
+	4096, 256, true));
+
 -- geometry encoding tests
 SELECT 'TG1', encode(ST_AsMVT(q, 'test', 4096, 'geom'), 'base64') FROM (SELECT 1 AS c1,
 	ST_AsMVTGeom(ST_GeomFromText('POINT(25 17)'),
