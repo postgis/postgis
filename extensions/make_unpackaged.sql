46ffeea164a4ee6c506665f5fd77f87415c817f0
Sandro Santilli <strk@kbt.io>
2018-09-11 09:19:25 +0000

    Make unpackaged from postgis_raster too
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16741 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/make_unpackaged.sql
+++ extensions/make_unpackaged.sql
@@ -1,4 +1,4 @@
-﻿-- for postgis
+-- for postgis
 SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || regexp_replace(
     regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
     E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
@@ -6,6 +6,16 @@ FROM pg_catalog.pg_depend AS d
 INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
 WHERE d.refclassid = 'pg_catalog.pg_extension'::pg_catalog.regclass
 AND deptype = 'e' AND e.extname = 'postgis'
+ORDER BY sqladd;
+
+-- for postgis_raster
+SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || regexp_replace(
+    regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
+    E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
+FROM pg_catalog.pg_depend AS d
+INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
+WHERE d.refclassid = 'pg_catalog.pg_extension'::pg_catalog.regclass
+AND deptype = 'e' AND e.extname = 'postgis_raster'
 ORDER BY sqladd;
 
 -- for topology
