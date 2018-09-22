82defd9f6df586ef31705a3d612d5dacb7cb0523
Raúl Marín Rodríguez <rmrodriguez@carto.com>
2018-09-22 13:06:55 +0000

    St_AsMVTGeom: Avoid type changes due to validation
    References #4181
    
    
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.5@16832 b70326c6-7e19-0410-871a-916f4a2858ee

--- postgis/mvt.c
+++ postgis/mvt.c
@@ -849,14 +849,24 @@ LWGEOM *mvt_geom(LWGEOM *lwgeom, const GBOX *gbox, uint32_t extent, uint32_t buf
 	if (lwgeom == NULL || lwgeom_is_empty(lwgeom))
 		return NULL;
 
-	/* if polygon(s) make valid and force clockwise as per MVT spec */
+
 	if (lwgeom->type == POLYGONTYPE ||
 		lwgeom->type == MULTIPOLYGONTYPE ||
 		lwgeom->type == COLLECTIONTYPE)
 	{
+		/* Force validation as per MVT spec */
 		lwgeom = lwgeom_make_valid(lwgeom);
-		/* In image coordinates CW actually comes out a CCW, so */
-		/* we also reverse. ¯\_(ツ)_/¯ */
+
+		/* Drop type changes tp play nice with MVT renderers */
+		if (!(lwgeom->type == POLYGONTYPE ||
+			lwgeom->type == MULTIPOLYGONTYPE ||
+			lwgeom->type == COLLECTIONTYPE))
+		{
+			lwgeom_free(lwgeom);
+			return NULL;
+		}
+
+		/* In image coordinates CW actually comes out a CCW, so we reverse */
 		lwgeom_force_clockwise(lwgeom);
 		lwgeom_reverse_in_place(lwgeom);
 	}
