978a2f9a7a46893e87c247cb9d817bdda0427d65
Darafei Praliaskouski <me@komzpa.net>
2018-09-28 14:07:43 -0400

    try enable logbt

--- ci/travis/run_tests.sh
+++ ci/travis/run_tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+curl -sSfL https://github.com/mapbox/logbt/archive/v2.0.3.tar.gz | sudo tar --gunzip --extract --strip-components=1 --exclude="*md" --exclude="test*" --directory=/usr/local/
+curl -sSfL https://raw.githubusercontent.com/mapbox/logbt/30c554dd37b6c96c23fc424f75910fc6d6696f00/bin/logbt | sudo tee /usr/local/bin/logbt > /dev/null
+sudo logbt --setp
+
 WARNINGS="-Werror -Wall -Wextra -Wformat -Werror=format-security"
 WARNINGS_DISABLED="-Wno-unused-parameter -Wno-implicit-fallthrough -Wno-unknown-warning-option -Wno-cast-function-type"
 
@@ -14,7 +18,7 @@ LDFLAGS_COV="--coverage"
 ./autogen.sh
 
 ./configure CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}"
-RUNTESTFLAGS="-v" make -j check
+logbt -- make -j check "RUNTESTFLAGS='--dumprestore --verbose'"
 
 ./configure CFLAGS="${CFLAGS_COV}" LDFLAGS="${LDFLAGS_COV}"
 make -j check
