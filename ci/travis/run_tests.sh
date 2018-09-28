faac4cf2ea42c265e6b4ba133e35d0476680978a
Daniel Baston <dbaston@gmail.com>
2018-09-28 14:13:34 +0000

    Add Travis build matrix using prebuilt Docker images
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16848 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ ci/travis/run_tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+WARNINGS="-Werror -Wall -Wextra -Wformat -Werror=format-security"
+WARNINGS_DISABLED="-Wno-unused-parameter -Wno-implicit-fallthrough -Wno-unknown-warning-option -Wno-cast-function-type"
+
+CFLAGS_STD="-g -O2 -mtune=generic -fno-omit-frame-pointer ${WARNINGS} ${WARNINGS_DISABLED}"
+LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
+
+CFLAGS_COV="-g -O0 --coverage"
+LDFLAGS_COV="--coverage"
+
+/usr/local/pgsql/bin/pg_ctl -l /tmp/logfile start
+./autogen.sh
+
+CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_COV}" ./configure
+make -j2
+make check
+
+CFLAGS="${CFLAGS_COV}" LDFLAGS="${LDFLAGS_COV}" ./configure
+make -j2
+make check
