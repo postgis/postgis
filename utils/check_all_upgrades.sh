79d476a0bff449c465dcef303eea56169a0ddff3
Sandro Santilli <strk@kbt.io>
2018-10-16 03:28:26 +0000

    [check_all_upgrades] Enable topology and raster tests
    ... rather than just core testing, this should now include
    raster and topology.
    
    Closes #4199
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16915 b70326c6-7e19-0410-871a-916f4a2858ee

--- utils/check_all_upgrades.sh
+++ utils/check_all_upgrades.sh
@@ -41,27 +41,49 @@ EXTDIR=`pg_config --sharedir`/extension/
 
 cd $EXTDIR
 failures=0
-files=`'ls' postgis--* | grep -v -- '--.*--' | sed 's/^postgis--\(.*\)\.sql/\1/'`
-for fname in unpackaged $files; do
-  from_version="$fname"
-  # only consider versions older than ${to_version}
-  cmp=`semver_compare "${from_version}" "${to_version}"`
-  if test $cmp -ge 0; then
-    continue
-  fi
-  UPGRADE_PATH="${from_version}--${to_version}"
-  if test -e postgis--${UPGRADE_PATH}.sql; then
-    echo "Testing upgrade $UPGRADE_PATH"
-    export RUNTESTFLAGS="-v --extension --upgrade-path=${UPGRADE_PATH}"
-    make -C ${BUILDDIR}/regress check && {
-      echo "PASS: upgrade $UPGRADE_PATH"
-    } || {
-      echo "FAIL: upgrade $UPGRADE_PATH"
-      failures=$((failures+1))
-    }
+
+INSTALLED_EXTENSIONS=postgis
+if test -f postgis_topology--${to_version}.sql; then
+  INSTALLED_EXTENSIONS="$INSTALLED_EXTENSIONS postgis_topology"
+fi
+if test -f postgis_raster--${to_version}.sql; then
+  INSTALLED_EXTENSIONS="$INSTALLED_EXTENSIONS postgis_raster"
+fi
+
+echo "INFO: installed extensions: $INSTALLED_EXTENSIONS"
+
+for EXT in ${INSTALLED_EXTENSIONS}; do
+  if test "${EXT}" = "postgis"; then
+    REGDIR=${BUILDDIR}/regress
+  elif test "${EXT}" = "postgis_topology"; then
+    REGDIR=${BUILDDIR}/topology/test
+  elif test "${EXT}" = "postgis_raster"; then
+    REGDIR=${BUILDDIR}/raster/test/regress
   else
-    echo "SKIP: upgrade $UPGRADE_PATH is missing"
+    echo "SKIP: don't know where to find regress tests for extension ${EXT}"
   fi
+  files=`'ls' ${EXT}--* | grep -v -- '--.*--' | sed "s/^${EXT}--\(.*\)\.sql/\1/"`
+  for fname in unpackaged $files; do
+    from_version="$fname"
+    # only consider versions older than ${to_version}
+    cmp=`semver_compare "${from_version}" "${to_version}"`
+    if test $cmp -ge 0; then
+      continue
+    fi
+    UPGRADE_PATH="${from_version}--${to_version}"
+    if test -e ${EXT}--${UPGRADE_PATH}.sql; then
+      echo "Testing ${EXT} upgrade $UPGRADE_PATH"
+      export RUNTESTFLAGS="-v --extension --upgrade-path=${UPGRADE_PATH}"
+      make -C ${REGDIR} check && {
+        echo "PASS: upgrade $UPGRADE_PATH"
+      } || {
+        echo "FAIL: upgrade $UPGRADE_PATH"
+        failures=$((failures+1))
+      }
+    else
+      echo "SKIP: ${EXT} upgrade $UPGRADE_PATH is missing"
+    fi
+  done
 done
 
 exit $failures
