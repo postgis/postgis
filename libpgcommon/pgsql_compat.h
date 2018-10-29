a77ce92797af6bf0a9652395914a49b171a828b6
Raul Marin <git@rmr.ninja>
2018-10-29 13:17:51 +0100

    Remove PG_NARGS macro
    Already available in 9.6 (currently oldest supported Postgres release)

--- libpgcommon/pgsql_compat.h
+++ libpgcommon/pgsql_compat.h
@@ -1,11 +1,6 @@
 #ifndef _PGSQL_COMPAT_H
 #define _PGSQL_COMPAT_H 1
 
-/* Make sure PG_NARGS is defined for older PostgreSQL versions */
-#ifndef PG_NARGS
-#define PG_NARGS() (fcinfo->nargs)
-#endif
-
 /* TupleDescAttr was backported into 9.6.5 but we support any 9.6.X */
 #ifndef TupleDescAttr
 #define TupleDescAttr(tupdesc, i) ((tupdesc)->attrs[(i)])
