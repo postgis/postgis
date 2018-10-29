b3222d8077593ac7c93d550761ffd0dcfeecb942
Raul Marin <git@rmr.ninja>
2018-10-29 13:27:22 +0100

    Header order is important

--- libpgcommon/pgsql_compat.h
+++ libpgcommon/pgsql_compat.h
@@ -1,6 +1,8 @@
 #ifndef _PGSQL_COMPAT_H
 #define _PGSQL_COMPAT_H 1
 
+#include "access/tupdesc.h"
+
 /* TupleDescAttr was backported into 9.6.5 but we support any 9.6.X */
 #ifndef TupleDescAttr
 #define TupleDescAttr(tupdesc, i) ((tupdesc)->attrs[(i)])
