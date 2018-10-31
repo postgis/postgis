fa5fee55fdfc02c9cc486defa5645628cd9bdad2
Regina Obe <lr@pcorp.us>
2018-10-31 03:25:23 +0000

    Fix sfcgal regress failure - Delete 4326 entry before readding. Also use integer format instead of string format. References #4210
    git-svn-id: http://svn.osgeo.org/postgis/trunk@16968 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/sfcgal/geography.sql
+++ regress/sfcgal/geography.sql
@@ -1,10 +1,11 @@
 SET postgis.backend = 'sfcgal';
 
+DELETE FROM spatial_ref_sys where srid = 4326;
 INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text)
 VALUES (
- '4326',
+ 4326,
  'EPSG',
- '4326',
+ 4326,
  'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]',
  '+proj=longlat +datum=WGS84 +no_defs'
 );
