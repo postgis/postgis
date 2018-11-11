a198fc73a389e1d7971a64daefd27f0d2e6e5bbe
Darafei Praliaskouski <me@komzpa.net>
2018-11-11 10:00:30 +0000

    Fix new GCC warnings.
    Updating GCC on travis to Debian Unstable exposed strncpy and if body alignment warnings.

--- CHANGELOG.carto.md
+++ CHANGELOG.carto.md
@@ -5,3 +5,4 @@
 Changes:
 - Start from `svn-2.5` ([427ed71](https://github.com/postgis/postgis/commit/427ed71c10683892d4f6b3f0898da9b5745562b4)).
 - Backport .travis from trunk (Sanitizers, multiple builds).
+- Backport [24efadd](https://github.com/postgis/postgis/commit/24efadd48d94ae7d6ce2aaab66ca940ab97a0a14): GCC warnings
--- raster/rt_pg/rtpg_mapalgebra.c
+++ raster/rt_pg/rtpg_mapalgebra.c
@@ -1466,9 +1466,9 @@ Datum RASTER_nMapAlgebraExpr(PG_FUNCTION_ARGS)
 			PG_RETURN_NULL();
 		}
 
-		strncpy(sql, "SELECT (", strlen("SELECT ("));
-		strncpy(sql + strlen("SELECT ("), expr, strlen(expr));
-		strncpy(sql + strlen("SELECT (") + strlen(expr), ")::double precision", strlen(")::double precision"));
+		memcpy(sql, "SELECT (", strlen("SELECT ("));
+		memcpy(sql + strlen("SELECT ("), expr, strlen(expr));
+		memcpy(sql + strlen("SELECT (") + strlen(expr), ")::double precision", strlen(")::double precision"));
 		sql[len] = '\0';
 
 		POSTGIS_RT_DEBUGF(3, "sql #%d: %s", exprpos[i], sql);
@@ -4632,9 +4632,9 @@ Datum RASTER_mapAlgebraExpr(PG_FUNCTION_ARGS)
         len = strlen("SELECT (") + strlen(expression) + strlen(")::double precision");
         initexpr = (char *)palloc(len + 1);
 
-        strncpy(initexpr, "SELECT (", strlen("SELECT ("));
-        strncpy(initexpr + strlen("SELECT ("), expression, strlen(expression));
-				strncpy(initexpr + strlen("SELECT (") + strlen(expression), ")::double precision", strlen(")::double precision"));
+        memcpy(initexpr, "SELECT (", strlen("SELECT ("));
+        memcpy(initexpr + strlen("SELECT ("), expression, strlen(expression));
+        memcpy(initexpr + strlen("SELECT (") + strlen(expression), ")::double precision", strlen(")::double precision"));
         initexpr[len] = '\0';
 
         POSTGIS_RT_DEBUGF(3, "RASTER_mapAlgebraExpr: Expression is %s", initexpr);
@@ -6677,9 +6677,9 @@ Datum RASTER_mapAlgebra2(PG_FUNCTION_ARGS)
 						PG_RETURN_NULL();
 					}
 
-					strncpy(sql, "SELECT (", strlen("SELECT ("));
-					strncpy(sql + strlen("SELECT ("), expr, strlen(expr));
-					strncpy(sql + strlen("SELECT (") + strlen(expr), ")::double precision", strlen(")::double precision"));
+					memcpy(sql, "SELECT (", strlen("SELECT ("));
+					memcpy(sql + strlen("SELECT ("), expr, strlen(expr));
+					memcpy(sql + strlen("SELECT (") + strlen(expr), ")::double precision", strlen(")::double precision"));
 					sql[len] = '\0';
 
 					POSTGIS_RT_DEBUGF(3, "sql #%d: %s", i, sql);
