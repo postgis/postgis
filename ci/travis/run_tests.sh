d4623a587c518e8c1a8541d646864c14cd03bd10
Daniel Baston <dbaston@gmail.com>
2018-11-12 12:06:26 -0500

    Enable debug code in Travis coverage build

--- ci/travis/run_tests.sh
+++ ci/travis/run_tests.sh
@@ -4,10 +4,12 @@ set -e
 WARNINGS="-Werror -Wall -Wextra -Wformat -Werror=format-security"
 WARNINGS_DISABLED="-Wno-unused-parameter -Wno-implicit-fallthrough -Wno-unknown-warning-option -Wno-cast-function-type"
 
+# Standard flags, as we might build PostGIS for production
 CFLAGS_STD="-g -O2 -mtune=generic -fno-omit-frame-pointer ${WARNINGS} ${WARNINGS_DISABLED}"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
-CFLAGS_COV="-g -O0 --coverage"
+# Second build with coverage and debugging code enabled
+CFLAGS_COV="-g -O0 --coverage --enable-debug"
 LDFLAGS_COV="--coverage"
 
 export CUNIT_WITH_VALGRIND=YES
