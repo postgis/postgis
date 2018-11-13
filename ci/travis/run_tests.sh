ec745cad54e3be4a8534961cfddb1bed2c8cc4b9
Darafei Praliaskouski <me@komzpa.net>
2018-11-14 00:23:15 +0300

    Split up debug and tests run

--- ci/travis/run_tests.sh
+++ ci/travis/run_tests.sh
@@ -17,8 +17,3 @@ export CUNIT_VALGRIND_FLAGS="--leak-check=full --error-exitcode=1"
 # Standard build
 ./configure CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}"
 bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
-
-# Check that compilation works at nonzero POSTGIS_DEBUG_LEVEL
-./configure --enable-debug # sets PARANOIA_LEVEL
-sed -i 's/POSTGIS_DEBUG_LEVEL [0-9]$/POSTGIS_DEBUG_LEVEL 4/' postgis_config.h
-make -j
