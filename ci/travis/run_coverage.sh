c737cd583483b99ae5e7db347ab9fe3b5e85cd9e
Darafei Praliaskouski <me@komzpa.net>
2018-11-14 00:01:07 +0300

    [travis] Build coverage after success, separately

---
+++ ci/travis/run_coverage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+# Flags for coverage build
+CFLAGS_COV="-g -O0 --coverage"
+LDFLAGS_COV="--coverage"
+
+/usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
+make -j clean
+./autogen.sh
+./configure CFLAGS="${CFLAGS_COV}" LDFLAGS="${LDFLAGS_COV}" --enable-debug
+make -j check
