46516a528104d5253d3925bdf9d7243cae8a1d18
Raul Marin <git@rmr.ninja>
2018-11-21 09:11:53 +0100

    clang UBSan: Try the old deprecated flag (common with gcc)

--- ci/travis/run_usan_clang.sh
+++ ci/travis/run_usan_clang.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Enable undefined behaviour sanitizer using traps
-CFLAGS_STD="-g -O2 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-trap=undefined"
+CFLAGS_STD="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
--- ci/travis/run_usan_gcc.sh
+++ ci/travis/run_usan_gcc.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Enable undefined behaviour sanitizer using traps to
-CFLAGS_STD="-g -O2 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
+CFLAGS_STD="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
