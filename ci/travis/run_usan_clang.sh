b0757ed7cc75d4320048f350abcedf5a9c12b60f
Raúl Marín Rodríguez <rmrodriguez@carto.com>
2018-11-22 15:24:41 +0000

    Travis: Add missing task file
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17055 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ ci/travis/run_usan_clang.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# Enable undefined behaviour sanitizer using traps
+CFLAGS_STD="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
+LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
+
+/usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
+./autogen.sh
+
+# Build with Clang and usan flags
+# TODO: Fix topology ubsan
+./configure CC=clang CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}" --without-topology
+bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
