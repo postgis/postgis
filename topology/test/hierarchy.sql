05a151876171bf3e343b35e8e2ff5ed76b11b462
Regina Obe <lr@pcorp.us>
2018-11-23 17:01:19 +0000

    Get rid of use of oids in topology regress was taken out in PostgreSQL 12. References #4251 for PostGIS 3.0
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17062 b70326c6-7e19-0410-871a-916f4a2858ee

--- topology/test/hierarchy.sql
+++ topology/test/hierarchy.sql
@@ -7,8 +7,8 @@
 --
 
 CREATE TABLE features.big_parcels (
-	feature_name varchar primary key
-) WITH OIDS;
+	feature_name varchar primary key, fid serial
+) ;
 
 SELECT topology.AddTopoGeometryColumn('city_data', 'features',
 	'big_parcels', 'feature', 'POLYGON',
@@ -17,21 +17,21 @@ SELECT topology.AddTopoGeometryColumn('city_data', 'features',
 
 SELECT AddGeometryColumn('features','big_parcels','the_geom',-1,'MULTIPOLYGON',2);
 
-INSERT INTO features.big_parcels VALUES ('P1P2', -- Feature name
+INSERT INTO features.big_parcels(feature_name, feature) VALUES ('P1P2', -- Feature name
   topology.CreateTopoGeom(
     'city_data', -- Topology name
     3, -- Topology geometry type (polygon/multipolygon)
     (SELECT layer_id FROM topology.layer WHERE table_name = 'big_parcels'),
     '{{1,1},{2,1}}')); -- P1 and P2
 
-INSERT INTO features.big_parcels VALUES ('P3P4', -- Feature name
+INSERT INTO features.big_parcels(feature_name, feature) VALUES ('P3P4', -- Feature name
   topology.CreateTopoGeom(
     'city_data', -- Topology name
     3, -- Topology geometry type (polygon/multipolygon)
     (SELECT layer_id FROM topology.layer WHERE table_name = 'big_parcels'),
     '{{3,1},{4,1}}')); -- P3 and P4
 
-INSERT INTO features.big_parcels VALUES ('F3F6', -- Feature name
+INSERT INTO features.big_parcels(feature_name, feature) VALUES ('F3F6', -- Feature name
   topology.CreateTopoGeom(
     'city_data', -- Topology name
     3, -- Topology geometry type (polygon/multipolygon)
@@ -46,15 +46,15 @@ INSERT INTO features.big_parcels VALUES ('F3F6', -- Feature name
 --
 
 CREATE TABLE features.big_streets (
-	feature_name varchar primary key
-) WITH OIDS;
+	feature_name varchar primary key, fid serial
+) ;
 
 SELECT topology.AddTopoGeometryColumn('city_data', 'features',
 	'big_streets', 'feature', 'LINE',
 	3 -- the city_streets layer id
 );
 
-INSERT INTO features.big_streets VALUES ('R1R2', -- Feature name
+INSERT INTO features.big_streets(feature_name, feature)VALUES ('R1R2', -- Feature name
   topology.CreateTopoGeom(
     'city_data', -- Topology name
     2, -- Topology geometry type (lineal)
@@ -64,7 +64,7 @@ INSERT INTO features.big_streets VALUES ('R1R2', -- Feature name
      WHERE feature_name in ('R1','R2')) -- R1 and R2
   ));
 
-INSERT INTO features.big_streets VALUES ('R4', -- Feature name
+INSERT INTO features.big_streets(feature_name, feature) VALUES ('R4', -- Feature name
   topology.CreateTopoGeom(
     'city_data', -- Topology name
     2, -- Topology geometry type (lineal)
@@ -79,17 +79,17 @@ INSERT INTO features.big_streets VALUES ('R4', -- Feature name
 --
 
 CREATE TABLE features.big_signs (
-	feature_name varchar primary key
-) WITH OIDS;
+	feature_name varchar primary key, fid serial
+) ;
 
 SELECT topology.AddTopoGeometryColumn('city_data', 'features',
 	'big_signs', 'feature', 'POINT',
 	2 -- the traffic_signs
 );
 
-SELECT AddGeometryColumn('features','big_signs','the_geom',-1,'MULTIPOINT',2);
+SELECT AddGeometryColumn('features','big_signs','the_geom',0,'MULTIPOINT',2);
 
-INSERT INTO features.big_signs VALUES ('S1S2', -- Feature name
+INSERT INTO features.big_signs(feature_name, feature) VALUES ('S1S2', -- Feature name
   topology.CreateTopoGeom(
     'city_data', -- Topology name
     1, -- Topology geometry type (point/multipoint)
