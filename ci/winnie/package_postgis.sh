9eb8a0fa51bb34c7f4f1dfa20f820b23357356b8
Regina Obe <lr@pcorp.us>
2018-12-12 06:04:16 +0000

    Flip to testing GEOS 3.8 for winnie
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17103 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/winnie/package_postgis.sh
+++ ci/winnie/package_postgis.sh
@@ -12,8 +12,8 @@
 #export OS_BUILD=32
 
 #export GCC_TYPE=
-export SFCGAL_VER=1.3.2
-export GEOS_VER=3.7.0
+export SFCGAL_VER=1.3.6
+export GEOS_VER=3.8.0dev
 export GDAL_VER=2.2.4
 export PROJ_VER=4.9.3
 export SFCGAL_VER=1.3.2
