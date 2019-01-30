26698e734e5e086ed4a1aa4e2c340204166c7911
Raul Marin <git@rmr.ninja>
2019-01-30 19:35:22 +0100

    Remove codecov script from repo (downloaded when needed)

--- ci/travis/run_coverage.sh
+++ ci/travis/run_coverage.sh
@@ -9,4 +9,5 @@ LDFLAGS_COV="--coverage"
 ./autogen.sh
 ./configure CFLAGS="${CFLAGS_COV}" LDFLAGS="${LDFLAGS_COV}" --enable-debug
 make -j check
-bash .github/codecov.bash -a "-l" -Z
+curl -S -f https://codecov.io/bash -o .github/codecov.bash
+bash .github/codecov.bash
