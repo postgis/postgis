0032c8817db651a1267c79e53cc44d4fc389fbcc
Paul Ramsey <pramsey@cleverelephant.ca>
2019-03-02 07:23:06 -0800

    Oops, wrong index strat for coveredby

--- postgis/gserialized_supportfn.c
+++ postgis/gserialized_supportfn.c
@@ -79,7 +79,7 @@ const IndexableFunction IndexableFunctions[] = {
 	{"st_touches", RTOverlapStrategyNumber, 2, 0},
 	{"st_3dintersects", RTOverlapStrategyNumber, 2, 0},
 	{"st_containsproperly", RTContainsStrategyNumber, 2, 0},
-	{"st_coveredby", RTContainsStrategyNumber, 2, 0},
+	{"st_coveredby", RTContainedByStrategyNumber, 2, 0},
 	{"st_overlaps", RTOverlapStrategyNumber, 2, 0},
 	{"st_covers", RTContainsStrategyNumber, 2, 0},
 	{"st_crosses", RTOverlapStrategyNumber, 2, 0},
