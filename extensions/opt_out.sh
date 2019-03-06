795cf2ac85eada6c3b64eae5b8908c075820cbaf
Sandro Santilli <strk@kbt.io>
2019-03-06 14:57:42 +0000

    Include raster and sfcgal extensions in opt_out.sh
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17293 b70326c6-7e19-0410-871a-916f4a2858ee

--- extensions/opt_out.sh
+++ extensions/opt_out.sh
@@ -9,7 +9,40 @@ db="$1"
 
 psql -XtA ${db} <<'EOF' | psql -XtA ${db}
 
+-----------------
+-- for sfcgal
+-----------------
+
+SELECT 'ALTER EXTENSION ' || extname || ' DROP ' || regexp_replace(
+    regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
+    E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
+FROM pg_catalog.pg_depend AS d
+INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
+WHERE d.refclassid = 'pg_catalog.pg_extension'::pg_catalog.regclass
+AND deptype = 'e' AND e.extname = 'postgis_sfcgal'
+ORDER BY sqladd;
+
+SELECT 'DROP EXTENSION postgis_sfcgal;';
+
+-----------------
+-- for raster
+-----------------
+
+SELECT 'ALTER EXTENSION ' || extname || ' DROP ' || regexp_replace(
+    regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
+    E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
+FROM pg_catalog.pg_depend AS d
+INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
+WHERE d.refclassid = 'pg_catalog.pg_extension'::pg_catalog.regclass
+AND deptype = 'e' AND e.extname = 'postgis_raster'
+ORDER BY sqladd;
+
+SELECT 'DROP EXTENSION postgis_raster;';
+
+-----------------
 -- for topology
+-----------------
+
 SELECT 'ALTER EXTENSION ' || extname || ' DROP ' || regexp_replace(
     regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
     E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
@@ -21,7 +54,10 @@ ORDER BY sqladd;
 
 SELECT 'DROP EXTENSION postgis_topology;';
 
--- for postgis
+--------------------
+-- for postgis core
+--------------------
+
 SELECT 'ALTER EXTENSION ' || extname || ' DROP ' || regexp_replace(
     regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
     E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
