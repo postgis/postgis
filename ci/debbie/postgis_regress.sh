198bd12a378e1af7e44f6b143d9fb8aa4acd0161
Regina Obe <lr@pcorp.us>
2019-04-14 01:42:07 +0000

    Change to use custom built sfcgal (packaged one drags in old gdal and openscenegraph still, so lets see if using our custom works)
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17389 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/debbie/postgis_regress.sh
+++ ci/debbie/postgis_regress.sh
@@ -11,6 +11,7 @@ set -e
 # export JENKINS_HOME=/var/lib/jenkins/workspace
 # export GEOS_VER=3.6.0dev
 # export GDAL_VER=2.0
+# export SFCGAL_VER=1.3
 export MAKE_GARDEN=0
 export MAKE_EXTENSION=1
 export DUMP_RESTORE=0
@@ -31,7 +32,7 @@ rm -rf ${WORKSPACE}/tmp/${POSTGIS_MAJOR_VERSION}_${POSTGIS_MINOR_VERSION}_pg${PG
 export PGIS_REG_TMPDIR=${WORKSPACE}/tmp/${POSTGIS_MAJOR_VERSION}_${POSTGIS_MINOR_VERSION}_pg${PG_VER}w${OS_BUILD}
 
 #adding this sleep so postgres instance has some grace period for starting
-#otherwise the attempt to drop the database, sometimes happes when pg is in middle of start
+#otherwise the attempt to drop the database, sometimes happens when pg is in middle of start
 for i in {0..60}; do psql -c 'select;' && break; sleep 0.5; done
 
 export POSTGIS_REGRESS_DB="postgis_reg" # FIXME: tweak to avoid clashes
@@ -63,7 +64,7 @@ fi
     --with-pgconfig=${PROJECTS}/pg/rel/pg${PG_VER}w${OS_BUILD}/bin/pg_config \
     --with-geosconfig=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}/bin/geos-config \
     --with-gdalconfig=${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}/bin/gdal-config \
-    --with-sfcgal=/usr/bin/sfcgal-config --without-interrupt-tests \
+    --with-sfcgal=/var/lib/jenkins/workspace/sfcgal/rel-sfcgal-${SFCGAL_VER}w${OS_BUILD} --without-interrupt-tests \
     --prefix=${PROJECTS}/pg/rel/pg${PG_VER}w${OS_BUILD}
 make clean
 make
