3b260e78e7569581d6bf7b53a683ad03ad204371
Darafei Praliaskouski <me@komzpa.net>
2019-04-20 15:56:58 +0300

    make garden report coverage

--- ci/travis/run_garden.sh
+++ ci/travis/run_garden.sh
@@ -2,11 +2,13 @@
 set -e
 
 # Debug-friendly flags
-CFLAGS_STD="-g -O0 -mtune=generic -fno-omit-frame-pointer"
-LDFLAGS_STD=""
+CFLAGS_STD="-g -O0 -mtune=generic -fno-omit-frame-pointer --coverage"
+LDFLAGS_STD="--coverage"
 
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
 ./autogen.sh
-./configure CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}"
+./configure CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}" --enable-debug
 make -j
 bash ./ci/travis/logbt -- make garden
+curl -S -f https://codecov.io/bash -o .github/codecov.bash
+bash .github/codecov.bash
