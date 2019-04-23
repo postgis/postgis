365e0f4ad06f51626613a62f40a22e0fa689e27f
Raul Marin <git@rmr.ninja>
2019-04-23 15:28:51 +0200

    Add implicit-conversion sanitizer to the clang ubsan build

--- ci/travis/run_usan_clang.sh
+++ ci/travis/run_usan_clang.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Enable undefined behaviour sanitizer using traps
-CFLAGS_STD="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
+CFLAGS_USAN="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined,implicit-conversion -fsanitize-undefined-trap-on-error -fno-sanitize-recover=implicit-conversion"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
 ./autogen.sh
 
 # Build with Clang and usan flags
-./configure CC=clang CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}"
+./configure CC=clang CFLAGS="${CFLAGS_USAN}" LDFLAGS="${LDFLAGS_STD}"
 bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
