b94b5eff947cbd4a53c840bb60d6d709a885efaa
Raul Marin <git@rmr.ninja>
2019-04-24 15:57:42 +0200

    Clang ubsan: Preload asan runtime

--- ci/travis/run_usan_clang.sh
+++ ci/travis/run_usan_clang.sh
@@ -5,9 +5,15 @@ set -e
 CFLAGS_USAN="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined,implicit-conversion -fsanitize-undefined-trap-on-error -fno-sanitize-recover=implicit-conversion"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
-/usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
-./autogen.sh
+# Sanitizer options to continue avoid stopping the runs on leaks (expected on postgres binaries)
+export ASAN_OPTIONS=halt_on_error=false,leak_check_at_exit=false,exitcode=0
+export MSAN_OPTIONS=halt_on_error=false,leak_check_at_exit=false,exitcode=0
+
+#Run postgres preloading sanitizer libs
+LD_PRELOAD=/usr/lib/clang/7/lib/linux/libclang_rt.asan-x86_64.so /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
+
 
 # Build with Clang and usan flags
+./autogen.sh
 ./configure CC=clang CFLAGS="${CFLAGS_USAN}" LDFLAGS="${LDFLAGS_STD}"
 bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
