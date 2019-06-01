bd7e77d74699a50540727b8b1436338638f884dd
Raúl Marín Rodríguez <rmrodriguez@carto.com>
2019-06-01 16:24:53 +0000

    Add removed file
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17454 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ ci/travis/run_nowagyu.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+WARNINGS="-Werror -Wall -Wextra -Wformat -Werror=format-security"
+WARNINGS_DISABLED="-Wno-unused-parameter -Wno-implicit-fallthrough -Wno-cast-function-type"
+
+# Build with coverage
+CFLAGS="-g -O0 --coverage -mtune=generic -fno-omit-frame-pointer ${WARNINGS} ${WARNINGS_DISABLED}"
+LDFLAGS="--coverage"
+
+/usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
+./autogen.sh
+
+# Standard build
+./configure --without-wagyu CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
+bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
+curl -S -f https://codecov.io/bash -o .github/codecov.bash
+bash .github/codecov.bash
