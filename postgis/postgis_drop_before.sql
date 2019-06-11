4999131948f6625853923b1ce893c1aed47748aa
Raul Marin <git@rmr.ninja>
2019-06-11 18:18:33 +0200

    postgis_drop_before.sql: Spaces -> Tabs

--- postgis/postgis_drop_before.sql
+++ postgis/postgis_drop_before.sql
@@ -23,40 +23,40 @@
 -- Helper function to drop functions when they match the full signature
 -- Requires schema, name and __identity_arguments__ as extracted from pg_catalog
 CREATE OR REPLACE FUNCTION _postgis_drop_function_if_needed(
-    function_schema text,
-    function_name text,
-    function_arguments text) RETURNS void AS $$
+	function_schema text,
+	function_name text,
+	function_arguments text) RETURNS void AS $$
 DECLARE
-    frec RECORD;
-    sql_drop text;
+	frec RECORD;
+	sql_drop text;
 BEGIN
-    FOR frec IN
-        SELECT  p.oid as oid,
-                n.nspname as schema,
-                p.proname as name,
-                pg_catalog.pg_get_function_arguments(p.oid) as arguments,
-                pg_catalog.pg_get_function_identity_arguments(p.oid) as identity_arguments
-            FROM pg_catalog.pg_proc p
-            LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
-            WHERE
-                LOWER(n.nspname) = LOWER(function_schema) AND
-                LOWER(p.proname) = LOWER(function_name) AND
-                LOWER(pg_catalog.pg_get_function_arguments(p.oid)) ~ LOWER(function_arguments) AND
-                pg_catalog.pg_function_is_visible(p.oid)
-            ORDER BY 1, 2, 4
-    LOOP
-        sql_drop := 'DROP FUNCTION ' || quote_ident(frec.schema) || '.' || quote_ident(frec.name) || ' ( ' || frec.identity_arguments || ' ) ';
-        RAISE DEBUG 'Name (%): %', frec.oid, frec.name;
-        RAISE DEBUG 'Arguments: %', frec.arguments;
-        RAISE DEBUG 'Identity arguments: %', frec.identity_arguments;
-        RAISE DEBUG 'SQL query: %', sql_drop;
-        BEGIN
-            EXECUTE sql_drop;
-        EXCEPTION
-            WHEN OTHERS THEN
-                RAISE EXCEPTION 'Could not drop function %.%. You might need to drop dependant objects. Postgres error: %', function_schema, function_name, SQLERRM;
-        END;
-    END LOOP;
+	FOR frec IN
+		SELECT  p.oid as oid,
+				n.nspname as schema,
+				p.proname as name,
+				pg_catalog.pg_get_function_arguments(p.oid) as arguments,
+				pg_catalog.pg_get_function_identity_arguments(p.oid) as identity_arguments
+			FROM pg_catalog.pg_proc p
+			LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+			WHERE
+				LOWER(n.nspname) = LOWER(function_schema) AND
+				LOWER(p.proname) = LOWER(function_name) AND
+				LOWER(pg_catalog.pg_get_function_arguments(p.oid)) ~ LOWER(function_arguments) AND
+				pg_catalog.pg_function_is_visible(p.oid)
+			ORDER BY 1, 2, 4
+	LOOP
+		sql_drop := 'DROP FUNCTION ' || quote_ident(frec.schema) || '.' || quote_ident(frec.name) || ' ( ' || frec.identity_arguments || ' ) ';
+		RAISE DEBUG 'Name (%): %', frec.oid, frec.name;
+		RAISE DEBUG 'Arguments: %', frec.arguments;
+		RAISE DEBUG 'Identity arguments: %', frec.identity_arguments;
+		RAISE DEBUG 'SQL query: %', sql_drop;
+		BEGIN
+			EXECUTE sql_drop;
+		EXCEPTION
+			WHEN OTHERS THEN
+				RAISE EXCEPTION 'Could not drop function %.%. You might need to drop dependant objects. Postgres error: %', function_schema, function_name, SQLERRM;
+		END;
+	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -64,31 +64,31 @@ $$ LANGUAGE plpgsql;
 -- FUNCTION AddGeometryColumn signature dropped
 -- (catalog_name character varying, schema_name character varying, table_name character varying, column_name character varying, new_srid integer, new_type character varying, new_dim integer, use_typmod boolean)
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    'AddGeometryColumn',
-    'catalog_name character varying, schema_name character varying, table_name character varying, column_name character varying, new_srid integer, new_type character varying, new_dim integer, use_typmod boolean'
-    );
+	(
+	'@extschema@',
+	'AddGeometryColumn',
+	'catalog_name character varying, schema_name character varying, table_name character varying, column_name character varying, new_srid integer, new_type character varying, new_dim integer, use_typmod boolean'
+	);
 
 -- FUNCTION ST_AsX3D was changed to add versioning for 2.0
 -- (geom geometry, prec integer, options integer)
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    'ST_AsX3D',
-    'geom geometry, prec integer, options integer'
-    );
+	(
+	'@extschema@',
+	'ST_AsX3D',
+	'geom geometry, prec integer, options integer'
+	);
 
 -- FUNCTION UpdateGeometrySRID changed the name of the args (http://trac.osgeo.org/postgis/ticket/1606) for 2.0
 -- It changed the paramenter `new_srid` to `new_srid_in`
 -- (catalogn_name character varying, schema_name character varying, table_name character varying, column_name character varying, new_srid integer)
 -- Dropping it conditionally since the same signature still exists.
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    'UpdateGeometrySRID',
-    'catalogn_name character varying, schema_name character varying, table_name character varying, column_name character varying, new_srid integer'
-    );
+	(
+	'@extschema@',
+	'UpdateGeometrySRID',
+	'catalogn_name character varying, schema_name character varying, table_name character varying, column_name character varying, new_srid integer'
+	);
 
 
 --deprecated and removed in 2.1
@@ -128,50 +128,50 @@ $$ ;
 -- FUNCTION ST_AsLatLonText went from multiple signatures to a single one with defaults for 2.2.0
 DROP FUNCTION IF EXISTS ST_AsLatLonText(geometry); -- Does not conflict
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    'ST_AsLatLonText',
-    'geometry, text'
-    );
+	(
+	'@extschema@',
+	'ST_AsLatLonText',
+	'geometry, text'
+	);
 
 
 -- FUNCTION _st_linecrossingdirection changed argument names in 3.0
 -- Was (geom1 geometry, geom2 geometry) and now (line1 geometry, line2 geometry)
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    '_st_linecrossingdirection',
-    'geom1 geometry, geom2 geometry'
-    );
+	(
+	'@extschema@',
+	'_st_linecrossingdirection',
+	'geom1 geometry, geom2 geometry'
+	);
 
 
 -- FUNCTION ST_LineCrossingDirection changed argument names in 3.0
 -- Was (geom1 geometry, geom2 geometry) and now (line1 geometry, line2 geometry)
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    'ST_LineCrossingDirection',
-    'geom1 geometry, geom2 geometry'
-    );
+	(
+	'@extschema@',
+	'ST_LineCrossingDirection',
+	'geom1 geometry, geom2 geometry'
+	);
 
 
 -- FUNCTION _st_orderingequals changed argument names in 3.0
 -- Was (GeometryA geometry, GeometryB geometry) and now (geom1 geometry, geom2 geometry)
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    '_st_orderingequals',
-    'GeometryA geometry, GeometryB geometry'
-    );
+	(
+	'@extschema@',
+	'_st_orderingequals',
+	'GeometryA geometry, GeometryB geometry'
+	);
 
 -- FUNCTION st_orderingequals changed argument names in 3.0
 -- Was (GeometryA geometry, GeometryB geometry) and now (geom1 geometry, geom2 geometry)
 SELECT _postgis_drop_function_if_needed
-    (
-    '@extschema@',
-    'st_orderingequals',
-    'GeometryA geometry, GeometryB geometry'
-    );
+	(
+	'@extschema@',
+	'st_orderingequals',
+	'GeometryA geometry, GeometryB geometry'
+	);
 
 
 -- FUNCTION st_askml changed to add defaults in 3.0 / r17357
@@ -197,35 +197,35 @@ DROP FUNCTION IF EXISTS ST_CurveToLine(geometry); -- Does not conflict
 DO  language 'plpgsql' $$
 BEGIN
 	IF EXISTS
-        (
-            WITH oids AS
-            (
-                SELECT c.oid as oid,
-                    n.nspname,
-                    c.relname
-                    FROM pg_catalog.pg_class c
-                    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                    WHERE c.relname = 'geometry_columns' AND
-                        n.nspname = 'public'
-                    AND pg_catalog.pg_table_is_visible(c.oid)
-                    ORDER BY 2, 3
+		(
+			WITH oids AS
+			(
+				SELECT c.oid as oid,
+					n.nspname,
+					c.relname
+					FROM pg_catalog.pg_class c
+					LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+					WHERE c.relname = 'geometry_columns' AND
+						n.nspname = 'public'
+					AND pg_catalog.pg_table_is_visible(c.oid)
+					ORDER BY 2, 3
 
-            ),
-            name_attribute AS
-            (
-                SELECT  a.attname as attname,
-                        pg_catalog.format_type(a.atttypid, a.atttypmod) as format_type
-                        FROM pg_catalog.pg_attribute a, oids
-                        WHERE a.attrelid = oids.oid AND a.attnum > 0 AND NOT a.attisdropped
-                        ORDER BY a.attnum
-            )
-            SELECT attname, format_type
-            FROM name_attribute
-            WHERE attname = 'f_table_schema' AND format_type = 'character varying(256)'
-        )
-        THEN
-            DROP VIEW geometry_columns;
-        END IF;
+			),
+			name_attribute AS
+			(
+				SELECT  a.attname as attname,
+						pg_catalog.format_type(a.atttypid, a.atttypmod) as format_type
+						FROM pg_catalog.pg_attribute a, oids
+						WHERE a.attrelid = oids.oid AND a.attnum > 0 AND NOT a.attisdropped
+						ORDER BY a.attnum
+			)
+			SELECT attname, format_type
+			FROM name_attribute
+			WHERE attname = 'f_table_schema' AND format_type = 'character varying(256)'
+		)
+		THEN
+			DROP VIEW geometry_columns;
+		END IF;
 END;
 $$;
 
