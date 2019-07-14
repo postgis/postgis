dea8f08ac368eca009344c38b98867fffb37c61a
Regina Obe <lr@pcorp.us>
2019-07-14 04:15:41 +0000

    revise to have tmp folder in code folder to try to get around issue can't create directory
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17597 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/berrie/postgis_regress.sh
+++ ci/berrie/postgis_regress.sh
@@ -1,12 +1,12 @@
 #Berrie is a 32-bit Rasberry Pi managed by Bruce Rindahl
 #BRANCH is passed in via jenkins which is set via gitea web hook
-mkdir -p ~/tmp
+mkdir -p tmp
 sh autogen.sh
 ./configure
 make clean
 make
 export PGUSER=postgres
-export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${BRANCH}
+export PGIS_REG_TMPDIR=tmp
 psql -c "DROP DATABASE IF EXISTS postgis_reg;"
 make check RUNTESTFLAGS="-v"
 #make install
