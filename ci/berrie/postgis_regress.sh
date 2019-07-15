618a67b1d6fc223dd5a4c0b02c824939f21dbd65
Regina Obe <lr@pcorp.us>
2019-07-15 23:14:22 +0000

    Try to get berrie to use workspace installed pg12 to get around make install installing in wrong folder References #4456
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17605 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/berrie/postgis_regress.sh
+++ ci/berrie/postgis_regress.sh
@@ -1,13 +1,45 @@
 #Berrie is a 32-bit Rasberry Pi managed by Bruce Rindahl
 #BRANCH is passed in via jenkins which is set via gitea web hook
-mkdir -p tmp
+export WORKSPACE=~/workspace
+export OS_BUILD=32
+export PG_VER=12
+export PGPATH=${WORKSPACE}/postgresql/rel/pg${PG_VER}w${OS_BUILD}
+export PATH=${PATH}:${PGPATH}/bin:${PGPATH}/lib
+export PGPORT=55432
+export PGDATA=${WORKSPACE}/postgresql/rel/pg${PG_VER}w${OS_BUILD}/data_${PGPORT}
+# What to use to start up the postmaster (we do NOT use pg_ctl for this,
+# as it adds no value and can cause the postmaster to misrecognize a stale
+# lock file)
+DAEMON="$PGPATH/bin/postmaster"
+
+# What to use to shut down the postmaster
+PGCTL="$PDPATH/bin/pg_ctl"
+
+# remove cluster if exists
+if [ -d $PGDATA ] ; then
+    rm $PGDATA
+fi;
+
+#initialize cluster
+$PGPATH/bin/initdb
+
+echo -n "Starting PostgreSQL: "
+$DAEMON
+sleep 5
+echo "ok"
+
 sh autogen.sh
-./configure
+./configure --with-pgconfig=${PGPATH}/bin/pg_config
 make clean
 make
 export PGUSER=postgres
 #export PGIS_REG_TMPDIR=tmp
 psql -c "DROP DATABASE IF EXISTS postgis_reg;"
 make check RUNTESTFLAGS="-v"
-sudo make install
+make install
 make check RUNTESTFLAGS="-v --extension"
+
+echo -n "Stopping PostgreSQL: "
+$PGCTL stop -D $PGDATA -s -m fast
+echo "ok"
+;;
