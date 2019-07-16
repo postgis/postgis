0c3e91c637f095c7f01387898567b1553b223dba
Regina Obe <lr@pcorp.us>
2019-07-16 14:19:07 +0000

    Revise berrie to use her own custom PostgreSQL build -- evidentally installing past the jenkins workspace is a no no for some reason
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17610 b70326c6-7e19-0410-871a-916f4a2858ee

---
+++ ci/berrie/pg_init_start.sh
@@ -0,0 +1,37 @@
+#Berrie is a 32-bit Rasberry Pi managed by Bruce Rindahl
+#This is script to launch custom compiled PostgreSQL
+export WORKSPACE=/home/jenkins/workspace
+
+cd ${WORKSPACE}/PostGIS_Worker_Run/label/berrie/$BRANCH
+export OS_BUILD=32
+export PG_VER=13
+export PGPATH=${WORKSPACE}/pg/label/berrie/rel/pg${PG_VER}w${OS_BUILD}
+export PATH=${PATH}:${PGPATH}/bin:${PGPATH}/lib
+export PGPORT=55532
+export PGDATA=$PGPATH/data_${PGPORT}
+export PGLOG="$PGDATA/pgsql.log"
+# What to use to start up the postmaster (we do NOT use pg_ctl for this,
+# as it adds no value and can cause the postmaster to misrecognize a stale
+# lock file)
+DAEMON="$PGPATH/bin/pg_ctl -D $PGDATA -l logfile start"
+
+# What to use to shut down the postmaster
+PGCTL="$PGPATH/bin/pg_ctl"
+
+# remove cluster if exists
+if [ -d $PGDATA ] ; then
+    if [ -d $PGDATA/postmaster.pid] ; then
+    	$PGCTL stop -D $PGDATA -s -m fast
+    fi;
+	
+    rm -rf $PGDATA
+fi;
+
+#initialize cluster
+$PGPATH/bin/initdb
+
+echo -n "Starting PostgreSQL: "
+$DAEMON &
+sleep 20
+echo "ok"
+exit 0
--- ci/berrie/postgis_regress.sh
+++ ci/berrie/postgis_regress.sh
@@ -1,45 +1,27 @@
 #Berrie is a 32-bit Rasberry Pi managed by Bruce Rindahl
 #BRANCH is passed in via jenkins which is set via gitea web hook
-export WORKSPACE=~/workspace
+#export BRANCH=618a67b1d6fc223dd5a4c0b02c824939f21dbd65
+export WORKSPACE=/home/jenkins/workspace
+
+cd ${WORKSPACE}/PostGIS_Worker_Run/label/berrie/$BRANCH
 export OS_BUILD=32
-export PG_VER=12
-export PGPATH=${WORKSPACE}/postgresql/rel/pg${PG_VER}w${OS_BUILD}
+export PG_VER=13
+export PGPATH=${WORKSPACE}/pg/label/berrie/rel/pg${PG_VER}w${OS_BUILD}
+
 export PATH=${PATH}:${PGPATH}/bin:${PGPATH}/lib
-export PGPORT=55432
-export PGDATA=${WORKSPACE}/postgresql/rel/pg${PG_VER}w${OS_BUILD}/data_${PGPORT}
-# What to use to start up the postmaster (we do NOT use pg_ctl for this,
-# as it adds no value and can cause the postmaster to misrecognize a stale
-# lock file)
-DAEMON="$PGPATH/bin/postmaster"
-
-# What to use to shut down the postmaster
-PGCTL="$PDPATH/bin/pg_ctl"
-
-# remove cluster if exists
-if [ -d $PGDATA ] ; then
-    rm $PGDATA
-fi;
-
-#initialize cluster
-$PGPATH/bin/initdb
-
-echo -n "Starting PostgreSQL: "
-$DAEMON
-sleep 5
-echo "ok"
+export PGPORT=55532
+export PGDATA=$PGPATH/data_${PGPORT}
+export PGHOST=localhost
 
 sh autogen.sh
 ./configure --with-pgconfig=${PGPATH}/bin/pg_config
-make clean
-make
-export PGUSER=postgres
-#export PGIS_REG_TMPDIR=tmp
-psql -c "DROP DATABASE IF EXISTS postgis_reg;"
-make check RUNTESTFLAGS="-v"
+#make clean
+#make
+
+#make check RUNTESTFLAGS="-v"
 make install
 make check RUNTESTFLAGS="-v --extension"
 
-echo -n "Stopping PostgreSQL: "
-$PGCTL stop -D $PGDATA -s -m fast
-echo "ok"
-;;
+if [ -d $PGDATA/postmaster.pid ] ; then
+	$PGCTL stop -D $PGDATA -s -m fast
+fi
