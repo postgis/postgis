f600b9925b6660b4793bd5231d2c8da62e495c2c
Paul Ramsey <pramsey@cleverelephant.ca>
2019-08-14 12:07:02 -0700

    Add the library in linking step, hm

--- postgis/postgis_libprotobuf.c
+++ postgis/postgis_libprotobuf.c
@@ -14,12 +14,12 @@
 PG_FUNCTION_INFO_V1(postgis_libprotobuf_version);
 Datum postgis_libprotobuf_version(PG_FUNCTION_ARGS)
 {
-#ifndef HAVE_LIBPROTOBUF
-	PG_RETURN_NULL();
-#elif LIBPROTOBUF_VERSION >= 20600 /* Older versions don't have  */
+#ifdef HAVE_LIBPROTOBUF && LIBPROTOBUF_VERSION >= 20600
 	const char *ver = protobuf_c_version();
 	text *result = cstring_to_text(ver);
 	PG_RETURN_POINTER(result);
+#else
+	PG_RETURN_NULL();
 #endif
 }
 
