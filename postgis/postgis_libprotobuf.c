713fbda7b2919589b09b74b46cd6e2cb42a630b6
Paul Ramsey <pramsey@cleverelephant.ca>
2019-08-14 22:07:40 +0000

    Reorganize protobuf detection so that separate directories can be specified, and make version detection more robust over different protobuf installs. References #4481
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17711 b70326c6-7e19-0410-871a-916f4a2858ee

--- postgis/postgis_libprotobuf.c
+++ postgis/postgis_libprotobuf.c
@@ -14,12 +14,12 @@
 PG_FUNCTION_INFO_V1(postgis_libprotobuf_version);
 Datum postgis_libprotobuf_version(PG_FUNCTION_ARGS)
 {
-#ifndef HAVE_LIBPROTOBUF
-	PG_RETURN_NULL();
-#else /* HAVE_LIBPROTOBUF  */
+#ifdef HAVE_PROTOBUF_C_VERSION
 	const char *ver = protobuf_c_version();
 	text *result = cstring_to_text(ver);
 	PG_RETURN_POINTER(result);
+#else
+	PG_RETURN_NULL();
 #endif
 }
 
