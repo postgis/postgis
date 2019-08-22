0af30567cf840c904cf5c2871a129e25ef83a597
Sandro Santilli <strk@kbt.io>
2019-08-22 11:40:47 +0200

    [travis] Also check installs

--- ci/travis/run_tests.sh
+++ ci/travis/run_tests.sh
@@ -17,3 +17,10 @@ export CUNIT_VALGRIND_FLAGS="--leak-check=full --error-exitcode=1"
 # Standard build
 ./configure CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}"
 bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
+
+# Install
+sudo make install
+
+# Install-check
+bash ./ci/travis/logbt -- make installcheck RUNTESTFLAGS=--verbose
+
