e8b5c3993e288e00a840b7a41f643901a280f5a9
Sandro Santilli <strk@kbt.io>
2019-08-22 18:07:17 +0000

    [travis] don't assume "make check" will work parallel w/out "make"
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17766 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/travis/run_coverage.sh
+++ ci/travis/run_coverage.sh
@@ -8,5 +8,6 @@ LDFLAGS_COV="--coverage"
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
 ./autogen.sh
 ./configure CFLAGS="${CFLAGS_COV}" LDFLAGS="${LDFLAGS_COV}" --enable-debug
+make -j
 make -j check
 (curl -S -f https://codecov.io/bash -o .github/codecov.bash && bash .github/codecov.bash) || echo "Coverage report failed"
