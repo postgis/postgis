3c236859f79a7a13b5e911b94baa89eabab5c973
Raúl Marín Rodríguez <rmrodriguez@carto.com>
2019-08-29 11:56:36 +0000

    Fix ST_Simplify ignoring the value of the 3rd parameter
    Closes #4492
    
    
    
    git-svn-id: http://svn.osgeo.org/postgis/branches/2.3@17785 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/simplify.sql
+++ regress/simplify.sql
@@ -12,3 +12,6 @@ SELECT '9', ST_astext(ST_Simplify('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 5, 5
 SELECT '10', ST_astext(ST_Simplify('LINESTRING(0 0, 0 10)', 20));
 SELECT '11', ST_astext(ST_Simplify('MULTIPOLYGON(((100 100, 100 130, 130 130, 130 100, 100 100)), ((0 0, 10 0, 10 10, 0 10, 0 0),(5 5, 5 6, 6 6, 8 5, 5 5)) )', 20));
 SELECT '12', ST_astext(ST_Simplify('MULTIPOLYGON(((0 0, 10 0, 10 10, 0 10, 0 0),(5 5, 5 6, 6 6, 8 5, 5 5)),((100 100, 100 130, 130 130, 130 100, 100 100)))', 20));
+
+SELECT '13', ST_astext(ST_Simplify('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))', 20, false));
+SELECT '14', ST_astext(ST_Simplify('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))', 20, true));
