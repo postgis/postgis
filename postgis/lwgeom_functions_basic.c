7a6ab51d90a001f5acd236283a0f662bdc9b36dc
Raúl Marín Rodríguez <rmrodriguez@carto.com>
2019-09-16 12:32:05 +0000

    Speed up ST_RemoveRepeatedPoints
    Closes #4491
    Closes https://github.com/postgis/postgis/pull/468
    
    
    
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17812 b70326c6-7e19-0410-871a-916f4a2858ee

--- postgis/lwgeom_functions_basic.c
+++ postgis/lwgeom_functions_basic.c
@@ -2748,12 +2748,12 @@ Datum ST_RemoveRepeatedPoints(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(ST_RemoveRepeatedPoints);
 Datum ST_RemoveRepeatedPoints(PG_FUNCTION_ARGS)
 {
-	GSERIALIZED *g_in = PG_GETARG_GSERIALIZED_P(0);
-	int type = gserialized_get_type(g_in);
+	GSERIALIZED *g_in = PG_GETARG_GSERIALIZED_P_COPY(0);
+	uint32_t type = gserialized_get_type(g_in);
 	GSERIALIZED *g_out;
 	LWGEOM *lwgeom_in = NULL;
-	LWGEOM *lwgeom_out = NULL;
 	double tolerance = 0.0;
+	int modified = LW_FALSE;
 
 	/* Don't even start to think about points */
 	if (type == POINTTYPE)
@@ -2763,22 +2763,16 @@ Datum ST_RemoveRepeatedPoints(PG_FUNCTION_ARGS)
 		tolerance = PG_GETARG_FLOAT8(1);
 
 	lwgeom_in = lwgeom_from_gserialized(g_in);
-	lwgeom_out = lwgeom_remove_repeated_points(lwgeom_in, tolerance);
-
-	/* COMPUTE_BBOX TAINTING */
-	if (lwgeom_in->bbox)
-		lwgeom_refresh_bbox(lwgeom_out);
-
-	g_out = geometry_serialize(lwgeom_out);
-
-	if (lwgeom_out != lwgeom_in)
+	modified = lwgeom_remove_repeated_points_in_place(lwgeom_in, tolerance);
+	if (!modified)
 	{
-		lwgeom_free(lwgeom_out);
+		/* Since there were no changes, we can return the input to avoid the serialization */
+		PG_RETURN_POINTER(g_in);
 	}
 
-	lwgeom_free(lwgeom_in);
+	g_out = geometry_serialize(lwgeom_in);
 
-	PG_FREE_IF_COPY(g_in, 0);
+	pfree(g_in);
 	PG_RETURN_POINTER(g_out);
 }
 
