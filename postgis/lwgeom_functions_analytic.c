031428cf3e6767eaa6cfb57c55ce3848ce7bae2f
Raul Marin <git@rmr.ninja>
2019-09-20 18:46:00 +0200

    Revert "LWGEOM_simplify2d: Use geometry_serialize_reuse"
    This reverts commit c5fc253652503319d9af577d8df9e0a63fc126b6.

--- postgis/lwgeom_functions_analytic.c
+++ postgis/lwgeom_functions_analytic.c
@@ -88,7 +88,12 @@ Datum LWGEOM_simplify2d(PG_FUNCTION_ARGS)
 	if (!in || lwgeom_is_empty(in))
 		PG_RETURN_NULL();
 
-	result = geometry_serialize_reuse(in, geom);
+	/* COMPUTE_BBOX TAINTING */
+	if (in->bbox)
+		lwgeom_refresh_bbox(in);
+
+	result = geometry_serialize(in);
+	PG_FREE_IF_COPY(geom, 0);
 	PG_RETURN_POINTER(result);
 }
 
