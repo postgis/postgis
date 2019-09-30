7c11a87771347eb02f5a5eb8850ab1d1a36476de
Regina Obe <lr@pcorp.us>
2019-09-30 18:55:41 +0000

    Add CXX and CXXFLAGS from pg_config seems to fix wagyu load issue Closes #4474
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17838 b70326c6-7e19-0410-871a-916f4a2858ee

--- ci/bessie32/postgis_regress.sh
+++ ci/bessie32/postgis_regress.sh
@@ -1,7 +1,9 @@
 #export PATH=${PATH}:/usr/local:/usr/local/lib:/usr/local/bin
 sh autogen.sh
 CC=gcc8   \
-CFLAGS="-Wall -Wmissing-prototypes -Wpointer-arith -Wdeclaration-after-statement -Wendif-labels -Wmissing-format-attribute -Wformat-security -fno-strict-aliasing -fwrapv -fexcess-precision=standard -Wno-format-trunc" \
+CXX=g++8 \
+CXXFLAGS="-O2 -pipe  -fstack-protector-strong -Wl,-rpath=/usr/local/lib/gcc8  -nostdinc++ -isystem /usr/include/c++/v1 -Wl,-rpath=/usr/local/lib/gcc8" \
+CFLAGS="-Wall -Wmissing-prototypes -Wpointer-arith -Wendif-labels -Wmissing-format-attribute -Wformat-security -fno-strict-aliasing -fwrapv -fexcess-precision=standard -Wno-format-trunc" \
  ./configure --with-libiconv=/usr/local --without-interrupt-tests
 make clean
 make
