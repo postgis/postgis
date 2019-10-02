162f91c7ca4e2547909108bd6f39f173b5340433
Raul Marin <git@rmr.ninja>
2019-10-02 16:05:57 +0200

    Revert ci changes

--- ci/travis/run_usan_clang.sh
+++ ci/travis/run_usan_clang.sh
@@ -2,18 +2,12 @@
 set -e
 
 # Enable undefined behaviour sanitizer using traps
-CFLAGS_USAN="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined,implicit-conversion -fsanitize-undefined-trap-on-error -fno-sanitize-recover=implicit-conversion"
+CFLAGS_STD="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
-# Sanitizer options to continue avoid stopping the runs on leaks (expected on postgres binaries)
-export ASAN_OPTIONS=halt_on_error=false,leak_check_at_exit=false,exitcode=0
-export MSAN_OPTIONS=halt_on_error=false,leak_check_at_exit=false,exitcode=0
-
-#Run postgres preloading sanitizer libs
-LD_PRELOAD=/usr/lib/clang/8/lib/linux/libclang_rt.asan-x86_64.so /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
-
+/usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
+./autogen.sh
 
 # Build with Clang and usan flags
-./autogen.sh
-./configure CC=clang CFLAGS="${CFLAGS_USAN}" LDFLAGS="${LDFLAGS_STD}"
+./configure CC=clang CFLAGS="${CFLAGS_STD}" LDFLAGS="${LDFLAGS_STD}"
 bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
