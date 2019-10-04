f85c8e85dffac771fb7c65b5b1d8fa20e750e21c
Paul Ramsey <pramsey@cleverelephant.ca>
2019-10-04 08:30:37 -0700

    header guard

--- postgis/lwgeom_accum.h
+++ postgis/lwgeom_accum.h
@@ -1,3 +1,29 @@
+/**********************************************************************
+ *
+ * PostGIS - Spatial Types for PostgreSQL
+ * http://postgis.net
+ *
+ * PostGIS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PostGIS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PostGIS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************
+ *
+ * Copyright (c) 2019, Paul Ramsey <pramsey@cleverelephant.ca>
+ *
+ **********************************************************************/
+
+#ifndef _LWGEOM_ACCUM_H
+#define _LWGEOM_ACCUM_H 1
 
 /**
 ** To pass the internal state of our collection between the
@@ -13,3 +39,6 @@ typedef struct CollectionBuildState
 	Datum data[CollectionBuildStateDataSize];
 	Oid geomOid;
 } CollectionBuildState;
+
+
+#endif /* _LWGEOM_ACCUM_H */
