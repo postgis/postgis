70bf29bade8dcdaf73a1948316c8a3b2871ad0c3
Paul Ramsey <pramsey@cleverelephant.ca>
2019-10-07 22:58:12 +0000

    Remove view testing from upgrade objects testing References #4386
    git-svn-id: http://svn.osgeo.org/postgis/trunk@17882 b70326c6-7e19-0410-871a-916f4a2858ee

--- regress/run_test.pl
+++ regress/run_test.pl
@@ -309,14 +309,14 @@ sub create_upgrade_test_objects
     exit(1);
   }
 
-  my $query = "create view upgrade_view_test as ";
-  $query .= "select st_union(g1) from upgrade_test;";
-  my $ret = sql($query);
-  unless ( $ret =~ /^CREATE/ ) {
-    `dropdb $DB`;
-    print "\nSomething went wrong creating upgrade_view_test view: $ret.\n";
-    exit(1);
-  }
+  # my $query = "create view upgrade_view_test as ";
+  # $query .= "select st_union(g1) from upgrade_test;";
+  # my $ret = sql($query);
+  # unless ( $ret =~ /^CREATE/ ) {
+  #   `dropdb $DB`;
+  #   print "\nSomething went wrong creating upgrade_view_test view: $ret.\n";
+  #   exit(1);
+  # }
 
   if ( $OPT_WITH_RASTER )
   {
@@ -347,12 +347,12 @@ sub drop_upgrade_test_objects
 {
   # TODO: allow passing the "upgrade-cleanup" script via commandline
 
-  my $ret = sql("drop view upgrade_view_test;");
-  unless ( $ret =~ /^DROP/ ) {
-    `dropdb $DB`;
-    print "\nSomething went wrong dropping spatial view: $ret.\n";
-    exit(1);
-  }
+  # my $ret = sql("drop view upgrade_view_test;");
+  # unless ( $ret =~ /^DROP/ ) {
+  #   `dropdb $DB`;
+  #   print "\nSomething went wrong dropping spatial view: $ret.\n";
+  #   exit(1);
+  # }
 
   my $ret = sql("drop table upgrade_test;");
   unless ( $ret =~ /^DROP/ ) {
