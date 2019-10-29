76e34fa547b91f17ed1a274411578f76406f6784
Sandro Santilli <strk@kbt.io>
2019-10-29 16:22:20 +0100

    Drop SVN support for dist generator, add ChangeLog generation step
    [ci skip]

--- make_dist.sh
+++ make_dist.sh
@@ -33,32 +33,17 @@ if [ "$MAKE" = "" ]; then
     MAKE=make
 fi
 
-if [ -d ".git" ]; then
- git=yes
- # Extract tag from git or default to trunk
- tag=`git branch | grep \* | awk '{print $2}'`
-else
- git=no
- # Extract tag from svn or default to trunk
- tag=`svn info 2> /dev/null | grep ^URL | sed 's/.*\///'`
- [ -z "$tag" ] && tag=trunk
- [ "$tag" != "trunk" ] && tag=branches/$tag
-fi
+# Extract tag from git or default to trunk
+tag=`git branch | grep \* | awk '{print $2}'`
 
 if [ -n "$1" ]; then
   if [ "$1" = "-b" ]; then
     shift
-    if [ "$git" = "yes" ]; then
-      tag=svn-$1
-    else
-      tag="$1"
-      [ "$tag" != "trunk" ] && tag="branches/$tag"
-    fi
+    tag=svn-$1
     branch=yes
   else
+    tag=$1
     version="$1"
-    tag="tags/$version"
-    [ "$git" = "yes" ] && tag=$1
   fi
 fi
 
@@ -69,19 +54,7 @@ if [ -d "$outdir" ]; then
 	exit 1
 fi
 
-if [ "$git" = "no" ]; then
-  echo "Exporting tag $tag"
-  svnurl="http://svn.osgeo.org/postgis/$tag"
-  svn export $svnurl "$outdir"
-  if [ $? -gt 0 ]; then
-    exit 1
-  fi
-else
-  git clone -b $tag . $outdir || exit 1
-  #cd $outdir
-  #git checkout $tag || exit 1
-  #cd -
-fi
+git clone -b $tag . $outdir || exit 1
 
 echo "Removing make_dist.sh and HOWTO_RELEASE"
 rm -fv "$outdir"/make_dist.sh "$outdir"/HOWTO_RELEASE
@@ -95,10 +68,12 @@ cd "$outdir"
 # generating postgis_revision.h for >= 2.0.0 tags
 if test -f utils/repo_revision.pl; then
 	echo "Generating postgis_revision.h"
-	perl utils/repo_revision.pl $svnurl
+	perl utils/repo_revision.pl
 fi
-#make
+# generate ChangeLog
+make ChangeLog
 cd "$owd"
+
 
 # generating comments
 echo "Generating documentation"
@@ -130,7 +105,7 @@ if test "$version" = "dev"; then
   VMIN=`grep ^POSTGIS_MINOR_VERSION "$outdir"/Version.config | cut -d= -f2`
   VMIC=`grep ^POSTGIS_MICRO_VERSION "$outdir"/Version.config | cut -d= -f2`
   VREV=`cat "$outdir"/postgis_revision.h | awk '{print $3}'`
-  version="${VMAJ}.${VMIN}.${VMIC}-r${VREV}"
+  version="${VMAJ}.${VMIN}.${VMIC}-${VREV}"
   #if newoutdir is not already set, then set it
   if test "x$newoutdir" = "x"; then
       newoutdir=postgis-${version}
