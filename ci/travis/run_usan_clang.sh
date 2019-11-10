0cbdd4d61558320575c6ec9f7e775ed785bdcfee
Darafei Praliaskouski <me@komzpa.net>
2019-11-10 20:15:42 +0300

    Enable Link Time Optimization if available in compiler

--- ci/travis/run_usan_clang.sh
+++ ci/travis/run_usan_clang.sh
@@ -3,7 +3,7 @@ set -e
 
 # Enable undefined behaviour sanitizer using traps
 CFLAGS_USAN="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined,implicit-conversion -fsanitize-undefined-trap-on-error -fno-sanitize-recover=implicit-conversion"
-LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
+LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro -fsanitize=undefined,implicit-conversion -fsanitize-undefined-trap-on-error -fno-sanitize-recover=implicit-conversion"
 
 # Sanitizer options to continue avoid stopping the runs on leaks (expected on postgres binaries)
 export ASAN_OPTIONS=halt_on_error=false,leak_check_at_exit=false,exitcode=0
@@ -15,5 +15,5 @@ LD_PRELOAD=/usr/lib/clang/8/lib/linux/libclang_rt.asan-x86_64.so /usr/local/pgsq
 
 # Build with Clang and usan flags
 ./autogen.sh
-./configure CC=clang CFLAGS="${CFLAGS_USAN}" LDFLAGS="${LDFLAGS_STD}"
+./configure --enable-debug CC=clang CFLAGS="${CFLAGS_USAN}" LDFLAGS="${LDFLAGS_STD}"
 bash ./ci/travis/logbt -- make -j check RUNTESTFLAGS=--verbose
