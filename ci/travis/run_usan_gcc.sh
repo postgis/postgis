2839c9415a6ffa9ca4cd850e2119f1b9b877fef0
Darafei Praliaskouski <me@komzpa.net>
2019-11-22 21:40:49 +0300

    Revert "Enable Link Time Optimization if available in compiler"
    Reverted due to bug in FreeBSD and Windows linkers, more if's required to work around for them.
    This reverts commit 0cbdd4d61558320575c6ec9f7e775ed785bdcfee.

--- ci/travis/run_usan_gcc.sh
+++ ci/travis/run_usan_gcc.sh
@@ -3,7 +3,7 @@ set -e
 
 # Enable undefined behaviour sanitizer using traps to
 CFLAGS_STD="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined -fsanitize-undefined-trap-on-error"
-LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro -fsanitize=undefined"
+LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
 
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile start
 ./autogen.sh
