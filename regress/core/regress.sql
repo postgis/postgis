a0b49ba017ccdc4f5012f2dedf96ae980a4e79b7
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2019-12-09 13:59:45 -0500

    Fix unit tests

--- regress/core/regress.sql
+++ regress/core/regress.sql
@@ -280,7 +280,11 @@ select '238', ST_AsEWKT(ST_TileEnvelope(2, 1, 3, margin => 0.5));
 select '239', ST_AsEWKT(ST_TileEnvelope(0, 0, 0, margin => -0.5));
 select '240', ST_AsEWKT(ST_TileEnvelope(0, 0, 0, margin => -0.51));
 select '241', ST_AsEWKT(ST_TileEnvelope(0, 0, 0, margin => -0.4));
-
+select '250', ST_AsEWKT(ST_TileEnvelope(10,300,387));
+select '251', ST_AsEWKT(ST_TileEnvelope(10,300,387, margin => 0.1));
+select '252', ST_AsEWKT(ST_TileEnvelope(10,300,387, margin => 0.5));
+select '253', ST_AsEWKT(ST_TileEnvelope(10,300,387, margin => 2));
+select '254', ST_AsEWKT(ST_TileEnvelope(10,300,387, margin => -0.3));
 
 -- Drop test table
 DROP table test;
