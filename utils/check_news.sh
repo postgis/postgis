705f1e34ba08b2202adf8acb075125bd579c323d
Sandro Santilli <strk@kbt.io>
2019-12-20 16:13:03 +0100

    NEWS checker now also checks in stable branches for ticket refs
    This is to give an hint about when a ticket reference is really
    completely missing OR is belongs to an in-dev stable branch.

--- utils/check_news.sh
+++ utils/check_news.sh
@@ -21,7 +21,7 @@ while [ $# -gt 0 ]; do
     TICKET_REFS="yes"
   elif [ "$1" = "--ticket-refs-skip-commits" ]; then
     shift
-    TICKET_REFS_SKIP_COMMITS=$1
+    TICKET_REFS_SKIP_COMMITS=$( cd $( dirname $1 ) && pwd )
   elif [ -z "${RD}" ]; then
     RD=$1
   else
@@ -79,7 +79,38 @@ if test "${TICKET_REFS}" = "yes"; then
     { failures=0; while read ref; do
       ref="#${ref}"
       if ! grep -qw "${ref}" NEWS; then
-        echo "FAIL: Ticket ${ref} MISSING from NEWS"
+        # See if it is found in any stable branch older than this one
+        # and hint if found
+        foundin=
+        for b in $(
+            git branch --list stable-* |
+            sort -n | sed '/^\*/q' | tac |
+            grep -v '\*'
+            )
+        do
+          if git show $b:NEWS |
+              grep -qw "${ref}"
+          then
+            foundin=$b
+            break
+          fi
+        done
+        if test -n "$foundin"; then
+          # Check if the match is in a published version
+          if git show $foundin:NEWS |
+              sed '/^[0-9]\{4\}\/[0-9]\{2\}\/[0-9]\{2\}/q' |
+              grep -qw "${ref}"
+          then
+            # The match is in a still unpublished section, this should
+            # not be considered an error in development, but should be
+            # fixed before closing the releasing.
+            echo "WARN: Ticket ${ref} missing from NEWS (but found in dev section of branch $foundin)"
+          else
+            echo "FAIL: Ticket ${ref} missing from NEWS (and found in release from branch $foundin!)"
+          fi
+        else
+          echo "FAIL: Ticket ${ref} MISSING from NEWS (here and in older branches)"
+        fi
         failures=$((failures+1))
         #exit 1
       else
