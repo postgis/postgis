8ae06ea8248c0807e8eb615d1cff890fe80a8b70
Regina Obe <lr@pcorp.us>
2020-04-20 03:21:28 -0400

    Add alpine-edge to mix

---
+++ ci/dronie/postgis_regress_alpine.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit on first error
+set -e
+
+su - postgres -c "export PGDATA=/var/lib/postgresql/data && pg_ctl start"
+#export PGPORT=`grep ^port /var/lib/postgresql/postgresql.conf | awk '{print $3}'`
+export PGPORT=5432
+export PATH=/usr/bin:$PATH
+psql --version
+./autogen.sh
+./configure CFLAGS="-O2 -Wall -fno-omit-frame-pointer -Werror" --without-interrupt-tests
+make clean
+make -j
+# we should maybe wait for postgresql service to startup here...
+psql -c "select version()" template1
+RUNTESTFLAGS=-v make check
+make install
+RUNTESTFLAGS=-v make installcheck
+utils/check_all_upgrades.sh -s \
+  `grep '^POSTGIS_' Version.config | cut -d= -f2 | paste -sd '.'`
