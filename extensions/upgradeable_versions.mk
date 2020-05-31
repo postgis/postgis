54c7108861316d7511df42016afb9b5c5c4b8605
Regina Obe <lr@pcorp.us>
2020-05-31 15:25:24 -0400

    Update how to release, readme, and extension to inclde dev

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -36,7 +36,8 @@ UPGRADEABLE_VERSIONS = \
 	2.3.7 \
 	2.3.8 \
 	2.3.9 \
-	2.3.10
+	2.3.10 \
+	2.3.11dev
 
 # This is to avoid forcing "check-installed-upgrades" as a default
 # rule, see https://trac.osgeo.org/postgis/ticket/3420
