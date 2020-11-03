d1eed39947597725da795a18e841297fde80e223
Sandro Santilli <strk@kbt.io>
2020-11-03 19:07:26 +0100

    Travis: do not run check target in parallel when testing coverage

--- ci/travis/run_coverage.sh
+++ ci/travis/run_coverage.sh
@@ -8,5 +8,6 @@ LDFLAGS_COV="--coverage"
 /usr/local/pgsql/bin/pg_ctl -c -l /tmp/logfile -o '-F' start
 ./autogen.sh
 ./configure CFLAGS="${CFLAGS_COV}" LDFLAGS="${LDFLAGS_COV}" --enable-debug
-make -j check
+make -j
+make check
 (curl -S -f https://codecov.io/bash -o .github/codecov.bash && bash .github/codecov.bash) || echo "Coverage report failed"
