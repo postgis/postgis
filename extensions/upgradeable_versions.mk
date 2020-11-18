d2d5c9f98b109157e34e34e41367b82783df6be6
Regina Obe <lr@pcorp.us>
2020-11-18 17:46:41 -0500

    Prep for 3.1.0alpha3 release

--- extensions/upgradeable_versions.mk
+++ extensions/upgradeable_versions.mk
@@ -53,14 +53,9 @@ UPGRADEABLE_VERSIONS = \
 	2.5.2 \
 	2.5.3 \
 	2.5.4 \
-	3.0.0alpha1 \
-	3.0.0alpha2 \
-	3.0.0alpha3 \
-	3.0.0alpha4 \
-	3.0.0beta1 \
-	3.0.0rc1 \
-	3.0.0rc2 \
 	3.0.0 \
 	3.0.1 \
+	3.0.2 \
+	3.0.3 \
 	3.1.0alpha1 \
 	3.1.0alpha2
