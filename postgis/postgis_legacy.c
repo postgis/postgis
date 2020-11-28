4d1c8d8711df49047ab258229ec981ea00165bca
Darafei Praliaskouski <me@komzpa.net>
2020-11-29 01:56:17 +0300

    locate_between_measures and locate_along_measure are deprecated
    References #4214

--- postgis/postgis_legacy.c
+++ postgis/postgis_legacy.c
@@ -99,3 +99,5 @@ POSTGIS_DEPRECATE("3.1.0", sfcgal_make_solid);
 POSTGIS_DEPRECATE("3.1.0", sfcgal_is_solid);
 POSTGIS_DEPRECATE("3.1.0", postgis_sfcgal_noop);
 /**End: SFCGAL functions moved to postgis_sfcgal lib, to ease pg_upgrade **/
+
+POSTGIS_DEPRECATE("3.1.0", LWGEOM_locate_between_m);
