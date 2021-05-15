9af659188e710cb439265dc4da7af1aa55b0c5d2
Paul Ramsey <pramsey@cleverelephant.ca>
2021-05-14 17:30:13 -0700

    9.6 build

--- libpgcommon/lwgeom_pg.c
+++ libpgcommon/lwgeom_pg.c
@@ -29,7 +29,9 @@
 #include <utils/guc.h>
 #include <utils/guc_tables.h>
 #include <utils/fmgroids.h>
+#if POSTGIS_PGSQL_VERSION > 96
 #include <utils/regproc.h>
+#endif
 #include <catalog/namespace.h>
 #include <catalog/pg_extension.h>
 #include <commands/extension.h>
