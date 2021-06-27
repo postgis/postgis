ef5bd1843704e6d1eaee93e203bb0ad6952079f1
Regina Obe <lr@pcorp.us>
2021-06-27 19:12:48 -0400

    Update copyright note to my name :)

--- regress/real/tests.mk
+++ regress/real/tests.mk
@@ -3,7 +3,7 @@
 # * PostGIS - Spatial Types for PostgreSQL
 # * http://postgis.net
 # *
-# * Copyright (C) 2020 Sandro Santilli <strk@kbt.io>
+# * Copyright (C) 2021 Regina Obe <lr@pcorp.us>
 # *
 # * This is free software; you can redistribute and/or modify it under
 # * the terms of the GNU General Public Licence. See the COPYING file.
