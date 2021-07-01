1af801238df1aebfc2372f0045fa1c0af40416e4
Regina Obe <lr@pcorp.us>
2021-07-01 17:08:43 -0400

    Remove accidentally committed generated file. Minor typo fix in reallie pg_init_start.sh

--- regress/real/load_data.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE tiger.zcta5 DROP CONSTRAINT IF EXISTS enforce_geotype_the_geom;
-\i C:/ming64gcc81/projects/postgis/tmp/download_data/tiger_national.sql
-\i C:/ming64gcc81/projects/postgis/tmp/download_data/tiger_dc.sql
-\i C:/ming64gcc81/projects/postgis/tmp/download_data/osm_china.sql
-\i C:/ming64gcc81/projects/postgis/tmp/download_data/osm_belarus.sql
