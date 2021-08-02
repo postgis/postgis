876e74cd8617cd75b79b2708867fcc70ffa75b01
Sandro Santilli <strk@kbt.io>
2021-08-02 22:17:13 +0200

    Make multiprocess population less random

--- topology/test/check_multiprocess_topology_population.sh
+++ topology/test/check_multiprocess_topology_population.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # TODO: turn to perl ?
 
@@ -15,10 +15,9 @@ trap cleanup EXIT
 psql ${PSQL_OPTS} -c "SELECT NULL FROM CreateTopology('mt_topo')" > /dev/null
 
 # $RANDOM: random integer between 0 and 32767
-for i in `seq 1 10`; do
-  x=${RANDOM} # requires bash
-  y=${RANDOM}
-  cat <<EOF | psql ${PSQL_OPTS} | grep -v '^$' &
+for x in `seq 1 4`; do
+  for y in `seq 1 4`; do
+    cat <<EOF | psql ${PSQL_OPTS} | grep -v '^$' &
 SELECT NULL FROM
   topology.TopoGeo_addPolygon(
     'mt_topo',
@@ -31,14 +30,16 @@ SELECT NULL FROM
     )
   )
 EOF
-  pids[${i}]=$!
+    echo $! >> pids.$$
+  done
 done
 
 # wait for all pids
-for pid in ${pids[*]}; do
+for pid in `cat pids.$$`; do
     wait $pid
 done
+rm -f pids.$$
 
 psql ${PSQL_OPTS} -c "SELECT TopologySummary('mt_topo')" | grep -v '^$'
-psql ${PSQL_OPTS} -c "SELECT * FROM ValidateTopology('mt_topo')" | grep -v '^$'
+psql ${PSQL_OPTS} -c "SELECT 'INVALIDITY', * FROM ValidateTopology('mt_topo')" | grep -v '^$'
 
