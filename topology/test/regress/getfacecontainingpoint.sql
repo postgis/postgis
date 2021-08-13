9f12ffaa0bc2495e7b9f6f027f32bdaf6941ce4c
Sandro Santilli <strk@kbt.io>
2021-08-13 09:33:58 +0200

    Improve test for getFaceContainingPoint
    Allow inspecting query points graphically
    
    Add tests for query point near the start
    of a dangling edge

--- topology/test/regress/getfacecontainingpoint.sql
+++ topology/test/regress/getfacecontainingpoint.sql
@@ -24,45 +24,6 @@ FROM city_data.face
 WHERE face_id > 0
 ORDER BY face_id;
 
--- Query point near isolated edge in a face
-SELECT 't1', topology.GetFaceContainingPoint('city_data', 'POINT(11 34)');
-
--- Query point near node of closed edge, outside of it
-SELECT 't2', topology.GetFaceContainingPoint('city_data', 'POINT(3.8 30.8)');
-
--- Query point near node of closed edge, inside of it
-SELECT 't3', topology.GetFaceContainingPoint('city_data', 'POINT(4.2 31.2)');
-
--- Query point on a node of degree 1 incident to single face
-SELECT 't4', topology.GetFaceContainingPoint('city_data', 'POINT(25 35)');
-
--- Query point on an dangling edge
-SELECT 't5', topology.GetFaceContainingPoint('city_data', 'POINT(25 33)');
-
--- Query point on a node of degree 2 incident to single face
-SELECT 't6', topology.GetFaceContainingPoint('city_data', 'POINT(57 33)');
-
--- Query point in hole touching shell, near edge having hole on the left
-SELECT 't7', topology.GetFaceContainingPoint('city_data', 'POINT(32 16.9)');
-
--- Query point in hole touching shell, near edge having hole on the right
-SELECT 't8', topology.GetFaceContainingPoint('city_data', 'POINT(24 16.9)');
-
--- Query point collinear with closest incoming segment (right of next one)
-SELECT 't9', topology.GetFaceContainingPoint('city_data', 'POINT(7.1 31)');
-
--- Query point on the right of the closest segment but left of ring
-SELECT 't10', topology.GetFaceContainingPoint('city_data', 'POINT(26 16.8)');
-
--- Query point on the left of the closest segment and left of ring
-SELECT 't11', topology.GetFaceContainingPoint('city_data', 'POINT(26 17.2)');
-
--- Query point on the left of the closest segment but right of ring
-SELECT 't12', topology.GetFaceContainingPoint('city_data', 'POINT(33.1 17.8)');
-
--- Query point on the right of the closest segment and right of ring
-SELECT 't13', topology.GetFaceContainingPoint('city_data', 'POINT(33.1 17.2)');
-
 -- Query point on a node incident to multiple faces
 SELECT 'e1', topology.GetFaceContainingPoint('city_data', 'POINT(21 14)');
 
@@ -74,6 +35,41 @@ SELECT 'e3', topology.GetFaceContainingPoint('city_data', 'POINT(28 14)');
 
 -- Query point on a non-dangling edge binding universe and proper face
 SELECT 'e4', topology.GetFaceContainingPoint('city_data', 'POINT(28 22)');
+
+CREATE TABLE city_data.query_points(id int primary key, g geometry);
+INSERT INTO city_data.query_points VALUES
+-- Query point near isolated edge in a face
+( 1, 'POINT(11 34)' ),
+-- Query point near node of closed edge, outside of it
+( 2, 'POINT(3.8 30.8)' ),
+-- Query point near node of closed edge, inside of it
+( 3, 'POINT(4.2 31.2)' ),
+-- Query point on a node of degree 1 incident to single face
+( 4, 'POINT(25 35)' ),
+-- Query point on an dangling edge
+( 5, 'POINT(25 33)' ),
+-- Query point on a node of degree 2 incident to single face
+( 6, 'POINT(57 33)' ),
+-- Query point in hole touching shell, near edge having hole on the left
+( 7, 'POINT(32 16.9)' ),
+-- Query point in hole touching shell, near edge having hole on the right
+( 8, 'POINT(24 16.9)' ),
+-- Query point collinear with closest incoming segment (right of next one)
+( 9, 'POINT(7.1 31)' ),
+-- Query point on the right of the closest segment but left of ring
+( 10, 'POINT(26 16.8)' ),
+-- Query point on the left of the closest segment and left of ring
+( 11, 'POINT(26 17.2)' ),
+-- Query point on the left of the closest segment but right of ring
+( 12, 'POINT(33.05 17.21)' ),
+-- Query point on the right of the closest segment and right of ring
+( 13, 'POINT(33.1 17.2)' ),
+-- Query point near the start of a dangling edge
+( 14, 'POINT(8 36)' );
+
+SELECT 't'||id, topology.GetFaceContainingPoint('city_data', g)
+FROM city_data.query_points
+ORDER BY id;
 
 
 
