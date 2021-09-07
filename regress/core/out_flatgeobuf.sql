729049e3196f736686cfbbc0a5ec0071005586fe
Björn Harrtell <bjorn@wololo.org>
2021-09-08 00:04:18 +0200

    Reinstate null geom test case

--- regress/core/out_flatgeobuf.sql
+++ regress/core/out_flatgeobuf.sql
@@ -1,5 +1,7 @@
 --set client_min_messages to DEBUG3;
 SELECT 'T1', ST_AsFlatGeobuf(q, 'geom')
     FROM (SELECT ST_MakePoint(1.1, 2.1) AS geom) AS q;
+SELECT 'T2', ST_AsFlatGeobuf(q, 'geom')
+    FROM (SELECT null::geometry AS geom) AS q;
 SELECT 'T3', ST_AsFlatGeobuf(q, 'geom')
     FROM (SELECT ST_MakePoint(8678678.67867, 678678.546456) AS geom) AS q;
