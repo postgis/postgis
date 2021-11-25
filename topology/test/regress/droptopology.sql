83b23368293d3b7391c5216e9aa1f05d057b596a
Sandro Santilli <strk@kbt.io>
2021-11-25 23:52:15 +0100

    Allow droppping TopoGeometry columns in presence of UPDATE triggers
    References #5026 in 3.0 branch (3.0.5dev)
    Includes regression test

--- topology/test/regress/droptopology.sql
+++ topology/test/regress/droptopology.sql
@@ -1,18 +1,35 @@
 set client_min_messages to WARNING;
 
-SELECT topology.CreateTopology('t1') > 0;
-SELECT topology.CreateTopology('t2') > 0;
+SELECT NULL FROM topology.CreateTopology('t1');
+SELECT NULL FROM topology.CreateTopology('t2');
 
 CREATE TABLE t1f (id int);
-SELECT topology.AddTopoGeometryColumn('t1', 'public', 't1f', 'geom_t1', 'LINE') > 0;
+SELECT NULL FROM topology.AddTopoGeometryColumn('t1', 'public', 't1f', 'geom_t1', 'LINE');
 
 CREATE TABLE t2f (id int);
-SELECT topology.AddTopoGeometryColumn('t2', 'public', 't2f', 'geom_t2', 'LINE') > 0;
+SELECT NULL FROM topology.AddTopoGeometryColumn('t2', 'public', 't2f', 'geom_t2', 'LINE');
 
 SELECT topology.DropTopology('t1');
 SELECT topology.DropTopology('t2');
 DROP TABLE t2f;
 DROP TABLE t1f;
+
+SELECT NULL FROM topology.CreateTopology('t3');
+CREATE FUNCTION t3.bail_out() RETURNS trigger AS $BODY$
+BEGIN
+  RAISE EXCEPTION '%: trigger prevents % on %', TG_ARGV[0], TG_OP, TG_RELNAME;
+END;
+$BODY$ LANGUAGE 'plpgsql';
+
+-- Test DropTopology in presence of triggers
+-- See https://trac.osgeo.org/postgis/ticket/5026
+CREATE TABLE t3.f(id serial);
+SELECT NULL FROM topology.AddTopoGeometryColumn('t3', 't3', 'f', 'g', 'POINT');
+CREATE TRIGGER prevent_delete AFTER DELETE OR UPDATE ON t3.f
+FOR EACH STATEMENT EXECUTE PROCEDURE t3.bail_out('t3');
+INSERT INTO t3.f(g) VALUES (toTopoGeom('POINT(0 0)', 't3', 1));
+--SELECT topology.DropTopoGeometryColumn('t3', 'f', 'g');
+SELECT topology.DropTopology('t3');
 
 -- Exceptions
 SELECT topology.DropTopology('topology');
