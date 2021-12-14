82d84900fdd26678767fee72677664f3f76b3db6
Sandro Santilli <strk@kbt.io>
2021-12-14 22:54:55 +0100

    Allow upgrades in presence of views using deprecated functions
    Support adding a 'Replaces' comment above functions to improve upgrade
    
    When a function has a line like the following in in the comments
    above its signature:
    
      Replaces ST_Intersection(geometry, geometry) deprecated in 3.1.0
    
    The upgrade script will try to drop the deprecated functions and in case
    of failure leave them behind renamed with a _deprecated_by_postgis_<version>
    suffix, warns the user about such occurrence and gives her hints on
    how to fix.
    
    The postgis_full_version() will report presence of such deprecated
    functions as an "incomplete upgrade".
    
    References #5033 in 3.2 branch (3.2.1dev)
    
    Includes regression tests

--- regress/hooks/hook-after-upgrade.sql
+++ regress/hooks/hook-after-upgrade.sql
@@ -1,2 +1,5 @@
-DROP VIEW IF EXISTS upgrade_view_test;
+DROP VIEW IF EXISTS upgrade_view_test_overlay;
+DROP VIEW IF EXISTS upgrade_view_test_unaryunion;
+DROP VIEW IF EXISTS upgrade_view_test_subdivide;
+DROP VIEW IF EXISTS upgrade_view_test_union;
 DROP TABLE upgrade_test;
