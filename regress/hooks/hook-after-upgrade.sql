7678d927de8c1822a9b655966c3d2eb58ad1ae59
Sandro Santilli <strk@kbt.io>
2021-12-19 19:38:19 +0100

    Allow upgrades from <3.1 when using ST_Force{3D,3DM,3DZ,4D} in views

--- regress/hooks/hook-after-upgrade.sql
+++ regress/hooks/hook-after-upgrade.sql
@@ -2,4 +2,5 @@ DROP VIEW IF EXISTS upgrade_view_test_overlay;
 DROP VIEW IF EXISTS upgrade_view_test_unaryunion;
 DROP VIEW IF EXISTS upgrade_view_test_subdivide;
 DROP VIEW IF EXISTS upgrade_view_test_union;
+DROP VIEW IF EXISTS upgrade_view_test_force_dims;
 DROP TABLE upgrade_test;
