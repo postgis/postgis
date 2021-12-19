89949433f40e6586c8fa2568f8d26ccba268fd16
Sandro Santilli <strk@kbt.io>
2021-12-19 22:48:45 +0100

    Replace ST_AsKML deprecated signatures drop with Replaces comments
    Includes regression test

--- regress/hooks/hook-after-upgrade.sql
+++ regress/hooks/hook-after-upgrade.sql
@@ -3,6 +3,7 @@ DROP VIEW IF EXISTS upgrade_view_test_unaryunion;
 DROP VIEW IF EXISTS upgrade_view_test_subdivide;
 DROP VIEW IF EXISTS upgrade_view_test_union;
 DROP VIEW IF EXISTS upgrade_view_test_force_dims;
+DROP VIEW IF EXISTS upgrade_view_test_askml;
 DROP TABLE upgrade_test;
 
 -- Drop functions deprecated on upgrade
