43f92d9a8f90eb3541a5d22296a74d136ab59fd4
Sandro Santilli <strk@kbt.io>
2021-12-22 15:10:59 +0100

    Do not rewrite rules on upgrade (too dangerous)
    References #5033 in master/main branch (3.3.0dev)
    Drop deprecated function after updgrade in CI

--- regress/hooks/hook-after-upgrade.sql
+++ regress/hooks/hook-after-upgrade.sql
@@ -5,3 +5,16 @@ DROP VIEW IF EXISTS upgrade_view_test_union;
 DROP VIEW IF EXISTS upgrade_view_test_force_dims;
 DROP VIEW IF EXISTS upgrade_view_test_askml;
 DROP TABLE upgrade_test;
+
+-- Drop functions deprecated on upgrade
+DROP FUNCTION IF EXISTS st_force3dz_deprecated_by_postgis_301(geometry);
+DROP FUNCTION IF EXISTS st_force3d_deprecated_by_postgis_301(geometry);
+DROP FUNCTION IF EXISTS st_force3dm_deprecated_by_postgis_301(geometry);
+DROP FUNCTION IF EXISTS st_force4d_deprecated_by_postgis_301(geometry);
+DROP FUNCTION IF EXISTS st_intersection_deprecated_by_postgis_301(geometry,geometry);
+DROP FUNCTION IF EXISTS st_difference_deprecated_by_postgis_301(geometry,geometry);
+DROP FUNCTION IF EXISTS st_symdifference_deprecated_by_postgis_301(geometry,geometry);
+DROP FUNCTION IF EXISTS st_unaryunion_deprecated_by_postgis_301(geometry);
+DROP FUNCTION IF EXISTS st_subdivide_deprecated_by_postgis_301(geometry,integer);
+DROP FUNCTION IF EXISTS st_askml_deprecated_by_postgis_200(geometry,integer);
+DROP FUNCTION IF EXISTS st_askml_deprecated_by_postgis_200(geography,integer);
