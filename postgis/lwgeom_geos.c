0f2afd2ac3f1a1e8faddd8b4e1aa3df6301848a5
Paul Ramsey <pramsey@cleverelephant.ca>
2022-01-28 14:18:00 -0800

    ST_ConcaveHull implementation from GEOS 3.11+ A native code concave hull implementation with better performance and fewer failure cases than the current PL/PgSQL implementation.

--- postgis/lwgeom_geos.c
+++ postgis/lwgeom_geos.c
@@ -105,6 +105,7 @@ Datum ST_Equals(PG_FUNCTION_ARGS);
 Datum ST_BuildArea(PG_FUNCTION_ARGS);
 Datum ST_DelaunayTriangles(PG_FUNCTION_ARGS);
 Datum ST_MaximumInscribedCircle(PG_FUNCTION_ARGS);
+Datum ST_ConcaveHull(PG_FUNCTION_ARGS);
 
 Datum pgis_union_geometry_array(PG_FUNCTION_ARGS);
 Datum pgis_geometry_union_finalfn(PG_FUNCTION_ARGS);
@@ -888,6 +889,35 @@ Datum convexhull(PG_FUNCTION_ARGS)
 	PG_FREE_IF_COPY(geom1, 0);
 	PG_RETURN_POINTER(result);
 }
+
+
+PG_FUNCTION_INFO_V1(ST_ConcaveHull);
+Datum ST_ConcaveHull(PG_FUNCTION_ARGS)
+{
+#if POSTGIS_GEOS_VERSION < 31100
+
+	lwpgerror("The GEOS version this PostGIS binary "
+				"was compiled against (%d) doesn't support "
+				"'GEOSConcaveHull' function (3.11.0+ required)",
+				POSTGIS_GEOS_VERSION);
+	PG_RETURN_NULL();
+
+#else /* POSTGIS_GEOS_VERSION >= 31100 */
+	GSERIALIZED* geom = PG_GETARG_GSERIALIZED_P(0);
+	double ratio = PG_GETARG_FLOAT8(1);
+	bool allow_holes = PG_GETARG_BOOL(2);
+
+	LWGEOM* lwgeom = lwgeom_from_gserialized(geom);
+	LWGEOM* lwresult = lwgeom_concavehull(lwgeom, ratio, allow_holes);
+	GSERIALIZED* result = geometry_serialize(lwresult);
+
+	lwgeom_free(lwgeom);
+	lwgeom_free(lwresult);
+	PG_FREE_IF_COPY(geom, 0);
+	PG_RETURN_POINTER(result);
+#endif
+}
+
 
 PG_FUNCTION_INFO_V1(topologypreservesimplify);
 Datum topologypreservesimplify(PG_FUNCTION_ARGS)
