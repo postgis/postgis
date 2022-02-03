3040f59392e80381d886ded352cd906f2fd0084e
Regina Obe <lr@pcorp.us>
2022-02-03 15:44:07 -0500

    Update berrie and berrie64 scripts to try to avoid using packaged installed pg

--- ci/berrie/pg_init_start.sh
+++ ci/berrie/pg_init_start.sh
@@ -1,24 +1,23 @@
-#Berrie is a 32-bit Rasberry Pi Arm managed by Bruce Rindahl
+#!/usr/bin/env bash
+#Berrie is a 32-bit Rasberry Pi managed by Bruce Rindahl
 #This is script to launch custom compiled PostgreSQL
-#export label=berrie64 #this is passed in via Jenkins
-export WORKSPACE=/home/jenkins/workspace
-
-export OS_BUILD=32
-export PG_VER=12
-export PGPATH=${WORKSPACE}/pg/label/${label}/rel/pg${PG_VER}w${OS_BUILD}
-export PATH=${PATH}:${PGPATH}/bin:${PGPATH}/lib
-export PGPORT=55432
-export PGDATA=$PGPATH/data_${PGPORT}
+#export label=berrie #this is passed in via Jenkins
+SCRIPT=$(readlink -f "$0")
+export CUR_DIR=$(dirname "$SCRIPT")
+echo $CUR_DIR
+export CONFIG_FILE="$CUR_DIR/configs.sh"
+. $CONFIG_FILE
+echo $PATH
 export PGLOG="$PGDATA/pgsql.log"
 # What to use to start up the postmaster
-DAEMON="$PGPATH/bin/pg_ctl -D $PGDATA -l logfile start"
+DAEMON="$PGPATH/bin/pg_ctl -D $PGDATA -o '-F' -l logfile start"
 
 # What to use to shut down the postmaster
 PGCTL="$PGPATH/bin/pg_ctl"
 
 # remove cluster if exists
 if [ -d $PGDATA ] ; then
-    if [ -d $PGDATA/postmaster.pid] ; then
+    if [ -d $PGDATA/postmaster.pid ] ; then
     	$PGCTL stop -D $PGDATA -s -m fast
     fi;
 
