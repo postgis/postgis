4dd1f4a5dd2d420cf9512539bd33ab6656ceb8c3
Paul Ramsey <pramsey@cleverelephant.ca>
2022-02-11 10:48:21 -0800

    multi-valued insert

--- utils/create_spatial_ref_sys.pl
+++ utils/create_spatial_ref_sys.pl
@@ -14,7 +14,7 @@ use warnings;
 use strict;
 
 (@ARGV) ||
-	die "Usage: perl $0 -u <spatial_ref_sys>\n" .
+	die "Usage: perl $0 <spatial_ref_sys>\n" .
 		"Creates an SQL script load or update records of spatial_ref_sys.\n";
 
 my $UPDATE = 0;
@@ -39,19 +39,27 @@ for (my $i=0; $i<@ARGV; $i++)
 open SRSFILE, "<$SRSFILE" || die "Can't open $SRSFILE for reading\n";
 
 print "BEGIN;\n";
-
+print "INSERT INTO \"$SRSTABLE\" (\"srid\", \"auth_name\", \"auth_srid\", \"srtext\", \"proj4text\") VALUES\n";
+my $count = 0;
 while(<SRSFILE>) {
 	chop;
 	my ($srid, $auth_name, $auth_srid, $srtext, $proj4text) = split (/\t/);
 	$auth_name =~ s/\'/\'\'/g;
 	$srtext =~ s/\'/\'\'/g;
 	$proj4text =~ s/\'/\'\'/g;
-	print "INSERT INTO \"$SRSTABLE\" (\"srid\", \"auth_name\", \"auth_srid\", \"srtext\", \"proj4text\") VALUES ($srid, '$auth_name', $auth_srid, '$srtext', '$proj4text')";
-	if ($UPDATE) {
-		print "\nON CONFLICT (srid) DO NOTHING";
-	}
-	print ";\n\n";
+	print ",\n" if $count++;
+	print "($srid, '$auth_name', $auth_srid, '$srtext', '$proj4text')";
 }
+
+print "\nON CONFLICT (srid) DO NOTHING;\n";
+
+
+# INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text)
+# VALUES
+# (200003, 'test3', 3, 'test3', 'test3'),
+# (200002, 'test2', 2, 'test2', 'test2'),
+# (200001, 'test1', 1, 'test1', 'test1')
+# ON CONFLICT DO NOTHING;
 
 print "COMMIT;\n";
 print "ANALYZE \"$SRSTABLE\";\n";
