394232faf4390de258cd5ad652fdf07470e9fbff
Paul Ramsey <pramsey@cleverelephant.ca>
2022-02-10 16:00:22 -0800

    Build spatial_ref_sys.sql from a data file and also build an upgrade file that is included in the extension upgrade script

---
+++ utils/create_spatial_ref_sys.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+#
+# PostGIS - Spatial Types for PostgreSQL
+# http://postgis.net
+#
+# Copyright (C) 2022 Paul Ramsey <pramsey@cleverelephant.ca>
+#
+# This is free software; you can redistribute and/or modify it under
+# the terms of the GNU General Public Licence. See the COPYING file.
+#
+
+use warnings;
+use strict;
+
+(@ARGV) ||
+	die "Usage: perl $0 -u <spatial_ref_sys>\n" .
+		"Creates an SQL script load or update records of spatial_ref_sys.\n";
+
+my $UPDATE = 0;
+my $SRSFILE = 0;
+my $SRSTABLE = "spatial_ref_sys";
+
+for (my $i=0; $i<@ARGV; $i++)
+{
+	if ( $ARGV[$i] =~ m/^-/ )
+	{
+		if ( $ARGV[$i] eq '-u' )
+		{
+			$UPDATE = 1;
+		}
+	}
+	elsif ( ! $SRSFILE )
+	{
+		$SRSFILE = $ARGV[$i];
+	}
+}
+
+open SRSFILE, "<$SRSFILE" || die "Can't open $SRSFILE for reading\n";
+
+print "BEGIN;\n";
+
+while(<SRSFILE>) {
+	chop;
+	my ($srid, $auth_name, $auth_srid, $srtext, $proj4text) = split (/\t/);
+	$auth_name =~ s/\'/\'\'/g;
+	$srtext =~ s/\'/\'\'/g;
+	$proj4text =~ s/\'/\'\'/g;
+	print "INSERT INTO \"$SRSTABLE\" (\"srid\", \"auth_name\", \"auth_srid\", \"srtext\", \"proj4text\") VALUES ($srid, '$auth_name', $auth_srid, '$srtext', '$proj4text')";
+	if ($UPDATE) {
+		print "\n";
+		print "ON CONFLICT (srid) DO UPDATE SET \"auth_name\"='$auth_name', \"auth_srid\"=$auth_srid, \"srtext\"='$srtext', \"proj4text\"='$proj4text'";
+	}
+	print ";\n\n";
+}
+
+print "COMMIT;\n";
+print "ANALYZE \"$SRSTABLE\";\n";
