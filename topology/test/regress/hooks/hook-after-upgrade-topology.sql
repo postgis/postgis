4cd780c1b40aac54a036c700dd3876d24be557c8
Sandro Santilli <strk@kbt.io>
2022-02-22 14:35:51 +0100

    Add test for CopyTopology backward compatibility

--- topology/test/regress/hooks/hook-after-upgrade-topology.sql
+++ topology/test/regress/hooks/hook-after-upgrade-topology.sql
@@ -1,2 +1,6 @@
+-- See https://trac.osgeo.org/postgis/ticket/5102
+SELECT topology.CopyTopology('upgrade_test', 'upgrade_test_copy');
+
 SELECT topology.DropTopology('upgrade_test');
+SELECT topology.DropTopology('upgrade_test_copy');
 
