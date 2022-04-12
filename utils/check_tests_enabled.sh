postgis/postgis

commit 5bb5a91c807cfc85a3b50c577dfbaf8950fa17db

Alter check_tests_enabled.sh to also work on MacOS

utils/check_tests_enabled.sh
@@ -21,17 +21,24 @@ check_enabled() {
   suffix=$2
   bd=`dirname ${mk}`/${suffix}
 
-  #echo "MK file: ${mk}"
+  #cho "MK file: ${mk}"
   #echo "Suffix: ${suffix}"
   #echo "Basedir: ${bd}"
 
   grep 'topsrcdir)' ${mk} |
       sed 's|.*topsrcdir)/||;s/ .*$//' |
       sed 's|\\||' |
-      sed 's|\.sql\>||' > ${TMPDIR}/enabled_tests
+      sed 's/\.sql$//' > ${TMPDIR}/enabled_tests
+
+  #cat ${TMPDIR}/enabled_tests
 
   find ${bd} -name '*.sql' |
+    grep -v '\-post.sql' | grep -v '\-pre.sql' |
+    sed 's|\.select.sql$||' |
+    sed 's|//|/|' |
     sed 's|\.sql$||' > ${TMPDIR}/available_tests
+
+  #cat ${TMPDIR}/available_tests
 
   MISSING=`grep -vf ${TMPDIR}/enabled_tests ${TMPDIR}/available_tests`
   if test -n "${MISSING}"; then
@@ -69,7 +76,7 @@ if [ -z "${RD}" ]; then
 fi
 
 cd ${RD}
-check_enabled topology/test/tests.mk regress &&
+check_enabled topology/test/tests.mk regress #&&
 check_enabled regress/loader/tests.mk &&
 check_enabled regress/dumper/tests.mk &&
 check_enabled sfcgal/regress/tests.mk &&

topology/test/tests.mk
@@ -18,9 +18,9 @@ override RUNTESTFLAGS_INTERNAL := \
   --after-upgrade-script $(topsrcdir)/topology/test/regress/hooks/hook-after-upgrade-topology.sql
 
 TESTS += \
-  $(topsrcdir)/topology/test/regress/addedge.sql \
+	$(topsrcdir)/topology/test/regress/addedge.sql \
 	$(topsrcdir)/topology/test/regress/addface2.5d.sql \
-  $(topsrcdir)/topology/test/regress/addface.sql \
+	$(topsrcdir)/topology/test/regress/addface.sql \
 	$(topsrcdir)/topology/test/regress/addnode.sql \
 	$(topsrcdir)/topology/test/regress/addtopogeometrycolumn.sql \
 	$(topsrcdir)/topology/test/regress/cleartopogeom.sql \

commit 724bb8a66cc78d4398c523a6d3c38cfba902cc5a

Do not hide disabled tests in topology
Problem was introduced by 5bb5a91c807cfc85a3b50c577dfbaf8950fa17db

utils/check_tests_enabled.sh
@@ -76,7 +76,7 @@ if [ -z "${RD}" ]; then
 fi
 
 cd ${RD}
-check_enabled topology/test/tests.mk regress #&&
+check_enabled topology/test/tests.mk regress &&
 check_enabled regress/loader/tests.mk &&
 check_enabled regress/dumper/tests.mk &&
 check_enabled sfcgal/regress/tests.mk &&