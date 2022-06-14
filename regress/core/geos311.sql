6d6395d7677318bdd29b71e83ae0bb765e3f434b
Paul Ramsey <pramsey@cleverelephant.ca>
2022-06-14 12:32:13 -0700

    ST_TriangulatePolygon binding for GEOSj 3.11+

--- regress/core/geos311.sql
+++ regress/core/geos311.sql
@@ -51,3 +51,20 @@ SELECT 'simplifypolygonhull-3' AS test,
 		'POLYGON((0 0, 0 1, 0.5 0.1, 0.5 0.1, 1 1, 1 0, 0 0))'::geometry,
 		0.85),1);
 
+-- ST_TriangulatePolygon --
+select 'triangulatepolygon-1' AS text,
+	ST_AsText(ST_Normalize(ST_TriangulatePolygon('POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))')), 1);
+
+select 'triangulatepolygon-2' AS text,
+	ST_AsText(ST_Normalize(ST_TriangulatePolygon('MULTIPOLYGON(((0 0, 0 1, 1 1, 1 0, 0 0)))')), 1);
+
+select 'triangulatepolygon-3' AS text,
+	ST_AsText(ST_Normalize(ST_TriangulatePolygon('LINESTRING(0 0, 0 1, 1 1, 1 0, 0 0)')), 1);
+
+select 'triangulatepolygon-4' AS text,
+	ST_AsText(ST_Normalize(ST_TriangulatePolygon('POINT(1 0)')), 1);
+
+select 'triangulatepolygon-5' AS text,
+	ST_AsText(ST_Normalize(ST_TriangulatePolygon(
+		'GEOMETRYCOLLECTION(POLYGON((0 0,0 1,1 1,0 0)),POLYGON((1 1,1 0,0 0,1 1)), POINT(1 1))'
+		)), 1);
