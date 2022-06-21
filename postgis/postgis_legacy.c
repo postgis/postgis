7e42fcc4c861a00340b6679e597c8fa9bfb3fa84
sergei sh <sshoulbakov@kontur.io>
2022-06-21 15:36:00 +0300

    parallel ST_Union pgis_geometry_union_finalfn function removed

--- postgis/postgis_legacy.c
+++ postgis/postgis_legacy.c
@@ -102,3 +102,4 @@ POSTGIS_DEPRECATE("3.1.0", postgis_sfcgal_noop);
 
 POSTGIS_DEPRECATE("3.1.0", LWGEOM_locate_between_m);
 POSTGIS_DEPRECATE("3.1.0", postgis_svn_version);
+POSTGIS_DEPRECATE("3.3.0", pgis_geometry_union_finalfn);
