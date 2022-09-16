postgis/postgis

commit 956ffd5f949c8dbef9ba14f70907b50673c403c2

Make sure to install upgrade paths to the correct target PostgreSQL

extensions/upgrade-paths-rules.mk
@@ -2,6 +2,8 @@ EXTDIR=$(DESTDIR)$(datadir)/$(datamoduledir)
 
 TAG_UPGRADE=$(EXTENSION)--TEMPLATED--TO--ANY.sql
 
+PG_SHAREDIR=$(shell $(PG_CONFIG) --sharedir)
+
 install: install-upgrade-paths
 
 # The "next" lines are a cludge to allow upgrading between different
@@ -21,7 +23,11 @@ install-upgrade-paths: tag-as-any
 	$(INSTALL_DATA) sql/$${tpl} "$(EXTDIR)/$${tpl}"; \
 	$(INSTALL_DATA) "sql/$(TAG_UPGRADE)" "$(EXTDIR)/$(TAG_UPGRADE)"; \
 	ln -fs "$(TAG_UPGRADE)" $(EXTDIR)/$(EXTENSION)--$(EXTVERSION)--ANY.sql; \
-	$(abs_topbuilddir)/loader/postgis install-extension-upgrades $(UPGRADEABLE_VERSIONS)
+	$(abs_topbuilddir)/loader/postgis \
+	$(abs_topbuilddir)/loader/postgis \
+		install-extension-upgrades \
+		--pg_sharedir $(PG_SHAREDIR) \
+		$(UPGRADEABLE_VERSIONS)
 
 all: sql/$(TAG_UPGRADE)
 

commit f717af8513ee12d2310ee92ee909d4401e6244b6

Fix make install, broken by 956ffd5f9

extensions/upgrade-paths-rules.mk
@@ -24,7 +24,6 @@ install-upgrade-paths: tag-as-any
 	$(INSTALL_DATA) "sql/$(TAG_UPGRADE)" "$(EXTDIR)/$(TAG_UPGRADE)"; \
 	ln -fs "$(TAG_UPGRADE)" $(EXTDIR)/$(EXTENSION)--$(EXTVERSION)--ANY.sql; \
 	$(abs_topbuilddir)/loader/postgis \
-	$(abs_topbuilddir)/loader/postgis \
 		install-extension-upgrades \
 		--pg_sharedir $(PG_SHAREDIR) \
 		$(UPGRADEABLE_VERSIONS)