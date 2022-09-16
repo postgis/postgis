postgis/postgis

commit 1fdafbffdc6087330e1e8324a81f247b1c1a0e51

Check for the presence of link target when installing upgrade paths

loader/postgis.pl
@@ -70,14 +70,14 @@ sub install_upgrade_from
 
 		#print " EXTENSION: [${extname}]\n";
 
-		# TODO: if the target is before 3.3.0 we need to symlink
-		#       ${extname}--ANY--${to} to ${extname}--${from}--${to}
+		die "File ${EXTDIR}/${extname}--TEMPLATED--TO--ANY.sql is missing"
+			unless -f "${EXTDIR}/${extname}--TEMPLATED-TO-ANY.sql";
 
 		my $shcmd = "ln -fvs '${extname}--TEMPLATED--TO--ANY.sql' '${EXTDIR}/${extname}--${from}--ANY.sql'";
 		#print " CMD: ${shcmd}\n";
 		my $rv = system($shcmd);
 		if ( $rv ) {
-			die "Error encountered running: $cmd: $!";
+			die "Error encountered running: $shcmd: $!";
 		}
 
 	}

commit 5698f9b0b4565bb57cdc82b8f77a0120d380a9ba

Fix detection of templated upgrade, broken in 1fdafbffd

loader/postgis.pl
@@ -71,7 +71,7 @@ sub install_upgrade_from
 		#print " EXTENSION: [${extname}]\n";
 
 		die "File ${EXTDIR}/${extname}--TEMPLATED--TO--ANY.sql is missing"
-			unless -f "${EXTDIR}/${extname}--TEMPLATED-TO-ANY.sql";
+			unless -f "${EXTDIR}/${extname}--TEMPLATED--TO--ANY.sql";
 
 		my $shcmd = "ln -fvs '${extname}--TEMPLATED--TO--ANY.sql' '${EXTDIR}/${extname}--${from}--ANY.sql'";
 		#print " CMD: ${shcmd}\n";