4af1190b7ce3ece4712c95dbf918e2879c79a2fd
Regina Obe <lr@pcorp.us>
2022-10-29 00:47:24 -0400

    Berrie64  changes 1. Error on first fail 2. Do garden tests 3. Use Geos 3.9 build run

--- ci/berrie64/postgis_regress.sh
+++ ci/berrie64/postgis_regress.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 # Berrie64 is a 64-bit Rasberry Pi Arm managed by Bruce Rindahl
 ## BRANCH is passed in via jenkins which is set via gitea web hook
 #export BRANCH=618a67b1d6fc223dd5a4c0b02c824939f21dbd65
 ## label is set by jenkins
 #export label=${label}
 
+export GEOS_VER=3.9.4
 export WORKSPACE=/home/jenkins/workspace
+export GEOS_PATH=${WORKSPACE}/geos/rel-${GEOS_VER}
 
 cd ${WORKSPACE}/PostGIS_Worker_Run/label/${label}/$BRANCH
 export OS_BUILD=64
@@ -18,13 +21,15 @@ export PGDATA=$PGPATH/data_${PGPORT}
 export PGHOST=localhost
 
 sh autogen.sh
-./configure --with-pgconfig=${PGPATH}/bin/pg_config --with-library-minor-version
+./configure --with-pgconfig=${PGPATH}/bin/pg_config --with-geosconfig=${GEOS_PATH}/bin/geos-config --with-library-minor-version
 #make clean
 make
 export err_status=0
 make check RUNTESTFLAGS="-v"
 make install
 make check RUNTESTFLAGS="-v --extension"
+err_status=$?
+make garden
 err_status=$?
 
 if [ -d $PGDATA/postmaster.pid ] ; then
