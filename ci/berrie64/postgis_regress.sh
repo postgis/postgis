d939bbf43e6b854f9fa379f47e7e0134cb68ab5e
Regina Obe <lr@pcorp.us>
2022-10-28 21:52:23 -0400

    1. Have Berrie64 fail on first failure 2. Have build postgis with lib-version so multiple can be installed in same cluster

--- ci/berrie64/postgis_regress.sh
+++ ci/berrie64/postgis_regress.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 # Berrie64 is a 64-bit Rasberry Pi Arm managed by Bruce Rindahl
 ## BRANCH is passed in via jenkins which is set via gitea web hook
 #export BRANCH=618a67b1d6fc223dd5a4c0b02c824939f21dbd65
@@ -15,12 +16,13 @@ echo $PATH
 cd ${WORKSPACE}/PostGIS_Worker_Run/label/${label}/$BRANCH
 
 sh autogen.sh
-./configure --with-pgconfig=${PGPATH}/bin/pg_config --with-geosconfig=${GEOS_PATH}/bin/geos-config
+./configure --with-pgconfig=${PGPATH}/bin/pg_config --with-geosconfig=${GEOS_PATH}/bin/geos-config --with-library-minor-version
 #make clean
 make
 export err_status=0
 make check RUNTESTFLAGS="-v"
 make install
+
 make check RUNTESTFLAGS="-v --extension"
 err_status=$?
 
