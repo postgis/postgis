732064a157813018763ce97a1ad68409c420a1e7
Sandro Santilli <strk@kbt.io>
2022-12-21 18:37:57 +0100

    Make TopoGeo_addLinestring performance test more costly
    Use more edges for rings, have more nodes of degree 2.
    Raises topology build time from ~3 secs to ~30 secs on my system.

--- topology/test/perf/TopoGeo_addLinestring.sql
+++ topology/test/perf/TopoGeo_addLinestring.sql
@@ -4,8 +4,9 @@ SELECT topology.CreateTopology('topoperf');
 
 \timing on
 
-CREATE TABLE topoperf.case_full_coverage_no_holes
-AS SELECT ST_Boundary(geom) g FROM ST_SquareGrid(4, ST_MakeEnvelope(0,0,120,120));
+CREATE TABLE topoperf.case_full_coverage_no_holes AS
+SELECT ST_Subdivide(ST_Segmentize(ST_Boundary(geom), 0.5), 5) g
+FROM ST_SquareGrid(4, ST_MakeEnvelope(0,0,120,120));
 
 SELECT count(*) FROM (
   SELECT topology.TopoGeo_addLinestring('topoperf', g)
@@ -13,3 +14,5 @@ SELECT count(*) FROM (
 ) foo;
 
 \timing off
+
+SELECT TopologySummary('topoperf');
