f7885e500686145b098a633356a70b49bfb1c1eb
Sandro Santilli <strk@kbt.io>
2023-02-16 17:39:35 +0100

    Add test for #5338

--- raster/test/regress/hooks/hook-after-upgrade-raster.sql
+++ raster/test/regress/hooks/hook-after-upgrade-raster.sql
@@ -1,1 +1,2 @@
 DROP TABLE upgrade_test_raster;
+DROP TABLE upgrade_test_raster_with_regular_blocking;
--- raster/test/regress/hooks/hook-before-upgrade-raster.sql
+++ raster/test/regress/hooks/hook-before-upgrade-raster.sql
@@ -14,3 +14,20 @@ INSERT INTO upgrade_test_raster(r) VALUES
 
 --SET client_min_messages TO ERROR;
 SELECT AddRasterConstraints('upgrade_test_raster', 'r');
+
+CREATE TABLE upgrade_test_raster_with_regular_blocking(r raster);
+INSERT INTO upgrade_test_raster_with_regular_blocking(r) VALUES
+(
+	ST_AddBand(
+		ST_MakeEmptyRaster(
+			10, 10, 1, 1, 2, 2, 0, 0,4326
+		),
+		1,
+		'8BSI'::text,
+		-129,
+		NULL
+	)
+);
+
+--SET client_min_messages TO ERROR;
+SELECT AddRasterConstraints('upgrade_test_raster_with_regular_blocking', 'r', 'regular_blocking');
