9d9c7290406ecfe5e2368813ca945185679fe983
Sandro Santilli <strk@kbt.io>
2023-05-23 09:44:30 +0200

    Cleanup xml entities fixer script

--- utils/fix_xml_entities.sh
+++ utils/fix_xml_entities.sh
@@ -3,9 +3,6 @@
 cd $(dirname $0)/..
 
 perl -CSDL -pi \
-	-e 's/\&([_a-zA-Z]*)[\s\h][\s\h]*;/&\1;/g' \
-	$(find doc/po/ -name *.po)
-
-perl -CSDL -pi \
-	-e 's/\&([_a-zA-Z]*)[\s\h][\s\h]*;/&\1;/g' \
+	-e 's/\&([_a-zA-Z]*)[\s\h][\s\h]*;/&\1;/g;' \
+	-e 's/&#x([0-9A-F]*)[\s\h][\s\h]*;/&#x\1;/g' \
 	$(find doc/po/ -name *.po)
