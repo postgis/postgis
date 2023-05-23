494c0f510ee92f66e923a8cb66c14ce429b35796
Sandro Santilli <strk@kbt.io>
2023-05-23 09:37:57 +0200

    Catch more broken xml entities

--- utils/fix_xml_entities.sh
+++ utils/fix_xml_entities.sh
@@ -3,5 +3,9 @@
 cd $(dirname $0)/..
 
 perl -CSDL -pi \
-	-e 's/\&([_a-z]*)[\s\h][\s\h]*;/&\1;/g' \
+	-e 's/\&([_a-zA-Z]*)[\s\h][\s\h]*;/&\1;/g' \
+	$(find doc/po/ -name *.po)
+
+perl -CSDL -pi \
+	-e 's/\&([_a-zA-Z]*)[\s\h][\s\h]*;/&\1;/g' \
 	$(find doc/po/ -name *.po)
