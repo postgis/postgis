924b50d9e6db7b845562d9689f53d66256fe535a
Regina Obe <lr@pcorp.us>
2023-06-04 00:29:59 -0400

    Downgrade JSON and make it conditionally set if not set

--- ci/winnie/regress_postgis.sh
+++ ci/winnie/regress_postgis.sh
@@ -10,13 +10,16 @@ if  [[ "${OVERRIDE}" == '' ]] ; then
 	export ZLIB_VER=1.2.11
   export PROTOBUF_VER=3.2.0
 	export PROTOBUFC_VER=1.2.1
-	export JSON_VER=0.16
+	export JSON_VER=0.12
 	export PROJSO=libproj-19.dll
 fi;
 
 export PROTOBUF_VER=3.2.0
 export PROTOBUFC_VER=1.2.1
-export JSON_VER=0.16
+if  [[ "${JSON_VER}" == '' ]] ; then
+  export JSON_VER=0.12
+fi;
+
 export PCRE_VER=8.33
 
 if  [[ "${ICON_VER}" == '' ]] ; then
