06bed87f309bc0c207b5d0ca22680d368c7347ed
Regina Obe <lr@pcorp.us>
2023-06-12 03:30:06 -0400

    debug_standardize_address function for debugging address standardizer rules.  Json formatted output References #5397
    Includes
    - rulestring (currently missing the rule type and weight
    - tokens which includes input words and corresponding standardize output
    - the stdaddr (best guess) address

--- extensions/address_standardizer/address_standardizer.c
+++ extensions/address_standardizer/address_standardizer.c
@@ -18,7 +18,7 @@ PG_MODULE_MAGIC;
 
 Datum standardize_address(PG_FUNCTION_ARGS);
 Datum standardize_address1(PG_FUNCTION_ARGS);
-
+Datum debug_standardize_address(PG_FUNCTION_ARGS);
 
 /*
  * The signature for standardize_address follows. The lextab, gaztab and
@@ -52,6 +52,266 @@ Datum standardize_address1(PG_FUNCTION_ARGS);
  *
  *    rule text
 */
+PG_FUNCTION_INFO_V1(debug_standardize_address);
+
+Datum
+debug_standardize_address(PG_FUNCTION_ARGS)
+{
+    STANDARDIZER        *std;
+    char                *lextab;
+    char                *gaztab;
+    char                *rultab;
+    char                *micro;
+    char                *macro;
+    //Datum                result;
+    STDADDR             *stdaddr;
+    char               **values;
+	int k;
+    char rule_in[100];
+	char rule_out[100];
+	char temp[10];
+    int stz_no , n ;
+	//SEG *rseg;
+	StringInfo	result  = makeStringInfo();
+
+	elog(DEBUG2, "Start %s", __func__);
+	initStringInfo(result);
+
+	appendStringInfoChar(result, '{');
+
+    lextab = text_to_cstring(PG_GETARG_TEXT_P(0));
+    gaztab = text_to_cstring(PG_GETARG_TEXT_P(1));
+    rultab = text_to_cstring(PG_GETARG_TEXT_P(2));
+    micro  = text_to_cstring(PG_GETARG_TEXT_P(3));
+
+	if ( (PG_NARGS()  > 4) && (!PG_ARGISNULL(4)) ) {
+		macro  = text_to_cstring(PG_GETARG_TEXT_P(4));
+	}
+	else {
+		ADDRESS             *paddr;
+		HHash               *stH;
+		stH = (HHash *) palloc0(sizeof(HHash));
+		int                  err;
+		if (!stH) {
+			elog(ERROR, "%s: Failed to allocate memory for hash!", __func__);
+			return -1;
+		}
+
+		elog(DEBUG1, "going to load_state_hash");
+
+		err = load_state_hash(stH);
+		if (err) {
+			elog(DEBUG2, "got err=%d from load_state_hash().", err);
+#ifdef USE_HSEARCH
+			elog(DEBUG2, "calling hdestroy_r(stH).");
+			hdestroy_r(stH);
+#endif
+			elog(ERROR, "standardize_address: load_state_hash() failed(%d)!", err);
+			return -1;
+		}
+
+		elog(DEBUG2, "calling parseaddress()");
+		paddr = parseaddress(stH, micro, &err);
+
+		if (!paddr) {
+			elog(ERROR, "parse_address: parseaddress() failed!");
+			return -1;
+		}
+
+		/* check for errors and comput length of macro string */
+		if (paddr->street2)
+			elog(ERROR, "standardize_address() can not be passed an intersection.");
+		if (! paddr-> address1)
+			elog(ERROR, "standardize_address() could not parse the address into components.");
+
+		k = 1;
+		if (paddr->city) k += strlen(paddr->city) + 1;
+		if (paddr->st)   k += strlen(paddr->st)   + 1;
+		if (paddr->zip)  k += strlen(paddr->zip)  + 1;
+		if (paddr->cc)   k += strlen(paddr->cc)   + 1;
+
+		/* create micro and macro from paddr */
+		micro = pstrdup(paddr->address1);
+		macro = (char *) palloc(k * sizeof(char));
+		*macro = '\0';
+		if (paddr->city) { strcat(macro, paddr->city); strcat(macro, ","); }
+		if (paddr->st  ) { strcat(macro, paddr->st  ); strcat(macro, ","); }
+		if (paddr->zip ) { strcat(macro, paddr->zip ); strcat(macro, ","); }
+		if (paddr->cc  ) { strcat(macro, paddr->cc  ); strcat(macro, ","); }
+	}
+	appendStringInfoString(result, "\"micro\":");
+	appendStringInfo(result, "%s", quote_identifier(micro));
+	appendStringInfoString(result, ",");
+
+	appendStringInfoString(result, "\"macro\":");
+	appendStringInfo(result, "%s", quote_identifier(macro));
+	appendStringInfoString(result, ",");
+
+    std = GetStdUsingFCInfo(fcinfo, lextab, gaztab, rultab);
+    if (!std)
+        elog(ERROR, "%s failed to create the address standardizer object!",  __func__);
+
+	//output_rule_statistics( std->pagc_p->rules, std->err_p );
+	STAND_PARAM *ms = std->misc_stand;
+	elog(DEBUG2, "%s: calling std_standardize_mm('%s', '%s')", __func__ , micro, macro);
+    stdaddr = std_standardize_mm( std, micro, macro, 0 );
+	elog(DEBUG2, "%s back from fetch_stdaddr",  __func__);
+
+	DEF *__def__ ;
+    STZ **__stz_list__;
+
+	STZ_PARAM *__stz_info__ = ms->stz_info ;
+
+	int lex_pos;
+	elog(DEBUG2, "Input tokenization candidates:\n");
+	appendStringInfoString(result, "\"input_tokens\":[");
+	int started = 0;
+	for (lex_pos = FIRST_LEX_POS;lex_pos < ms->LexNum;lex_pos ++)
+	{
+
+		for ( __def__ = ms->lex_vector[lex_pos].DefList; __def__ != NULL; __def__ = __def__->Next)
+		{
+			if (started > 0 ){
+				appendStringInfoChar(result, ',');
+			}
+			appendStringInfo(result, "{\"pos\": %u,", lex_pos);
+			appendStringInfoString(result, "\"word\":");
+			appendStringInfoString(result, quote_identifier(ms->lex_vector[lex_pos].Text) );
+			appendStringInfoString(result, ",\"stdword\":");
+			appendStringInfoString(result, quote_identifier(((__def__->Protect )? ms->lex_vector[lex_pos].Text : __def__->Standard)) );
+			appendStringInfoString(result, ",\"token\":");
+			appendStringInfoString(result, quote_identifier(in_symb_name(__def__->Type)) );
+			appendStringInfo(result, ",\"token-code\": %u}", __def__->Type);
+			elog(DEBUG2, "\t(%d) stdword: %s, tok: %d (%s)\n",lex_pos,((__def__->Protect )? ms->lex_vector[lex_pos].Text : __def__->Standard),__def__->Type,in_symb_name(__def__->Type));
+			started++;
+
+		}
+	}
+	appendStringInfoChar(result, ']');
+
+	n = __stz_info__->stz_list_size ;
+	__stz_list__ = __stz_info__->stz_array ;
+	started = 0;
+
+	appendStringInfoString(result, ", \"rules\":[");
+	for ( stz_no = 0 ; stz_no < n ; stz_no ++ )
+	{
+		if  (stz_no > 0 ){
+			appendStringInfoChar(result, ',');
+		}
+		strcpy(rule_in, "");
+		strcpy(rule_out, "");
+
+		STZ *__cur_stz__ = __stz_list__[stz_no] ;
+
+		KW *ruleref = __cur_stz__->build_key;
+		elog( DEBUG2, "Raw standardization %d with score %f:\n" , ( stz_no  ) , __cur_stz__->score ) ;
+		appendStringInfo(result, "{\"score\": %f,", __cur_stz__->score);
+		appendStringInfo(result, "\"raw_score\": %f,", __cur_stz__->raw_score);
+		appendStringInfo(result, "\"no\": %d,", stz_no);
+
+		appendStringInfoString(result, "\"rule_tokens\":[");
+		started = 0;
+		for ( lex_pos = FIRST_LEX_POS ; lex_pos < ms->LexNum ; lex_pos ++ )
+		{
+			SYMB k2;
+			__def__ = __cur_stz__->definitions[lex_pos];
+			k2 = __cur_stz__->output[lex_pos] ;
+			if (started > 0){
+				appendStringInfoChar(result, ',');
+				strcat(rule_out, " ");
+				strcat(rule_in, " ");
+			}
+			sprintf(temp, "%d", __def__->Type);
+			strcat(rule_in, temp);
+			sprintf(temp, "%d", k2);
+			strcat(rule_out, temp);
+
+			appendStringInfo(result, "{\"pos\": %u,", lex_pos);
+			appendStringInfo(result, "\"input-token-code\": %d,",  __def__->Type);
+			appendStringInfo(result, "\"input-token\": %s,", quote_identifier( in_symb_name( __def__->Type ) ) );
+			appendStringInfo(result, "\"input-word\": %s,", quote_identifier(ms->lex_vector[lex_pos].Text) );
+			appendStringInfo(result, "\"mapped-word\": %s,",
+				quote_identifier((( __def__->Protect )? ms->lex_vector[lex_pos].Text : __def__->Standard )) );
+			appendStringInfo(result, "\"output-token-code\": %d,",  k2);
+			appendStringInfo(result, "\"output-token\": %s", quote_identifier( out_symb_name( k2 ) ) );
+			appendStringInfoChar(result, '}');
+			elog( DEBUG2, "\t(%d) Input %d (%s) text %s mapped to output %d (%s)\n" , lex_pos , __def__->Type , in_symb_name( __def__->Type ) , (( __def__->Protect )? ms->lex_vector[lex_pos].Text : __def__->Standard ) , k2 , (( k2 == FAIL )? "NONE" : out_symb_name( k2 ))) ;
+			started++;
+			if ( k2 == FAIL ) break ;
+		}
+
+		appendStringInfo(result, "], \"rule_string\":\"%s",rule_in);
+		appendStringInfoString(result, " -1 ");
+		appendStringInfo(result, "%s",rule_out);
+
+		/**
+		 * TODO:  Figure out how to add the type and weight rule type
+		 * **/
+		//appendStringInfo(result, " %d", ruleref->Type);
+		//elog(DEBUG2, "Rule  type  %d",  ruleref->Type);
+		/** rule weight **/
+		//appendStringInfo(result, " %d", ruleref->Weight);
+
+		appendStringInfoString(result, "\"}");
+	}
+	appendStringInfoChar(result, ']');
+	elog(DEBUG2, "%s: setup values json", __func__);
+	appendStringInfoString(result, ",\"stdaddr\": {");
+    if (stdaddr) {
+        appendStringInfo(result, "\"building\": %s", (stdaddr->building ?
+				quote_identifier(pstrdup(stdaddr->building)) : "null") );
+
+		appendStringInfo(result, ",\"house_num\": %s", (stdaddr->house_num ?
+				quote_identifier(pstrdup(stdaddr->house_num)) : "null") );
+
+		appendStringInfo(result, ",\"predir\": %s", (stdaddr->predir ?
+				quote_identifier(pstrdup(stdaddr->predir)) : "null") );
+
+		appendStringInfo(result, ",\"qual\": %s", (stdaddr->qual ?
+				quote_identifier(pstrdup(stdaddr->qual)) : "null") );
+
+		appendStringInfo(result, ",\"pretype\": %s", (stdaddr->pretype ?
+				quote_identifier(pstrdup(stdaddr->pretype)) : "null") );
+
+		appendStringInfo(result, ",\"name\": %s", (stdaddr->name ?
+				quote_identifier(pstrdup(stdaddr->name)) : "null") );
+
+		appendStringInfo(result, ",\"suftype\": %s", (stdaddr->suftype ?
+				quote_identifier(pstrdup(stdaddr->suftype)) : "null") );
+
+		appendStringInfo(result, ",\"sufdir\": %s", (stdaddr->sufdir ?
+				quote_identifier(pstrdup(stdaddr->sufdir)) : "null") );
+
+		appendStringInfo(result, ",\"ruralroute\": %s", (stdaddr->ruralroute ?
+			quote_identifier(pstrdup(stdaddr->ruralroute)) : "null") );
+
+		appendStringInfo(result, ",\"extra\": %s", (stdaddr->extra ?
+		quote_identifier(pstrdup(stdaddr->extra)) : "null") );
+
+		appendStringInfo(result, ",\"city\": %s", (stdaddr->city ?
+			quote_identifier(pstrdup(stdaddr->city)) : "null") );
+
+		appendStringInfo(result, ",\"state\": %s", (stdaddr->state ?
+			quote_identifier(pstrdup(stdaddr->state)) : "null") );
+
+		appendStringInfo(result, ",\"country\": %s", (stdaddr->country ?
+			quote_identifier(pstrdup(stdaddr->country)) : "null") );
+
+		appendStringInfo(result, ",\"postcode\": %s", (stdaddr->postcode ?
+			quote_identifier(pstrdup(stdaddr->postcode)) : "null") );
+
+		appendStringInfo(result, ",\"box\": %s", (stdaddr->box ?
+			quote_identifier(pstrdup(stdaddr->box)) : "null") );
+
+		appendStringInfo(result, ",\"unit\": %s", (stdaddr->unit ?
+			quote_identifier(pstrdup(stdaddr->unit)) : "null") );
+    }
+    stdaddr_free(stdaddr);
+	appendStringInfoString(result, "}");
+	appendStringInfoString(result, "}");
+	PG_RETURN_TEXT_P(cstring_to_text_with_len(result->data, result->len));
+}
 
 PG_FUNCTION_INFO_V1(standardize_address);
 
