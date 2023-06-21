61bdf43a72ca68968ee19a59bbe3720edc2003e7
Paul Ramsey <pramsey@cleverelephant.ca>
2023-06-21 15:39:13 -0700

    Add results of proj_context_get_database_path to proj version string

--- postgis/lwgeom_transform.c
+++ postgis/lwgeom_transform.c
@@ -213,15 +213,20 @@ Datum postgis_proj_version(PG_FUNCTION_ARGS)
 #endif
 
 #if POSTGIS_PROJ_VERSION >= 70
+
 	stringbuffer_aprintf(&sb,
-		" PROJ_NETWORK=%s",
+		" NETWORK_ENABLED=%s",
 		proj_context_is_network_enabled(NULL) ? "ON" : "OFF");
 
 	if (proj_context_get_url_endpoint(NULL))
-		stringbuffer_aprintf(&sb, " PROJ_URL_ENDPOINT=%s", proj_context_get_url_endpoint(NULL));
+		stringbuffer_aprintf(&sb, " URL_ENDPOINT=%s", proj_context_get_url_endpoint(NULL));
 
 	if (proj_context_get_user_writable_directory(NULL, 0))
-		stringbuffer_aprintf(&sb, " PROJ_GRID_DIRECTORY=%s", proj_context_get_user_writable_directory(NULL, 0));
+		stringbuffer_aprintf(&sb, " USER_WRITABLE_DIRECTORY=%s", proj_context_get_user_writable_directory(NULL, 0));
+
+	if (proj_context_get_database_path(NULL))
+		stringbuffer_aprintf(&sb, " DATABASE_PATH=%s", proj_context_get_database_path(NULL));
+
 #endif
 
 	PG_RETURN_POINTER(cstring_to_text(stringbuffer_getstring(&sb)));
