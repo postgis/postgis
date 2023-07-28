5da1de75214888c621f1a31d380369894c4db1aa
Bo Zhou <Bo.Schwarzstein@gmail.com>
2023-07-28 11:08:33 +0900

    Updated USD Writer

--- deps/usd/writer.cpp
+++ deps/usd/writer.cpp
@@ -2,6 +2,46 @@
 
 using namespace USD;
 
-Writer::Writer() {}
+void
+Writer::WritePoint(const LWPOINT *p)
+{}
+
+void
+Writer::WriteCollection(const LWCOLLECTION *coll)
+{}
+
+Writer::Writer()
+{
+	m_stage = pxr::UsdStage::CreateInMemory();
+}
 
 Writer::~Writer() {}
+
+void
+Writer::Write(LWGEOM *geom)
+{
+	try
+	{
+		int type = geom->type;
+		switch (type)
+		{
+		case POINTTYPE:
+			WritePoint((LWPOINT *)geom);
+			break;
+		case COLLECTIONTYPE:
+			WriteCollection((LWCOLLECTION *)geom);
+			break;
+		default:
+			lwerror("usd: geometry type '%s' not supported", lwtype_name(type));
+			break;
+		}
+	}
+	catch (const std::exception &e)
+	{
+		lwerror("usd: caught standard c++ exception '%s'", e.what());
+	}
+	catch (...)
+	{
+		lwerror("usd: caught unknown c++ exception");
+	}
+}
--- deps/usd/writer.h
+++ deps/usd/writer.h
@@ -1,4 +1,11 @@
-#pragma once
+#ifndef USD_WRITER_H
+#define USD_WRITER_H
+
+/* do not forget that lwgeom is C library */
+extern "C" {
+#include "lwgeom_log.h"
+#include "liblwgeom.h"
+}
 
 #include <pxr/usd/usd/stage.h>
 
@@ -6,11 +13,18 @@ namespace USD {
 
 class Writer {
 	private:
-		pxr::UsdStageRefPtr m_stage;
+		void WritePoint(const LWPOINT *p);
+		void WriteCollection(const LWCOLLECTION *coll);
 
 	public:
 		Writer();
 		~Writer();
+
+		void Write(LWGEOM *geom);
+
+		pxr::UsdStageRefPtr m_stage;
 };
 
 }; // namespace USD
+
+#endif
