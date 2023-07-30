44a8c62a4db97325dcb49230540198e8720937fa
Regina Obe <lr@pcorp.us>
2023-07-30 05:40:16 -0400

    Wrong command just comments

--- ci/debbie/postgis_release_docs.sh
+++ ci/debbie/postgis_release_docs.sh
@@ -116,9 +116,9 @@ make -C ../doc/po/fr local-pdf-install
 make -C ../doc/po/de local-pdf-install
 
 # build comments files
-make -C ../doc/po/ja local-comments-install
-make -C ../doc/po/fr local-comments-install
-make -C ../doc/po/de local-comments-install
+make -C ../doc/po/ja comments
+make -C ../doc/po/fr comments
+make -C ../doc/po/de comments
 # Build and install localized pdf and epub
 # make pdf-install-localized # || : survive failures
 #make epub-install-localized # || : survive failures
