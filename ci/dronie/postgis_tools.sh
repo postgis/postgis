ef2708d6145847e80390aa950e279a40ef5b6b89
Regina Obe <lr@pcorp.us>
2023-08-11 22:19:12 -0400

    Move dronie tool steps to dedicated script

---
+++ ci/dronie/postgis_tools.sh
@@ -0,0 +1,12 @@
+ #!/usr/bin/env bash
+
+# Exit on first error
+set -e
+
+./configure --without-pgconfig --prefix=/tmp/pgx
+make
+make check
+make install
+/tmp/pgx/bin/postgis help
+/tmp/pgx/bin/shp2pgsql
+/tmp/pgx/bin/raster2pgsql
