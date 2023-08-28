e1b7db45486fea9cb3c0be8d58cbd2a785b631f4
Sandro Santilli <strk@kbt.io>
2023-08-28 19:10:20 +0200

    Fix upgrades in presence of views using ST_Clip
    Includes regress test
    
    References #5488 in master branch (3.5.0dev)

--- raster/test/regress/hooks/hook-after-upgrade-raster.sql
+++ raster/test/regress/hooks/hook-after-upgrade-raster.sql
@@ -1,6 +1,10 @@
 DROP VIEW upgrade_test_raster_view_st_value;
+DROP VIEW upgrade_test_raster_view_st_clip;
+
 DROP TABLE upgrade_test_raster;
 DROP TABLE upgrade_test_raster_with_regular_blocking;
 
 -- Drop functions deprecated on upgrade
 DROP FUNCTION IF EXISTS st_value_deprecated_by_postgis_201(raster,geometry,boolean);
+
+--DROP FUNCTION IF EXISTS st_value_deprecated_by_postgis_201(raster,geometry,boolean);
