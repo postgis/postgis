56368ceddf1a2c35122f5c68cf8f8445f18546a7
Sandro Santilli <strk@kbt.io>
2023-08-29 22:50:10 +0200

    Fix the all-functions view code to skip bugs and complications

--- regress/hooks/use-all-functions.sql
+++ regress/hooks/use-all-functions.sql
@@ -7,17 +7,31 @@ BEGIN
 			array_agg(
 				regexp_replace(
 					regexp_replace(
-						oid::regprocedure::text,
+						regexp_replace(
+							oid::regprocedure::text,
+							'double precision', 'float8'
+						),
 						'\(([^)])',
 						'(NULL::\1'
 					),
 					',',
 					',NULL::',
 					'g'
-				) || ' as f' || oid
+				)
+				||
+				CASE WHEN prokind = 'w' THEN ' OVER()' ELSE '' END
+				|| ' as f' || oid
 			) f
 		FROM pg_catalog.pg_proc
 		WHERE proname like 'st\_%'
+		AND prokind in ('f','w')
+		-- See https://trac.osgeo.org/postgis/ticket/5498 and
+		-- remove when fixed
+		AND proname != 'st_approxquantile'
+		-- We skip functions taking anyelement for now
+		-- as those ones would need us to cast to some
+		-- real type
+		AND proname in ( 'st_asmvt' )
 	LOOP
 		EXECUTE format(
 			'CREATE VIEW postgis_locking_view AS SELECT %s',
