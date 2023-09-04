86a094e823d060250eba5379e4f8f800a1537c65
Sandro Santilli <strk@kbt.io>
2023-09-04 10:21:59 +0200

    Have check_all_upgrades test with all functions locked

--- regress/hooks/hook-after-upgrade.sql
+++ regress/hooks/hook-after-upgrade.sql
@@ -1,3 +1,4 @@
+-- TODO: move all these views and tables under postgis_upgrade_test_data
 DROP VIEW IF EXISTS upgrade_view_test_overlay;
 DROP VIEW IF EXISTS upgrade_view_test_unaryunion;
 DROP VIEW IF EXISTS upgrade_view_test_subdivide;
@@ -7,21 +8,22 @@ DROP VIEW IF EXISTS upgrade_view_test_askml;
 DROP VIEW IF EXISTS upgrade_view_test_dwithin;
 DROP VIEW IF EXISTS upgrade_view_test_clusterkmeans;
 DROP VIEW IF EXISTS upgrade_view_test_distance;
-DROP TABLE upgrade_test;
+DROP TABLE IF EXISTS upgrade_test;
+
+-- Drop any upgrade test data
+DROP SCHEMA IF EXISTS postgis_upgrade_test_data CASCADE;
 
 -- Drop functions deprecated on upgrade
-DROP FUNCTION IF EXISTS st_force3dz_deprecated_by_postgis_301(geometry);
-DROP FUNCTION IF EXISTS st_force3d_deprecated_by_postgis_301(geometry);
-DROP FUNCTION IF EXISTS st_force3dm_deprecated_by_postgis_301(geometry);
-DROP FUNCTION IF EXISTS st_force4d_deprecated_by_postgis_301(geometry);
-DROP FUNCTION IF EXISTS st_intersection_deprecated_by_postgis_301(geometry,geometry);
-DROP FUNCTION IF EXISTS st_difference_deprecated_by_postgis_301(geometry,geometry);
-DROP FUNCTION IF EXISTS st_symdifference_deprecated_by_postgis_301(geometry,geometry);
-DROP FUNCTION IF EXISTS st_unaryunion_deprecated_by_postgis_301(geometry);
-DROP FUNCTION IF EXISTS st_subdivide_deprecated_by_postgis_301(geometry,integer);
-DROP FUNCTION IF EXISTS st_askml_deprecated_by_postgis_200(geometry,integer);
-DROP FUNCTION IF EXISTS st_askml_deprecated_by_postgis_200(geography,integer);
-DROP FUNCTION IF EXISTS st_dwithin_deprecated_by_postgis_300(geography,geography,float8);
-DROP FUNCTION IF EXISTS st_dwithin_deprecated_by_postgis_300(text,text,float8);
-DROP FUNCTION IF EXISTS st_clusterkmeans_deprecated_by_postgis_302(geometry,integer);
-DROP FUNCTION IF EXISTS st_distance_deprecated_by_postgis_300(geography,geography);
+DO LANGUAGE 'plpgsql' $DROPALL$
+DECLARE
+	rec pg_catalog.pg_proc;
+BEGIN
+	FOR rec IN
+		SELECT * FROM pg_catalog.pg_proc
+		WHERE proname ~ '_deprecated_by_postgis'
+	LOOP
+		RAISE NOTICE 'Dropping function %', rec.oid::regprocedure;
+		EXECUTE pg_catalog.format('DROP FUNCTION %s', rec.oid::regprocedure);
+	END LOOP;
+END;
+$DROPALL$;
