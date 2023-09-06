46762f988e7e45f5fc04a6df527bf687a85ec8b1
Sandro Santilli <strk@kbt.io>
2023-09-06 12:26:10 +0200

    [woodie] Reduce code duplication for docs workflow

--- .woodpecker/update-docs-localized.sh
+++ .woodpecker/update-docs-localized.sh
@@ -9,10 +9,6 @@ TARGETS="check-xml html cheatsheets check-cheatsheets pdf"
 
 cat docs.yml | sed '/DO NOT EDIT/q' > docs.yml.new
 
-cat docs.yml.new |
-  sed '/# Common docs path/,/# END OF COMMON/!d;/# END OF COMMON/q' |
-  sed 's/^/  /' > docs.yml.common_paths
-
 exec >> docs.yml.new
 for target in ${TARGETS}
 do
@@ -21,10 +17,7 @@ do
   do
     group=localized-${target}
     sed "s/@LANG@/${lang}/;s/@TARGET@/${target}/;s/@GROUP@/${group}/" docs-localized.yml.in
-    # Add common docs paths
-    cat docs.yml.common_paths
   done
 done
 
 mv -b docs.yml.new docs.yml
-rm docs.yml.common_paths
