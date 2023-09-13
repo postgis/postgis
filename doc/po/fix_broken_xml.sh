813f796d33279cfc756c8584afcddb8b179e69fa
Sandro Santilli <strk@kbt.io>
2023-09-13 09:18:13 +0200

    Add code to fix the kind of broken URI as found with 42a513fa1b

--- doc/po/fix_broken_xml.sh
+++ doc/po/fix_broken_xml.sh
@@ -6,3 +6,6 @@ perl -pi -e 's/&([#a-z_0-9]+)[^#a-z0-9;]+;/&\1;/g' $@
 # Fix malformed CDATA
 perl -pi -e 's/<[^[>\\]*!\[CDATA\[/<!\[CDATA\[/g' $@
 
+# Fix malformed URIs (-C is for unicode)
+perl -C -pi -e 's/(https?)[[:blank:]]*:/\1:/g' $@
+
