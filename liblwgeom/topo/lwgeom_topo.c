6a6d7dadd98e79daa4abec3ae2d8dfc3f58618f9
Sandro Santilli <strk@kbt.io>
2024-03-04 19:13:47 +0100

    Use GEOS-3.7+ GEOSDistanceIndexed to find existing edges
    Closes #3587 in master branch (3.5.0dev)

--- liblwgeom/topo/lwgeom_topo.c
+++ liblwgeom/topo/lwgeom_topo.c
@@ -5669,21 +5669,32 @@ _lwt_AddLine(LWT_TOPOLOGY* topo, LWLINE* line, double tol, int* nedges,
     line->points->npoints, numedges);
   if ( numedges )
   {{
+    /* TODO: compute earlier for reuse later ? */
+    GEOSGeometry *noded_g = LWGEOM2GEOS(noded, 0);
     /* collect those whose distance from us is < tol */
     nearbycount += numedges;
     nearby = lwalloc(numedges * sizeof(LWGEOM *));
     for (i=0; i<numedges; ++i)
-    {
+    {{
       LW_ON_INTERRUPT(return NULL);
       LWT_ISO_EDGE *e = &(edges[i]);
       LWGEOM *g = lwline_as_lwgeom(e->geom);
-      LWDEBUGF(2, "Computing distance from edge %d having %d points", i, e->geom->points->npoints);
-      double dist = lwgeom_mindistance2d(g, noded);
-      /* must be closer than tolerated, unless distance is zero */
+      GEOSGeometry *edge_g = LWGEOM2GEOS(g, 0);
+      LWDEBUGF(2, "Computing distance from edge %d with %d points", i, e->geom->points->npoints);
+      double dist;
+      if ( 0 == GEOSDistanceIndexed(edge_g, noded_g, &dist) ) {
+        GEOSGeom_destroy(edge_g);
+        GEOSGeom_destroy(noded_g);
+        lwgeom_free(noded);
+        lwerror("GEOSDistanceIndexed error: %s", lwgeom_geos_errmsg);
+        return NULL;
+      }
       if ( dist && dist >= tol ) continue;
       nearby[nearbyindex++] = g;
-    }
+      GEOSGeom_destroy(edge_g);
+    }}
     LWDEBUGF(1, "Found %d edges closer than tolerance (%g)", nearbyindex, tol);
+    GEOSGeom_destroy(noded_g);
   }}
   int nearbyedgecount = nearbyindex;
 
