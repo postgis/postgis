3085fb74de93e63951afcaffb7975ed2cb849ca6
Regina Obe <lr@pcorp.us>
2024-03-22 21:37:59 -0400

    Increase alloted time some more for interrupt buffer and relate

--- regress/core/interrupt_relate.sql
+++ regress/core/interrupt_relate.sql
@@ -19,32 +19,32 @@ SELECT 1::int as id, ST_Collect(g) g FROM (
 SET statement_timeout TO 100;
 
 select ST_Contains(g,g) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('contains', '200ms');
+SELECT _timecheck('contains', '220ms');
 
 select ST_Covers(g,g) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('covers', '200ms');
+SELECT _timecheck('covers', '220ms');
 
 select ST_CoveredBy(g,g) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('coveredby', '200ms');
+SELECT _timecheck('coveredby', '250ms');
 
 select ST_Crosses(g,g) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('crosses', '200ms');
+SELECT _timecheck('crosses', '220ms');
 
 -- NOTE: we're reversing one of the operands to avoid the
 --       short-circuit described in #3226
 select ST_Equals(g,st_reverse(g)) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('equals', '200ms');
+SELECT _timecheck('equals', '220ms');
 
 -- NOTE: intersects became very fast, so we segmentize
 --       input to make it slower
 select ST_Intersects(g,ST_Segmentize(g,1e-4)) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('intersects', '200ms');
+SELECT _timecheck('intersects', '210ms');
 
 select ST_Overlaps(g,g) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('overlaps', '200ms');
+SELECT _timecheck('overlaps', '220ms');
 
 select ST_Relate(g,g) from _inputs WHERE id = 1; -- 6+ seconds
-SELECT _timecheck('relate', '200ms');
+SELECT _timecheck('relate', '220ms');
 
 DROP FUNCTION _timecheck(text, interval);
 DROP TABLE _inputs;
