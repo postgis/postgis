b802f32c8a56a3d59cc16cb71429f483b1d4c878
Sandro Santilli <strk@kbt.io>
2024-06-07 09:38:43 +0200

    Override GEOS typedef to add attribute when needed

--- liblwgeom/lwgeom_geos.h
+++ liblwgeom/lwgeom_geos.h
@@ -23,10 +23,17 @@
  *
  **********************************************************************/
 
+#if POSTGIS_GEOS_VERSION < 31300
+/* See https://github.com/libgeos/geos/pull/1097 */
+typedef void (*GEOSMessageHandler)(const char *fmt, ...) __attribute__ (( format(printf, 1, 0) ));
+#endif
+
 #include "geos_c.h"
 
 #include "liblwgeom.h"
 #include "lwunionfind.h"
+
+
 
 /*
 ** Public prototypes for GEOS utility functions.
