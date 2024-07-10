6c135f9ca3a9b60889eae2bc2adc67350e0c0e4a
Regina Obe <lr@pcorp.us>
2024-07-10 12:25:14 -0400

    REMOVE topology robustness test failing on windows     References #5727 for PostGIS 3.5.0     Will put back after troubleshoot some more

--- topology/test/tests.mk
+++ topology/test/tests.mk
@@ -74,7 +74,6 @@ TESTS += \
 	$(top_srcdir)/topology/test/regress/topoelementarray_agg.sql \
 	$(top_srcdir)/topology/test/regress/topoelement_cast.sql \
 	$(top_srcdir)/topology/test/regress/topoelement.sql \
-	$(top_srcdir)/topology/test/regress/topogeo_addlinestring_robust.sql \
 	$(top_srcdir)/topology/test/regress/topogeo_addlinestring.sql \
 	$(top_srcdir)/topology/test/regress/topogeo_addpoint_merge_edges.sql \
 	$(top_srcdir)/topology/test/regress/topogeo_addpoint.sql \
