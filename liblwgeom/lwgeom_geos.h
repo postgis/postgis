5e09dcbd53fd43fbc31809cfb095204c60f68579
Paul Ramsey <pramsey@cleverelephant.ca>
2024-08-22 11:21:56 -0700

    Back out __STDC_VERSION__ check, it is not effective in general

--- liblwgeom/lwgeom_geos.h
+++ liblwgeom/lwgeom_geos.h
@@ -24,10 +24,8 @@
  **********************************************************************/
 
 #if POSTGIS_GEOS_VERSION < 31300
-#if __STDC_VERSION__ >= 201112L
 /* See https://github.com/libgeos/geos/pull/1097 */
 typedef void (*GEOSMessageHandler)(const char *fmt, ...) __attribute__ (( format(printf, 1, 0) ));
-#endif
 #endif
 
 #include "geos_c.h"
