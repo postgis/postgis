31f672e4228e371b462d3c6988615a1854cca7fe
Paul Ramsey <pramsey@cleverelephant.ca>
2025-04-04 12:20:25 -0700

    Remove extraneous debug line

--- liblwgeom/lwgeom_api.c
+++ liblwgeom/lwgeom_api.c
@@ -136,7 +136,6 @@ getPoint4d_p(const POINTARRAY *pa, uint32_t n, POINT4D *op)
 	if ( n>=pa->npoints )
 	{
 		lwerror("%s [%d] called with n=%d and npoints=%d", __FILE__, __LINE__, n, pa->npoints);
-		LWDEBUGF(2, "%s [%d] called with n=%d and npoints=%d", __FILE__, __LINE__, n, pa->npoints);
 		return 0;
 	}
 
