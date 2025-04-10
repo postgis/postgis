0743615c8fb0fbee2e1b676e917958cf0fa4e123
Paul Ramsey <pramsey@cleverelephant.ca>
2025-04-10 11:06:38 -0700

    Remove unneeded header (and fix pg18 build)

--- postgis/postgis_legacy.c
+++ postgis/postgis_legacy.c
@@ -34,7 +34,7 @@
 #include "postgres.h"
 #include "utils/builtins.h"
 #include "../postgis_config.h"
-#include "lwgeom_pg.h"
+// #include "lwgeom_pg.h"
 
 #define POSTGIS_DEPRECATE(version, funcname) \
 	Datum funcname(PG_FUNCTION_ARGS); \
