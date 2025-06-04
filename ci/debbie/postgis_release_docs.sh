746fd6acd22c5ff9072464512299e8efd72f7348
Regina Obe <lr@pcorp.us>
2025-06-04 12:30:27 -0400

    Add Swedish, thanks to Daniel Nylander

--- ci/debbie/postgis_release_docs.sh
+++ ci/debbie/postgis_release_docs.sh
@@ -117,6 +117,7 @@ make -C ../doc/po/fr local-pdf-install
 make -C ../doc/po/de local-pdf-install
 make -C ../doc/po/zh_Hans local-pdf-install
 make -C ../doc/po/ko_KR local-pdf-install
+make -C ../doc/po/sv local-pdf-install
 
 # build comments files
 make -C ../doc/po/ja comments
@@ -124,6 +125,7 @@ make -C ../doc/po/fr comments
 make -C ../doc/po/de comments
 make -C ../doc/po/zh_Hans comments
 make -C ../doc/po/ko_KR comments
+make -C ../doc/po/sv comments
 # Build and install localized pdf and epub
 # make pdf-install-localized # || : survive failures
 #make epub-install-localized # || : survive failures
