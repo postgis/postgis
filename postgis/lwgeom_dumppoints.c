cbc1c79321f6978b6c4ebe233251d3bbf9886383
Paul Ramsey <pramsey@cleverelephant.ca>
2025-06-05 19:53:57 -0700

    Guard against overly nested collections in dump functions, references #5905

--- postgis/lwgeom_dumppoints.c
+++ postgis/lwgeom_dumppoints.c
@@ -271,6 +271,10 @@ Datum LWGEOM_dumppoints(PG_FUNCTION_ARGS) {
 
 		/* if a collection and we have more geoms */
 		if (node->idx < lwcoll->ngeoms) {
+
+			if(state->stacklen >= MAXDEPTH)
+				elog(ERROR, "Unabled to dump overly nested collection");
+
 			/* push the next geom on the path and the stack */
 			lwgeom = lwcoll->geoms[node->idx++];
 			state->path[state->pathlen++] = Int32GetDatum(node->idx);
