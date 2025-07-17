ad6820b35a493063b84a8ce3e07462496ea650e5
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-07-17 14:18:51 +0200

    fix(SFCGAL): CG_Visibilty returns empty polygon if one input is empty

--- sfcgal/regress/visibility.sql
+++ sfcgal/regress/visibility.sql
@@ -2,3 +2,8 @@ SELECT 'Visibility point in polygon', ST_AsText(CG_Visibility('POLYGON((0.0 4.0,
 SELECT 'Visibility point in polygon with hole', ST_AsText(CG_Visibility('POLYGON((0.0 4.0,0.0 0.0,3.0 2.0,4.0 0.0,4.0 4.0,1.0 2.0,0.0 4.0),(0.2 1.8,0.9 1.8,0.7 1.2,0.2 1.8))', 'POINT(0.5 2.0)'), 2);
 SELECT 'Visibility segment in polygon', ST_AsText(CG_Visibility('POLYGON((0.0 4.0,0.0 0.0,3.0 2.0,4.0 0.0,4.0 4.0,1.0 2.0,0.0 4.0))', 'POINT(1.0 2.0)', 'POINT(4.0 4.0)'), 2);
 SELECT 'Visibility segment in polygon with hole', ST_AsText(CG_Visibility('POLYGON((1.0 2.0,12.0 3.0,19.0 -2.0,12.0 6.0,14.0 14.0,9.0 5.0,1.0 2.0),(8.0 3.0,8.0 4.0,10.0 3.0,8.0 3.0),(10.0 6.0,11.0 7.0,11.0 6.0,10.0 6.0))', 'POINT(19.0 -2.0)', 'POINT(12.0 6.0)'), 2);
+SELECT 'Visibility point empty polygon', ST_AsText(CG_Visibility(ST_GeomFromText('POLYGON EMPTY',4326), ST_Point(1,2,4326)));
+SELECT 'Visibility point empty point', ST_AsText(CG_Visibility(ST_GeomFromText('POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))',4326), ST_GeomFromText('POINT EMPTY', 4326)));
+SELECT 'Visibility segment empty polygon', ST_AsText(CG_Visibility(ST_GeomFromText('POLYGON EMPTY',4326), ST_Point(1,2,4326) , ST_Point(1,2,4326)));
+SELECT 'Visibility segment empty pointA', ST_AsText(CG_Visibility(ST_GeomFromText('POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))',4326), ST_GeomFromText('POINT EMPTY', 4326) , ST_Point(1,2,4326)));
+SELECT 'Visibility segment empty pointB', ST_AsText(CG_Visibility(ST_GeomFromText('POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))',4326), ST_Point(1,2,4326), ST_GeomFromText('POINT EMPTY', 4326)));
