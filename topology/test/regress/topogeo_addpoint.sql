c81b55d6a9dec0b97e86cfde1eca429bc0f397df
Sandro Santilli <strk@kbt.io>
2025-07-19 09:47:42 +0200

    Have GetFaceContainingPoint catch EMPTY edges in corrupted topology
    Fixes #5925, #5946 in 3.5 branch (3.5.4dev)
    
    Includes regression test

--- topology/test/regress/topogeo_addpoint.sql
+++ topology/test/regress/topogeo_addpoint.sql
@@ -86,3 +86,11 @@ SELECT NULL FROM topology.TopoGeo_addLinestring('t', 'LINESTRING(0 0, 10 0)');
 SELECT 't5794', 'N', topology.TopoGeo_addPoint('t', 'POINT(9 5)', 5);
 SELECT 't5794', 'V', * FROM ValidateTopology('t');
 ROLLBACK;
+
+-- See https://trac.osgeo.org/postgis/ticket/5925
+BEGIN;
+SELECT NULL FROM topology.CreateTopology ('t');
+SELECT NULL FROM topology.TopoGeo_addLinestring('t', 'LINESTRING(0 0, 10 0)');
+UPDATE t.edge_data SET geom = 'LINESTRING EMPTY';
+SELECT 't5925.1', topology.TopoGeo_addPoint('t', 'POINT(5 5)', 0);
+ROLLBACK;
