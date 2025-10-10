d1c28c62c4c5de585a11c6b07cd34e7576546d09
Sandro Santilli <strk@kbt.io>
2025-10-10 21:59:20 +0200

    Be safe about expanding $@

--- doc/po/fix_broken_xml.sh
+++ doc/po/fix_broken_xml.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Fix malformed entities
-perl -pi -e 's/&([#a-z_0-9]+)[^#a-z0-9;]+;/&\1;/g' $@
+perl -pi -e 's/&([#a-z_0-9]+)[^#a-z0-9;]+;/&\1;/g' "$@"
 
 # Fix malformed CDATA
-perl -pi -e 's/<[^[>\\]*!\[CDATA\[/<!\[CDATA\[/g' $@
+perl -pi -e 's/<[^[>\\]*!\[CDATA\[/<!\[CDATA\[/g' "$@"
 
 # Fix malformed URIs (-C is for unicode)
-perl -C -pi -e 's/(https?)[[:blank:]]*:/\1:/g' $@
+perl -C -pi -e 's/(https?)[[:blank:]]*:/\1:/g' "$@"
 
 # Fix smart quotes
-perl -pi -e 's/”/\\"/g' $@
-perl -pi -e 's/“/\\"/g' $@
-perl -pi -e 's/«/\\"/g' $@
-perl -pi -e 's/»/\\"/g' $@
+perl -pi -e 's/”/\\"/g' "$@"
+perl -pi -e 's/“/\\"/g' "$@"
+perl -pi -e 's/«/\\"/g' "$@"
+perl -pi -e 's/»/\\"/g' "$@"
 
