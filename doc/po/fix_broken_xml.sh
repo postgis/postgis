11337be06b6937bd23caa889af1f9a5a319a62c6
Sandro Santilli <strk@kbt.io>
2025-10-10 21:39:28 +0200

    Add fixing of smart quote characters in fix_broken_xml.sh script

--- doc/po/fix_broken_xml.sh
+++ doc/po/fix_broken_xml.sh
@@ -9,3 +9,9 @@ perl -pi -e 's/<[^[>\\]*!\[CDATA\[/<!\[CDATA\[/g' $@
 # Fix malformed URIs (-C is for unicode)
 perl -C -pi -e 's/(https?)[[:blank:]]*:/\1:/g' $@
 
+# Fix smart quotes
+perl -pi -e 's/”/\\"/g' $@
+perl -pi -e 's/“/\\"/g' $@
+perl -pi -e 's/«/\\"/g' $@
+perl -pi -e 's/»/\\"/g' $@
+
