26639a287dfb03c669c599105fbdac1a07bad29e
Regina Obe <lr@pcorp.us>
2025-10-30 00:08:40 -0400

    WIP: fix topogeometry column function addition

--- topology/test/regress/hooks/hook-after-upgrade-topology.sql
+++ topology/test/regress/hooks/hook-after-upgrade-topology.sql
@@ -1,6 +1,9 @@
 -- See https://trac.osgeo.org/postgis/ticket/5102
 SELECT topology.CopyTopology('upgrade_test', 'upgrade_test_copy');
 
+-- check if corruption
+select geometrytype(tg) from upgrade_test.feature limit 2;
+
 SELECT topology.DropTopology('upgrade_test');
 SELECT topology.DropTopology('upgrade_test_copy');
 
