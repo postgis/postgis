6eb81a1c1648fd64269738b52402c8ae9e5492e0
Regina Obe <lr@pcorp.us>
2025-11-15 02:48:59 -0500

    Force order of results of fix topogeometry test

--- topology/test/regress/fix_topogeometry_columns.sql
+++ topology/test/regress/fix_topogeometry_columns.sql
@@ -5,7 +5,8 @@ set client_min_messages to WARNING;
 \i ../more_features.sql
 \i ../hierarchy.sql
 SELECT topology.FixCorruptTopoGeometryColumn(schema_name, table_name, feature_column)
-FROM topology.layer;
+FROM topology.layer
+ORDER BY schema_name, table_name, feature_column;
 
 SELECT topology.DropTopology('city_data');
 DROP SCHEMA features CASCADE;
