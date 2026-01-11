1a44e0fdca9141e7e5c844ec4b537015219b4be9
Regina Obe <lr@pcorp.us>
2026-01-10 22:29:38 -0500

    Disable topology testing on bessie until regressions fixed

--- ci/bessie/postgis_regress.sh
+++ ci/bessie/postgis_regress.sh
@@ -8,7 +8,8 @@ sh autogen.sh
 ./configure \
   --with-projdir=/usr/local \
   --with-libiconv=/usr/local \
-  --with-library-minor-version
+  --with-library-minor-version \
+  --without-topology
 #make distclean
 make
 export PGUSER=postgres
