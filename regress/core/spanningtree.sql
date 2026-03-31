f92f80d776d2d8eb88b2c8f67908b69b2922251c
Paul Ramsey <pramsey@cleverelephant.ca>
2026-03-31 15:36:29 -0700

    Add Window function ST_MinimumSpanningTree(geometry) Takes in SetOf LineString, builds a graph with start/end points as nodes, finds the MST for each connected graph, and returns an integer for each LineString, with a non-zero value indicating participation in the MST, and those numbers being distinct for each connected graph.

---
+++ regress/core/spanningtree.sql
@@ -0,0 +1,42 @@
+
+-- Connected graph (all one cluster)
+SELECT 'connected', id, ST_MinimumSpanningTree(geom) OVER (ORDER BY id) FROM (VALUES
+    (1, 'LINESTRING(0 0,1 0)'::geometry),
+    (2, 'LINESTRING(0 0,0 1)'::geometry),
+    (3, 'LINESTRING(1 1,0 1)'::geometry),
+    (4, 'LINESTRING(1 1,1 0)'::geometry),
+    (5, 'LINESTRING(0 0,1 1)'::geometry),
+    (6, 'LINESTRING(1 0,0 1)'::geometry)
+) AS t(id, geom);
+
+-- Disconnected graph (two clusters)
+SELECT 'disconnected', id, ST_MinimumSpanningTree(geom) OVER (ORDER BY id) FROM (VALUES
+    (1, 'LINESTRING(0 0,1 0)'::geometry),
+    (2, 'LINESTRING(0 0,0 1)'::geometry),
+    (3, 'LINESTRING(1 1,0 1)'::geometry),
+    (4, 'LINESTRING(11 11,11 10)'::geometry),
+    (5, 'LINESTRING(10 10,11 11)'::geometry),
+    (6, 'LINESTRING(11 10,10 11)'::geometry)
+) AS t(id, geom);
+
+
+-- NULL handling
+SELECT 'nulls', id, ST_MinimumSpanningTree(geom) OVER (ORDER BY id), ST_AsText(geom) FROM (VALUES
+    (1, 'LINESTRING(0 0,1 0)'::geometry),
+    (2, 'LINESTRING(0 0,0 1)'::geometry),
+    (3, NULL::geometry),
+    (4, 'LINESTRING(1 1,1 0)'::geometry),
+    (5, 'LINESTRING(0 0,1 1)'::geometry),
+    (6, 'LINESTRING(1 0,0 1)'::geometry)
+) AS t(id, geom);
+
+-- Empty geometry handling
+SELECT 'empty', id, ST_MinimumSpanningTree(geom) OVER (ORDER BY id), ST_AsText(geom) FROM (VALUES
+    (1, 'LINESTRING(0 0,1 0)'::geometry),
+    (2, 'LINESTRING EMPTY'::geometry),
+    (3, 'LINESTRING EMPTY'::geometry),
+    (4, 'LINESTRING(1 1,1 0)'::geometry),
+    (5, 'LINESTRING(0 0,1 1)'::geometry),
+    (6, 'LINESTRING(1 0,0 1)'::geometry)
+) AS t(id, geom);
+
