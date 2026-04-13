af86ae40655391f797ad95cf97b6e7fca5cb7e33
Sandro Santilli <strk@kbt.io>
2026-04-13 10:17:37 +0200

    Drop duplicated tests

--- sfcgal/regress/orientation.sql
+++ sfcgal/regress/orientation.sql
@@ -1,4 +1,2 @@
 SELECT 'CG_Orientation_1', CG_Orientation(CG_ForceLHR('POLYGON((0 0,0 1,1 1,1 0,0 0))'));
 SELECT 'CG_Orientation_2', CG_Orientation(ST_ForceRHR('POLYGON((0 0,0 1,1 1,1 0,0 0))'));
-SELECT 'CG_Orientation_1', CG_Orientation(CG_ForceLHR('POLYGON((0 0,0 1,1 1,1 0,0 0))'));
-SELECT 'CG_Orientation_2', CG_Orientation(ST_ForceRHR('POLYGON((0 0,0 1,1 1,1 0,0 0))'));
