2099dd6d81933dcfb12ee7ff24110ad681f31c65
Jean Felder <jean.felder@oslandia.com>
2026-05-03 19:02:44 +0200

    sfcgal: Adapt CG_ExtrudeStraightSkeleton test to triangulation improvements
    The geometry returned by CG_ExtrudeStraightSkeleton, when extrude and
    roof parameters are set was triangulated. Since SFCGAL 2.3, the
    triangles can be merged when the underlying surface is coplanar. This
    gives a smoother result and a polyhedralsurface which has less
    patches.
    
    Adapt the unit test to work with all SFCGAL versions by comparing the
    number of generated patches.
    
    See: https://gitlab.com/sfcgal/SFCGAL/-/merge_requests/730
    (cherry picked from commit 05682d312a8b67ba7b90dfd5f89993ff082c054b)

--- sfcgal/regress/extrudestraigthskeleton.sql
+++ sfcgal/regress/extrudestraigthskeleton.sql
@@ -1,43 +1,72 @@
-SELECT 'Extrude roof', ST_AsText(CG_ExtrudeStraightSkeleton('POLYGON (( 0 0, 5 0, 5 5, 4 5, 4 4, 0 4, 0 0 ), (1 1, 1 2,2 2, 2 1, 1 1))', 2.0));
-WITH
-  version_info AS (
-    -- The result depends on the SFCGAL Version.
-    -- Prior to version 2.2, it contained extra patches.
-    SELECT
-      string_to_array(postgis_sfcgal_version (), '.')::INT[] AS sfcgal_version_array
-  ),
-  skeleton AS (
-    SELECT
-      ST_NumPatches (
-        CG_ExtrudeStraightSkeleton (
-          'POLYGON (( 0 0, 5 0, 5 5, 4 5, 4 4, 0 4, 0 0 ), (1 1, 1 2,2 2, 2 1, 1 1))',
-          3.0,
-          2.0
-        )
-      ) AS num_patches
-  ),
-  check_patches AS (
-    SELECT
-      sfcgal_version_array,
-      num_patches,
-      (
-        (
-          sfcgal_version_array <= ARRAY[2, 1, 0]
-          AND num_patches = 49
-        )
-        OR (
-          sfcgal_version_array > ARRAY[2, 1, 0]
-          AND num_patches = 39
-        )
-      ) AS test_result
-    FROM
-      skeleton,
-      version_info
-  )
-SELECT
-  'Extrude building and roof',
-  test_result
-FROM
-  check_patches;
+WITH version_info AS (
+  SELECT string_to_array(postgis_sfcgal_version(), '.')::int[] AS sfcgal_version_array
+),
+
+roof AS (
+  SELECT
+    ST_NumPatches(
+      CG_ExtrudeStraightSkeleton(
+        'POLYGON ((0 0, 5 0, 5 5, 4 5, 4 4, 0 4, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1))',
+        2.0
+      )
+    ) AS num_patches,
+    sfcgal_version_array
+  FROM version_info
+),
+
+building_and_roof AS (
+  SELECT
+    ST_NumPatches(
+      CG_ExtrudeStraightSkeleton(
+        'POLYGON ((0 0, 5 0, 5 5, 4 5, 4 4, 0 4, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1))',
+        3.0,
+        2.0
+      )
+    ) AS num_patches,
+    sfcgal_version_array
+  FROM version_info
+)
+
+SELECT 'Extrude roof',
+       -- The result depends on the SFCGAL version.
+       -- Since version 2.3, the result is simplified by merging
+       -- triangles before storing the result, resulting in fewer
+       -- patches.
+       (
+         (
+	   sfcgal_version_array < ARRAY[2,3,0]
+	   AND num_patches = 38
+	 )
+         OR (
+	   sfcgal_version_array >= ARRAY[2,3,0]
+	   AND num_patches = 11
+	 )
+       )
+FROM roof
+
+UNION ALL
+
+SELECT 'Extrude building and roof',
+       -- The result depends on the SFCGAL version.
+       -- Prior to version 2.2, it contained extra patches.
+       -- Since version 2.3, the result is simplified by merging
+       -- triangles before storing the result, resulting in fewer
+       -- patches.
+       (
+         (
+	   sfcgal_version_array < ARRAY[2,2,0]
+	   AND num_patches = 49
+	 )
+         OR (
+	   sfcgal_version_array >= ARRAY[2,2,0]
+	   AND sfcgal_version_array < ARRAY[2,3,0]
+	   AND num_patches = 39
+	 )
+         OR (
+	   sfcgal_version_array >= ARRAY[2,3,0]
+	   AND num_patches = 21
+	 )
+       )
+FROM building_and_roof;
 SELECT 'Empty building and roof', ST_AsText(CG_ExtrudeStraightSkeleton(ST_GeomFromText('POLYGON EMPTY',4326), 20.1, 20.1));
 SELECT 'Empty roof', ST_AsText(CG_ExtrudeStraightSkeleton(ST_GeomFromText('POLYGON EMPTY',4326), 20.1));
