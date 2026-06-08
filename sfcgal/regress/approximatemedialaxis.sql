38fbf0f4d0acfacb2f90c7d2fc6d324bfa9021f4
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2026-06-08 12:52:30 +0200

    test(sfcgal): add tests for CG_ApproximateMedialAxis projected overload

--- sfcgal/regress/approximatemedialaxis.sql
+++ sfcgal/regress/approximatemedialaxis.sql
@@ -1,2 +1,5 @@
 SELECT 'square', ST_AsEwkt(CG_ApproximateMedialAxis('SRID=3857;POLYGON((0 0,1 0,1 1,0 1,0 0))'));
-SELECT 'rect1', ST_AsEwkt(CG_ApproximateMedialAxis('SRID=4326;POLYGON((0 0,2 0,2 1,0 1,0 0))'));
+SELECT 'rect', ST_AsEwkt(CG_ApproximateMedialAxis('SRID=3857;POLYGON((0 0,2 0,2 1,0 1,0 0))'));
+-- projected=false must equal 1-arg overload
+SELECT 'square_proj_false', ST_AsEwkt(CG_ApproximateMedialAxis('SRID=3857;POLYGON((0 0,1 0,1 1,0 1,0 0))', false));
+SELECT 'rect_proj_false', ST_AsEwkt(CG_ApproximateMedialAxis('SRID=3857;POLYGON((0 0,2 0,2 1,0 1,0 0))', false));
