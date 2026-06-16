bda98a677d1ca2bf90d34816c0e39a0e6eeadc93
Darafei Praliaskouski <me@komzpa.net>
2026-06-16 23:10:59 +0400

    ci: harden utility scripts and doc version list
    (cherry picked from commit 5a974d15e1b2d1b85f6bcdd453e51fdfdf6db921)

--- ci/winnie/regress_postgis.sh
+++ ci/winnie/regress_postgis.sh
@@ -9,15 +9,12 @@ echo PATH AFTER: $PATH
 export PGWINVER=${PG_VER}edb
 export WORKSPACE=`pwd`
 
-export PGPATH=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}
-export PGPATHEDB=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}edb
-
 export POSTGIS_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}
 export POSTGIS_MICRO_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}.${POSTGIS_MICRO_VERSION}
 
 if [ -n "$SOURCE_FOLDER" ]; then
   export POSTGIS_SRC=${PROJECTS}/postgis/$SOURCE_FOLDER
-  cd $POSTGIS_SRC
+	cd $POSTGIS_SRC
 else
   if [[ "$POSTGIS_MICRO_VERSION"  == *SVN* || "$POSTGIS_MICRO_VERSION"  == *dev* ]] ; then
     export POSTGIS_SRC=${PROJECTS}/postgis/branches/${POSTGIS_VER}
@@ -42,10 +39,10 @@ echo PATH AFTER: $PATH
 
 echo WORKSPACE IS $WORKSPACE
 #mkdir ${PROJECTS}/postgis/tmp
-export PGIS_REG_TMPDIR=${PROJECTS}/postgis/tmp/${POSTGIS_MICRO_VER}_pg${PG_VER}_geos${GEOS_VER}_gdal${GDAL_VER}w${OS_BUILD}
-rm -rf ${PGIS_REG_TMPDIR}
-mkdir ${PGIS_REG_TMPDIR}
-export TMPDIR=${PGIS_REG_TMPDIR}
+#export PGIS_REG_TMPDIR=${PROJECTS}/postgis/tmp/${POSTGIS_MICRO_VER}_pg${PG_VER}_geos${GEOS_VER}_gdal${GDAL_VER}w${OS_BUILD}
+#rm -rf ${PGIS_REG_TMPDIR}
+#mkdir ${PGIS_REG_TMPDIR}
+#export TMPDIR=${PGIS_REG_TMPDIR}
 
 #rm -rf ${PGIS_REG_TMPDIR}
 #TMPDIR=${PROJECTS}/postgis/tmp/${POSTGIS_VER}_${PG_VER}_${GEOS_VERSION}_${PROJ_VER}
@@ -53,26 +50,31 @@ echo PORT IS $PGPORT
 echo PGIS_REG_TMPDIR IS $PGIS_REG_TMPDIR
 export XSLTPROCFLAGS=
 cd ${POSTGIS_SRC}
+
 if [ -e ./GNUMakefile ]; then
 	make distclean
 fi
 
 sh autogen.sh
 
+# excluding topology cause it's erroring out on some tests
+#EXTRA_CONFIGURE_ARGS="${EXTRA_CONFIGURE_ARGS} --without-topology"
+
 if [ -n "$PCRE_VER" ]; then
     export PATH="${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}/include:${PROJECTS}/pcre/rel-${PCRE_VER}w${OS_BUILD}${GCC_TYPE}/lib:${PATH}"
 fi
 
+if [ "${INCLUDE_MINOR_LIB:-0}" == "1" ]; then
+  EXTRA_CONFIGURE_ARGS="${EXTRA_CONFIGURE_ARGS} --with-library-minor-version"
+fi
 
+if [ "${REGRESS_WITHOUT_TOPOLOGY:-0}" == "1" ]; then
+   EXTRA_CONFIGURE_ARGS="${EXTRA_CONFIGURE_ARGS} --without-topology"
+fi
 
-if [ -n "$SFCGAL_VER" ]; then
-	##hard code versions of cgal etc. for now
-	#export CGAL_VER=4.11
-	BOOST_VER=1.73.0
-	#BOOST_VER_WU=1_49_0
-	export BOOST_VER_WU=1_73_0
-	export PATH="${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/CGAL/rel-sfcgal-${SFCGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin:${PROJECTS}/boost/rel-${BOOST_VER_WU}w${OS_BUILD}${GCC_TYPE}/lib:${PATH}"
-
+if [ "${REGRESS_WITHOUT_RASTER:-0}" == "1" ]; then
+   EXTRA_CONFIGURE_ARGS="${EXTRA_CONFIGURE_ARGS} --without-raster"
+fi
 
 #CPPFLAGS="-I${PGPATH}/include -I${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/include" \
 #CFLAGS="-Wall -fno-omit-frame-pointer"
@@ -80,7 +82,7 @@ if [ -n "$SFCGAL_VER" ]; then
 #LDFLAGS="-Wl,--enable-auto-import -L${PGPATH}/lib -L${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/lib" \
 
 CPPFLAGS="-I${PGPATH}/include -I${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/include" \
-LDFLAGS="-Wl,--enable-auto-import -L${PGPATH}/lib -L${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${PROJECTS}/zlib/rel-zlib-${ZLIB_VER}w${OS_BUILD}${GCC_TYPE}/lib" \
+LDFLAGS="-Wl,--enable-auto-import -L${PGPATH}/lib -L${LZ4_PATH}/lib -L${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${PROJECTS}/zlib/rel-zlib-${ZLIB_VER}w${OS_BUILD}${GCC_TYPE}/lib" \
 ./configure \
   --host=${MINGHOST} --with-xml2config=${PROJECTS}/libxml/rel-libxml2-${LIBXML_VER}w${OS_BUILD}${GCC_TYPE}/bin/xml2-config  \
   --with-pgconfig=${PGPATH}/bin/pg_config \
@@ -88,67 +90,117 @@ LDFLAGS="-Wl,--enable-auto-import -L${PGPATH}/lib -L${PROJECTS}/rel-libiconv-${I
   --with-libiconv=${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE} \
   --with-gui --with-gettext=no \
   --with-sfcgal=${PROJECTS}/CGAL/rel-sfcgal-${SFCGAL_VER}w${OS_BUILD}${GCC_TYPE}/bin/sfcgal-config \
-  --without-interrupt-tests \
-  --prefix=${PROJECTS}/postgis/liblwgeom-${POSTGIS_VER}w${OS_BUILD}${GCC_TYPE}
-  #exit
-else
-CPPFLAGS="-I${PGPATH}/include -I${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/include" \
-CFLAGS="-Wall -fno-omit-frame-pointer" \
-LDFLAGS="-L${PGPATH}/lib -L${PROJECTS}/gdal/rel-${GDAL_VER}w${OS_BUILD}${GCC_TYPE}/lib -L${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE}/lib" ./configure \
-  --host=${MINGHOST} --with-xml2config=${PROJECTS}/libxml/rel-libxml2-${LIBXML_VER}w${OS_BUILD}${GCC_TYPE}/bin/xml2-config \
-  --with-pgconfig=${PGPATH}/bin/pg_config \
-  --with-geosconfig=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE}/bin/geos-config \
-  --with-gui --with-gettext=no \
-  --with-libiconv=${PROJECTS}/rel-libiconv-${ICON_VER}w${OS_BUILD}${GCC_TYPE} \
-  --without-interrupt-tests \
-  --prefix=${PROJECTS}/postgis/liblwgeom-${POSTGIS_VER}w${OS_BUILD}${GCC_TYPE}
-fi;
+  --prefix=${PROJECTS}/postgis/liblwgeom-${POSTGIS_VER}w${OS_BUILD}${GCC_TYPE} \
+  ${EXTRA_CONFIGURE_ARGS}
 
 
-make clean
+#make distclean
 
 #patch liblwgeom generated make to get rid of dynamic linking
 #sed -i 's/LDFLAGS += -no-undefined//g' liblwgeom/Makefile
 
-make
+make -j 2
 make install
-make check RUNTESTFLAGS=-v
 
-if [ "$MAKE_EXTENSION" == "1" ]; then
+# don't run tests twice. Only run regular if extension test is not asked for
+if [ "${MAKE_EXTENSION:-0}" == "0" ]; then
+  make check RUNTESTFLAGS=-v
+fi
+
+
+if [ "${MAKE_EXTENSION:-0}" == "1" ]; then
  export PGUSER=postgres
  #need to copy install files to EDB install (since not done by make install
  cd ${POSTGIS_SRC}
  echo "Postgis src dir is ${POSTGIS_SRC}"
- strip postgis/postgis-*.dll
- strip raster/rt_pg/postgis_raster-*.dll
- cp topology/*.dll ${PGPATHEDB}/lib
+ #strip postgis/postgis-*.dll
+ #strip raster/rt_pg/postgis_raster-*.dll
+ #strip sfcgal/*.dll
+
+ if [ "${REGRESS_WITHOUT_TOPOLOGY:-0}" == "0" ]; then
+    cp -r topology/*.dll ${PGPATHEDB}/lib
+ fi
  cp postgis/postgis*.dll ${PGPATHEDB}/lib
  cp sfcgal/*.dll ${PGPATHEDB}/lib
- cp raster/rt_pg/postgis_raster-*.dll ${PGPATHEDB}/lib
- cp -r ${PGPATH}/share/extension/postgis*${POSTGIS_MICRO_VER}.sql ${PGPATHEDB}/share/extension
- cp -r ${PGPATH}/share/extension/postgis*${POSTGIS_MICRO_VER}next.sql ${PGPATHEDB}/share/extension
- cp -r ${PGPATH}/share/extension/address_standardizer*${POSTGIS_MICRO_VER}.sql ${PGPATHEDB}/share/extension
- cp -r extensions/*/*.control ${PGPATHEDB}/share/extension
- cp -r extensions/*/*.dll ${PGPATHEDB}/lib
 
- make check RUNTESTFLAGS="--extension -v"
+ if [ "${REGRESS_WITHOUT_RASTER:-0}" == "0" ]; then
+    cp raster/rt_pg/postgis_raster-*.dll ${PGPATHEDB}/lib
+ fi
 
- ##test address standardizer
+
+export POSTGIS_MINOR_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}
+export POSTGIS_MINOR_MAX_VER="ANY"
+export POSTGIS_MICRO_VER=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}.${POSTGIS_MICRO_VERSION}
+
+#export UPGRADE_VER_FILE="extensions/upgradeable_versions.mk"
+value=$(<extensions/upgradeable_versions.mk)
+export value=${value//\\/}
+value=${value//UPGRADEABLE_VERSIONS = /}
+#echo $value
+export UPGRADEABLE_VERSIONS=$value
+export WIN_RELEASED_VERSIONS="2.0.0 2.0.1 2.0.3 2.0.4 2.0.6 2.1.4 2.1.7 2.1.8 2.2.0 2.2.3 2.3.0 2.3.7 2.4.0 2.4.4"
+export extensions_to_install="postgis postgis_sfcgal postgis_tiger_geocoder address_standardizer"
+
+if [ "${REGRESS_WITHOUT_TOPOLOGY:-0}" == "0" ]; then
+  extensions_to_install="${extensions_to_install} postgis_topology"
+fi
+
+if [ "${REGRESS_WITHOUT_RASTER:-0}" == "0" ]; then
+  extensions_to_install="${extensions_to_install} postgis_raster"
+fi
+
+
+#echo "Versions are:  $UPGRADEABLE_VERSIONS"
+for EXTNAME in $extensions_to_install; do
+
+	cp extensions/$EXTNAME/sql/*  ${PGPATHEDB}/share/extension
+	cp extensions/$EXTNAME/sql/$EXTNAME--TEMPLATED--TO--ANY.sql  ${PGPATHEDB}/share/extension/$EXTNAME--$POSTGIS_MICRO_VER--${POSTGIS_MINOR_MAX_VER}.sql;
+
+	if test "$EXTNAME" = "address_standardizer"; then #repeat for address_standardizer_data_us
+		cp extensions/$EXTNAME/sql/${EXTNAME}_data_us--${POSTGIS_MICRO_VER}.sql  ${PGPATHEDB}/share/extension
+		cp extensions//$EXTNAME/sql/${EXTNAME}_data_us--ANY--${POSTGIS_MICRO_VER}.sql  ${PGPATHEDB}/share/extension
+		cp extensions/$EXTNAME/sql/${EXTNAME}--TEMPLATED--TO--ANY.sql ${PGPATHEDB}/share/extension/${EXTNAME}_data_us--$POSTGIS_MICRO_VER--ANY.sql;
+	fi
+
+	for OLD_VERSION in $UPGRADEABLE_VERSIONS; do \
+		if [ "$OLD_VERSION" > "2.5" ] || [ "$OLD_VERSION" in $WIN_RELEASED_VERSIONS ]; then
+			cp extensions/$EXTNAME/sql/$EXTNAME--TEMPLATED--TO--ANY.sql  ${PGPATHEDB}/share/extension/$EXTNAME--$OLD_VERSION--${POSTGIS_MINOR_MAX_VER}.sql; \
+			if test "$EXTNAME" = "address_standardizer"; then
+				cp extensions/$EXTNAME/sql/$EXTNAME--TEMPLATED--TO--ANY.sql  ${PGPATHEDB}/share/extension/${EXTNAME}_data_us--$OLD_VERSION--${POSTGIS_MINOR_MAX_VER}.sql; \
+			fi
+		fi
+	done
+done
+ #cp -r ${PGPATH}/share/extension/postgis*${POSTGIS_MICRO_VER}.sql ${PGPATHEDB}/share/extension
+ #cp -r ${PGPATH}/share/extension/postgis*${POSTGIS_MICRO_VER}next.sql ${PGPATHEDB}/share/extension
+ #cp -r ${PGPATH}/share/extension/address_standardizer*${POSTGIS_MICRO_VER}.sql ${PGPATHEDB}/share/extension
+cp -r extensions/*/*.control ${PGPATHEDB}/share/extension
+cp -r extensions/*/*.dll ${PGPATHEDB}/lib
+
+make check RUNTESTFLAGS="--extension -v"
+
+if [ "${UPGRADE_TEST:-0}" == "1" ]; then
+  export CURRENTVERSION=${POSTGIS_MAJOR_VERSION}.${POSTGIS_MINOR_VERSION}.${POSTGIS_MICRO_VERSION}
+  RUNTESTFLAGS='--extension' ${POSTGIS_SRC}/utils/check_all_upgrades.sh -s "${CURRENTVERSION}" --skip "unpackaged"
+fi
+
+ #test address standardizer
  cd ${POSTGIS_SRC}
  cd extensions/address_standardizer
  make installcheck
 
- ##test tiger geocoder
- cd ${POSTGIS_SRC}
- cd extensions/postgis_tiger_geocoder
- make installcheck
- if [ "$?" != "0" ]; then
-  exit $?
- fi
+#test tiger geocoder
+#  cd ${POSTGIS_SRC}
+#  cd extensions/postgis_tiger_geocoder
+#  make installcheck
+#  if [ "$?" != "0" ]; then
+#   exit $?
+#  fi
+#end extension
 fi
 
-if [ "$DUMP_RESTORE" = "1" ]; then
- echo "Dum restore test"
+if [ "${DUMP_RESTORE:-0}" == "1" ]; then
+ echo "Dump restore test"
  make install
  make check RUNTESTFLAGS="-v --dumprestore"
  if [ "$?" != "0" ]; then
@@ -156,7 +208,7 @@ if [ "$DUMP_RESTORE" = "1" ]; then
  fi
 fi
 
-if [ "$MAKE_GARDEN" == "1" ]; then
+if [ "${MAKE_GARDEN:-0}" == "1" ]; then
  export PGUSER=postgres
  make garden
 fi
