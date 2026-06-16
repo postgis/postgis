9773ea397f349f6fa77ae4c8f80872307620abb5
Darafei Praliaskouski <me@komzpa.net>
2026-06-16 23:34:07 +0400

    geometry: fix planner and precision regressions
    (cherry picked from commit cfc1632eb51c25e464580f1ecee8181a71e7bc80)

--- regress/core/tickets.sql
+++ regress/core/tickets.sql
@@ -1558,6 +1558,37 @@ SELECT '#5686', ST_NumInteriorRings('TRIANGLE (( -71.0821 42.3036, -71.0821 42.3
 
 SELECT '#5747', ST_Length('MULTISURFACE (((0 0, 1 0, 1 1, 0 1, 0 0)), CURVEPOLYGON (CIRCULARSTRING (10 10, 11 11, 12 10, 11 9, 10 10)))'::geometry);
 
+-- #5855
+CREATE TEMP TABLE TEST (street text, extent geometry(Polygon,32633));
+INSERT INTO test VALUES ('Knosesmauet','0103000020797F0000010000000500000010B2468761BDDFC06390523AA1B0594110B2468761BDDFC030554D9BC3B0594107992AF50799DFC030554D9BC3B0594107992AF50799DFC06390523AA1B0594110B2468761BDDFC06390523AA1B05941');
+CREATE INDEX test_idx ON test USING GIST ( extent );
+
+-- These checks protect index-support rewrites, so keep sequential scans
+-- disabled only around the #5855 queries that must exercise GiST paths.
+SET enable_seqscan = off;
+SELECT '#5855', street
+FROM test
+WHERE ST_DFullyWithin(
+    ST_SetSRID(ST_GeomFromText('POINT(-32356 6734606)'), 32633),
+    extent,
+    1700
+);
+
+CREATE TEMP TABLE dfullywithin_indexed_first_arg (id integer, geom geometry);
+INSERT INTO dfullywithin_indexed_first_arg
+VALUES (1, 'POLYGON((-10 -10,-10 10,10 10,10 -10,-10 -10))');
+CREATE INDEX dfullywithin_indexed_first_arg_gist
+ON dfullywithin_indexed_first_arg USING GIST (geom);
+
+SELECT '#5855.1', id
+FROM dfullywithin_indexed_first_arg
+WHERE ST_DFullyWithin(
+    geom,
+    ST_GeomFromText('POINT(0 0)'),
+    0
+);
+RESET enable_seqscan;
+
 SELECT '#5876', ST_AsText(ST_AddPoint(
 		'LINESTRING (1 1, 2 2)'::geometry,
 		'POINT EMPTY'::geometry), 2);
