47c0ab6dc4153351d0cae8b5d6d889b2e80b471f
Darafei Praliaskouski <me@komzpa.net>
2026-06-16 23:34:07 +0400

    geometry: fix planner and precision regressions
    (cherry picked from commit 563ffa9d186eaef174fab3539e610777f35a0d47)

--- liblwgeom/ptarray.c
+++ liblwgeom/ptarray.c
@@ -1154,8 +1154,9 @@ ptarray_signed_area(const POINTARRAY *pa)
 	const POINT2D *P1;
 	const POINT2D *P2;
 	const POINT2D *P3;
-	double sum = 0.0;
-	double x0, x, y1, y2;
+	long double sum = 0.0;
+	double x0;
+	long double x, y1, y2;
 	uint32_t i;
 
 	if (! pa || pa->npoints < 3 )
@@ -1167,7 +1168,7 @@ ptarray_signed_area(const POINTARRAY *pa)
 	for ( i = 2; i < pa->npoints; i++ )
 	{
 		P3 = getPoint2d_cp(pa, i);
-		x = P2->x - x0;
+		x = (long double)P2->x - x0;
 		y1 = P3->y;
 		y2 = P1->y;
 		sum += x * (y2-y1);
@@ -1176,7 +1177,7 @@ ptarray_signed_area(const POINTARRAY *pa)
 		P1 = P2;
 		P2 = P3;
 	}
-	return sum / 2.0;
+	return (double)(sum / 2.0);
 }
 
 int
