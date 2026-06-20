a78318e451ae606cd121befce47e957f8903ee33
Darafei Praliaskouski <me@komzpa.net>
2026-06-20 15:26:01 +0400

    Add raster ST_Blur
    Closes #2598

--- raster/test/regress/rt_mapalgebra_mask.sql
+++ raster/test/regress/rt_mapalgebra_mask.sql
@@ -120,5 +120,50 @@ FROM (SELECT rid, st_dumpvalues(st_mapalgebra(rast,1,'raster_nmapalgebra_test(do
     from raster_nmapalgebra_mask_in) AS f
 ORDER BY rid, (dv).nband;
 
+SELECT 'st_blur_empty_args';
+SELECT _ST_Blur4ma(
+	ARRAY[[[1]]]::double precision[][][],
+	ARRAY[[1, 1], [1, 1]]::integer[][],
+	VARIADIC ARRAY[]::text[]
+);
+
+SELECT 'st_blur_null_sigma';
+SELECT _ST_Blur4ma(
+	ARRAY[[[1]]]::double precision[][][],
+	ARRAY[[1, 1], [1, 1]]::integer[][],
+	VARIADIC ARRAY[NULL]::text[]
+);
+
+WITH r AS (
+	SELECT ST_SetValues(
+		ST_AddBand(ST_MakeEmptyRaster(3, 3, 0, 0, 1, -1, 0, 0, 0), 1, '8BUI', 0, NULL),
+		1, 1, 1, ARRAY[
+			[0, 0, 0],
+			[0, 100, 0],
+			[0, 0, 0]
+		]::double precision[][]
+	) AS rast
+), b AS (
+	SELECT ST_Blur(rast, 1, 1.0) AS rast
+	FROM r
+)
+SELECT
+	'st_blur_impulse',
+	round(ST_Value(rast, 2, 2)::numeric, 6),
+	round(ST_Value(rast, 1, 1)::numeric, 6)
+FROM b;
+
+WITH r AS (
+	SELECT ST_SetValues(
+		ST_AddBand(ST_MakeEmptyRaster(3, 3, 0, 0, 1, -1, 0, 0, 0), 1, '16BUI', 10, 10),
+		1, 2, 2, ARRAY[[100]]::double precision[][]
+	) AS rast
+), b AS (
+	SELECT ST_Blur(rast, 1, 1.0) AS rast
+	FROM r
+)
+SELECT 'st_blur_nodata', round(ST_Value(rast, 2, 2)::numeric, 6)
+FROM b;
+
 DROP FUNCTION IF EXISTS raster_nmapalgebra_test(double precision[], int[], text[]);
 DROP TABLE IF EXISTS raster_nmapalgebra_mask_in;
