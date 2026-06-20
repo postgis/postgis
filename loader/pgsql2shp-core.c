e231b058f6382b851216b687ccd21c61ac81fed9
Darafei Praliaskouski <me@komzpa.net>
2026-06-20 11:14:09 +0400

    pgsql2shp: prompt for required passwords
    Let libpq attempt the initial connection without a supplied password, so .pgpass and other libpq credential sources still work first. When the server reports that a password is required and stdin is a terminal, prompt once without echo and retry the connection.
    
    Also escape password values before embedding them in the generated conninfo string.
    
    Closes #398

--- loader/pgsql2shp-core.c
+++ loader/pgsql2shp-core.c
@@ -34,6 +34,10 @@
 #include <unistd.h>
 #endif
 
+#if HAVE_TERMIOS_H
+#include <termios.h>
+#endif
+
 #ifdef __CYGWIN__
 #include <sys/param.h>
 #endif
@@ -1202,7 +1206,12 @@ ShpDumperGetConnectionStringFromConn(SHPCONNECTIONCONFIG *conn)
 		stringbuffer_aprintf(&cs, " user=%s", conn->username);
 
 	if (conn->password)
-		stringbuffer_aprintf(&cs, " password='%s'", conn->password);
+	{
+		char *password = escape_connection_string(conn->password);
+		stringbuffer_aprintf(&cs, " password='%s'", password);
+		if (password != conn->password)
+			free(password);
+	}
 
 	if (conn->database)
 		stringbuffer_aprintf(&cs, " dbname=%s", conn->database);
@@ -1213,6 +1222,71 @@ ShpDumperGetConnectionStringFromConn(SHPCONNECTIONCONFIG *conn)
 	return cs.str_start;
 }
 
+static PGconn *
+ShpDumperConnectWithConn(SHPCONNECTIONCONFIG *conn)
+{
+	PGconn *pgconn;
+	char *connstring = ShpDumperGetConnectionStringFromConn(conn);
+	pgconn = PQconnectdb(connstring);
+	free(connstring);
+	return pgconn;
+}
+
+static char *
+ShpDumperPromptPassword(void)
+{
+	char password[1024];
+	size_t length;
+
+#if HAVE_TERMIOS_H
+	struct termios old_termios;
+	struct termios new_termios;
+	int has_termios = (tcgetattr(fileno(stdin), &old_termios) == 0);
+#endif
+
+	if (!isatty(fileno(stdin)))
+		return NULL;
+
+#if HAVE_TERMIOS_H
+	if (has_termios)
+	{
+		new_termios = old_termios;
+		new_termios.c_lflag &= ~ECHO;
+		if (tcsetattr(fileno(stdin), TCSAFLUSH, &new_termios) != 0)
+			has_termios = 0;
+	}
+#endif
+
+	fprintf(stderr, "Password: ");
+	fflush(stderr);
+
+	if (!fgets(password, sizeof(password), stdin))
+	{
+#if HAVE_TERMIOS_H
+		if (has_termios)
+		{
+			(void)tcsetattr(fileno(stdin), TCSAFLUSH, &old_termios);
+			fprintf(stderr, "\n");
+		}
+#endif
+		return NULL;
+	}
+
+#if HAVE_TERMIOS_H
+	if (has_termios)
+	{
+		(void)tcsetattr(fileno(stdin), TCSAFLUSH, &old_termios);
+		fprintf(stderr, "\n");
+	}
+#endif
+
+	length = strlen(password);
+	while (length > 0 && (password[length - 1] == '\n' || password[length - 1] == '\r'))
+		password[--length] = '\0';
+
+	return strdup(password);
+}
+
 /* Connect to the database and identify the version of PostGIS (and any other
 capabilities required) */
 int
@@ -1221,15 +1295,27 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	PGresult *res;
 	char *tmpvalue;
 
-	/* Generate the PostgreSQL connection string */
-	char *connstring = ShpDumperGetConnectionStringFromConn(state->config->conn);
-
 	/* Connect to the database */
-	state->conn = PQconnectdb(connstring);
+	state->conn = ShpDumperConnectWithConn(state->config->conn);
+	if (PQstatus(state->conn) == CONNECTION_BAD)
+	{
+		if (!state->config->conn->password && PQconnectionNeedsPassword(state->conn))
+		{
+			char *password = ShpDumperPromptPassword();
+			if (password)
+			{
+				SHPCONNECTIONCONFIG retry_conn = *state->config->conn;
+				retry_conn.password = password;
+				PQfinish(state->conn);
+				state->conn = ShpDumperConnectWithConn(&retry_conn);
+				free(password);
+			}
+		}
+	}
+
 	if (PQstatus(state->conn) == CONNECTION_BAD)
 	{
 		snprintf(state->message, SHPDUMPERMSGLEN, "%s", PQerrorMessage(state->conn));
-		free(connstring);
 		return SHPDUMPERERR;
 	}
 
@@ -1239,7 +1325,6 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	{
 		snprintf(state->message, SHPDUMPERMSGLEN, "%s", PQresultErrorMessage(res));
 		PQclear(res);
-		free(connstring);
 		return SHPDUMPERERR;
 	}
 
@@ -1251,7 +1336,6 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	{
 		snprintf(state->message, SHPDUMPERMSGLEN, "%s", PQresultErrorMessage(res));
 		PQclear(res);
-		free(connstring);
 		return SHPDUMPERERR;
 	}
 
@@ -1266,7 +1350,6 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	{
 		snprintf(state->message, SHPDUMPERMSGLEN, _("Error looking up geometry oid: %s"), PQresultErrorMessage(res));
 		PQclear(res);
-		free(connstring);
 		return SHPDUMPERERR;
 	}
 
@@ -1279,7 +1362,6 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	{
 		snprintf(state->message, SHPDUMPERMSGLEN, _("Geometry type unknown (have you enabled postgis?)"));
 		PQclear(res);
-		free(connstring);
 		return SHPDUMPERERR;
 	}
 
@@ -1291,7 +1373,6 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	{
 		snprintf(state->message, SHPDUMPERMSGLEN, _("Error looking up geography oid: %s"), PQresultErrorMessage(res));
 		PQclear(res);
-		free(connstring);
 		return SHPDUMPERERR;
 	}
 
@@ -1303,8 +1384,6 @@ ShpDumperConnectDatabase(SHPDUMPERSTATE *state)
 	}
 
 	PQclear(res);
-
-	free(connstring);
 
 	return SHPDUMPEROK;
 }
