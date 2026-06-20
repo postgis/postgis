4b4d02f0cb5b248897d7d435c25943881bd83d88
Darafei Praliaskouski <me@komzpa.net>
2026-06-20 08:00:42 +0400

    topology: add pairwise ST_Union for TopoGeometry
    Add a topology.ST_Union overload for two TopoGeometry inputs by copying the first input and folding in the second with TopoGeom_addTopoGeom, so neither input is changed.
    
    Document the new function and cover the non-mutating result in the existing TopoGeom_addTopoGeom regression.
    
    Closes #4847

--- topology/test/regress/topogeom_addtopogeom.sql
+++ topology/test/regress/topogeom_addtopogeom.sql
@@ -28,6 +28,15 @@ SELECT 'simple_collection_layer', AddTopoGeometryColumn('tt', 'tt', 'f_coll','g'
 CREATE TABLE tt.f_hier_puntal(id serial);
 SELECT 'hierarchical_puntual_layer', AddTopoGeometryColumn('tt', 'tt', 'f_hier_puntal','g','POINT', 1);
 
+INSERT INTO tt.f_puntal(g) VALUES
+  (toTopoGeom('POINT(1 1)'::geometry, 'tt', 1)),
+  (toTopoGeom('POINT(0 0)'::geometry, 'tt', 1)),
+  (topology.CreateTopoGeom('tt', 1, 1));
+
+INSERT INTO tt.f_hier_puntal(g)
+SELECT topology.CreateTopoGeom('tt', 1, 5, ARRAY[ARRAY[id(g), layer_id(g)]]::topology.TopoElementArray)
+FROM tt.f_puntal
+WHERE id IN (1, 2);
 
 -- point to point
 SELECT 'tg2tg.poi2poi', ST_AsText(TopoGeom_addTopoGeom(
@@ -65,6 +74,73 @@ SELECT 'tg2tg.pol2pol', ST_AsText(ST_Simplify(ST_Normalize(TopoGeom_addTopoGeom(
     toTopoGeom(ST_MakeEnvelope(0,0,10,10), 'tt', 4)
 )::geometry), 0));
 
+WITH unioned AS (
+  SELECT topology.ST_Union(a.g, b.g) AS g, a.g AS a, b.g AS b
+  FROM tt.f_puntal a, tt.f_puntal b
+  WHERE a.id = 1 AND b.id = 2
+)
+SELECT 'st_union.poi2poi',
+  ST_AsText(g),
+  id(g) NOT IN (id(a), id(b)),
+  GetTopoGeomElementArray(a),
+  GetTopoGeomElementArray(b)
+FROM unioned;
+
+WITH unioned AS (
+  SELECT topology.ST_Union(a.g, b.g) AS g, a.g AS a, b.g AS b
+  FROM tt.f_puntal a, tt.f_puntal b
+  WHERE a.id = 3 AND b.id = 1
+)
+SELECT 'st_union.empty_poi',
+  ST_AsText(g),
+  id(g) NOT IN (id(a), id(b)),
+  GetTopoGeomElementArray(a),
+  GetTopoGeomElementArray(b)
+FROM unioned;
+
+INSERT INTO tt.f_hier_puntal(g)
+SELECT topology.ST_Union(a.g, b.g)
+FROM tt.f_hier_puntal a, tt.f_hier_puntal b
+WHERE a.id = 1 AND b.id = 2;
+
+SELECT 'st_union.hier_poi2poi',
+  ST_AsText(u.g),
+  id(u.g) NOT IN (id(a.g), id(b.g)),
+  (SELECT topology.TopoElementArray_agg(ARRAY[element_id, element_type]
+    ORDER BY element_id, element_type)
+   FROM tt.relation
+   WHERE topogeo_id = id(u.g)
+     AND layer_id = layer_id(u.g)),
+  (SELECT topology.TopoElementArray_agg(ARRAY[element_id, element_type])
+   FROM tt.relation
+   WHERE topogeo_id = id(a.g)
+     AND layer_id = layer_id(a.g)),
+  (SELECT topology.TopoElementArray_agg(ARRAY[element_id, element_type])
+   FROM tt.relation
+   WHERE topogeo_id = id(b.g)
+     AND layer_id = layer_id(b.g))
+FROM tt.f_hier_puntal u, tt.f_hier_puntal a, tt.f_hier_puntal b
+WHERE u.id = 3 AND a.id = 1 AND b.id = 2;
+
+CREATE FUNCTION tt.try_st_union(a topology.TopoGeometry, b topology.TopoGeometry)
+RETURNS text
+AS $$
+BEGIN
+  PERFORM topology.ST_Union(a, b);
+  RETURN 'unexpected success';
+EXCEPTION WHEN OTHERS THEN
+  RETURN SQLERRM;
+END
+$$ LANGUAGE 'plpgsql' VOLATILE;
+
+SELECT 'st_union.hier_simple', tt.try_st_union(h.g, s.g)
+FROM tt.f_hier_puntal h, tt.f_puntal s
+WHERE h.id = 1 AND s.id = 1;
+
+SELECT 'st_union.simple_hier', tt.try_st_union(s.g, h.g)
+FROM tt.f_puntal s, tt.f_hier_puntal h
+WHERE s.id = 1 AND h.id = 1;
+
 -- TODO: point to point (hierarchical)
 -- TODO: point to mixed (hierarchical)
 -- TODO: line to line (hierarchical)
@@ -78,4 +154,5 @@ DROP TABLE tt.f_areal;
 DROP TABLE tt.f_lineal;
 DROP TABLE tt.f_hier_puntal;
 DROP TABLE tt.f_puntal;
+DROP FUNCTION tt.try_st_union(topology.TopoGeometry, topology.TopoGeometry);
 select NULL from droptopology('tt');
