bd68d8d93ed995255b42e82cf2a339d483be82c8
Darafei Praliaskouski <me@komzpa.net>
2026-06-30 23:05:14 +0400

    ci, tiger_geocoder: fix stable-3.4 upgrade helpers

--- extras/tiger_geocoder/tiger_loader_2014.sql
+++ extras/tiger_geocoder/tiger_loader_2014.sql
@@ -562,7 +562,7 @@ RETURNS integer AS
 $$
 -- exclude this set list of columns if no exclusion list is specified
 
-   SELECT  loader_load_staged_data($1, $2,(SELECT COALESCE(columns_exclude,ARRAY['gid', 'geoid','cpi','suffix1ce', 'statefp00', 'statefp10', 'countyfp00','countyfp10'
+   SELECT  tiger.loader_load_staged_data($1, $2,(SELECT COALESCE(columns_exclude,ARRAY['gid', 'geoid','cpi','suffix1ce', 'statefp00', 'statefp10', 'countyfp00','countyfp10'
    ,'tractce00','tractce10', 'blkgrpce00', 'blkgrpce10', 'blockce00', 'blockce10'
       , 'cousubfp00', 'submcdfp00', 'conctyfp00', 'placefp00', 'aiannhfp00', 'aiannhce00',
        'comptyp00', 'trsubfp00', 'trsubce00', 'anrcfp00', 'elsdlea00', 'scsdlea00',
