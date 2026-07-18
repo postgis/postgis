f2bf57388f2fcbb7bdb7f69344e535b9e98c6db6
Darafei Praliaskouski <me@komzpa.net>
2026-07-19 00:36:58 +0400

    ci: isolate the Bessie PostgreSQL cluster
    (cherry picked from commit 23a1f5deddb5947c830ec6cc54d4e44dc19ebbdc)

--- ci/bessie/postgis_regress.sh
+++ ci/bessie/postgis_regress.sh
@@ -1,5 +1,30 @@
 #BRANCH is passed in via jenkins which is set via svn hook
 export PATH=${PATH}:/usr/local:/usr/local/lib:/usr/local/bin
+
+PGIS_TMP_ROOT=$(mktemp -d "${TMPDIR:-/tmp}/postgis-bessie.XXXXXX")
+export PGDATA="${PGIS_TMP_ROOT}/data"
+export PGHOST="${PGIS_TMP_ROOT}"
+export PGUSER=postgres
+export PGIS_REG_TMPDIR="${PGIS_TMP_ROOT}/regress"
+PGLOG="${PGIS_TMP_ROOT}/postgresql.log"
+
+cleanup() {
+  status=$?
+  trap - EXIT
+  if test -s "${PGDATA}/postmaster.pid"; then
+    pg_ctl -D "${PGDATA}" -m immediate stop >/dev/null 2>&1 || true
+  fi
+  if test "${status}" -ne 0 && test -s "${PGLOG}"; then
+    echo "PostgreSQL log follows:" >&2
+    tail -n 100 "${PGLOG}" >&2
+  fi
+  rm -rf -- "${PGIS_TMP_ROOT}"
+  exit "${status}"
+}
+
+trap cleanup EXIT
+trap 'exit 1' HUP INT TERM
+
 sh autogen.sh
 ./configure \
   --with-projdir=/usr/local \
@@ -8,9 +33,17 @@ sh autogen.sh
   --with-library-minor-version
 make clean
 make
-export PGUSER=postgres
-export PGIS_REG_TMPDIR=~/tmp/pgis_reg_${BRANCH}
-psql -c "DROP DATABASE IF EXISTS postgis_reg;"
+
+initdb -N --no-data-checksums -U "${PGUSER}" -D "${PGDATA}"
+printf '%s\n' \
+  'fsync = off' \
+  'full_page_writes = off' \
+  'synchronous_commit = off' \
+  >> "${PGDATA}/postgresql.conf"
+pg_ctl -D "${PGDATA}" -l "${PGLOG}" \
+  -o "-F -c listen_addresses='' -c unix_socket_directories='${PGHOST}'" start
+mkdir -p "${PGIS_REG_TMPDIR}"
+
 make check RUNTESTFLAGS="-v"
 sudo make install
 make check RUNTESTFLAGS="-v --extension"
