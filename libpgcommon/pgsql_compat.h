e1ae2c0ef88a0d63f5845cf816d68c8b17cfd816
Darafei Praliaskouski <me@komzpa.net>
2026-07-20 15:51:55 +0400

    Fix TupleDescAttr compatibility on stable 3.3

--- libpgcommon/pgsql_compat.h
+++ libpgcommon/pgsql_compat.h
@@ -3,9 +3,4 @@
 
 #include "access/tupdesc.h"
 
-/* TupleDescAttr was backported into 9.6.5 but we support any 9.6.X */
-#ifndef TupleDescAttr
-#define TupleDescAttr(tupdesc, i) ((tupdesc)->attrs[(i)])
-#endif
-
 #endif /* _PGSQL_COMPAT_H */
