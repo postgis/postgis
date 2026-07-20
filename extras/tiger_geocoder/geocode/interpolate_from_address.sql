96015253cc36874f9bcc1fc598a9f61669befeb7
Regina Obe <lr@pcorp.us>
2026-07-20 12:58:46 -0400

    tiger_geocoder: qualify PostGIS schema references
    Ported-from: https://gitea.osgeo.org/postgis/tiger-geocoder/commit/904b8935ca585cb37336931606ef89b8cb22f042
    
    Ported-from: https://gitea.osgeo.org/postgis/tiger-geocoder/commit/a819b8303b0f7409e6ff5ef3712790e00bf965d4
    
    Ported-from: https://gitea.osgeo.org/postgis/tiger-geocoder/commit/1b3373b9eb3dbd17835498c55f6573adf673da7b
    
    Ported-from: https://gitea.osgeo.org/postgis/tiger-geocoder/commit/b50024a9d3d86a0ccb634ceb24d8babcc56279dc

--- extras/tiger_geocoder/geocode/interpolate_from_address.sql
+++ extras/tiger_geocoder/geocode/interpolate_from_address.sql
@@ -7,16 +7,16 @@
 -- fifth are from the other.
 -- in_side Side of street -- either 'L', 'R' or if blank ignores side of road
 -- in_offset_m -- number of meters offset to the side
-CREATE OR REPLACE FUNCTION interpolate_from_address(given_address INTEGER, in_addr1 VARCHAR, in_addr2 VARCHAR, in_road GEOMETRY,
-	in_side VARCHAR DEFAULT '',in_offset_m float DEFAULT 10) RETURNS GEOMETRY
+CREATE OR REPLACE FUNCTION interpolate_from_address(given_address INTEGER, in_addr1 VARCHAR, in_addr2 VARCHAR, in_road @extschema:postgis@.GEOMETRY,
+	in_side VARCHAR DEFAULT '',in_offset_m float DEFAULT 10) RETURNS @extschema:postgis@.GEOMETRY
 AS $_$
 DECLARE
   addrwidth INTEGER;
   part DOUBLE PRECISION;
-  road GEOMETRY;
-  result GEOMETRY;
+  road @extschema:postgis@.GEOMETRY;
+  result @extschema:postgis@.GEOMETRY;
   var_addr1 INTEGER; var_addr2 INTEGER;
-  center_pt GEOMETRY; cl_pt GEOMETRY;
+  center_pt @extschema:postgis@.GEOMETRY; cl_pt @extschema:postgis@.GEOMETRY;
   npos integer;
   delx float; dely float;  x0 float; y0 float; x1 float; y1 float; az float;
   var_dist float; dir integer;
@@ -28,11 +28,11 @@ BEGIN
 	var_addr1 := to_number( CASE WHEN in_addr1 ~ '^[0-9]+$' THEN in_addr1 ELSE '0' END, '999999');
 	var_addr2 := to_number( CASE WHEN in_addr2 ~ '^[0-9]+$' THEN in_addr2 ELSE '0' END, '999999');
 
-    IF geometrytype(in_road) = 'LINESTRING' THEN
-      road := ST_Transform(in_road, utmzone(ST_StartPoint(in_road)) );
-    ELSIF geometrytype(in_road) = 'MULTILINESTRING' THEN
-    	road := ST_GeometryN(in_road,1);
-    	road := ST_Transform(road, utmzone(ST_StartPoint(road)) );
+    IF @extschema:postgis@.GeometryType(in_road) = 'LINESTRING' THEN
+      road := @extschema:postgis@.ST_Transform(in_road, tiger.utmzone(@extschema:postgis@.ST_StartPoint(in_road)) );
+    ELSIF @extschema:postgis@.GeometryType(in_road) = 'MULTILINESTRING' THEN
+	road := @extschema:postgis@.ST_GeometryN(in_road,1);
+	road := @extschema:postgis@.ST_Transform(road, tiger.utmzone(@extschema:postgis@.ST_StartPoint(road)) );
     ELSE
       RETURN NULL;
     END IF;
@@ -51,28 +51,28 @@ BEGIN
         part := 0.5;
     END IF;
 
-    center_pt = ST_LineInterpolatePoint(road, part);
+    center_pt = @extschema:postgis@.ST_LineInterpolatePoint(road, part);
     IF in_side > '' AND in_offset_m > 0 THEN
     /** Compute point the point to the in_side of the geometry **/
     /**Take into consideration non-straight so we consider azimuth
-    	of the 2 points that straddle the center location**/
-    	IF part = 0 THEN
-    		az := ST_Azimuth (ST_StartPoint(road), ST_PointN(road,2));
-    	ELSIF part = 1 THEN
-    		az := ST_Azimuth (ST_PointN(road,ST_NPoints(road) - 1), ST_EndPoint(road));
-    	ELSE
-    		/** Find the largest nth point position that is before the center point
-    			This will be the start of our azimuth calc **/
-    		SELECT i INTO npos
-    			FROM generate_series(1,ST_NPoints(road)) As i
-    					WHERE part > ST_LineLocatePoint(road,ST_PointN(road,i))
-    					ORDER BY i DESC;
-    		IF npos < ST_NPoints(road) THEN
-    			az := ST_Azimuth (ST_PointN(road,npos), ST_PointN(road, npos + 1));
-    		ELSE
-    			az := ST_Azimuth (center_pt, ST_PointN(road, npos));
-    		END IF;
-    	END IF;
+	of the 2 points that straddle the center location**/
+	IF part = 0 THEN
+		az := @extschema:postgis@.ST_Azimuth (@extschema:postgis@.ST_StartPoint(road), @extschema:postgis@.ST_PointN(road,2));
+	ELSIF part = 1 THEN
+		az := @extschema:postgis@.ST_Azimuth (@extschema:postgis@.ST_PointN(road,@extschema:postgis@.ST_NPoints(road) - 1), @extschema:postgis@.ST_EndPoint(road));
+	ELSE
+		/** Find the largest nth point position that is before the center point
+			This will be the start of our azimuth calc **/
+		SELECT i INTO npos
+			FROM generate_series(1,@extschema:postgis@.ST_NPoints(road)) As i
+					WHERE part > @extschema:postgis@.ST_LineLocatePoint(road,@extschema:postgis@.ST_PointN(road,i))
+					ORDER BY i DESC;
+		IF npos < @extschema:postgis@.ST_NPoints(road) THEN
+			az := @extschema:postgis@.ST_Azimuth (@extschema:postgis@.ST_PointN(road,npos), @extschema:postgis@.ST_PointN(road, npos + 1));
+		ELSE
+			az := @extschema:postgis@.ST_Azimuth (center_pt, @extschema:postgis@.ST_PointN(road, npos));
+		END IF;
+	END IF;
 
         dir := CASE WHEN az < pi() THEN -1 ELSE 1 END;
         --dir := 1;
@@ -80,19 +80,19 @@ BEGIN
         delx := ABS(COS(az)) * var_dist * dir;
         dely := ABS(SIN(az)) * var_dist * dir;
         IF az > pi()/2 AND az < pi() OR az > 3 * pi()/2 THEN
-			result := ST_Translate(center_pt, delx, dely) ;
+			result := @extschema:postgis@.ST_Translate(center_pt, delx, dely) ;
 		ELSE
-			result := ST_Translate(center_pt, -delx, dely);
+			result := @extschema:postgis@.ST_Translate(center_pt, -delx, dely);
 		END IF;
     ELSE
-    	result := center_pt;
+	result := center_pt;
     END IF;
-    result :=  ST_Transform(result, ST_SRID(in_road));
-    --RAISE NOTICE 'start: %, center: %, new: %, side: %, offset: %, az: %', ST_AsText(ST_Transform(ST_StartPoint(road),ST_SRID(in_road))), ST_AsText(ST_Transform(center_pt,ST_SRID(in_road))),ST_AsText(result), in_side, in_offset_m, az;
+    result :=  @extschema:postgis@.ST_Transform(result, @extschema:postgis@.ST_SRID(in_road));
+    --RAISE NOTICE 'start: %, center: %, new: %, side: %, offset: %, az: %', @extschema:postgis@.ST_AsText(@extschema:postgis@.ST_Transform(@extschema:postgis@.ST_StartPoint(road),@extschema:postgis@.ST_SRID(in_road))), @extschema:postgis@.ST_AsText(@extschema:postgis@.ST_Transform(center_pt,@extschema:postgis@.ST_SRID(in_road))),@extschema:postgis@.ST_AsText(result), in_side, in_offset_m, az;
     RETURN result;
 END;
 $_$ LANGUAGE plpgsql IMMUTABLE COST 10 PARALLEL SAFE;
 -- needed to ban stupid warning about how we are using deprecated functions
 -- yada yada yada need this to work in 2.0 too bah
-ALTER FUNCTION interpolate_from_address(integer, character varying, character varying, geometry, character varying, double precision)
+ALTER FUNCTION tiger.interpolate_from_address(integer, character varying, character varying, @extschema:postgis@.geometry, character varying, double precision)
   SET client_min_messages='ERROR';
