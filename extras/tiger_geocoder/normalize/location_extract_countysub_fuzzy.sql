e6017695b58a2bc0d773fb27e74fdab42bb7bbb2
Regina Obe <lr@pcorp.us>
2026-07-20 15:53:02 -0400

    tiger_geocoder: qualify fuzzystrmatch calls
    Ported-from: https://gitea.osgeo.org/postgis/tiger-geocoder/commit/d7d7db5b775620462fcf081a59dba19922025c5e

--- extras/tiger_geocoder/normalize/location_extract_countysub_fuzzy.sql
+++ extras/tiger_geocoder/normalize/location_extract_countysub_fuzzy.sql
@@ -31,10 +31,10 @@ BEGIN
     lstate := statefp FROM tiger.state WHERE stusps = stateAbbrev;
     SELECT INTO tempInt count(*) FROM tiger.cousub
         WHERE cousub.statefp = lstate
-        AND soundex(tempString) = tiger.end_soundex(name);
+        AND :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name);
   ELSE
     SELECT INTO tempInt count(*) FROM tiger.cousub
-        WHERE soundex(tempString) = tiger.end_soundex(name);
+        WHERE :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name);
   END IF;
 
   IF tempInt > 0 THEN
@@ -43,13 +43,13 @@ BEGIN
     IF stateAbbrev IS NOT NULL THEN
       FOR rec IN SELECT name FROM tiger.cousub
           WHERE cousub.statefp = lstate
-          AND soundex(tempString) = tiger.end_soundex(name) LOOP
+          AND :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name) LOOP
         word_count := tiger.count_words(rec.name);
         test := TRUE;
         tempString := tiger.get_last_words(fullStreet, word_count);
         FOR i IN 1..word_count LOOP
-          IF soundex(split_part(tempString, ' ', i)) !=
-            soundex(split_part(rec.name, ' ', i)) THEN
+          IF :fuzzystrmatch@.soundex(split_part(tempString, ' ', i)) !=
+            :fuzzystrmatch@.soundex(split_part(rec.name, ' ', i)) THEN
             test := FALSE;
           END IF;
         END LOOP;
@@ -63,13 +63,13 @@ BEGIN
       END LOOP;
     ELSE
       FOR rec IN SELECT name FROM tiger.cousub
-          WHERE soundex(tempString) = tiger.end_soundex(name) LOOP
+          WHERE :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name) LOOP
         word_count := tiger.count_words(rec.name);
         test := TRUE;
         tempString := tiger.get_last_words(fullStreet, word_count);
         FOR i IN 1..word_count LOOP
-          IF soundex(split_part(tempString, ' ', i)) !=
-            soundex(split_part(rec.name, ' ', i)) THEN
+          IF :fuzzystrmatch@.soundex(split_part(tempString, ' ', i)) !=
+            :fuzzystrmatch@.soundex(split_part(rec.name, ' ', i)) THEN
             test := FALSE;
           END IF;
         END LOOP;
--- extras/tiger_geocoder/normalize/location_extract_place_fuzzy.sql
+++ extras/tiger_geocoder/normalize/location_extract_place_fuzzy.sql
@@ -30,10 +30,10 @@ BEGIN
     lstate := statefp FROM tiger.state WHERE stusps = stateAbbrev;
     SELECT into tempInt count(*) FROM tiger.place
         WHERE place.statefp = lstate
-        AND soundex(tempString) = tiger.end_soundex(name);
+        AND :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name);
   ELSE
     SELECT into tempInt count(*) FROM tiger.place
-        WHERE soundex(tempString) = tiger.end_soundex(name);
+        WHERE :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name);
   END IF;
 
   IF tempInt > 0 THEN
@@ -42,13 +42,13 @@ BEGIN
     IF stateAbbrev IS NOT NULL THEN
       FOR rec IN SELECT name FROM tiger.place
           WHERE place.statefp = lstate
-          AND soundex(tempString) = tiger.end_soundex(name) LOOP
+          AND :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name) LOOP
         word_count := tiger.count_words(rec.name);
         test := TRUE;
         tempString := tiger.get_last_words(fullStreet, word_count);
         FOR i IN 1..word_count LOOP
-          IF soundex(split_part(tempString, ' ', i)) !=
-            soundex(split_part(rec.name, ' ', i)) THEN
+          IF :fuzzystrmatch@.soundex(split_part(tempString, ' ', i)) !=
+            :fuzzystrmatch@.soundex(split_part(rec.name, ' ', i)) THEN
             test := FALSE;
           END IF;
         END LOOP;
@@ -62,13 +62,13 @@ BEGIN
       END LOOP;
     ELSE
       FOR rec IN SELECT name FROM tiger.place
-          WHERE soundex(tempString) = tiger.end_soundex(name) LOOP
+          WHERE :fuzzystrmatch@.soundex(tempString) = tiger.end_soundex(name) LOOP
         word_count := tiger.count_words(rec.name);
         test := TRUE;
         tempString := tiger.get_last_words(fullStreet, word_count);
         FOR i IN 1..word_count LOOP
-          IF soundex(split_part(tempString, ' ', i)) !=
-            soundex(split_part(rec.name, ' ', i)) THEN
+          IF :fuzzystrmatch@.soundex(split_part(tempString, ' ', i)) !=
+            :fuzzystrmatch@.soundex(split_part(rec.name, ' ', i)) THEN
             test := FALSE;
           END IF;
         END LOOP;
