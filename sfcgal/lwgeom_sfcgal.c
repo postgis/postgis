de31a859edbd348d3ffa7627ea7011c2352738d8
Darafei Praliaskouski <me@komzpa.net>
2026-07-24 12:44:55 +0400

    Fix PostgreSQL 19 SFCGAL builds
    Backpatch the SFCGAL part of 0b003a82991bf05022ab1b924b5f8e1e407cd043 to avoid relying on pqsignal() returning the previous handler. New PostgreSQL headers expose pqsignal() as void, which breaks Worker_Run builds on older stable branches.
    
    References https://trac.osgeo.org/postgis/ticket/5841

--- sfcgal/lwgeom_sfcgal.c
+++ sfcgal/lwgeom_sfcgal.c
@@ -31,22 +31,12 @@
 
 #include "utils/elog.h"
 #include "utils/guc.h"
-#include "libpq/pqsignal.h"
 #include "../liblwgeom/liblwgeom.h"
 
 /*
  * This is required for builds against pgsql
  */
 PG_MODULE_MAGIC;
-#ifdef WIN32
-static void interruptCallback() {
-  if (UNBLOCKED_SIGNAL_QUEUE())
-    pgwin32_dispatch_queued_signals();
-}
-#endif
-
-static pqsigfunc coreIntHandler = 0;
-static void handleInterrupt(int sig);
 
 /*
  * Module load callback
@@ -55,16 +45,8 @@ void _PG_init(void);
 void
 _PG_init(void)
 {
-
-  coreIntHandler = pqsignal(SIGINT, handleInterrupt);
-
-#ifdef WIN32
-  GEOS_interruptRegisterCallback(interruptCallback);
-  lwgeom_register_interrupt_callback(interruptCallback);
-#endif
-
-    /* install PostgreSQL handlers */
-    pg_install_lwgeom_handlers();
+	/* install PostgreSQL handlers */
+	pg_install_lwgeom_handlers();
 }
 
 /*
@@ -74,27 +56,7 @@ void _PG_fini(void);
 void
 _PG_fini(void)
 {
-  elog(NOTICE, "Goodbye from PostGIS SFCGAL %s", POSTGIS_VERSION);
-  pqsignal(SIGINT, coreIntHandler);
-}
-
-
-static void
-handleInterrupt(int sig)
-{
-  /* NOTE: printf here would be dangerous, see
-   * https://trac.osgeo.org/postgis/ticket/3644
-   *
-   * TODO: block interrupts during execution, to fix the problem
-   */
-  /* printf("Interrupt requested\n"); fflush(stdout); */
-
-  /* request interruption of liblwgeom as well */
-  lwgeom_request_interrupt();
-
-  if ( coreIntHandler ) {
-    (*coreIntHandler)(sig);
-  }
+	elog(NOTICE, "Goodbye from PostGIS SFCGAL %s", POSTGIS_VERSION);
 }
 
 Datum postgis_sfcgal_version(PG_FUNCTION_ARGS);
