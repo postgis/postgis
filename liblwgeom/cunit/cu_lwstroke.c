17e1f4d636a5c34519db79618403b9db2fe54912
Darafei Praliaskouski <me@komzpa.net>
2026-07-26 20:05:41 +0400

    test: avoid platform-specific unstroked WKT text

--- liblwgeom/cunit/cu_lwstroke.c
+++ liblwgeom/cunit/cu_lwstroke.c
@@ -374,6 +374,7 @@ static void test_lwcurve_linearize(void)
 static void test_unstroke()
 {
 	LWGEOM *in, *out;
+	POINT2D pt;
 	char *str;
 
 	/* It would be nice if this example returned two arcs (it's the intersection of two circles)
@@ -401,12 +402,21 @@ static void test_unstroke()
 	lwfree(str);
 
 	out = lwgeom_unstroke_per_quad(in, 9);
-	str = lwgeom_to_wkt(out, WKT_ISO, 8, NULL);
-	ASSERT_STRING_EQUAL(
-	    str,
-	    "LINESTRING(-1 0,-0.98078528 0.19509032,-0.92387953 0.38268343,-0.83146961 0.55557023,-0.70710678 0.70710678,-0.55557023 0.83146961,-0.38268343 0.92387953,-0.19509032 0.98078528,6.123234e-17 1,0.19509032 0.98078528,0.38268343 0.92387953,0.55557023 0.83146961,0.70710678 0.70710678,0.83146961 0.55557023,0.92387953 0.38268343,0.98078528 0.19509032,1 0,0.98078528 -0.19509032,0.92387953 -0.38268343,0.83146961 -0.55557023,0.70710678 -0.70710678,0.55557023 -0.83146961,0.38268343 -0.92387953,0.19509032 -0.98078528,0 -1)");
+	CU_ASSERT_EQUAL(out->type, LINETYPE);
+	CU_ASSERT_EQUAL(((LWLINE *)out)->points->npoints, 25);
+	getPoint2d_p(((LWLINE *)out)->points, 0, &pt);
+	CU_ASSERT_DOUBLE_EQUAL(pt.x, -1.0, 1e-12);
+	CU_ASSERT_DOUBLE_EQUAL(pt.y, 0.0, 1e-12);
+	getPoint2d_p(((LWLINE *)out)->points, 8, &pt);
+	CU_ASSERT_DOUBLE_EQUAL(pt.x, 0.0, 1e-12);
+	CU_ASSERT_DOUBLE_EQUAL(pt.y, 1.0, 1e-12);
+	getPoint2d_p(((LWLINE *)out)->points, 16, &pt);
+	CU_ASSERT_DOUBLE_EQUAL(pt.x, 1.0, 1e-12);
+	CU_ASSERT_DOUBLE_EQUAL(pt.y, 0.0, 1e-12);
+	getPoint2d_p(((LWLINE *)out)->points, 24, &pt);
+	CU_ASSERT_DOUBLE_EQUAL(pt.x, 0.0, 1e-12);
+	CU_ASSERT_DOUBLE_EQUAL(pt.y, -1.0, 1e-12);
 	lwgeom_free(out);
-	lwfree(str);
 
 	cu_error_msg_reset();
 	out = lwgeom_unstroke_per_quad(in, 0);
