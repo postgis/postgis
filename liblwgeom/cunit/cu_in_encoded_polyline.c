618c525bf3830be843c6cbb09c4a7e931e2b264d
Darafei Praliaskouski <me@komzpa.net>
2026-08-08 12:22:14 +0400

    fix(liblwgeom): reject truncated encoded polylines
    (cherry picked from commit 491813e413afff546779b988e79ad7eb20559c46)

--- liblwgeom/cunit/cu_in_encoded_polyline.c
+++ liblwgeom/cunit/cu_in_encoded_polyline.c
@@ -53,6 +53,13 @@ static void in_encoded_polyline_test_precision(void)
 	    "SRID=4326;LINESTRING(-0.250691 49.283048,-0.250633 49.283376,-0.250502 49.283972,-0.251245 49.284028,-0.251938 49.284232,-0.251938 49.2842)");
 }
 
+static void in_encoded_polyline_test_truncated_input(void)
+{
+	/* A latitude without a longitude and an unterminated latitude varint. */
+	CU_ASSERT_PTR_NULL(lwgeom_from_encoded_polyline("A", 5));
+	CU_ASSERT_PTR_NULL(lwgeom_from_encoded_polyline("`", 5));
+}
+
 /*
 ** Used by test harness to register the tests in this file.
 */
@@ -62,4 +69,5 @@ void in_encoded_polyline_suite_setup(void)
 	CU_pSuite suite = CU_add_suite("encoded_polyline_input", NULL, NULL);
 	PG_ADD_TEST(suite, in_encoded_polyline_test_geoms);
 	PG_ADD_TEST(suite, in_encoded_polyline_test_precision);
+	PG_ADD_TEST(suite, in_encoded_polyline_test_truncated_input);
 }
