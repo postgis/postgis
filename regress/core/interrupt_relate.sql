62db192c8fb61636a12a0af54cf35eb3ffbd9ec5
Darafei Praliaskouski <me@komzpa.net>
2026-08-21 15:45:51 +0400

    regress: bound interrupt relate assertions

--- regress/core/interrupt_relate.sql
+++ regress/core/interrupt_relate.sql
@@ -16,27 +16,11 @@ SELECT 1::int as id, ST_Collect(g) g FROM (
 -- IM9 based predicates
 -----------------------------
 
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Contains(g,g) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('contains');
-END;
-$$;
-
 BEGIN;
 SET LOCAL statement_timeout TO 100;
 select ST_Contains(g,g) from _inputs WHERE id = 1; -- 6+ seconds
 ROLLBACK;
 SELECT _timecheck('contains');
-
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Covers(g,g) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('covers');
-END;
-$$;
 
 BEGIN;
 SET LOCAL statement_timeout TO 100;
@@ -44,41 +28,17 @@ select ST_Covers(g,g) from _inputs WHERE id = 1; -- 6+ seconds
 ROLLBACK;
 SELECT _timecheck('covers');
 
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_CoveredBy(g,g) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('coveredby');
-END;
-$$;
-
 BEGIN;
 SET LOCAL statement_timeout TO 100;
 select ST_CoveredBy(g,g) from _inputs WHERE id = 1; -- 6+ seconds
 ROLLBACK;
 SELECT _timecheck('coveredby');
 
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Crosses(g,g) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('crosses');
-END;
-$$;
-
 BEGIN;
 SET LOCAL statement_timeout TO 100;
 select ST_Crosses(g,g) from _inputs WHERE id = 1; -- 6+ seconds
 ROLLBACK;
 SELECT _timecheck('crosses');
-
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Equals(g,st_reverse(g)) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('equals');
-END;
-$$;
 
 BEGIN;
 SET LOCAL statement_timeout TO 100;
@@ -88,14 +48,6 @@ select ST_Equals(g,st_reverse(g)) from _inputs WHERE id = 1; -- 6+ seconds
 ROLLBACK;
 SELECT _timecheck('equals');
 
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Intersects(g,ST_Segmentize(g,1e-4)) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('intersects');
-END;
-$$;
-
 BEGIN;
 SET LOCAL statement_timeout TO 100;
 -- NOTE: intersects became very fast, so we segmentize
@@ -104,27 +56,11 @@ select ST_Intersects(g,ST_Segmentize(g,1e-4)) from _inputs WHERE id = 1; -- 6+ s
 ROLLBACK;
 SELECT _timecheck('intersects');
 
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Overlaps(g,g) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('overlaps');
-END;
-$$;
-
 BEGIN;
 SET LOCAL statement_timeout TO 100;
 select ST_Overlaps(g,g) from _inputs WHERE id = 1; -- 6+ seconds
 ROLLBACK;
 SELECT _timecheck('overlaps');
-
-DO $$
-BEGIN
-  PERFORM _timecheck_start();
-  PERFORM ST_Relate(g,g) FROM _inputs WHERE id = 1;
-  PERFORM _timecheck_baseline('relate');
-END;
-$$;
 
 BEGIN;
 SET LOCAL statement_timeout TO 100;
