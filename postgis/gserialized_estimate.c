postgis/postgis

commit c84b3f996e2eaea3ba3afeecb783113ab16f0982

Catch crash when query somehow sneaks a NULL query into the selectivity calculator, references #4919

postgis/gserialized_estimate.c
@@ -2244,6 +2244,7 @@ gserialized_sel_internal(PlannerInfo *root, List *args, int varRelid, int mode)
 
 	GBOX search_box;
 	float8 selectivity = 0;
+	Const *otherConst;
 
 	POSTGIS_DEBUGF(2, "%s: entered function", __func__);
 
@@ -2260,7 +2261,15 @@ gserialized_sel_internal(PlannerInfo *root, List *args, int varRelid, int mode)
 		return DEFAULT_ND_SEL;
 	}
 
-	if (!gserialized_datum_get_gbox_p(((Const*)other)->constvalue, &search_box))
+	otherConst = (Const*)other;
+	if ((!otherConst) || otherConst->constisnull)
+	{
+		ReleaseVariableStats(vardata);
+		POSTGIS_DEBUGF(2, "%s: constant argument is NULL", __func__);
+		return DEFAULT_ND_SEL;
+	}
+
+	if (!gserialized_datum_get_gbox_p(otherConst->constvalue, &search_box))
 	{
 		ReleaseVariableStats(vardata);
 		POSTGIS_DEBUGF(2, "%s: search box is EMPTY", __func__);

commit e1cabf91a2c80a8dbc32a88391b10dc404415dad

Make selectivity NULL-argument guard NULL-safe
get_restriction_variable() can return true with *other NULL when
estimate_expression_value() folds an expression to NULL (e.g. NULLIF
or a NULL Const). The existing guard relied on IsA(other, Const),
which dereferences other; a NULL other would hit nodeTag(NULL) and
crash before the check ever ran.

Fixes: c84b3f996e2eaea3ba3afeecb783113ab16f0982

Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

postgis/gserialized_estimate.c
@@ -2052,6 +2052,13 @@ gserialized_sel_internal(PlannerInfo *root, List *args, int varRelid, int mode)
 		return DEFAULT_ND_SEL;
 	}
 
+	if (other == NULL)
+	{
+		ReleaseVariableStats(vardata);
+		POSTGIS_DEBUGF(2, "%s: no other argument, returning default selectivity %g", __func__, DEFAULT_ND_SEL);
+		return DEFAULT_ND_SEL;
+	}
+
 	if (!IsA(other, Const))
 	{
 		ReleaseVariableStats(vardata);
@@ -2060,7 +2067,7 @@ gserialized_sel_internal(PlannerInfo *root, List *args, int varRelid, int mode)
 	}
 
 	otherConst = (Const*)other;
-	if ((!otherConst) || otherConst->constisnull)
+	if (otherConst->constisnull)
 	{
 		ReleaseVariableStats(vardata);
 		POSTGIS_DEBUGF(2, "%s: constant argument is NULL", __func__);